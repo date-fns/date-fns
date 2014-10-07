73af5d9eef5f678780588ab660d8c3a6d206ab35
Sasha Koss <koss@nocorp.me>
2014-10-07 22:06:54 +0400

    isSameMonth function

---
+++ src/is_same_month.js
@@ -0,0 +1,17 @@
+/**
+ * Is passed dates has the same month (and year)?
+ * @param {date|string} dirtyDateLeft
+ * @param {date|string} dirtyDateRight
+ * @returns {boolean}
+ */
+var isSameMonth = function(dirtyDateLeft, dirtyDateRight) {
+  var dateLeft = new Date(dirtyDateLeft);
+  var dateRight = new Date(dirtyDateRight);
+  return(
+    dateLeft.getFullYear() == dateRight.getFullYear() &&
+    dateLeft.getMonth() == dateRight.getMonth()
+  );
+};
+
+module.exports = isSameMonth;
+
