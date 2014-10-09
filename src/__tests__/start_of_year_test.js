264e8f5bd195e2b0c6ba9179b46add07d5163213
Andrey <dolby0@gmail.com>
2014-10-09 17:30:56 +0700

    Koss's fixes

--- src/__tests__/start_of_year_test.js
+++ src/__tests__/start_of_year_test.js
@@ -2,7 +2,7 @@ var startOfYear = require('../start_of_year');
 
 describe('startOfYear', function() {
   it('returns date with time setted to 00:00:00', function() {
-    var date = new Date('2014-09-02T11:55:00');
+    var date = new Date(2014, 8, 2 , 11, 55, 00);
     var result = startOfYear(date);
     expect(result).to.be.eql(
       new Date(2014, 0, 1, 0, 0, 0, 0)
@@ -10,8 +10,8 @@ describe('startOfYear', function() {
   });
 
   it('accepts string', function() {
-    var date = '2014-09-02T11:55:00';
-    var result = startOfYear(date);
+    var date = new Date(2014, 8, 2 , 11, 55, 00);
+    var result = startOfYear(date.toISOString());
     expect(result).to.be.eql(
       new Date(2014, 0, 1, 0, 0, 0, 0)
     );
