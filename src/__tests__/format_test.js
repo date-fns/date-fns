1fa4355315ab91fa8763eef075c1d430d8ba1e32
Sasha Koss <koss@nocorp.me>
2014-10-13 12:12:26 +0400

    Fix small formatting issues

--- src/__tests__/format_test.js
+++ src/__tests__/format_test.js
@@ -1,55 +1,58 @@
 var format = require('../format');
 
-describe('format', function(){
+describe('format', function() {
   beforeEach(function () {
     this._date = new Date(1986, 3, 4, 10, 32, 0, 900);
   });
 
-  it('simple YY', function(){
+  it('simple YY', function() {
     var b = new Date(2009, 1, 14, 15, 25, 50, 125);
     expect(format(b, 'YY')).to.equal('09');
   });
 
-  it('accepts string as a date', function(){
+  it('accepts string as a date', function() {
     expect(format('2014-04-04', 'YYYY-MM-DD')).to.be.equal('2014-04-04');
-  })
+  });
 
-  it('return default ISO string format if format is unknown', function(){
+  it('return default ISO string format if format is unknown', function() {
     expect(format(this._date)).to.be.equal('1986-04-04T10:32:00.900Z');
   });
 
-  describe('format escape brackets', function(){
-    it('should ignore escaped chars that in [] brackets', function(){
-      expect(format(this._date, '[not a date] MM')).to.be.equal('not a date 04');
+  describe('format escape brackets', function() {
+    it('should ignore escaped chars that in [] brackets', function() {
+      var result = format(this._date, '[not a date] MM');
+      expect(result).to.be.equal('not a date 04');
     });
-  })
+  });
 
-  describe('ordinal', function(){
-    it('should return 1st', function(){
+  describe('ordinal', function() {
+    it('should return 1st', function() {
       var date = format(this._date, 'Do of t[h][e] Mo in YYYY');
       expect(date).to.be.equal('4th of the 4th in 1986');
     });
-  })
+  });
 
-  describe('Months', function(){
-    it('return months names', function(){
+  describe('Months', function() {
+    it('return months names', function() {
       var date = format(this._date, 'MMM MMMM');
       expect(date).to.equal('Apr April');
     });
-    it('return months names reverse parse', function(){
+    it('return months names reverse parse', function() {
       var date = format(this._date, 'MMMM MMM');
       expect(date).to.equal('April Apr');
     });
-    it('all month variants', function(){
+    it('all month variants', function() {
       var date = format(this._date, 'M Mo MM MMM MMMM');
       expect(date).to.equal('4 4th 04 Apr April');
     });
   });
 
-  describe('days', function(){
-    it('return day of the year', function(){
+  describe('days', function() {
+    it('return day of the year', function() {
       var firstDay = format(new Date(1992, 0, 1, 0, 0, 0, 0), 'DDD');
-      var lastDayLeapYear = format(new Date(1992, 11, 31, 23, 59, 59, 999), 'DDD');
+      var lastDayLeapYear = format(
+        new Date(1992, 11, 31, 23, 59, 59, 999), 'DDD'
+      );
       var lastDay = format(new Date(1986, 11, 31, 23, 59, 59, 999), 'DDD');
 
       expect(firstDay).to.be.equal('1');
@@ -57,54 +60,55 @@ describe('format', function(){
       expect(lastDay).to.be.equal('365');
     });
 
-    it('return ordinal day of the year', function(){
+    it('return ordinal day of the year', function() {
       var firstDay = format(new Date(1992, 0, 1, 0, 0, 0, 0), 'DDDo');
       expect(firstDay).to.be.equal('1st');
     });
 
-    it('return zero filled day of year', function(){
+    it('return zero filled day of year', function() {
       var firstDay = format(new Date(1992, 0, 1, 0, 0, 0, 0), 'DDDD');
       expect(firstDay).to.be.equal('001');
     });
-
   });
 
-  describe('Quartal', function(){
-    it('right quartal', function(){
-      var dates = [];
-      for(var i=0; i!=12; i++){
-        dates.push(format(new Date(1986, i, 1), 'Q'));
+  describe('Quartal', function() {
+    it('right quartal', function() {
+      var result = [];
+      for (var i = 0; i != 12; i++){
+        result.push(format(new Date(1986, i, 1), 'Q'));
       }
-      expect(dates).to.deep.equal(['1','1','1', '2', '2', '2', '3', '3', '3', '4', '4', '4']);
+      expect(result).to.deep.equal(
+        ['1','1','1', '2', '2', '2', '3', '3', '3', '4', '4', '4']
+      );
     });
   });
 
-  describe('day of week', function(){
-    it('display', function(){
-      expect(format(this._date, 'd do dd ddd dddd')).to.be.equal('5 5th Fr Fri Friday');
-    })
+  describe('day of week', function() {
+    it('display', function() {
+      var result = format(this._date, 'd do dd ddd dddd');
+      expect(result).to.be.equal('5 5th Fr Fri Friday');
+    });
 
-    it('ISO', function(){
+    it('ISO', function() {
       expect(format(this._date, 'E')).to.be.equal('6');
     });
 
-    it('parses ok for different variants', function(){
+    it('parses ok for different variants', function() {
       var firstDay = format(this._date, 'dddd ddd d do [d] do dd ddd dddd');
       expect(firstDay).to.be.equal('Friday Fri 5 5th d 5th Fr Fri Friday');
     });
-
   });
 
-  describe('hours', function(){
-    it('am/pm', function(){
+  describe('hours', function() {
+    it('am/pm', function() {
       expect(format(this._date, 'hh:mm a')).to.be.equal('10:32 am');
-    })
+    });
   });
 
-  describe('seconds', function(){
-    it('show', function(){
+  describe('seconds', function() {
+    it('show', function() {
       expect(format(this._date, 's ss')).to.be.equal('0 00');
-    })
+    });
   });
+});
 
-})
--- src/format.js
+++ src/format.js
@@ -16,109 +16,111 @@ var format = function(date, format) {
 
   var formatFunction = makeFormatFunction(format);
   return formatFunction(date);
-}
+};
 
 var formats = {
-  'M': function(){
+  'M': function() {
     return this.getMonth() + 1;
   },
-  'MM': function(){
+  'MM': function() {
     return leftZeroFill(this.getMonth() + 1, 2);
   },
-  'MMM': function(){
+  'MMM': function() {
     return locale.monthsShort[this.getMonth()];
   },
-  'MMMM': function(){
+  'MMMM': function() {
     return locale.months[this.getMonth()];
   },
-  'Q': function(){
+  'Q': function() {
     return Math.ceil((this.getMonth() + 1) / 3);
   },
-  'D': function(){
+  'D': function() {
     return this.getDate();
   },
-  'DD': function(){
+  'DD': function() {
     return leftZeroFill(this.getDate(), 2);
   },
-  'DDD': function(){
+  'DDD': function() {
     return Math.round((startOfDay(this) - startOfYear(this)) / 864e5) + 1;
   },
-  'DDDD': function(){
+  'DDDD': function() {
     return leftZeroFill(formats['DDD'].apply(this), 3);
   },
-  'd': function(){
+  'd': function() {
     return this.getDay();
   },
-  'dd': function(){
+  'dd': function() {
     return locale.dayNamesMin[this.getDay()];
   },
-  'ddd': function(){
+  'ddd': function() {
     return locale.dayNamesShort[this.getDay()];
   },
-  'dddd': function(){
+  'dddd': function() {
     return locale.dayNames[this.getDay()];
   },
-  'E': function(){
+  'E': function() {
     return this.getDay() + 1;
   },
-  'YY': function(){
+  'YY': function() {
     return String(this.getFullYear()).substr(2);
   },
-  'YYYY': function(){
+  'YYYY': function() {
     return this.getFullYear()
   },
-  'A': function(){
+  'A': function() {
     return (this.getHours() / 12) >= 1 ? 'PM' : 'AM';
   },
-  'a': function(){
+  'a': function() {
     return (this.getHours() / 12) >= 1 ? 'pm' : 'am';
   },
-  'H': function(){
+  'H': function() {
     return this.getHours();
   },
-  'HH': function(){
+  'HH': function() {
     return leftZeroFill(this.getHours(), 2);
   },
-  'h': function(){
+  'h': function() {
     return this.getHours() % 12;
   },
-  'hh': function(){
+  'hh': function() {
     return leftZeroFill(this.getHours() % 12, 2);
   },
-  'm': function(){
+  'm': function() {
     return this.getMinutes();
   },
-  'mm': function(){
+  'mm': function() {
     return leftZeroFill(this.getMinutes());
   },
-  's': function(){
+  's': function() {
     return this.getSeconds();
   },
-  'ss': function(){
+  'ss': function() {
     return leftZeroFill(this.getSeconds(), 2);
   },
-  'S': function(){
+  'S': function() {
     return this.getMilliseconds();
   },
-  'SS': function(){
+  'SS': function() {
     return leftZeroFill(this.getMilliseconds(), 2);
   },
-  'SSS': function(){
+  'SSS': function() {
     return leftZeroFill(this.getMilliseconds(), 3);
   }
-}
+};
 
 var ordinalFunctions = ['M', 'D', 'DDD', 'd'];
 ordinalFunctions.forEach(function(functionName){
-  formats[functionName + 'o'] = function(){
+  formats[functionName + 'o'] = function() {
     return locale.ordinal(formats[functionName].apply(this));
   };
 });
 
 var formattingTokens = Object.keys(formats).sort().reverse();
-var formattingTokensRegexp = new RegExp('(\\[[^\\[]*\\])|(\\\\)?' + '(' + formattingTokens.join('|') + '|.)', 'g');
+var formattingTokensRegexp = new RegExp(
+  '(\\[[^\\[]*\\])|(\\\\)?' + '(' + formattingTokens.join('|') + '|.)', 'g'
+);
 
-function makeFormatFunction(format) {
+var makeFormatFunction = function(format) {
   var array = format.match(formattingTokensRegexp), i, length;
 
   for (i = 0, length = array.length; i < length; i++) {
@@ -129,34 +131,37 @@ function makeFormatFunction(format) {
     }
   }
 
-  return function (mom) {
+  return function(mom) {
     var output = '';
     for (i = 0; i < length; i++) {
-      output += array[i] instanceof Function ? array[i].call(mom, format) : array[i];
+      if (array[i] instanceof Function) {
+        output += array[i].call(mom, format);
+      } else {
+        output += array[i];
+      }
     }
     return output;
   };
-}
+};
 
-function removeFormattingTokens(input) {
+var removeFormattingTokens = function(input) {
   if (input.match(/\[[\s\S]/)) {
     return input.replace(/^\[|\]$/g, '');
   }
   return input.replace(/\\/g, '');
-}
+};
 
-function leftZeroFill(number, targetLength) {
+var leftZeroFill = function(number, targetLength) {
   var output = '' + Math.abs(number);
 
   while (output.length < targetLength) {
     output = '0' + output;
   }
   return output;
-}
-
+};
 
 var locale = {
-  ordinal: function (number) {
+  ordinal: function(number) {
     var b = number % 10,
       output = (+(number % 100 / 10) === 1) ? 'th' :
       (b === 1) ? 'st' :
@@ -164,11 +169,12 @@ var locale = {
       (b === 3) ? 'rd' : 'th';
     return number + output;
   },
-  months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-  monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-  dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-  dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-  dayNamesMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
-}
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+};
 
 module.exports = format;
+
