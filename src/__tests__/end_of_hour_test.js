c6ca953b004b06737fba24aa3cd5b20532e9e8c5
Andrey <dolby0@gmail.com>
2014-10-23 16:46:30 +0700

    endOfHour function, closes #7

---
+++ src/__tests__/end_of_hour_test.js
@@ -0,0 +1,21 @@
+var endOfHour = require('../end_of_hour');
+
+describe('endOfHour', function () {
+  it('returns date with time setted to last millisecond before the hour ends', function () {
+    var date = new Date(2014, 11, 1, 22, 15);
+    var result = endOfHour(date);
+    expect(result).to.be.eql(new Date(2014, 11, 1, 22, 59, 59, 999));
+  });
+
+  it('supports string as a date', function () {
+    var result = endOfHour('2014-12-01T13:00:00.000Z');
+    expect(result).to.be.eql(new Date(Date.UTC(2014, 11, 1, 13, 59, 59, 999)));
+  });
+
+  it('does not mutate original date', function () {
+    var date = new Date(2014, 11, 1, 22, 15);
+    endOfHour(date);
+    expect(date).to.be.eql(new Date(2014, 11, 1, 22, 15));
+  });
+
+});
---
+++ src/end_of_hour.js
@@ -0,0 +1,13 @@
+/**
+ * Returns end of a hour for given date. Date will be in local timezone.
+ * @param {date|string} dirtyDate
+ * @returns {date}
+ */
+var endOfHour = function(dirtyDate) {
+  var date = new Date(dirtyDate);
+  date.setMinutes(59, 59, 999);
+  return date;
+};
+
+module.exports = endOfHour;
+
