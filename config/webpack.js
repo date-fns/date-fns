e59c00d91945b9cbda99e4e0cba88b72a5111ebb
Ivan Shornikov <paraboom@gmail.com>
2014-12-04 14:33:41 +0700

    Version of plugin for window context

--- config/webpack.js
+++ config/webpack.js
@@ -1,15 +1,27 @@
 var path = require('path');
 
-var config = {
-  cache: true,
-  entry: {
-    'date_fns': './src/date_fns.js'
+var config = [
+  {
+    cache: true,
+    entry: {
+      'date_fns': './src/date_fns.js'
+    },
+    output: {
+      path: path.join(process.cwd(), 'dist'),
+      filename: '[name].js'
+    }
   },
-  output: {
-    path: path.join(process.cwd(), 'dist'),
-    filename: '[name].js'
+  {
+    cache: true,
+    entry: {
+      'date_fns_global': './src/date_fns_global.js'
+    },
+    output: {
+      path: path.join(process.cwd(), 'dist'),
+      filename: '[name].js'
+    }
   }
-};
+];
 
 module.exports = config;
 
