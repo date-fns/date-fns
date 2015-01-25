ea6c15c886b52370bc616330a4a9f5e1f64d492a
Lesha Koss <regiusprod@gmail.com>
2015-01-26 01:08:46 +0600

    compareAsc function, closes #13

---
+++ src/__tests__/compare_asc_test.js
@@ -0,0 +1,53 @@
+var compareAsc = require('../compare_asc');
+
+describe('compareAsc', function() {
+  it('returns 0 if passed dates are equal', function() {
+    var result = compareAsc(
+      new Date(1989, 6 /* Jul */, 10),
+      new Date(1989, 6 /* Jul */, 10)
+    );
+    expect(result).to.equal(0);
+  })
+
+  it('returns -1 if first date before second one', function() {
+    var result = compareAsc(
+      new Date(1987, 1 /* Feb */, 11),
+      new Date(1989, 6 /* Jul */, 10)
+    );
+    expect(result).to.equal(-1);
+  });
+
+  it('returns 1 if first date after second one', function() {
+    var result = compareAsc(
+      new Date(1989, 6 /* Jul */, 10),
+      new Date(1987, 1 /* Feb */, 11)
+    );
+    expect(result).to.equal(1);
+  });
+
+  it('sorts dates array in chronological order when passed as argument to Array.prototype.sort()', function() {
+    var unsortedArray = [
+      new Date(1995, 6 /* Jul */, 2),
+      new Date(1987, 1 /* Feb */, 11),
+      new Date(1989, 6 /* Jul */, 10)
+    ];
+
+    var sortedArray = [
+      new Date(1987, 1 /* Feb */, 11),
+      new Date(1989, 6 /* Jul */, 10),
+      new Date(1995, 6 /* Jul */, 2)
+    ];
+
+    var result = unsortedArray.sort(compareAsc);
+
+    expect(result).to.eql(sortedArray);
+  });
+
+  it('allows to pass string', function() {
+    var result = compareAsc(
+      new Date(1987, 1 /* Feb */, 11).toISOString(),
+      new Date(1989, 6 /* Jul */, 10).toISOString()
+    );
+    expect(result).to.equal(-1);
+  });
+});
---
+++ src/compare_asc.js
@@ -0,0 +1,24 @@
+/**
+ * Compares the two dates and returns -1, 0 or 1.
+ * @param {date|string} dirtyDateLeft
+ * @param {date|string} dirtyDateRight
+ * @returns {number}
+ */
+var compareAsc = function(dirtyDateLeft, dirtyDateRight) {
+  var dateLeft = new Date(dirtyDateLeft);
+  var timeLeft = dateLeft.getTime();
+  var dateRight = new Date(dirtyDateRight);
+  var timeRight = dateRight.getTime();
+
+  if (timeLeft < timeRight) {
+    return -1;
+  }
+
+  if (timeLeft > timeRight) {
+    return 1;
+  }
+
+  return 0;
+};
+
+module.exports = compareAsc;
