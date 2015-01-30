f2bd67545273ca14f5d2e223b9e2a478d9fe3efd
Lesha Koss <regiusprod@gmail.com>
2015-01-30 23:55:54 +0600

    getQuarter and setQuarter functions

---
+++ src/__tests__/set_quarter_test.js
@@ -0,0 +1,20 @@
+var setQuarter = require('../set_quarter');
+
+describe('setQuarter', function() {
+  it('sets quarter of year', function() {
+    var result = setQuarter(new Date(2014, 6 /* Jul */, 2), 1);
+    expect(result).to.be.eql(new Date(2014, 0 /* Jan */, 2));
+  });
+
+  it('accepts string', function() {
+    var result = setQuarter(new Date(2014, 6 /* Jul */, 1).toISOString(), 4);
+    expect(result).to.be.eql(new Date(2014, 9 /* Oct */, 1));
+  });
+
+  it('do not mutates original date', function() {
+    var date = new Date(2014, 6 /* Jul */, 1);
+    setQuarter(date, 2);
+    expect(date).to.be.eql(new Date(2014, 6 /* Jul */, 1));
+  });
+});
+
