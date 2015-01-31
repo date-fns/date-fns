0a0459127545938d5efa8a9b3124ac3e8bd1f1ce
Lesha Koss <regiusprod@gmail.com>
2015-01-31 11:21:02 +0600

    Improved startOfISOYear and getISOYear algorithm

--- src/get_iso_year.js
+++ src/get_iso_year.js
@@ -8,18 +8,16 @@ var startOfWeek = require('./start_of_week');
  */
 var getISOYear = function(dirtyDate) {
   var date = new Date(dirtyDate);
-  var startOfThisWeek = startOfWeek(date, 1);
-  var startOfThisYear = startOfWeek(new Date(date.getFullYear(), 0, 4), 1);
-  var startOfNextYear = startOfWeek(new Date(date.getFullYear() + 1, 0, 4), 1);
+  var year = date.getFullYear();
+  var startOfNextYear = startOfWeek(new Date(year + 1, 0, 4), 1);
+  var startOfThisYear = startOfWeek(new Date(year, 0, 4), 1);
 
-  // January 4th of given date's next full year is on the same week
-  if (startOfThisWeek.getTime() == startOfNextYear.getTime()) {
-    return date.getFullYear() + 1;
-  // January 4th of given date's full year is on the next week
-  } else if (startOfThisWeek.getTime() + 604800000 == startOfThisYear.getTime()) {
-    return date.getFullYear() - 1;
+  if (date.getTime() >= startOfNextYear.getTime()) {
+    return year + 1;
+  } else if (date.getTime() >= startOfThisYear.getTime()) {
+    return year;
   } else {
-    return date.getFullYear();
+    return year - 1;
   }
 }
 
--- src/start_of_iso_year.js
+++ src/start_of_iso_year.js
@@ -9,18 +9,15 @@ var startOfWeek = require('./start_of_week');
  */
 var startOfISOYear = function(dirtyDate) {
   var date = new Date(dirtyDate);
-  var startOfThisWeek = startOfWeek(date, 1);
-  var startOfThisYear = startOfWeek(new Date(date.getFullYear(), 0, 4), 1);
   var startOfNextYear = startOfWeek(new Date(date.getFullYear() + 1, 0, 4), 1);
+  var startOfThisYear = startOfWeek(new Date(date.getFullYear(), 0, 4), 1);
 
-  // January 4th of given date's next full year is on the same week
-  if (startOfThisWeek.getTime() == startOfNextYear.getTime()) {
+  if (date.getTime() >= startOfNextYear.getTime()) {
     return startOfNextYear;
-  // January 4th of given date's full year is on the next week
-  } else if (startOfThisWeek.getTime() + 604800000 == startOfThisYear.getTime()) {
-    return startOfWeek(new Date(date.getFullYear()-1, 0, 4), 1);
-  } else {
+  } else if (date.getTime() >= startOfThisYear.getTime()) {
     return startOfThisYear;
+  } else {
+    return startOfWeek(new Date(date.getFullYear() - 1, 0, 4), 1);
   }
 }
 
