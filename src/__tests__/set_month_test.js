f85228f3184edd4f84dce7c84ec166399e7df67b
Lesha Koss <regiusprod@gmail.com>
2015-08-08 02:11:40 +0600

    Make setMonth set last day of month if original date was last day of longer month

--- src/__tests__/set_month_test.js
+++ src/__tests__/set_month_test.js
@@ -6,6 +6,11 @@ describe('setMonth', function() {
     expect(result).to.be.eql(new Date(2014, 1 /* Feb */, 1));
   });
 
+  it('sets last day of month if original date was last day of longer month', function() {
+    var result = setMonth(new Date(2014, 11 /* Dec */, 31), 1);
+    expect(result).to.be.eql(new Date(2014, 1 /* Feb */, 28));
+  });
+
   it('accepts string', function() {
     var result = setMonth(new Date(2014, 8 /* Sep */, 1).toISOString(), 11);
     expect(result).to.be.eql(new Date(2014, 11 /* Dec */, 1));
--- src/__tests__/set_quarter_test.js
+++ src/__tests__/set_quarter_test.js
@@ -6,6 +6,11 @@ describe('setQuarter', function() {
     expect(result).to.be.eql(new Date(2014, 0 /* Jan */, 2));
   });
 
+  it('sets last day of month if original date was last day of longer month', function() {
+    var result = setQuarter(new Date(2014, 10 /* Nov */, 30), 1);
+    expect(result).to.be.eql(new Date(2014, 1 /* Feb */, 28));
+  });
+
   it('accepts string', function() {
     var result = setQuarter(new Date(2014, 6 /* Jul */, 1).toISOString(), 4);
     expect(result).to.be.eql(new Date(2014, 9 /* Oct */, 1));
