4bab1eb12155cd0086a2bafe4d88a8da167303d6
Lesha Koss <regiusprod@gmail.com>
2015-08-09 02:38:19 +0600

    lastDayOfYear and lastDayOfMonth functions

---
+++ src/last_day_of_year.js
@@ -0,0 +1,15 @@
+/**
+ * Returns last day of a year for given date. Date will be in local timezone.
+ * @param {date|string} dirtyDate
+ * @returns {date}
+ */
+var lastDayOfYear = function(dirtyDate) {
+  var date = new Date(dirtyDate);
+  var year = date.getFullYear();
+  date.setHours(0, 0, 0, 0);
+  date.setFullYear(year + 1, 0, 0);
+  return date;
+};
+
+module.exports = lastDayOfYear;
+
