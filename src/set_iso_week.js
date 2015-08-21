82056f5ec8ed7d1abaa15569ae5c5f6c57b73070
Andrey <dolby0@gmail.com>
2015-08-21 19:26:59 +0200

    setIsoWeek sets time to start of the day

--- src/set_iso_week.js
+++ src/set_iso_week.js
@@ -1,4 +1,5 @@
 var getISOWeek = require('./get_iso_week');
+var startOfDay = require('./start_of_day');
 
 var MILLISECONDS_IN_WEEK = 604800000;
 
@@ -15,7 +16,7 @@ var setISOWeek = function(dirtyDate, isoWeek) {
   var date = new Date(dirtyDate);
   var diff = getISOWeek(date) - isoWeek;
   date.setTime(date.getTime() - diff * MILLISECONDS_IN_WEEK);
-  return date;
+  return startOfDay(date);
 };
 
 module.exports = setISOWeek;
