1268f5ed47dd18cd8e52117c17f9044d49d0991e
Lesha Koss <regiusprod@gmail.com>
2015-08-26 13:02:41 +0800

    Add missing time zones

--- scripts/tz_test.sh
+++ scripts/tz_test.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env sh
 
-for i in -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 11 12
+for tz in UTC-12:00 UTC-11:00 UTC-10:00 UTC-09:30 UTC-09:00 \
+  UTC-08:00 UTC-07:00 UTC-06:00 UTC-05:00 UTC-04:30 UTC-04:00 \
+  UTC-03:30 UTC-03:00 UTC-02:00 UTC-01:00 UTC UTC+01:00 UTC+02:00 \
+  UTC+03:00 UTC+03:30 UTC+04:00 UTC+04:30 UTC+05:00 UTC+05:30 \
+  UTC+05:45 UTC+06:00 UTC+06:30 UTC+07:00 UTC+08:00 UTC+08:30 \
+  UTC+08:45 UTC+09:00 UTC+09:30 UTC+10:00 UTC+10:30 UTC+11:00 \
+  UTC+11:30 UTC+12:00 UTC+12:45 UTC+13:00 UTC+14:00
 do
-  if [ $i -lt 0 ]
-  then
-    tz=UTC${i}
-  elif [ $i -eq 0 ]
-  then
-    tz=UTC
-  else
-    tz=UTC+${i}
-  fi
   printf "\nRun test in time zone $tz\n"
   env TZ=$tz npm test -- --single-run || exit 1
 done
