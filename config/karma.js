8247e0401e384aee88594f5491939c607d78db72
Sasha Koss <koss@nocorp.me>
2015-09-03 21:11:38 +0800

    Implement basic Sauce Lab tests

--- config/karma.js
+++ config/karma.js
@@ -1,5 +1,14 @@
 var webpackConfig = require('./webpack')
 
+var sauceLabsLaunchers = {
+  ie10: {
+    base: 'SauceLabs',
+    browserName: 'Internet Explorer',
+    platform: 'Windows 7',
+    version: '10'
+  }
+}
+
 var config = function(config) {
   config.set({
     frameworks: ['mocha', 'chai-sinon'],
@@ -15,11 +24,16 @@ var config = function(config) {
         version: false
       }
     },
-    browsers: ['PhantomJS'],
-    reporters: ['mocha'],
+
+    sauceLabs: {testName: 'date-fns'},
+
     mochaReporter: {
       output: process.env.TEST_TZ ? 'minimal' : 'full'
-    }
+    },
+
+    customLaunchers: process.env.TEST_SAUCE ? sauceLabsLaunchers : {},
+    browsers: process.env.TEST_SAUCE ? Object.keys(sauceLabsLaunchers) : ['PhantomJS'],
+    reporters: process.env.TEST_SAUCE ? ['dots', 'saucelabs'] : ['mocha']
   })
 }
 
