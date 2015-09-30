b81c7586023e3a987368a7f432e729dd0eaae979
Lesha Koss <regiusprod@gmail.com>
2015-09-30 19:34:25 +0600

    Remove DST fix in `eachDay` function

--- src/each_day.js
+++ src/each_day.js
@@ -9,23 +9,13 @@ var parse = require('./parse')
 var eachDay = function(dirtyStart, dirtyEnd) {
   var endTime = parse(dirtyEnd).getTime()
   var dates = []
-  var tmpDate
 
-  var curDate = parse(dirtyStart)
-  curDate.setHours(0, 0, 0, 0)
+  var currentDate = parse(dirtyStart)
+  currentDate.setHours(0, 0, 0, 0)
 
-  while (curDate.getTime() <= endTime) {
-    dates.push(new Date(curDate))
-
-    curDate.setDate(curDate.getDate() + 1)
-
-    /**
-     * add additional 5 hours to get next day,
-     * because of possible troubles with daylight savings dates
-     */
-    tmpDate = new Date(curDate.setTime(curDate.getTime() + 5 * 60 * 60 * 1000))
-    tmpDate = new Date(tmpDate.setHours(0, 0, 0, 0))
-    curDate = tmpDate
+  while (currentDate.getTime() <= endTime) {
+    dates.push(new Date(currentDate))
+    currentDate.setDate(currentDate.getDate() + 1)
   }
 
   return dates
