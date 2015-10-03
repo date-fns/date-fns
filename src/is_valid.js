e0ae3c66455edf034358a9cdbb8e39dba601f64b
Lesha Koss <regiusprod@gmail.com>
2015-10-03 23:55:32 +0600

    Make isValid documentation more detailed

--- src/is_valid.js
+++ src/is_valid.js
@@ -1,7 +1,12 @@
 /**
- * Is the given date valid?
+ * Returns false if argument is Invalid Date and true otherwise.
+ * Invalid Date is a Date, whose time value is NaN.
+ *
+ * Time value of Date: http://es5.github.io/#x15.9.1.1
+ *
  * @param {Date|String|Number} date to check
  * @returns {Boolean} date is valid
+ * @throws {TypeError} Argument must be an instance of Date
  */
 var isValid = function(date) {
   if (date instanceof Date) {
