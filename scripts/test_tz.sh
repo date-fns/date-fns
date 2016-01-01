3bde1dfb9426c7e1ecfb7f5f4621877ca73534b4
Lesha Koss <regiusprod@gmail.com>
2016-01-02 00:00:00 +0600

    Condense timezone tests output

--- scripts/test_tz.sh
+++ scripts/test_tz.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "\n"
+
 for tz in UTC-12:00 UTC-11:00 UTC-10:00 UTC-09:30 UTC-09:00 \
   UTC-08:00 UTC-07:00 UTC-06:00 UTC-05:00 UTC-04:30 UTC-04:00 \
   UTC-03:30 UTC-03:00 UTC-02:00 UTC-01:00 UTC UTC+01:00 UTC+02:00 \
@@ -8,6 +10,7 @@ for tz in UTC-12:00 UTC-11:00 UTC-10:00 UTC-09:30 UTC-09:00 \
   UTC+08:45 UTC+09:00 UTC+09:30 UTC+10:00 UTC+10:30 UTC+11:00 \
   UTC+11:30 UTC+12:00 UTC+12:45 UTC+13:00 UTC+14:00
 do
-  printf "\nRun test in time zone $tz\n"
-  env TEST_TZ=true USE_STATIC_TESTS=true TZ=$tz npm run test-ci || exit 1
+  printf "Run test in time zone $tz\n"
+  env TEST_TZ=true USE_STATIC_TESTS=true TZ=$tz npm run test-ci \
+    &>tmp/last_test_output.txt || (cat tmp/last_test_output.txt && exit 1) || exit 1
 done
