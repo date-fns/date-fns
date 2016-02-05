f5307f96716cbdf666a4a976a70a661e196b2341
Matt Johnson <mj1856@hotmail.com>
2016-02-04 21:07:53 -0800

    Fix leap day bug with add/sub years

--- src/sub_years/test.js
+++ src/sub_years/test.js
@@ -22,5 +22,10 @@ describe('subYears', function() {
     subYears(date, 12)
     assert.deepEqual(date, new Date(2014, 8 /* Sep */, 1))
   })
+
+  it('handles leap years', function() {
+    var result = subYears(new Date(2016, 1 /* Feb */, 29), 1)
+    assert.deepEqual(result, new Date(2015, 1 /* Feb */, 28))
+  })
 })
 
