a1daba2ef60ffb34c73932f68fefb727d45d4568
Sasha Koss <koss@nocorp.me>
2016-05-29 13:22:46 +0800

    Use handlebars string format in distanceInWords localize function

--- src/locale/en/build_distance_in_words_localize_fn/index.js
+++ src/locale/en/build_distance_in_words_localize_fn/index.js
@@ -2,54 +2,54 @@ module.exports = function buildDistanceInWordsLocalizeFn () {
   var distanceInWordsLocale = {
     lessThanXSeconds: {
       one: 'less than a second',
-      other: 'less than ${count} seconds'
+      other: 'less than {{count}} seconds'
     },
 
     halfAMinute: 'half a minute',
 
     lessThanXMinutes: {
       one: 'less than a minute',
-      other: 'less than ${count} minutes'
+      other: 'less than {{count}} minutes'
     },
 
     xMinutes: {
       one: '1 minute',
-      other: '${count} minutes'
+      other: '{{count}} minutes'
     },
 
     aboutXHours: {
       one: 'about 1 hour',
-      other: 'about ${count} hours'
+      other: 'about {{count}} hours'
     },
 
     xDays: {
       one: '1 day',
-      other: '${count} days'
+      other: '{{count}} days'
     },
 
     aboutXMonths: {
       one: 'about 1 month',
-      other: 'about ${count} months'
+      other: 'about {{count}} months'
     },
 
     xMonths: {
       one: '1 month',
-      other: '${count} months'
+      other: '{{count}} months'
     },
 
     aboutXYears: {
       one: 'about 1 year',
-      other: 'about ${count} years'
+      other: 'about {{count}} years'
     },
 
     overXYears: {
       one: 'over 1 year',
-      other: 'over ${count} years'
+      other: 'over {{count}} years'
     },
 
     almostXYears: {
       one: 'almost 1 year',
-      other: 'almost ${count} years'
+      other: 'almost {{count}} years'
     }
   }
 
@@ -59,7 +59,7 @@ module.exports = function buildDistanceInWordsLocalizeFn () {
     } else if (count === 1) {
       return distanceInWordsLocale[token].one
     } else {
-      return distanceInWordsLocale[token].other.replace('${count}', count)
+      return distanceInWordsLocale[token].other.replace('{{count}}', count)
     }
   }
 }
