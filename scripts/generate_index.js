ffbf171d3a060ca3bed5ba8bdb8c6e021b09c473
Sasha Koss <koss@nocorp.me>
2016-10-25 10:54:09 +0200

    Add file exts (.js) in the index file for SystemJS sake

--- scripts/generate_index.js
+++ scripts/generate_index.js
@@ -3,7 +3,7 @@ import path from 'path'
 import listFiles from './_lib/list_files'
 
 const propertyRequireLines = listFiles()
-  .map((fn) => `  ${fn.name}: require('${fn.path.replace(/\.js$/, '')}/index')`)
+  .map((fn) => `  ${fn.name}: require('${fn.path.replace(/\.js$/, '')}/index.js')`)
 
 const indexLines = ['module.exports = {']
   .concat(propertyRequireLines.join(',\n'))
