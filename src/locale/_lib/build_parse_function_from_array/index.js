b84c047d2bf28bd1fc60a17210a4c1a0e349fad0
Lesha Koss <regiusprod@gmail.com>
2016-11-25 22:05:49 +0600

    Add parse property for English locale

---
+++ src/locale/_lib/build_parse_function_from_array/index.js
@@ -0,0 +1,14 @@
+function buildParseFunctionFromArray (array) {
+  var length = array.length
+  return function (matchResult) {
+    for (var i = 0; i < length; i++) {
+      if (matchResult[1] === array[i]) {
+        return i
+      }
+    }
+
+    return -1
+  }
+}
+
+module.exports = buildParseFunctionFromArray
