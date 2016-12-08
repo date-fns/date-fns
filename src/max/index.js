f01a888033efe3e4a484c6d7030f8493fa12b9e6
mihirk <mihir.khatwani@gmail.com>
2016-12-08 13:19:11 +0530

    Min/Max accepts array instead of spread arguments (resolves #266)

--- src/max/index.js
+++ src/max/index.js
@@ -13,16 +13,18 @@ var parse = require('../parse/index.js')
  * @example
  * // Which of these dates is the latest?
  * var result = max(
- *   new Date(1989, 6, 10),
- *   new Date(1987, 1, 11),
- *   new Date(1995, 6, 2),
- *   new Date(1990, 0, 1)
+ *  [
+ *    new Date(1989, 6, 10),
+ *    new Date(1987, 1, 11),
+ *    new Date(1995, 6, 2),
+ *    new Date(1990, 0, 1)
+ *  ]
  * )
  * //=> Sun Jul 02 1995 00:00:00
+ * @param datesArray
  */
-function max () {
-  var dirtyDates = Array.prototype.slice.call(arguments)
-  var dates = dirtyDates.map(function (dirtyDate) {
+function max (datesArray) {
+  var dates = datesArray.map(function (dirtyDate) {
     return parse(dirtyDate)
   })
   var latestTimestamp = Math.max.apply(null, dates)
