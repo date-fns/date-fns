4441528868dd7e28f9d56e625e83dcd2762582f7
mihirk <mihir.khatwani@gmail.com>
2016-12-08 13:19:11 +0530

    Min/Max accepts array instead of spread arguments (resolves #266)

--- src/min/test.js
+++ src/min/test.js
@@ -7,34 +7,42 @@ var min = require('./')
 describe('min', function () {
   it('returns the earliest date', function () {
     var result = min(
-      new Date(1989, 6 /* Jul */, 10),
-      new Date(1987, 1 /* Feb */, 11)
+      [
+        new Date(1989, 6 /* Jul */, 10),
+        new Date(1987, 1 /* Feb */, 11)
+      ]
     )
     assert.deepEqual(result, new Date(1987, 1 /* Feb */, 11))
   })
 
   it('allows to pass more than 2 arguments', function () {
     var result = min(
-      new Date(1987, 1 /* Feb */, 11),
-      new Date(1989, 6 /* Jul */, 10),
-      new Date(1985, 6 /* Jul */, 2),
-      new Date(1990, 0 /* Jan */, 1)
+      [
+        new Date(1987, 1 /* Feb */, 11),
+        new Date(1989, 6 /* Jul */, 10),
+        new Date(1985, 6 /* Jul */, 2),
+        new Date(1990, 0 /* Jan */, 1)
+      ]
     )
     assert.deepEqual(result, new Date(1985, 6 /* Jul */, 2))
   })
 
   it('accepts strings', function () {
     var result = min(
-      new Date(1987, 1 /* Feb */, 11).toISOString(),
-      new Date(1989, 6 /* Jul */, 10).toISOString()
+      [
+        new Date(1987, 1 /* Feb */, 11).toISOString(),
+        new Date(1989, 6 /* Jul */, 10).toISOString()
+      ]
     )
     assert.deepEqual(result, new Date(1987, 1 /* Feb */, 11))
   })
 
   it('accepts timestamps', function () {
     var result = min(
-      new Date(1989, 6 /* Jul */, 10).getTime(),
-      new Date(1987, 1 /* Feb */, 11).getTime()
+      [
+        new Date(1989, 6 /* Jul */, 10).getTime(),
+        new Date(1987, 1 /* Feb */, 11).getTime()
+      ]
     )
     assert.deepEqual(result, new Date(1987, 1 /* Feb */, 11))
   })
