8f1f58d10565d61db1cfb2b66a0f4a377e847ce3
Marek Suscak <marek.suscak@gmail.com>
2016-12-13 16:56:30 +0100

    Add slovak locale. (#336)
    * Add slovak format locale.
    
    * Remove unnecessary test cases.
    
    * Fix typo.
    
    * Add skeleton for distance in words.
    
    * Fix some bugs.
    
    * Fix all bugs.
    
    * Enable all test suites.
    
    * Make linter happy.

---
+++ src/locale/sk/build_format_locale/index.js
@@ -0,0 +1,73 @@
+var buildFormattingTokensRegExp = require('../../_lib/build_formatting_tokens_reg_exp/index.js')
+
+function buildFormatLocale () {
+  var months3char = ['jan', 'feb', 'mar', 'apr', 'máj', 'jún', 'júl', 'aug', 'sep', 'okt', 'nov', 'dec']
+  var monthsFull = ['január', 'február', 'marec', 'apríl', 'máj', 'jún', 'júl', 'august', 'september', 'október', 'november', 'december']
+  var weekdays2char = ['ne', 'po', 'ut', 'st', 'št', 'pi', 'so']
+  var weekdays3char = ['neď', 'pon', 'uto', 'str', 'štv', 'pia', 'sob']
+  var weekdaysFull = ['nedeľa', 'pondelok', 'utorok', 'streda', 'štvrtok', 'piatok', 'sobota']
+  var meridiemUppercase = ['AM', 'PM']
+  var meridiemLowercase = ['am', 'pm']
+  var meridiemFull = ['a.m.', 'p.m.']
+
+  var formatters = {
+    // Month: jan, feb, ..., dec
+    'MMM': function (date) {
+      return months3char[date.getMonth()]
+    },
+
+    // Month: január, február, ..., december
+    'MMMM': function (date) {
+      return monthsFull[date.getMonth()]
+    },
+
+    // Day of week: ne, po, ..., so
+    'dd': function (date) {
+      return weekdays2char[date.getDay()]
+    },
+
+    // Day of week: neď, pon, ..., sob
+    'ddd': function (date) {
+      return weekdays3char[date.getDay()]
+    },
+
+    // Day of week: neďeľa, pondelok, ..., sobota
+    'dddd': function (date) {
+      return weekdaysFull[date.getDay()]
+    },
+
+    // AM, PM
+    'A': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemUppercase[1] : meridiemUppercase[0]
+    },
+
+    // am, pm
+    'a': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemLowercase[1] : meridiemLowercase[0]
+    },
+
+    // a.m., p.m.
+    'aa': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemFull[1] : meridiemFull[0]
+    }
+  }
+
+  // Generate ordinal version of formatters: M -> Mo, D -> Do, etc.
+  var ordinalFormatters = ['M', 'D', 'DDD', 'd', 'Q', 'W']
+  ordinalFormatters.forEach(function (formatterToken) {
+    formatters[formatterToken + 'o'] = function (date, formatters) {
+      return ordinal(formatters[formatterToken](date))
+    }
+  })
+
+  return {
+    formatters: formatters,
+    formattingTokensRegExp: buildFormattingTokensRegExp(formatters)
+  }
+}
+
+function ordinal (number) {
+  return number + '.'
+}
+
+module.exports = buildFormatLocale
