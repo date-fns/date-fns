abb31d8931a50ca1e546858a8ef09de6f69ac216
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-12-22 00:33:32 +0100

    Add toObject duration function which converts an ISO8601 duration string into a js object. (cherry picked from commit 03e6f73)

---
+++ src/durations/to_object/test.js
@@ -0,0 +1,39 @@
+/* eslint-env mocha */
+
+var assert = require('power-assert')
+var toObject = require('./')
+
+describe('toObject', function () {
+  it('returns object with units as keys', function () {
+    var result = toObject('P1Y2M3W4DT5H6M7.89S')
+    assert.deepEqual(result, {
+      years: 1,
+      months: 2,
+      weeks: 3,
+      days: 4,
+      hours: 5,
+      minutes: 6,
+      seconds: 7.89
+    })
+  })
+
+  it('undefined values are returned as 0', function () {
+    var result = toObject('PT')
+    assert.deepEqual(result, {
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    })
+  })
+
+  it('duration object can\'t be modified', function() {
+    var result = toObject('PT')
+    result.years = 1
+    assert(result.years !== 1)
+  })
+})
+
