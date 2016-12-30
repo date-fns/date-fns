484db7feb7b1107f7f47d0ae918a154687065a30
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-12-30 17:04:39 +0100

    Add documentation to addDuration and subtract duration.

--- src/add_duration/index.js
+++ src/add_duration/index.js
@@ -9,6 +9,22 @@ var addHours = require('../add_hours')
 var addMinutes = require('../add_minutes')
 var addSeconds = require('../add_seconds')
 
+/**
+ * @category Duration Helpers
+ * @summary Add the specified ISO duration to a given date.
+ *
+ * @description
+ * Add the specified ISO duration to the given date.
+ *
+ * @param {Date|String|Number} date - the date to be changed
+ * @param {String} ISO Duration- the duration to be added
+ * @returns {Date} the new date with the duration added
+ *
+ * @example
+ * // Add P10D (10 Days) to 1 September 2014:
+ * var result = addDuration(new Date(2014, 8, 1), 'P10D')
+ * //=> Thu Sep 11 2014 00:00:00
+ */
 function addDuration (dirtyDate, dirtyDuration) {
   var date = parseDate(dirtyDate)
   var duration = parseDuration(dirtyDuration)
--- src/sub_duration/index.js
+++ src/sub_duration/index.js
@@ -9,6 +9,22 @@ var subHours = require('../sub_hours')
 var subMinutes = require('../sub_minutes')
 var subSeconds = require('../sub_seconds')
 
+/**
+ * @category Duration Helpers
+ * @summary Subtract the specified ISO duration from a given date.
+ *
+ * @description
+ * Subtract the specified ISO duration from the given date.
+ *
+ * @param {Date|String|Number} date - the date to be changed
+ * @param {String} ISO Duration- the duration to be subtracted
+ * @returns {Date} the new date with the duration subtracted
+ *
+ * @example
+ * // Subtract P10D (10 Days) from 1 September 2014:
+ * var result = subDuration(new Date(2014, 8, 1), 'P10D')
+ * //=> Fri Aug 22 2014 00:00:00
+ */
 function subDuration (dirtyDate, dirtyDuration) {
   var date = parseDate(dirtyDate)
   var duration = parseDuration(dirtyDuration)
