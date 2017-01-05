57207a5d3fc5997e9fe8986474b467b4ae5a2180
Pyry-Samuli Lahti <pyppe@iki.fi>
2017-01-05 18:22:34 +0200

    Add Finnish locale (#361)
    Closes date-fns/date-fns#360

---
+++ src/locale/fi/build_distance_in_words_locale/index.js
@@ -0,0 +1,136 @@
+function buildDistanceInWordsLocale () {
+  function futureSeconds (text) {
+    return text.replace(/sekuntia?/, 'sekunnin')
+  }
+
+  function futureMinutes (text) {
+    return text.replace(/minuuttia?/, 'minuutin')
+  }
+
+  function futureHours (text) {
+    return text.replace(/tuntia?/, 'tunnin')
+  }
+
+  function futureDays (text) {
+    return text.replace(/päivää?/, 'päivän')
+  }
+
+  function futureMonths (text) {
+    return text.replace(/(kuukausi|kuukautta)/, 'kuukauden')
+  }
+
+  function futureYears (text) {
+    return text.replace(/(vuosi|vuotta)/, 'vuoden')
+  }
+
+  var distanceInWordsLocale = {
+    lessThanXSeconds: {
+      one: 'alle sekunti',
+      other: 'alle {{count}} sekuntia',
+      futureTense: futureSeconds
+    },
+
+    xSeconds: {
+      one: 'sekunti',
+      other: '{{count}} sekuntia',
+      futureTense: futureSeconds
+    },
+
+    halfAMinute: {
+      one: 'puoli minuuttia',
+      other: 'puoli minuuttia',
+      futureTense: function (text) {
+        return 'puolen minuutin'
+      }
+    },
+
+    lessThanXMinutes: {
+      one: 'alle minuutti',
+      other: 'alle {{count}} minuuttia',
+      futureTense: futureMinutes
+    },
+
+    xMinutes: {
+      one: 'minuutti',
+      other: '{{count}} minuuttia',
+      futureTense: futureMinutes
+    },
+
+    aboutXHours: {
+      one: 'noin tunti',
+      other: 'noin {{count}} tuntia',
+      futureTense: futureHours
+    },
+
+    xHours: {
+      one: 'tunti',
+      other: '{{count}} tuntia',
+      futureTense: futureHours
+    },
+
+    xDays: {
+      one: 'päivä',
+      other: '{{count}} päivää',
+      futureTense: futureDays
+    },
+
+    aboutXMonths: {
+      one: 'noin kuukausi',
+      other: 'noin {{count}} kuukautta',
+      futureTense: futureMonths
+    },
+
+    xMonths: {
+      one: 'kuukausi',
+      other: '{{count}} kuukautta',
+      futureTense: futureMonths
+    },
+
+    aboutXYears: {
+      one: 'noin vuosi',
+      other: 'noin {{count}} vuotta',
+      futureTense: futureYears
+    },
+
+    xYears: {
+      one: 'vuosi',
+      other: '{{count}} vuotta',
+      futureTense: futureYears
+    },
+
+    overXYears: {
+      one: 'yli vuosi',
+      other: 'yli {{count}} vuotta',
+      futureTense: futureYears
+    },
+
+    almostXYears: {
+      one: 'lähes vuosi',
+      other: 'lähes {{count}} vuotta',
+      futureTense: futureYears
+    }
+  }
+
+  function localize (token, count, options) {
+    options = options || {}
+
+    var distance = distanceInWordsLocale[token]
+    var result = count === 1 ? distance.one : distance.other.replace('{{count}}', count)
+
+    if (options.addSuffix) {
+      if (options.comparison > 0) {
+        return distance.futureTense(result) + ' kuluttua'
+      } else {
+        return result + ' sitten'
+      }
+    }
+
+    return result
+  }
+
+  return {
+    localize: localize
+  }
+}
+
+module.exports = buildDistanceInWordsLocale
---
+++ src/locale/fi/build_distance_in_words_locale/test.js
@@ -0,0 +1,266 @@
+// @flow
+/* eslint-env mocha */
+
+var assert = require('power-assert')
+var buildDistanceInWordsLocale = require('./')
+
+var FUTURE_OPTIONS = {
+  addSuffix: true,
+  comparison: 1
+}
+
+describe('fi locale > buildDistanceInWordsLocale', function () {
+  it('returns an object', function () {
+    assert(typeof buildDistanceInWordsLocale() === 'object')
+  })
+
+  it('localize property is a function', function () {
+    assert(typeof buildDistanceInWordsLocale().localize === 'function')
+  })
+
+  describe('lessThanXSeconds', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXSeconds', 1) === 'alle sekunti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXSeconds', 2) === 'alle 2 sekuntia')
+      })
+    })
+  })
+
+  describe('xSeconds', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xSeconds', 1) === 'sekunti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xSeconds', 2) === '2 sekuntia')
+      })
+    })
+  })
+
+  describe('halfAMinute', function () {
+    it('returns a proper string', function () {
+      assert(buildDistanceInWordsLocale().localize('halfAMinute') === 'puoli minuuttia')
+    })
+
+    it('ignores the second argument', function () {
+      assert(buildDistanceInWordsLocale().localize('halfAMinute', 123) === 'puoli minuuttia')
+    })
+  })
+
+  describe('lessThanXMinutes', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXMinutes', 1) === 'alle minuutti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXMinutes', 2) === 'alle 2 minuuttia')
+      })
+    })
+  })
+
+  describe('xMinutes', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMinutes', 1) === 'minuutti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMinutes', 2) === '2 minuuttia')
+      })
+    })
+  })
+
+  describe('aboutXHours', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXHours', 1) === 'noin tunti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXHours', 2) === 'noin 2 tuntia')
+      })
+    })
+  })
+
+  describe('xHours', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xHours', 1) === 'tunti')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xHours', 2) === '2 tuntia')
+      })
+    })
+  })
+
+  describe('xDays', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xDays', 1) === 'päivä')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xDays', 2) === '2 päivää')
+      })
+    })
+  })
+
+  describe('aboutXMonths', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXMonths', 1) === 'noin kuukausi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXMonths', 2) === 'noin 2 kuukautta')
+      })
+    })
+  })
+
+  describe('xMonths', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMonths', 1) === 'kuukausi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMonths', 2) === '2 kuukautta')
+      })
+    })
+  })
+
+  describe('aboutXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXYears', 1) === 'noin vuosi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXYears', 2) === 'noin 2 vuotta')
+      })
+    })
+  })
+
+  describe('xYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xYears', 1) === 'vuosi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xYears', 2) === '2 vuotta')
+      })
+    })
+  })
+
+  describe('overXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('overXYears', 1) === 'yli vuosi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('overXYears', 2) === 'yli 2 vuotta')
+      })
+    })
+  })
+
+  describe('almostXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('almostXYears', 1) === 'lähes vuosi')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('almostXYears', 2) === 'lähes 2 vuotta')
+      })
+    })
+  })
+
+  context('with a past suffix', function () {
+    it('adds `sitten` to a string', function () {
+      var result = buildDistanceInWordsLocale().localize('aboutXYears', 1, {
+        addSuffix: true,
+        comparison: -1
+      })
+      assert(result === 'noin vuosi sitten')
+    })
+  })
+
+  describe('future suffix', function () {
+    context('when the context is `xSeconds`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('xSeconds', 40, FUTURE_OPTIONS)
+        assert(result === '40 sekunnin kuluttua')
+      })
+    })
+
+    context('when the context is `lessThanXMinutes`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('lessThanXMinutes', 1, FUTURE_OPTIONS)
+        assert(result === 'alle minuutin kuluttua')
+      })
+    })
+
+    context('when the context is `aboutXHours`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('aboutXHours', 1, FUTURE_OPTIONS)
+        assert(result === 'noin tunnin kuluttua')
+      })
+    })
+
+    context('when the context is `xDays`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('xDays', 1, FUTURE_OPTIONS)
+        assert(result === 'päivän kuluttua')
+      })
+    })
+
+    context('when the context is `aboutXMonths`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('aboutXMonths', 1, FUTURE_OPTIONS)
+        assert(result === 'noin kuukauden kuluttua')
+      })
+    })
+
+    context('when the context is `almostXYears`', function () {
+      it('returns a proper string', function () {
+        var result = buildDistanceInWordsLocale().localize('almostXYears', 1, FUTURE_OPTIONS)
+        assert(result === 'lähes vuoden kuluttua')
+      })
+    })
+  })
+})
