170486ad0cf169d97b9fc8280d4416b3a3b759df
Lesha Koss <regiusprod@gmail.com>
2017-01-05 15:53:35 +0000

    Add flow typings generator

--- scripts/generate_typings.js
+++ scripts/generate_typings.js
@@ -16,15 +16,47 @@ function correctTypeCase (type) {
   return type
 }
 
-function getObjectParam (param) {
-  const name = param.name.split('.')[1]
-  if (param.optional) {
-    return `${name}?`
+function getParams (params, {indent = 1, leftBorder = '{', rightBorder = '}'} = {}) {
+  if (params.length === 0) {
+    return '()'
   }
-  return name
+
+  const props = params
+    .filter(param => param.name.includes('.'))
+    .map(param => {
+      const [parent, name] = param.name.split('.')
+      const {type, variable, optional} = param
+      return {
+        parent,
+        name,
+        type,
+        variable,
+        optional
+      }
+    })
+
+  const paramIndent = '  '.repeat(indent + 1)
+  const borderIndent = '  '.repeat(indent)
+
+  return [leftBorder]
+    .concat(params
+      .filter(param => !param.name.includes('.'))
+      .map(param => {
+        const name = param.name
+        const type = getType(param.type.names, {
+          props: props.filter(param => param.parent === name),
+          forceArray: param.variable,
+          indent
+        })
+        return `${paramIndent}${param.variable ? '...' : ''}${name}${param.optional ? '?' : ''}: ${type}`
+      })
+      .join(',\n')
+    )
+    .concat(borderIndent + rightBorder)
+    .join('\n')
 }
 
-function getTypescriptTypes (types, objectParams = [], forceArray) {
+function getType (types, {props = [], forceArray = false, indent = 1} = {}) {
   const typeStrings = types.map(type => {
     if (type === '*') {
       return 'any'
@@ -35,8 +67,8 @@ function getTypescriptTypes (types, objectParams = [], forceArray) {
       return `${correctTypeCase(arrayType)}[]`
     }
 
-    if (objectParams.length > 0) {
-      return `{${objectParams.map(param => `${getObjectParam(param)}: ${getTypescriptTypes(param.type.names)}`).join(', ')}}`
+    if (props.length > 0) {
+      return getParams(props, {indent: indent + 1})
     }
 
     const caseCorrectedType = correctTypeCase(type)
@@ -55,34 +87,26 @@ function getTypescriptTypes (types, objectParams = [], forceArray) {
   return typeStrings.join(' | ')
 }
 
-function getModuleDefinition (name, moduleSuffix = '') {
-  return `
-declare module 'date-fns/${camelCaseToSnakeCase(name)}${moduleSuffix}' {
-  import {${name}} from 'date-fns'
-  export = ${name}
-}`
+function getTypeScriptModuleDefinition (name, moduleSuffix = '') {
+  return [`declare module 'date-fns/${camelCaseToSnakeCase(name)}${moduleSuffix}' {`]
+    .concat(`  import {${name}} from 'date-fns'`)
+    .concat(`  export = ${name}`)
+    .concat('}')
+    .join('\n')
 }
 
-function getTypescriptDefinition (fn) {
+function getTypeScriptFnDefinition (fn) {
   const name = fn.content.name
 
-  const objectParams = fn.content.params.filter(param => param.name.includes('.'))
+  const params = getParams(fn.content.params, {indent: 1, leftBorder: '(', rightBorder: ')'})
+  const returns = getType(fn.content.returns[0].type.names)
 
-  const params = fn.content.params.filter(param => !param.name.includes('.')).map(param => {
-    const name = param.name
-    const types = getTypescriptTypes(param.type.names, objectParams.filter(param => param.name.startsWith(name)), param.variable)
-    return `${param.variable ? '...' : ''}${name}${param.optional ? '?' : ''}: ${types}`
-  }).join(',\n    ')
-  const returns = getTypescriptTypes(fn.content.returns[0].type.names)
+  const fnDefinition = [`  function ${name} ${params}: ${returns}`]
+    .concat(`  namespace ${name} {}`)
+    .join('\n')
 
-  const fnDefinition = `
-  function ${name}(
-    ${params}
-  ): ${returns}
-  namespace ${name} {}`
-
-  const moduleDefinition = getModuleDefinition(name)
-  const moduleIndexDefinition = getModuleDefinition(name, '/index')
+  const moduleDefinition = getTypeScriptModuleDefinition(name)
+  const moduleIndexDefinition = getTypeScriptModuleDefinition(name, '/index')
 
   return {
     fnDefinition,
@@ -91,18 +115,48 @@ function getTypescriptDefinition (fn) {
   }
 }
 
-const fns = Object.keys(docs)
-  .filter(key => key.endsWith(' Helpers'))
-  .map(category => docs[category])
-  .reduce((previousValue, newValue) => [...previousValue, ...newValue], [])
-  .sort((a, b) => a.content.name.localeCompare(b.content.name))
-  .map(getTypescriptDefinition)
+function generateFlowFnTyping (fn) {
+  const filename = `./src/${camelCaseToSnakeCase(fn.content.name)}/index.js.flow`
 
-const definitionString = `declare module 'date-fns' {
-  ${fns.map(fn => fn.fnDefinition).join('\n')}
+  const params = getParams(fn.content.params, {indent: 0, leftBorder: '(', rightBorder: ')'})
+  const returns = getType(fn.content.returns[0].type.names)
+
+  const typingString = ['// @flow']
+    .concat('')
+    .concat(`declare module.exports: ${params} => ${returns}\n`)
+    .join('\n')
+
+  fs.writeFileSync(filename, typingString)
 }
-${fns.map(fn => fn.moduleDefinition).join('\n')}
-${fns.map(fn => fn.moduleIndexDefinition).join('\n')}
-`
 
-fs.writeFileSync('./typings.d.ts', definitionString)
+function generateTypeScriptTypings () {
+  const fns = Object.keys(docs)
+    .filter(key => key.endsWith(' Helpers'))
+    .map(category => docs[category])
+    .reduce((previousValue, newValue) => [...previousValue, ...newValue], [])
+    .sort((a, b) => a.content.name.localeCompare(b.content.name))
+    .map(getTypeScriptFnDefinition)
+
+  const definitionString = ['declare module \'date-fns\' {']
+    .concat(`${fns.map(fn => fn.fnDefinition).join('\n\n')}`)
+    .concat('}')
+    .concat('')
+    .concat(`${fns.map(fn => fn.moduleDefinition).join('\n\n')}`)
+    .concat('')
+    .concat(`${fns.map(fn => fn.moduleIndexDefinition).join('\n\n')}`)
+    .concat('')
+    .join('\n')
+
+  fs.writeFileSync('./typings.d.ts', definitionString)
+}
+
+function generateFlowTypings () {
+  Object.keys(docs)
+    .filter(key => key.endsWith(' Helpers'))
+    .map(category => docs[category])
+    .reduce((previousValue, newValue) => [...previousValue, ...newValue], [])
+    .forEach(generateFlowFnTyping)
+}
+
+generateTypeScriptTypings()
+generateFlowTypings()
