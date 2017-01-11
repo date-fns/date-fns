a23ea4918d8dd16e71eecc5c3fa2d977f7ff8183
Nikolay Stoynov <arvigeus@gmail.com>
2017-01-11 22:16:26 +0200

    Added Bulgarian localization (#357)
    * Added Bulgarian localization
    
    * Lowercase month names
    
    * Fix month names test to use lowercase
    
    * Code fixes, add author
    
    * Remove unnecessary comments
    
    * Fix meridiem times

---
+++ src/locale/bg/build_distance_in_words_locale/index.js
@@ -0,0 +1,99 @@
+function buildDistanceInWordsLocale () {
+  var distanceInWordsLocale = {
+    lessThanXSeconds: {
+      one: 'по-малко от секунда',
+      other: 'по-малко от {{count}} секунди'
+    },
+
+    xSeconds: {
+      one: '1 секунда',
+      other: '{{count}} секунди'
+    },
+
+    halfAMinute: 'половин минута',
+
+    lessThanXMinutes: {
+      one: 'по-малко от минута',
+      other: 'по-малко от {{count}} минути'
+    },
+
+    xMinutes: {
+      one: '1 минута',
+      other: '{{count}} минути'
+    },
+
+    aboutXHours: {
+      one: 'около час',
+      other: 'около {{count}} часа'
+    },
+
+    xHours: {
+      one: '1 час',
+      other: '{{count}} часа'
+    },
+
+    xDays: {
+      one: '1 ден',
+      other: '{{count}} дни'
+    },
+
+    aboutXMonths: {
+      one: 'около месец',
+      other: 'около {{count}} месеца'
+    },
+
+    xMonths: {
+      one: '1 месец',
+      other: '{{count}} месеца'
+    },
+
+    aboutXYears: {
+      one: 'около година',
+      other: 'около {{count}} години'
+    },
+
+    xYears: {
+      one: '1 година',
+      other: '{{count}} години'
+    },
+
+    overXYears: {
+      one: 'над година',
+      other: 'над {{count}} години'
+    },
+
+    almostXYears: {
+      one: 'почти година',
+      other: 'почти {{count}} години'
+    }
+  }
+
+  function localize (token, count, options) {
+    options = options || {}
+
+    var result
+    if (typeof distanceInWordsLocale[token] === 'string') {
+      result = distanceInWordsLocale[token]
+    } else if (count === 1) {
+      result = distanceInWordsLocale[token].one
+    } else {
+      result = distanceInWordsLocale[token].other.replace('{{count}}', count)
+    }
+
+    if (options.addSuffix) {
+      if (options.comparison > 0) {
+        return 'след ' + result
+      } else {
+        return 'преди ' + result
+      }
+    }
+
+    return result
+  }
+
+  return {
+    localize: localize
+  }
+}
+
+module.exports = buildDistanceInWordsLocale
---
+++ src/locale/bg/build_format_locale/index.js
@@ -0,0 +1,87 @@
+var buildFormattingTokensRegExp = require('../../_lib/build_formatting_tokens_reg_exp/index.js')
+
+function buildFormatLocale () {
+  var months3char = ['яну', 'фев', 'мар', 'апр', 'май', 'юни', 'юли', 'авг', 'сеп', 'окт', 'ное', 'дек']
+  var monthsFull = ['януари', 'февруари', 'март', 'април', 'май', 'юни', 'юли', 'август', 'септември', 'октомври', 'ноември', 'декември']
+  var weekdays2char = ['нд', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб']
+  var weekdays3char = ['нед', 'пон', 'вто', 'сря', 'чет', 'пет', 'съб']
+  var weekdaysFull = ['неделя', 'понеделник', 'вторник', 'сряда', 'четвъртък', 'петък', 'събота']
+  var meridiem = ['сутринта', 'на обяд', 'следобед', 'вечерта']
+
+  var timeOfDay = function (date) {
+    var hours = date.getHours()
+    if (hours >= 4 && hours < 12) {
+      return meridiem[0]
+    } else if (hours >= 12 && hours < 14) {
+      return meridiem[1]
+    } else if (hours >= 14 && hours < 17) {
+      return meridiem[2]
+    } else {
+      return meridiem[3]
+    }
+  }
+
+  var formatters = {
+    // Month: Jan, Feb, ..., Dec
+    'MMM': function (date) {
+      return months3char[date.getMonth()]
+    },
+
+    // Month: January, February, ..., December
+    'MMMM': function (date) {
+      return monthsFull[date.getMonth()]
+    },
+
+    // Day of week: Su, Mo, ..., Sa
+    'dd': function (date) {
+      return weekdays2char[date.getDay()]
+    },
+
+    // Day of week: Sun, Mon, ..., Sat
+    'ddd': function (date) {
+      return weekdays3char[date.getDay()]
+    },
+
+    // Day of week: Sunday, Monday, ..., Saturday
+    'dddd': function (date) {
+      return weekdaysFull[date.getDay()]
+    },
+
+    // AM, PM
+    'A': timeOfDay,
+
+    // am, pm
+    'a': timeOfDay,
+
+    // a.m., p.m.
+    'aa': timeOfDay
+  }
+
+  // Generate ordinal version of formatters: M -> Mo, D -> Do, etc.
+  var ordinalFormatters = ['M', 'D', 'DDD', 'd', 'Q', 'W']
+  ordinalFormatters.forEach(function (formatterToken) {
+    formatters[formatterToken + 'o'] = function (date, formatters) {
+      return ordinal(formatters[formatterToken](date))
+    }
+  })
+
+  return {
+    formatters: formatters,
+    formattingTokensRegExp: buildFormattingTokensRegExp(formatters)
+  }
+}
+
+function ordinal (number) {
+  var rem100 = number % 100
+  if (rem100 > 20 || rem100 < 10) {
+    switch (rem100 % 10) {
+      case 1:
+        return number + '-ви'
+      case 2:
+        return number + '-ри'
+    }
+  }
+  return number + '-и'
+}
+
+module.exports = buildFormatLocale
