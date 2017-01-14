020efc1e9c82f538992f4ae2a5ba90e803851c9d
Olivier Colonna <olivier.colonna@drivetribe.com>
2017-01-14 00:14:28 +0000

    add nextMonth feature

---
+++ src/next_month/index.js
@@ -0,0 +1,26 @@
+var parse = require('../parse/index.js');
+var getMonth = require('../get_month/index.js');
+
+/**
+ * @category Month Helpers
+ * @summary return the previous month of the given date
+ * 
+ * @description
+ * Return the next month of the given date.
+ * 
+ * @returns {Number} the previous month
+ * 
+ * @example
+ * // Which is previous month from 13 Januray 2017?
+ * var result = nextMonth(new Date(2012, 12, 25));
+ * // => 0
+ */
+function nextMonth (dirtyDate) {
+  var date = parse(dirtyDate);
+  var month = getMonth(date);
+  var nextMonth = (month === 11) ? 0 : month + 1;
+
+  return nextMonth;
+}
+
+module.exports = nextMonth;
---
+++ src/next_month/test.js
@@ -0,0 +1,22 @@
+// @flow
+/* eslint-env mocha */
+
+var assert = require('power-assert');
+var nextMonth = require('./');
+
+describe('nextMonth', function () {
+  it('returns the next month of the given date', function () {
+    var result = nextMonth(next Date(2014, 11 /* Dev */, 25));
+    assert(result === 0);
+  });
+
+  it('accepts a string', function () {
+    var result = nextMonth(new Date(2014, 6 /* Jul */, 2).toISOString())
+    assert(result === 7)
+  })
+
+  it('accepts a timestamp', function () {
+    var result = getMonth(new Date(2014, 3 /* Apr */, 2).getTime())
+    assert(result === 4)
+  })
+});
