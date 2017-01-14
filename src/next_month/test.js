90f5004133dd75cb51b62167fa4dd79fded47d47
Olivier Colonna <olivier.colonna@drivetribe.com>
2017-01-14 11:35:00 +0000

    fixing test, semi-column, getMonth

--- src/next_month/test.js
+++ src/next_month/test.js
@@ -1,13 +1,13 @@
 // @flow
 /* eslint-env mocha */
 
-var assert = require('power-assert');
-var nextMonth = require('./');
+var assert = require('power-assert')
+var nextMonth = require('./')
 
 describe('nextMonth', function () {
   it('returns the next month of the given date', function () {
-    var result = nextMonth(new Date(2014, 11 /* Dev */, 25));
-    assert(result === 0);
+    var result = nextMonth(new Date(2014, 11 /* Dev */, 25))
+    assert(result === 0)
   });
 
   it('accepts a string', function () {
@@ -16,7 +16,7 @@ describe('nextMonth', function () {
   })
 
   it('accepts a timestamp', function () {
-    var result = getMonth(new Date(2014, 3 /* Apr */, 2).getTime())
+    var result = nextMonth(new Date(2014, 3 /* Apr */, 2).getTime())
     assert(result === 4)
   })
-});
+})
