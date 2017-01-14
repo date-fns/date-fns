b59f362978c6d98530b75c68e352613b61d7f270
Lesha Koss <regiusprod@gmail.com>
2017-01-14 13:00:05 +0000

    Fix shared docs

--- docs/Range.js
+++ docs/Range.js
@@ -6,7 +6,7 @@
  * An object that combines two dates to represent the range.
  *
  * @typedef {Object} Range
- * @param {Date|String|Number} start - the start of the range
- * @param {Date|String|Number} end - the end of the range
+ * @property {Date|String|Number} start - the start of the range
+ * @property {Date|String|Number} end - the end of the range
  */
 var Range = {}
