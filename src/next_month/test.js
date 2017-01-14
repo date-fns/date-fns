92a66bd536f0e28d2041366faa6897e2f0516b85
Olivier Colonna <olivier.colonna@drivetribe.com>
2017-01-14 12:14:23 +0000

    fixing test by removing extra semi-colon

--- src/next_month/test.js
+++ src/next_month/test.js
@@ -8,7 +8,7 @@ describe('nextMonth', function () {
   it('returns the next month of the given date', function () {
     var result = nextMonth(new Date(2014, 11 /* Dev */, 25))
     assert(result === 0)
-  });
+  })
 
   it('accepts a string', function () {
     var result = nextMonth(new Date(2014, 6 /* Jul */, 2).toISOString())
