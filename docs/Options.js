81b555d4f53d1636904a0b4f35a4d62944e73968
Lesha Koss <regiusprod@gmail.com>
2017-01-14 13:00:05 +0000

    Fix shared docs

--- docs/Options.js
+++ docs/Options.js
@@ -9,16 +9,16 @@
  * @property {Number} [weekStartsOn=0] - the index of the first day of the week (0 - Sunday).
  *   Used by `differenceInCalendarWeeks`, `endOfWeek`, `isSameWeek`,
  *   `isThisWeek`, `lastDayOfWeek`, `setDay`, and `startOfWeek`
- * @property {Number} [additionalDigits=2] - the additional number of digits in the extended year format.
- *   Options: 0, 1 or 2. Used by all functions that take String as Date-like argument.
+ * @property {0 | 1 | 2} [additionalDigits=2] - the additional number of digits in the extended year format.
+ *   Used by all functions that take String as Date-like argument.
  *   Internally, passed to `parse` to specify which way to convert extended year formatted String to Date.
  *   See [`parse`]{@link docs/parse}
  * @property {Object} [locale=enLocale] - the locale object.
  *   Used by `distanceInWords`, `distanceInWordsToNow`, `distanceInWordsStrict` and `format`
- * @param {String} [unit] - used by `distanceInWordsStrict`.
- *   If specified, will force a unit. Options: 's', 'm', 'h', 'd', 'M', 'Y'
- * @param {String} [partialMethod='floor'] - used by `distanceInWordsStrict`.
- *   Specifies, which way to round partial units. Options: 'floor', 'ceil', 'round'
+ * @property {'s'|'m'|'h'|'d'|'M'|'Y'} [unit] - used by `distanceInWordsStrict`.
+ *   If specified, will force a unit
+ * @property {'floor'|'ceil'|'round'} [partialMethod='floor'] - used by `distanceInWordsStrict`.
+ *   Specifies, which way to round partial units
  *
  * @example
  * // For 15 December 12345 AD, represent the start of the week in Esperanto,
