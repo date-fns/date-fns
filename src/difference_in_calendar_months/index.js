58f481c90b9f37c6f1bd21e892349b3ce2e6bba2
Lesha Koss <regiusprod@gmail.com>
2017-01-15 17:47:16 +0000

    Swap arguments in differenceInX functions (#396)
    * Swap arguments in differenceInX fns
    
    * Add entry to CHANGELOG.md

--- src/difference_in_calendar_months/index.js
+++ src/difference_in_calendar_months/index.js
@@ -7,16 +7,16 @@ var toDate = require('../to_date/index.js')
  * @description
  * Get the number of calendar months between the given dates.
  *
- * @param {Date|String|Number} dateLeft - the later date
- * @param {Date|String|Number} dateRight - the earlier date
+ * @param {Date|String|Number} dateLeft - the earlier date
+ * @param {Date|String|Number} dateRight - the later date
  * @param {Options} [options] - the object with options. See [Options]{@link docs/Options}
  * @returns {Number} the number of calendar months
  *
  * @example
  * // How many calendar months are between 31 January 2014 and 1 September 2014?
  * var result = differenceInCalendarMonths(
- *   new Date(2014, 8, 1),
- *   new Date(2014, 0, 31)
+ *   new Date(2014, 0, 31),
+ *   new Date(2014, 8, 1)
  * )
  * //=> 8
  */
@@ -24,8 +24,8 @@ function differenceInCalendarMonths (dirtyDateLeft, dirtyDateRight, options) {
   var dateLeft = toDate(dirtyDateLeft, options)
   var dateRight = toDate(dirtyDateRight, options)
 
-  var yearDiff = dateLeft.getFullYear() - dateRight.getFullYear()
-  var monthDiff = dateLeft.getMonth() - dateRight.getMonth()
+  var yearDiff = dateRight.getFullYear() - dateLeft.getFullYear()
+  var monthDiff = dateRight.getMonth() - dateLeft.getMonth()
 
   return yearDiff * 12 + monthDiff
 }
--- src/difference_in_quarters/index.js
+++ src/difference_in_quarters/index.js
@@ -7,16 +7,16 @@ var differenceInMonths = require('../difference_in_months/index.js')
  * @description
  * Get the number of full quarters between the given dates.
  *
- * @param {Date|String|Number} dateLeft - the later date
- * @param {Date|String|Number} dateRight - the earlier date
+ * @param {Date|String|Number} dateLeft - the earlier date
+ * @param {Date|String|Number} dateRight - the later date
  * @param {Options} [options] - the object with options. See [Options]{@link docs/Options}
  * @returns {Number} the number of full quarters
  *
  * @example
  * // How many full quarters are between 31 December 2013 and 2 July 2014?
  * var result = differenceInQuarters(
- *   new Date(2014, 6, 2),
- *   new Date(2013, 11, 31)
+ *   new Date(2013, 11, 31),
+ *   new Date(2014, 6, 2)
  * )
  * //=> 2
  */
