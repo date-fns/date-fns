99242534c38df4afb7f2d3d72de154bb8cb970cd
Olivier Colonna <olivier.colonna@drivetribe.com>
2017-01-16 21:53:37 +0000

    removing semi-colon

--- src/get_next_year/index.js
+++ src/get_next_year/index.js
@@ -4,13 +4,13 @@ var getYear = require('../get_year/index.js')
 /**
  * @category Month Helpers
  * @summary Get the next year of the given date.
- * 
+ *
  * @description
  * Get the next year of the given date.
- * 
+ *
  * @param {date|String|Number} date - the given date
  * @return {number} the year
- * 
+ *
  * @example
  * // Which year is after 28 February 2016?
  * var result = getNextYear(new Date(2016, 2, 28));
@@ -18,9 +18,9 @@ var getYear = require('../get_year/index.js')
  */
 function getNextYear (dirtyDate) {
   var date = parse(dirtyDate)
-  var year = getYear(date);
+  var year = getYear(date)
 
-  return year + 1;
+  return year + 1
 };
 
 module.exports = getNextYear
