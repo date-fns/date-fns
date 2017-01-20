647ca8fa772c26c7769672e6db99d91c80dea8bc
Lesha Koss <regiusprod@gmail.com>
2017-01-20 16:46:46 +0000

    Fix parse benchmark (#410)

--- src/parse/benchmark.js
+++ src/parse/benchmark.js
@@ -2,15 +2,19 @@
 /* eslint-env mocha */
 /* global suite, benchmark */
 
-var format = require('./')
+var parse = require('./')
 var moment = require('moment')
 
 suite('parse', function () {
   benchmark('date-fns', function () {
-    return parse("Tuesday, January 10th 2017, 11:07:40 am", 'dddd, MMMM Do YYYY, h:mm:ss a')
+    return parse("Tuesday, January 10th 2017, 11:07:40 am", 'dddd, MMMM Do YYYY, h:mm:ss a', this.date)
   })
 
   benchmark('Moment.js', function () {
     return moment("Tuesday, January 10th 2017, 11:07:40 am", 'dddd, MMMM Do YYYY, h:mm:ss a')
   })
+}, {
+  setup: function () {
+    this.date = new Date()
+  }
 })
