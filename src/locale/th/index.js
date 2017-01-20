44c291d400caa3f2ed8832035e21f1ab76f68fb9
Lesha Koss <regiusprod@gmail.com>
2017-01-20 16:58:25 +0000

    Options shared docs and minor docs fixes (#409)
    * Fix shared docs
    
    * Fix toDate and Options docs
    
    * Add Locale shared doc
    
    * Add module.exports for shared docs
    
    lint's `no-unused-vars`

--- src/locale/th/index.js
+++ src/locale/th/index.js
@@ -2,6 +2,7 @@ var buildDistanceInWordsLocale = require('./build_distance_in_words_locale/index
 var buildFormatLocale = require('./build_format_locale/index.js')
 
 /**
+ * @type {Locale}
  * @category Locales
  * @summary Thai locale.
  * @author Athiwat Hirunworawongkun [@athivvat]{@link https://github.com/athivvat}
