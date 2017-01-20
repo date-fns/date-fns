ce0bf300c621a561c9f1075594d64a816f5b5020
Lesha Koss <regiusprod@gmail.com>
2017-01-20 17:53:37 +0000

    Rename ranges to intervals

---
+++ docs/Interval.js
@@ -0,0 +1,14 @@
+/**
+ * @category Types
+ * @summary An object that combines two dates to represent the time interval.
+ *
+ * @description
+ * An object that combines two dates to represent the time interval.
+ *
+ * @typedef {Object} Interval
+ * @property {Date|String|Number} start - the start of the interval
+ * @property {Date|String|Number} end - the end of the interval
+ */
+var Interval = {}
+
+module.exports = Interval
