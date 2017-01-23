d02a3c1b67d79d50f04f12969a2c82cde4505657
Lesha Koss <regiusprod@gmail.com>
2017-01-23 16:24:10 +0000

    Shared doc types typing generation (#415)
    * Generate aliases for TypeScript and flow
    
    * Reword generate_* to build_*
    
    * Regenerate docs and typings
    
    * Fix parse/benchmark.js style

--- src/parse/benchmark.js
+++ src/parse/benchmark.js
@@ -7,11 +7,11 @@ var moment = require('moment')
 
 suite('parse', function () {
   benchmark('date-fns', function () {
-    return parse("Tuesday, January 10th 2017, 11:07:40 am", 'dddd, MMMM Do YYYY, h:mm:ss a', this.date)
+    return parse('Tuesday, January 10th 2017, 11:07:40 am', 'dddd, MMMM Do YYYY, h:mm:ss a', this.date)
   })
 
   benchmark('Moment.js', function () {
-    return moment("Tuesday, January 10th 2017, 11:07:40 am", 'dddd, MMMM Do YYYY, h:mm:ss a')
+    return moment('Tuesday, January 10th 2017, 11:07:40 am', 'dddd, MMMM Do YYYY, h:mm:ss a')
   })
 }, {
   setup: function () {
