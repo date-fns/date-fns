615a62d503a8d33441b2515a5d7693fd70b73fb6
Lesha Koss <regiusprod@gmail.com>
2017-02-01 15:22:01 +0600

    Rename ranges to intervals (closes #407) (#411)
    * Rename ranges to intervals
    
    * Rename eachDay -> eachDayOfInterval
    
    * Regenerate typings
    
    * Modify interval CHANGELOG.md entry

--- src/is_within_interval/test.js
+++ src/is_within_interval/test.js
@@ -2,27 +2,27 @@
 /* eslint-env mocha */
 
 var assert = require('power-assert')
-var isWithinRange = require('./')
+var isWithinInterval = require('./')
 
-describe('isWithinRange', function () {
-  it('returns true if the given date in within the given range', function () {
-    var result = isWithinRange(
+describe('isWithinInterval', function () {
+  it('returns true if the given date in within the given interval', function () {
+    var result = isWithinInterval(
       new Date(2014, 9 /* Oct */, 31),
       {start: new Date(2014, 8 /* Sep */, 1), end: new Date(2014, 11 /* Dec */, 31)}
     )
     assert(result === true)
   })
 
-  it('returns true if the given date has same time as the left boundary of the range', function () {
-    var result = isWithinRange(
+  it('returns true if the given date has same time as the left boundary of the interval', function () {
+    var result = isWithinInterval(
       new Date(2014, 8 /* Sep */, 1),
       {start: new Date(2014, 8 /* Sep */, 1), end: new Date(2014, 11 /* Dec */, 31)}
     )
     assert(result === true)
   })
 
-  it('returns true if the given date has same time as the right boundary of the range', function () {
-    var result = isWithinRange(
+  it('returns true if the given date has same time as the right boundary of the interval', function () {
+    var result = isWithinInterval(
       new Date(2014, 11 /* Dec */, 31),
       {start: new Date(2014, 8 /* Sep */, 1), end: new Date(2014, 11 /* Dec */, 31)}
     )
@@ -30,15 +30,15 @@ describe('isWithinRange', function () {
   })
 
   it('returns true if the given date and the both boundaries are the same', function () {
-    var result = isWithinRange(
+    var result = isWithinInterval(
       new Date(2014, 11 /* Dec */, 31),
       {start: new Date(2014, 11 /* Dec */, 31), end: new Date(2014, 11 /* Dec */, 31)}
     )
     assert(result === true)
   })
 
-  it('returns false if the given date is outside of the range', function () {
-    var result = isWithinRange(
+  it('returns false if the given date is outside of the interval', function () {
+    var result = isWithinInterval(
       new Date(2014, 1 /* Feb */, 11),
       {start: new Date(2014, 8 /* Sep */, 1), end: new Date(2014, 11 /* Dec */, 31)}
     )
@@ -46,7 +46,7 @@ describe('isWithinRange', function () {
   })
 
   it('accepts a string', function () {
-    var result = isWithinRange(
+    var result = isWithinInterval(
       new Date(2014, 9 /* Oct */, 31).toISOString(),
       {start: new Date(2014, 8 /* Sep */, 1).toISOString(), end: new Date(2014, 11 /* Dec */, 31).toISOString()}
     )
@@ -54,7 +54,7 @@ describe('isWithinRange', function () {
   })
 
   it('accepts a timestamp', function () {
-    var result = isWithinRange(
+    var result = isWithinInterval(
       new Date(2014, 9 /* Oct */, 31).getTime(),
       {start: new Date(2014, 8 /* Sep */, 1).getTime(), end: new Date(2014, 11 /* Dec */, 31).getTime()}
     )
@@ -62,7 +62,7 @@ describe('isWithinRange', function () {
   })
 
   it('throws an exception if the start date is after the end date', function () {
-    var block = isWithinRange.bind(
+    var block = isWithinInterval.bind(
       null,
       new Date(2014, 9 /* Oct */, 31),
       {start: new Date(2014, 11 /* Dec */, 31), end: new Date(2014, 8 /* Sep */, 1)}
