ffe0c57f342b60065481d661e81caba858374b0b
Lesha Koss <regiusprod@gmail.com>
2017-04-14 09:41:00 +0600

    Remove test-smoke test from test_travis.sh script

--- scripts/test_travis.sh
+++ scripts/test_travis.sh
@@ -5,7 +5,6 @@ then
   yarn run lint || exit 1
   yarn run flow-check || exit 1
   yarn run systemjs-check || exit 1
-  yarn run test-smoke || exit 1
   yarn run test-ci || exit 1
   yarn run test-tz || exit 1
 
