b76ef060b968aa7d8e8e1f53c9d2a35ddf7292d6
Lesha Koss <regiusprod@gmail.com>
2017-05-22 20:01:12 +0600

    Add JSDocs for locale helper functions

--- src/locale/_lib/buildParseFn/index.js
+++ src/locale/_lib/buildParseFn/index.js
@@ -1,3 +1,25 @@
+/**
+ * @name buildParseFn
+ * @category Locale Helpers
+ * @summary Build `match.weekday`, `match.month` and `match.timeOfDay` properties for the locale.
+ *
+ * @description
+ * Build `match.weekday`, `match.month` and `match.timeOfDay` properties for the locale used by `parse` function.
+ * The argument of the resulting function is the result of the corresponding match function
+ * (`match.weekdays`, `match.months` or `match.timesOfDay` respectively. See `buildMatchFn`).
+ *
+ * @param {Object} values - the object with arrays of RegExps
+ * @param {String} defaultType - the default type for the parser function
+ * @returns {Function} the resulting function
+ *
+ * @example
+ * var parseWeekdayPatterns = {
+ *   any: [/^su/i, /^m/i, /^tu/i, /^w/i, /^th/i, /^f/i, /^sa/i]
+ * }
+ * locale.match.weekday = buildParseFn(matchWeekdaysPatterns, 'long')
+ * var matchResult = locale.match.weekdays('Friday')
+ * locale.match.weekday(matchResult) //=> 5
+ */
 export default function buildParseFn (patterns, defaultType) {
   return function (matchResult, dirtyOptions) {
     var options = dirtyOptions || {}
