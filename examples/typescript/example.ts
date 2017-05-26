f84e74282cd1c0a7d32f778a70e95a97324dfc95
Lesha Koss <regiusprod@gmail.com>
2017-05-26 20:29:25 +0600

    Add locale tree-shaking smoke tests

--- examples/typescript/example.ts
+++ examples/typescript/example.ts
@@ -1,5 +1,5 @@
 import format from 'date-fns/esm/format'
-import eoLocale from 'date-fns/esm/locale/eo'
+import eo from 'date-fns/esm/locale/eo'
 
-const result = format('2017-01-25T21:28:15.000Z', 'dddd, DD MMMM HH:mm:ss', {locale: eoLocale})
+const result = format('2017-01-25T21:28:15.000Z', 'dddd, DD MMMM HH:mm:ss', {locale: eo})
 console.log(result === 'merkredo, 25 januaro 21:28:15')
--- examples/webpack-1.x/misc.js
+++ examples/webpack-1.x/misc.js
@@ -1,6 +1,6 @@
 var dateFns = require('date-fns')
 var format = dateFns.format
-var eoLocale = require('date-fns/locale/eo')
+var eo = require('date-fns/locale/eo')
 
-var result = format('2017-01-25T21:28:15.000Z', 'dddd, DD MMMM HH:mm:ss', {locale: eoLocale})
+var result = format('2017-01-25T21:28:15.000Z', 'dddd, DD MMMM HH:mm:ss', {locale: eo})
 console.log(result === 'merkredo, 25 januaro 21:28:15')
