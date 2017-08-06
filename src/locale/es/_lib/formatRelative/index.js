c08b66c0a89e4b853ad411d683caaf625ce0cd32
Gastón Haro <gaston.haro@fing.edu.uy>
2017-08-05 21:27:50 -0300

    Fixed parsing of plurals in formatRelative.

--- src/locale/es/_lib/formatRelative/index.js
+++ src/locale/es/_lib/formatRelative/index.js
@@ -1,12 +1,24 @@
 var formatRelativeLocale = {
-  lastWeek: '[el] dddd [pasado a la' + ((this.hours() !== 1) ? 's' : '') + '] LT',
-  yesterday: '[ayer a la' + ((this.hours() !== 1) ? 's' : '') + '] LT',
-  today: '[hoy a la' + ((this.hours() !== 1) ? 's' : '') + '] LT',
-  tomorrow: '[mañana a la' + ((this.hours() !== 1) ? 's' : '') + '] LT',
-  nextWeek: 'dddd [a la' + ((this.hours() !== 1) ? 's' : '') + '] LT',
+  lastWeek: '[el] dddd [pasado a la] LT',
+  yesterday: '[ayer a la] LT',
+  today: '[hoy a la] LT',
+  tomorrow: '[mañana a la] LT',
+  nextWeek: 'dddd [a la] LT',
+  other: 'L'
+}
+
+var formatRelativeLocalePlural = {
+  lastWeek: '[el] dddd [pasado a las] LT',
+  yesterday: '[ayer a las] LT',
+  today: '[hoy a las] LT',
+  tomorrow: '[mañana a las] LT',
+  nextWeek: 'dddd [a las] LT',
   other: 'L'
 }
 
 export default function formatRelative (token, date, baseDate, options) {
-  return formatRelativeLocale[token]
+  if(date.getHours() !== 1){
+    return formatRelativeLocalePlural[token];
+  }
+  return formatRelativeLocale[token];
 }
