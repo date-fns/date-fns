9d55471006e962bc44caed4d5575c4663bc00411
Sasha Koss <koss@nocorp.me>
2017-09-01 15:35:44 +0400

    Detect if the release is a prelease and publish it properly

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -13,7 +13,12 @@ env PACKAGE_OUTPUT_PATH="$PACKAGE_PATH" ./scripts/build/package.sh
 
 echo "//registry.npmjs.org/:_authToken=$NPM_KEY" > ~/.npmrc
 cd "$PACKAGE_PATH" || exit 1
-npm publish
+if [[ "$TRAVIS_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+-.+$ ]]
+then
+  npm publish --tag next
+else
+  npm publish
+fi
 cd - || exit
 
 ./scripts/release/updateFirebase.js
