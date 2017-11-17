6342bd51b308adb7078df0700d38071c3c549868
Lesha Koss <regiusprod@gmail.com>
2017-11-17 14:59:14 +0100

    Ping-pong 1

--- src/toDuration/test.js
+++ src/toDuration/test.js
@@ -8,7 +8,20 @@ describe('toDuration', function () {
   describe('string argument', function () {
     it('returns a duration object', function () {
       var result = toDuration('')
-      assert.deepEqual(result, {})
+      assert.deepEqual(result, {
+        months: 0,
+        days: 0,
+        milliseconds: 0
+      })
+    })
+
+    it('parses years', function () {
+      var result = toDuration('P5Y')
+      assert.deepEqual(result, {
+        months: 12,
+        days: 0,
+        milliseconds: 0
+      })
     })
   })
 })
