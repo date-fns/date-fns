8c7d25480f56b22465b0671bf3f1b6cada13b008
Lesha Koss <regiusprod@gmail.com>
2017-11-21 12:31:49 +0100

    Rebuild FP and typings

---
+++ src/fp/addISOWeekYearsWithOptions/index.d.ts
@@ -0,0 +1,4 @@
+// This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
+
+import {addISOWeekYearsWithOptions} from 'date-fns/fp'
+export = addISOWeekYearsWithOptions
--- src/fp/subISOYears/index.d.ts
+++
@@ -1,4 +0,0 @@
-// This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
-
-import {subISOYears} from 'date-fns/fp'
-export = subISOYears
