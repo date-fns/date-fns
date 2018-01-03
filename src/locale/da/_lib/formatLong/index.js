46cf3637d3963620c77494c4db37e922423c7074
Mikołaj Grzyb <mikolaj.grzyb@gmail.com>
2018-01-03 13:23:25 +0100

    Change danish date format 'L' to DD.MM.YYYY.

--- src/locale/da/_lib/formatLong/index.js
+++ src/locale/da/_lib/formatLong/index.js
@@ -3,7 +3,7 @@ import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 var formatLong = buildFormatLongFn({
   LT: 'HH:mm',
   LTS: 'HH:mm:ss',
-  L: 'DD/MM/YYYY',
+  L: 'DD.MM.YYYY',
   LL: 'D. MMMM YYYY',
   LLL: 'D. MMMM YYYY HH:mm',
   LLLL: 'dddd [d.] D. MMMM YYYY [kl.] HH:mm'
