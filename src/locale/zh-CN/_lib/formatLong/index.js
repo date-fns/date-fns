4616b0380bccc231ba2875b2db90c3585a917a05
weihongyu12 <weihongyu@515cn.com>
2018-03-07 12:35:08 +0800

    fix formatLong in Chinese Simplified locale

--- src/locale/zh-CN/_lib/formatLong/index.js
+++ src/locale/zh-CN/_lib/formatLong/index.js
@@ -1,12 +1,16 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 
 var formatLong = buildFormatLongFn({
-  LT: 'h:mm aa',
-  LTS: 'h:mm:ss aa',
-  L: 'MM/DD/YYYY',
-  LL: 'MMMM D YYYY',
-  LLL: 'MMMM D YYYY h:mm aa',
-  LLLL: 'dddd, MMMM D YYYY h:mm aa'
+  LT: 'HH:mm',
+  LTS: 'HH:mm:ss',
+  L: 'YYYY/MM/DD',
+  LL: 'YYYY年M月D日',
+  LLL: 'YYYY年M月D日Ah点mm分',
+  LLLL: 'YYYY年M月D日ddddAh点mm分',
+  l: 'YYYY/M/D',
+  ll: 'YYYY年M月D日',
+  lll: 'YYYY年M月D日 HH:mm',
+  llll: 'YYYY年M月D日dddd HH:mm'
 })
 
 export default formatLong
