f658f82dc0dc1e9d1a739e5bdc08451aa6d2f17c
Lesha Kostiuchenko <aleksei.kostiuchenko@ataccama.com>
2018-03-20 16:40:54 +0100

    Update en-CA and en-GB locales

--- src/esm/locale/index.js
+++ src/esm/locale/index.js
@@ -1,5 +1,7 @@
 // This file is generated automatically by `scripts/build/indices.js`. Please, don't change it.
 
+export {default as enCA} from './en-CA/index.js'
+export {default as enGB} from './en-GB/index.js'
 export {default as enUS} from './en-US/index.js'
 export {default as eo} from './eo/index.js'
 export {default as ru} from './ru/index.js'
--- src/locale/index.js
+++ src/locale/index.js
@@ -1,6 +1,8 @@
 // This file is generated automatically by `scripts/build/indices.js`. Please, don't change it.
 
 module.exports = {
+  enCA: require('./en-CA/index.js'),
+  enGB: require('./en-GB/index.js'),
   enUS: require('./en-US/index.js'),
   eo: require('./eo/index.js'),
   ru: require('./ru/index.js')
