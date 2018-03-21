4b5619551c7f5022308866df1d451527a64dac16
Martin Wind <martin.wind@im-c.at>
2018-03-21 11:29:12 +0100

    add BE locale

---
+++ src/locale/be/build_distance_in_words_locale/test.js
@@ -0,0 +1,227 @@
+// @flow
+/* eslint-env mocha */
+
+var assert = require('power-assert')
+var buildDistanceInWordsLocale = require('./')
+
+describe('be locale > buildDistanceInWordsLocale', function () {
+  it('returns an object', function () {
+    assert(typeof buildDistanceInWordsLocale() === 'object')
+  })
+
+  it('localize property is a function', function () {
+    assert(typeof buildDistanceInWordsLocale().localize === 'function')
+  })
+
+  describe('lessThanXSeconds', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXSeconds', 1) === 'менш секунды')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXSeconds', 2) === 'менш 2 секунд')
+      })
+    })
+  })
+
+  describe('xSeconds', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xSeconds', 1) === '1 секунда')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xSeconds', 2) === '2 секунд(-ы)')
+      })
+    })
+  })
+
+  describe('halfAMinute', function () {
+    it('returns a proper string', function () {
+      assert(buildDistanceInWordsLocale().localize('halfAMinute') === 'паўхвіліны')
+    })
+
+    it('ignores the second argument', function () {
+      assert(buildDistanceInWordsLocale().localize('halfAMinute', 123) === 'паўхвіліны')
+    })
+  })
+
+  describe('lessThanXMinutes', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXMinutes', 1) === 'менш хвіліны')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('lessThanXMinutes', 2) === 'менш 2 хвілін')
+      })
+    })
+  })
+
+  describe('xMinutes', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMinutes', 1) === '1 хвіліна')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMinutes', 2) === '2 хвілін(-ы)')
+      })
+    })
+  })
+
+  describe('aboutXHours', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXHours', 1) === 'каля 1 гадзіны')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXHours', 2) === 'каля 2 гадзін')
+      })
+    })
+  })
+
+  describe('xHours', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xHours', 1) === '1 гадзіна')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xHours', 2) === '2 гадзін(-ы)')
+      })
+    })
+  })
+
+  describe('xDays', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xDays', 1) === '1 дзень')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xDays', 2) === '2 дні (дзён)')
+      })
+    })
+  })
+
+  describe('aboutXMonths', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXMonths', 1) === 'каля 1 месяца')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXMonths', 2) === 'каля 2 месяцаў')
+      })
+    })
+  })
+
+  describe('xMonths', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMonths', 1) === '1 месяц')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xMonths', 2) === '2 месяцы(-аў)')
+      })
+    })
+  })
+
+  describe('aboutXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXYears', 1) === 'каля 1 года')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('aboutXYears', 2) === 'каля 2 гадоў')
+      })
+    })
+  })
+
+  describe('xYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xYears', 1) === '1 год')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('xYears', 2) === '2 гады(гадоў)')
+      })
+    })
+  })
+
+  describe('overXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('overXYears', 1) === 'больш 1 года')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('overXYears', 2) === 'больш 2 гадоў')
+      })
+    })
+  })
+
+  describe('almostXYears', function () {
+    context('when the count equals 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('almostXYears', 1) === 'амаль 1 год')
+      })
+    })
+
+    context('when the count is more than 1', function () {
+      it('returns a proper string', function () {
+        assert(buildDistanceInWordsLocale().localize('almostXYears', 2) === 'амаль 2 гады(-оў)')
+      })
+    })
+  })
+
+  context('with a past suffix', function () {
+    it('adds `ago` to a string', function () {
+      var result = buildDistanceInWordsLocale().localize('aboutXYears', 1, {
+        addSuffix: true,
+        comparison: -1
+      })
+      assert(result === 'каля 1 года таму')
+    })
+  })
+
+  context('with a future suffix', function () {
+    it('adds `in` to a string', function () {
+      var result = buildDistanceInWordsLocale().localize('halfAMinute', null, {
+        addSuffix: true,
+        comparison: 1
+      })
+      assert(result === 'у/праз паўхвіліны')
+    })
+  })
+})
---
+++ src/locale/be/build_format_locale/index.js
@@ -0,0 +1,73 @@
+var buildFormattingTokensRegExp = require('../../_lib/build_formatting_tokens_reg_exp/index.js')
+
+function buildFormatLocale () {
+  var months3char = ['студз', 'лют', 'сак', 'крас', 'май', 'чэрв', 'ліп', 'жн', 'вер', 'кастр', 'ліст', 'снеж']
+  var monthsFull = ['студзень', 'люты', 'сакавік', 'красавік', 'май', 'чэрвень', 'ліпень', 'жнівень', 'верасень', 'кастрычнік', 'лістапад', 'снежань']
+  var weekdays2char = ['нд', 'пн', 'аў', 'ср', 'чц', 'пт', 'сб']
+  var weekdays3char = ['нядз', 'пан', 'аўт', 'сер', 'чац', 'пят', 'суб']
+  var weekdaysFull = ['нядзеля', 'панядзелак', 'аўторак', 'серада', 'чацвер', 'пятніца', 'субота']
+  var meridiemUppercase = ['AM', 'PM']
+  var meridiemLowercase = ['am', 'pm']
+  var meridiemFull = ['a.m.', 'p.m.']
+
+  var formatters = {
+    // Month: Jan, Feb, ..., Dec
+    'MMM': function (date) {
+      return months3char[date.getMonth()]
+    },
+
+    // Month: January, February, ..., December
+    'MMMM': function (date) {
+      return monthsFull[date.getMonth()]
+    },
+
+    // Day of week: Su, Mo, ..., Sa
+    'dd': function (date) {
+      return weekdays2char[date.getDay()]
+    },
+
+    // Day of week: Sun, Mon, ..., Sat
+    'ddd': function (date) {
+      return weekdays3char[date.getDay()]
+    },
+
+    // Day of week: Sunday, Monday, ..., Saturday
+    'dddd': function (date) {
+      return weekdaysFull[date.getDay()]
+    },
+
+    // AM, PM
+    'A': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemUppercase[1] : meridiemUppercase[0]
+    },
+
+    // am, pm
+    'a': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemLowercase[1] : meridiemLowercase[0]
+    },
+
+    // a.m., p.m.
+    'aa': function (date) {
+      return (date.getHours() / 12) >= 1 ? meridiemFull[1] : meridiemFull[0]
+    }
+  }
+
+  // Generate ordinal version of formatters: M -> Mo, D -> Do, etc.
+  var ordinalFormatters = ['M', 'D', 'DDD', 'd', 'Q', 'W']
+  ordinalFormatters.forEach(function (formatterToken) {
+    formatters[formatterToken + 'o'] = function (date, formatters) {
+      return ordinal(formatters[formatterToken](date))
+    }
+  })
+
+  return {
+    formatters: formatters,
+    formattingTokensRegExp: buildFormattingTokensRegExp(formatters)
+  }
+}
+
+function ordinal (number) {
+  return number + '.'
+}
+
+module.exports = buildFormatLocale
