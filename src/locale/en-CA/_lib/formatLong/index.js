3077ff57727247ccc5452140cd9e5b2bbe033bbe
Lesha Koss <regiusprod@gmail.com>
2018-03-21 21:56:28 +0100

    Update en-CA and en-GB locales (#714)

--- src/locale/en-CA/_lib/formatLong/index.js
+++ src/locale/en-CA/_lib/formatLong/index.js
@@ -1,12 +1,41 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 
-var formatLong = buildFormatLongFn({
-  LT: 'h:mm A',
-  LTS: 'h:mm:ss A',
-  L: 'YYYY-MM-DD',
-  LL: 'MMMM D, YYYY',
-  LLL: 'MMMM D, YYYY h:mm A',
-  LLLL: 'dddd, MMMM D, YYYY h:mm A'
-})
+var dateFormats = {
+  full: 'EEEE, MMMM do, yyyy',
+  long: 'MMMM do, yyyy',
+  medium: 'MMM d, yyyy',
+  short: 'yyyy-MM-dd'
+}
+
+var timeFormats = {
+  full: 'h:mm:ss a zzzz',
+  long: 'h:mm:ss a z',
+  medium: 'h:mm:ss a',
+  short: 'h:mm a'
+}
+
+var dateTimeFormats = {
+  full: "{{date}} 'at' {{time}}",
+  long: "{{date}} 'at' {{time}}",
+  medium: '{{date}}, {{time}}',
+  short: '{{date}}, {{time}}'
+}
+
+var formatLong = {
+  date: buildFormatLongFn({
+    formats: dateFormats,
+    defaultWidth: 'full'
+  }),
+
+  time: buildFormatLongFn({
+    formats: timeFormats,
+    defaultWidth: 'full'
+  }),
+
+  dateTime: buildFormatLongFn({
+    formats: dateTimeFormats,
+    defaultWidth: 'full'
+  })
+}
 
 export default formatLong
--- src/locale/en-GB/index.js
+++ src/locale/en-GB/index.js
@@ -1,8 +1,8 @@
-// import formatDistance from '../en-US/_lib/formatDistance/index.js'
-// import formatLong from './_lib/formatLong/index.js'
-// import formatRelative from '../en-US/_lib/formatRelative/index.js'
-// import localize from '../en-US/_lib/localize/index.js'
-// import match from '../en-US/_lib/match/index.js'
+import formatDistance from '../en-US/_lib/formatDistance/index.js'
+import formatLong from './_lib/formatLong/index.js'
+import formatRelative from '../en-US/_lib/formatRelative/index.js'
+import localize from '../en-US/_lib/localize/index.js'
+import match from '../en-US/_lib/match/index.js'
 
 /**
  * @type {Locale}
@@ -12,18 +12,16 @@
  * @iso-639-2 eng
  * @author Alex [@glintik]{@link https://github.com/glintik}
  */
-// var locale = {
-//   formatDistance: formatDistance,
-//   formatLong: formatLong,
-//   formatRelative: formatRelative,
-//   localize: localize,
-//   match: match,
-//   options: {
-//     weekStartsOn: 1 /* Monday */,
-//     firstWeekContainsDate: 4
-//   }
-// }
+var locale = {
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 1 /* Monday */,
+    firstWeekContainsDate: 4
+  }
+}
 
-// export default locale
-
-throw new Error('en-GB locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA')
+export default locale
