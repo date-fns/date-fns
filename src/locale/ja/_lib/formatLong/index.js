6a8155517ee06ff498369daf93d014e9047fd757
Luca Ban <mesqueeb@users.noreply.github.com>
2018-04-14 20:58:16 +0900

    update

--- src/locale/ja/_lib/formatLong/index.js
+++ src/locale/ja/_lib/formatLong/index.js
@@ -3,10 +3,10 @@ import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 var formatLong = buildFormatLongFn({
   LT: 'HH:mm',
   LTS: 'HH:mm:ss',
-  L: 'YYYY年MM月DD日',
+  L: 'YYYY/MM/DD',
   LL: 'YYYY年MM月DD日',
   LLL: 'YYYY年MM月DD日 HH:mm',
-  LLLL: 'YYYY年MM月DD日 HH:mm'
+  LLLL: 'YYYY年MMMMDD日 HH時mm分'
 })
 
 export default formatLong
