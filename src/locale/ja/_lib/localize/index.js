324159b40d5f2acbe1170ee07daa0c7414542cee
Luca Ban <mesqueeb@users.noreply.github.com>
2018-04-16 13:20:52 +0900

    update "short" date values
    https://github.com/date-fns/date-fns/issues/620

--- src/locale/ja/_lib/localize/index.js
+++ src/locale/ja/_lib/localize/index.js
@@ -3,7 +3,7 @@ import buildLocalizeArrayFn from '../../../_lib/buildLocalizeArrayFn/index.js'
 
 var weekdayValues = {
   narrow: ['日', '月', '火', '水', '木', '金', '土'],
-  short: ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
+  short: ['日', '月', '火', '水', '木', '金', '土'],
   long: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日']
 }
 
