fc6085496d9e00193a9a872ab8112cc2e269cdc1
Lesha Kostiuchenko <aleksei.kostiuchenko@ataccama.com>
2018-04-17 19:02:22 +0200

    Fix lint error

--- src/locale/nl-BE/index.js
+++ src/locale/nl-BE/index.js
@@ -29,6 +29,6 @@
 //   }
 // }
 
-export default locale
+// export default locale
 
 throw new Error('nl locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA')
