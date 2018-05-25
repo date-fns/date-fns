a1ed82d95c3943fc93dc8f41175c1f85de9ac251
Lesha Kostiuchenko <aleksei.kostiuchenko@ataccama.com>
2018-05-25 08:24:48 +0200

    Rebuild locale indices

--- src/locale/index.js
+++ src/locale/index.js
@@ -5,6 +5,7 @@ module.exports = {
   enGB: require('./en-GB/index.js'),
   enUS: require('./en-US/index.js'),
   eo: require('./eo/index.js'),
+  nb: require('./nb/index.js'),
   ru: require('./ru/index.js'),
   sv: require('./sv/index.js')
 }
