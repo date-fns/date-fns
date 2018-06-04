8906776f3c35e35d21bb5e50547deaac1c74156c
Andriy Shcherbyak <andriyshcherbyak@gmail.com>
2018-06-04 22:40:19 +0300

    Update UK locale (#769)

--- src/locale/uk/_lib/formatLong/index.js
+++ src/locale/uk/_lib/formatLong/index.js
@@ -1,12 +1,41 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 
-var formatLong = buildFormatLongFn({
-  LT: 'HH:mm',
-  LTS: 'HH:mm:ss',
-  L: 'DD.MM.YYYY',
-  LL: 'D MMMM YYYY р.',
-  LLL: 'D MMMM YYYY р., HH:mm',
-  LLLL: 'dddd, D MMMM YYYY р., HH:mm'
-})
+var dateFormats = {
+  full: "EEEE, do MMMM y 'р.'",
+  long: "do MMMM y 'р.'",
+  medium: "d MMM y 'р.'",
+  short: 'dd.MM.y'
+}
+
+var timeFormats = {
+  full: 'H:mm:ss zzzz',
+  long: 'H:mm:ss z',
+  medium: 'H:mm:ss',
+  short: 'H:mm'
+}
+
+var dateTimeFormats = {
+  full: "{{date}} 'о' {{time}}",
+  long: "{{date}} 'о' {{time}}",
+  medium: '{{date}}, {{time}}',
+  short: '{{date}}, {{time}}'
+}
+
+var formatLong = {
+  date: buildFormatLongFn({
+    formats: dateFormats,
+    defaultWidth: 'full'
+  }),
+
+  time: buildFormatLongFn({
+    formats: timeFormats,
+    defaultWidth: 'full'
+  }),
+
+  dateTime: buildFormatLongFn({
+    formats: dateTimeFormats,
+    defaultWidth: 'full'
+  })
+}
 
 export default formatLong
--- src/locale/uk/index.js
+++ src/locale/uk/index.js
@@ -1,10 +1,8 @@
-// import formatDistance from './_lib/formatDistance/index.js'
-// import formatLong from './_lib/formatLong/index.js'
-// import formatRelative from './_lib/formatRelative/index.js'
-// import localize from './_lib/localize/index.js'
-// import match from './_lib/match/index.js'
-// import formatters from './_lib/formatters/index.js'
-// import buildTokensRegExp from '../_lib/buildTokensRegExp/index.js'
+import formatDistance from './_lib/formatDistance/index.js'
+import formatLong from './_lib/formatLong/index.js'
+import formatRelative from './_lib/formatRelative/index.js'
+import localize from './_lib/localize/index.js'
+import match from './_lib/match/index.js'
 
 /**
  * @type {Locale}
@@ -13,21 +11,18 @@
  * @language Ukrainian
  * @iso-639-2 ukr
  * @author Andrii Korzh [@korzhyk]{@link https://github.com/korzhyk}
+ * @author Andriy Shcherbyak [@shcherbyakdev]{@link https://github.com/shcherbyakdev}
  */
-// var locale = {
-//   formatDistance: formatDistance,
-//   formatLong: formatLong,
-//   formatRelative: formatRelative,
-//   localize: localize,
-//   match: match,
-//   formatters: formatters,
-//   formattingTokensRegExp: buildTokensRegExp(formatters),
-//   options: {
-//     weekStartsOn: 1 /* Monday */,
-//     firstWeekContainsDate: 1
-//   }
-// }
+var locale = {
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 1 /* Monday */,
+    firstWeekContainsDate: 1
+  }
+}
 
-// export default locale
-
-throw new Error('ua locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA')
+export default locale
