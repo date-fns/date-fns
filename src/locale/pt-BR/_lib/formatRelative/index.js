3a75fa409bc3472d81fa19ba5e9e47493a5a1f05
Yago <yagocarballo@gmail.com>
2018-07-11 21:02:51 +0200

    Updated pt-BR locale for 2.0

--- src/locale/pt-BR/_lib/formatRelative/index.js
+++ src/locale/pt-BR/_lib/formatRelative/index.js
@@ -2,13 +2,13 @@ var formatRelativeLocale = {
   lastWeek: function (date, baseDate, options) {
     var weekday = date.getUTCDay()
     var last = weekday === 0 || weekday === 6 ? 'último' : 'última'
-    return '[' + last + '] dddd [às] LT'
+    return "'" + last + "' eeee 'às' p"
   },
-  yesterday: '[ontem às] LT',
-  today: '[hoje às] LT',
-  tomorrow: '[amanhã às] LT',
-  nextWeek: 'dddd [às] LT',
-  other: 'L'
+  yesterday: "'ontem às' p",
+  today: "'hoje às' p",
+  tomorrow: "'amanhã às' p",
+  nextWeek: "eeee 'às' p",
+  other: 'P'
 }
 
 export default function formatRelative (token, date, baseDate, options) {
