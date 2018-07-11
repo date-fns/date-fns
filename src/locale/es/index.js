51e7f5c0a55eb518965d979eaa43c4a6006554bd
Lesha Kostiuchenko <aleksei.kostiuchenko@ataccama.com>
2018-07-11 17:17:48 +0200

    Add missing authors to locales

--- src/locale/es/index.js
+++ src/locale/es/index.js
@@ -14,6 +14,7 @@ import match from './_lib/match/index.js'
  * @author Guillermo Grau [@guigrpa]{@link https://github.com/guigrpa}
  * @author Fernando Agüero [@fjaguero]{@link https://github.com/fjaguero}
  * @author Gastón Haro [@harogaston]{@link https://github.com/harogaston}
+ * @author Yago Carballo [@YagoCarballo]{@link https://github.com/YagoCarballo}
  */
 var locale = {
   formatDistance: formatDistance,
