0b7c5b90854d32eafae52b544c8a9de479b95879
Touhid Rahman <touhidrahman1987@gmail.com>
2018-08-11 17:21:59 +0200

    [FEATURE] Supports bengali in date-fns and fixes all unit tests

---
+++ src/locale/bn/_lib/formatRelative/index.js
@@ -0,0 +1,12 @@
+var formatRelativeLocale = {
+  lastWeek: "'গত' eeee 'সময়' p",
+  yesterday: "'গতকাল' 'সময়' p",
+  today: "'আজ' 'সময়' p",
+  tomorrow: "'আগামীকাল' 'সময়' p",
+  nextWeek: "eeee 'সময়' p",
+  other: 'P'
+}
+
+export default function formatRelative (token, date, baseDate, options) {
+  return formatRelativeLocale[token]
+}
