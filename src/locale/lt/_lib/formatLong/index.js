7a3cb6c1f46a646a3573203729edf10286b0e500
eduardo <pardoe@ryanair.com>
2018-09-14 16:24:24 +0200

    update version of lt and hu localizations

--- src/locale/lt/_lib/formatLong/index.js
+++ src/locale/lt/_lib/formatLong/index.js
@@ -1,16 +1,41 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 
-var formatLong = buildFormatLongFn({
-  LT: 'HH:mm',
-  LTS: 'HH:mm:ss',
-  L: 'YYYY-MM-DD',
-  LL: 'YYYY [m.] MMMM D [d.]',
-  LLL: 'YYYY [m.] MMMM D [d.], HH:mm [val.]',
-  LLLL: 'YYYY [m.] MMMM D [d.], dddd, HH:mm [val.]',
-  l: 'YYYY-MM-DD',
-  ll: 'YYYY [m.] MMMM D [d.]',
-  lll: 'YYYY [m.] MMMM D [d.], HH:mm [val.]',
-  llll: 'YYYY [m.] MMMM D [d.], ddd, HH:mm [val.]'
-})
+var dateFormats = {
+  full: "y 'm'. MMMM d 'd'., EEEE",
+  long: "y 'm'. MMMM d 'd'.",
+  medium: 'y-MM-dd',
+  short: 'y-MM-dd'
+}
+
+var timeFormats = {
+  full: 'HH:mm:ss zzzz',
+  long: 'HH:mm:ss z',
+  medium: 'HH:mm:ss',
+  short: 'HH:mm'
+}
+
+var dateTimeFormats = {
+  full: '{{date}} {{time}}',
+  long: '{{date}} {{time}}',
+  medium: '{{date}} {{time}}',
+  short: '{{date}} {{time}}'
+}
+
+var formatLong = {
+  date: buildFormatLongFn({
+    formats: dateFormats,
+    defaultWidth: 'full'
+  }),
+
+  time: buildFormatLongFn({
+    formats: timeFormats,
+    defaultWidth: 'full'
+  }),
+
+  dateTime: buildFormatLongFn({
+    formats: dateTimeFormats,
+    defaultWidth: 'full'
+  })
+}
 
 export default formatLong
