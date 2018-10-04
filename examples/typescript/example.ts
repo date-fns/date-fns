87f4e51a740e439e9d2c40420260b7883c03a12b
Sasha Koss <koss@nocorp.me>
2018-10-04 12:50:40 +0200

    Update TypeScript example

--- examples/typescript/example.ts
+++ examples/typescript/example.ts
@@ -1,5 +1,7 @@
-import format from 'date-fns/esm/format'
-import eo from 'date-fns/esm/locale/eo'
+import { format } from 'date-fns'
+import { eo } from 'date-fns/locale'
 
-const result = format('2017-01-25T21:28:15.000Z', 'eeee, dd MMMM HH:mm:ss', {locale: eo})
+const result = format('2017-01-25T21:28:15.000Z', 'eeee, dd MMMM HH:mm:ss', {
+  locale: eo
+})
 console.log(result === 'merkredo, 25 januaro 21:28:15')
