33f579f0699fbc6628690c27e9a17cce7b4d12a2
fanixk <katsimpas.f@gmail.com>
2018-10-13 03:17:43 +0300

    Update Greek locale

--- src/locale/el/_lib/match/index.js
+++ src/locale/el/_lib/match/index.js
@@ -1,47 +1,137 @@
-import buildMatchFn from '../../../_lib/buildMatchFn/index.js'
-import buildParseFn from '../../../_lib/buildParseFn/index.js'
 import buildMatchPatternFn from '../../../_lib/buildMatchPatternFn/index.js'
-import parseDecimal from '../../../_lib/parseDecimal/index.js'
+import buildMatchFn from '../../../_lib/buildMatchFn/index.js'
 
-var matchOrdinalNumbersPattern = /^(\d+)(th|st|nd|rd)?/i
+var matchOrdinalNumberPattern = /^(\d+)(ος|η|ο)?/i
+var parseOrdinalNumberPattern = /\d+/i
 
-var matchWeekdaysPatterns = {
-  narrow: /^(su|mo|tu|we|th|fr|sa)/i,
-  short: /^(sun|mon|tue|wed|thu|fri|sat)/i,
-  long: /^(sunday|monday|tuesday|wednesday|thursday|friday|saturday)/i
+var matchEraPatterns = {
+  narrow: /^(πΧ|μΧ)/i,
+  abbreviated: /^(π\.?\s?χ\.?|π\.?\s?κ\.?\s?χ\.?|μ\.?\s?χ\.?|κ\.?\s?χ\.?)/i,
+  wide: /^(προ Χριστο(ύ|υ)|πριν απ(ό|ο) την Κοιν(ή|η) Χρονολογ(ί|ι)α|μετ(ά|α) Χριστ(ό|ο)ν|Κοιν(ή|η) Χρονολογ(ί|ι)α)/i
+}
+var parseEraPatterns = {
+  any: [/^π/i, /^(μ|κ)/i]
 }
 
-var parseWeekdayPatterns = {
-  any: [/^su/i, /^m/i, /^tu/i, /^w/i, /^th/i, /^f/i, /^sa/i]
+var matchQuarterPatterns = {
+  narrow: /^[1234]/i,
+  abbreviated: /^τ[1234]/i,
+  wide: /^[1234]ο? τρ(ί|ι)μηνο/i
+}
+var parseQuarterPatterns = {
+  any: [/1/i, /2/i, /3/i, /4/i]
 }
 
-var matchMonthsPatterns = {
-  short: /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/i,
-  long: /^(january|february|march|april|may|june|july|august|september|october|november|december)/i
+var matchMonthPatterns = {
+  narrow: /^[ιφμαμιιασονδ]/i,
+  abbreviated: /^(ιαν|φεβ|μαρ|απρ|μαι|ιουν|ιουλ|αυγ|σεπ|οκτ|νοε|δεκ)/i,
+  wide: /^(ιανου(ά|α)ριος|φεβρου(ά|α)ριος|μ(ά|α)ρτιος|απρ(ί|ί)λιος|μ(ά|α)ιος|ιο(ύ|υ)νιος|ιο(ύ|υ)λιος|α(ύ|υ)γουστος|σεπτ(έ|ε)μβριος|οκτ(ώ|ω)βριος|νο(έ|ε)μβριος|δεκ(έ|ε)μβριος)/i
 }
-
 var parseMonthPatterns = {
-  any: [/^ja/i, /^f/i, /^mar/i, /^ap/i, /^may/i, /^jun/i, /^jul/i, /^au/i, /^s/i, /^o/i, /^n/i, /^d/i]
+  narrow: [
+    /^ι/i,
+    /^φ/i,
+    /^μ/i,
+    /^α/i,
+    /^μ/i,
+    /^ι/i,
+    /^ι/i,
+    /^α/i,
+    /^σ/i,
+    /^ο/i,
+    /^ν/i,
+    /^δ/i
+  ],
+  any: [
+    /^ια/i,
+    /^φ/i,
+    /^μαρ/i,
+    /^απ/i,
+    /^μαι/i,
+    /^ιουν/i,
+    /^ιουλ/i,
+    /^αυ/i,
+    /^σ/i,
+    /^ο/i,
+    /^ν/i,
+    /^δ/i
+  ]
 }
 
-var matchTimesOfDayPatterns = {
-  short: /^(am|pm)/i,
-  long: /^([ap]\.?\s?m\.?)/i
+var matchDayPatterns = {
+  narrow: /^[κδτπ]/i,
+  short: /^(κυ|δε|τρ|τε|πε|πα|σα)/i,
+  abbreviated: /^(κυρ|δευ|τρι|τετ|πεμ|παρ|σαβ)/i,
+  wide: /^(κυριακ(ή|η)|δευτ(έ|ε)ρα|τρ(ί|ι)τη|τετ(ά|α)ρτη|π(έ|ε)μπτη|παρασκευ(ή|η)|σ(ά|α)ββατο)/i
+}
+var parseDayPatterns = {
+  narrow: [/^κ/i, /^δ/i, /^τ/i, /^τ/i, /^π/i, /^π/i, /^σ/i],
+  any: [/^κ/i, /^δ/i, /^τρ/i, /^τε/i, /^πε/i, /^πα/i, /^σ/i]
 }
 
-var parseTimeOfDayPatterns = {
-  any: [/^a/i, /^p/i]
+var matchDayPeriodPatterns = {
+  narrow: /^(πμ|μμ|μεσ(ά|α)νυχτα|μεσημ(έ|ε)ρι|(τη|το) (πρω(ί|ι)|απ(ό|ο)γευμα|βρ(ά|α)δυ|ν(ύ|υ)χτα))/i,
+  any: /^([πμ]\.?\s?μ\.?|μεσ(ά|α)νυχτα|μεσημ(έ|ε)ρι|(τη|το) (πρω(ί|ι)|απ(ό|ο)γευμα|βρ(ά|α)δυ|ν(ύ|υ)χτα))/i
+}
+var parseDayPeriodPatterns = {
+  any: {
+    am: /^πμ/i,
+    pm: /^μμ/i,
+    midnight: /^μεσάν/i,
+    noon: /^μεσημ(έ|ε)/i,
+    morning: /πρω(ί|ι)/i,
+    afternoon: /απ(ό|ο)γευμα/i,
+    evening: /βρ(ά|α)δυ/i,
+    night: /ν(ύ|υ)χτα/i
+  }
 }
 
 var match = {
-  ordinalNumbers: buildMatchPatternFn(matchOrdinalNumbersPattern),
-  ordinalNumber: parseDecimal,
-  weekdays: buildMatchFn(matchWeekdaysPatterns, 'long'),
-  weekday: buildParseFn(parseWeekdayPatterns, 'any'),
-  months: buildMatchFn(matchMonthsPatterns, 'long'),
-  month: buildParseFn(parseMonthPatterns, 'any'),
-  timesOfDay: buildMatchFn(matchTimesOfDayPatterns, 'long'),
-  timeOfDay: buildParseFn(parseTimeOfDayPatterns, 'any')
+  ordinalNumber: buildMatchPatternFn({
+    matchPattern: matchOrdinalNumberPattern,
+    parsePattern: parseOrdinalNumberPattern,
+    valueCallback: function(value) {
+      return parseInt(value, 10)
+    }
+  }),
+
+  era: buildMatchFn({
+    matchPatterns: matchEraPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseEraPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  quarter: buildMatchFn({
+    matchPatterns: matchQuarterPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseQuarterPatterns,
+    defaultParseWidth: 'any',
+    valueCallback: function(index) {
+      return index + 1
+    }
+  }),
+
+  month: buildMatchFn({
+    matchPatterns: matchMonthPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseMonthPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  day: buildMatchFn({
+    matchPatterns: matchDayPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseDayPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  dayPeriod: buildMatchFn({
+    matchPatterns: matchDayPeriodPatterns,
+    defaultMatchWidth: 'any',
+    parsePatterns: parseDayPeriodPatterns,
+    defaultParseWidth: 'any'
+  })
 }
 
 export default match
--- src/locale/el/test.js
+++ src/locale/el/test.js
@@ -1,9 +1,501 @@
 // @flow
 /* eslint-env mocha */
 
-// import assert from 'power-assert'
-// import locale from '.'
+import assert from 'power-assert'
+import locale from '.'
 
-describe.skip('el locale', function () {
+import format from '../../format'
+import formatDistance from '../../formatDistance'
+import formatDistanceStrict from '../../formatDistanceStrict'
+import formatRelative from '../../formatRelative'
+import parse from '../../parse'
 
+describe('el locale', function() {
+  context('with `format`', function() {
+    var date = new Date(1986, 3 /* Apr */, 5, 10, 32, 0, 900)
+
+    it('era', function() {
+      var result = format(date, 'G, GGGG, GGGGG', { locale: locale })
+      assert(result === 'μ.Χ., μετά Χριστόν, μΧ')
+    })
+
+    describe('year', function() {
+      it('ordinal regular year', function() {
+        var result = format(date, "yo 'χρόνος'", { locale: locale })
+        assert(result === '1986ος χρόνος')
+      })
+
+      it('ordinal local week-numbering year', function() {
+        var result = format(date, "Yo 'χρόνος'", {
+          locale: locale
+        })
+        assert(result === '1986ος χρόνος')
+      })
+    })
+
+    describe('quarter', function() {
+      it('formatting quarter', function() {
+        var result = format(date, "Qo 'τρίμηνο', QQQ, QQQQ, QQQQQ", {
+          locale: locale
+        })
+        assert(result === '2ο τρίμηνο, Τ2, 2ο τρίμηνο, 2')
+      })
+
+      it('stand-alone quarter', function() {
+        var result = format(date, "qo 'τρίμηνο', qqq, qqqq, qqqqq", {
+          locale: locale
+        })
+        assert(result === '2ο τρίμηνο, Τ2, 2ο τρίμηνο, 2')
+      })
+    })
+
+    describe('month', function() {
+      it('formatting month', function() {
+        var result = format(date, "do 'του' MMMM", { locale: locale })
+        assert(result === '5η του Απριλίου')
+      })
+
+      it('stand-alone month', function() {
+        var result = format(date, "Lo 'μήνας', LLL, LLLL, LLLLL", {
+          locale: locale
+        })
+        assert(result === '4ος μήνας, Απρ, Απρίλιος, Α')
+      })
+    })
+
+    describe('week', function() {
+      it('ordinal local week of year', function() {
+        var date = new Date(1986, 3 /* Apr */, 6)
+        var result = format(date, "wo 'εβδομάδα'", { locale: locale })
+        assert(result === '14η εβδομάδα')
+      })
+
+      it('ordinal ISO week of year', function() {
+        var date = new Date(1986, 3 /* Apr */, 6)
+        var result = format(date, "Io 'ISO εβδομάδα'", { locale: locale })
+        assert(result === '14η ISO εβδομάδα')
+      })
+    })
+
+    describe('day', function() {
+      it('ordinal date', function() {
+        var result = format(date, "'σήμερα είναι η' do", { locale: locale })
+        assert(result === 'σήμερα είναι η 5η')
+      })
+
+      it('ordinal day of year', function() {
+        var result = format(date, "Do 'ημέρα του χρόνου'", { locale: locale })
+        assert(result === '95η ημέρα του χρόνου')
+      })
+    })
+
+    describe('week day', function() {
+      it('day of week', function() {
+        var result = format(date, 'E, EEEE, EEEEE, EEEEEE', { locale: locale })
+        assert(result === 'Σάβ, Σάββατο, Σ, Σά')
+      })
+
+      it('ordinal day of week', function() {
+        var result = format(date, "eo 'ημέρα της εβδομάδας'", {
+          locale: locale
+        })
+        assert(result === '6η ημέρα της εβδομάδας')
+      })
+    })
+
+    describe('day period and hour', function() {
+      it('ordinal hour', function() {
+        var result = format(date, "ho 'ώρα'", { locale: locale })
+        assert(result === '10η ώρα')
+      })
+
+      it('AM, PM', function() {
+        var result = format(date, 'h a, h aaaa, haaaaa', { locale: locale })
+        assert(result === '10 π.μ., 10 π.μ., 10πμ')
+      })
+
+      it('AM, PM, noon, midnight', function() {
+        var result = format(
+          new Date(1986, 3 /* Apr */, 6, 0),
+          'b, bbbb, bbbbb',
+          { locale: locale }
+        )
+        assert(result === 'μεσάνυχτα, μεσάνυχτα, μεσάνυχτα')
+      })
+
+      it('flexible day periods', function() {
+        it('works as expected', function() {
+          var result = format(date, 'h B', { locale: locale })
+          assert(result === '10 το πρωί')
+        })
+      })
+    })
+
+    it('ordinal minute', function() {
+      var result = format(date, "mo 'λεπτό'", { locale: locale })
+      assert(result === '32ο λεπτό')
+    })
+
+    it('ordinal second', function() {
+      var result = format(date, "so 'δευτερόλεπτο'", { locale: locale })
+      assert(result === '0ο δευτερόλεπτο')
+    })
+
+    describe('long format', function() {
+      it('short date', function() {
+        var result = format(date, 'P', { locale: locale })
+        assert(result === '5/4/86')
+      })
+
+      it('medium date', function() {
+        var result = format(date, 'PP', { locale: locale })
+        assert(result === '5 Απρ 1986')
+      })
+
+      it('long date', function() {
+        var result = format(date, 'PPP', { locale: locale })
+        assert(result === '5 Απριλίου 1986')
+      })
+
+      it('full date', function() {
+        var result = format(date, 'PPPP', { locale: locale })
+        assert(result === 'Σάββατο, 5 Απριλίου 1986')
+      })
+
+      it('short time', function() {
+        var result = format(date, 'p', { locale: locale })
+        assert(result === '10:32 π.μ.')
+      })
+
+      it('medium time', function() {
+        var result = format(date, 'pp', { locale: locale })
+        assert(result === '10:32:00 π.μ.')
+      })
+
+      it('short date + time', function() {
+        var result = format(date, 'Pp', { locale: locale })
+        assert(result === '5/4/86, 10:32 π.μ.')
+      })
+
+      it('medium date + time', function() {
+        var result = format(date, 'PPpp', { locale: locale })
+        assert(result === '5 Απρ 1986, 10:32:00 π.μ.')
+      })
+
+      it('long date + time', function() {
+        var result = format(date, 'PPPp', { locale: locale })
+        assert(result === '5 Απριλίου 1986 - 10:32 π.μ.')
+      })
+
+      it('full date + time', function() {
+        var result = format(date, 'PPPPp', { locale: locale })
+        assert(result === 'Σάββατο, 5 Απριλίου 1986 - 10:32 π.μ.')
+      })
+    })
+  })
+
+  context('with `formatDistance`', function() {
+    it('works as expected', function() {
+      var result = formatDistance(
+        new Date(1986, 3, 4, 10, 32, 25),
+        new Date(1986, 3, 4, 10, 32, 0),
+        { locale: locale, includeSeconds: true }
+      )
+      assert(result === 'μισό λεπτό')
+    })
+
+    context('when `addSuffix` option is true', function() {
+      it('adds a future suffix', function() {
+        var result = formatDistance(
+          new Date(1986, 3, 4, 10, 32, 7),
+          new Date(1986, 3, 4, 10, 32, 0),
+          { locale: locale, includeSeconds: true, addSuffix: true }
+        )
+        assert(result === 'σε λιγότερο από 10 δευτερόλεπτα')
+      })
+
+      it('adds a past suffix', function() {
+        var result = formatDistance(
+          new Date(1986, 3, 4, 10, 32, 0),
+          new Date(1986, 3, 4, 11, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === 'περίπου 1 ώρα πριν')
+      })
+    })
+  })
+
+  context('with `formatDistanceStrict`', function() {
+    it('works as expected', function() {
+      var result = formatDistanceStrict(
+        new Date(1986, 3, 4, 10, 32, 0),
+        new Date(1986, 3, 4, 12, 32, 0),
+        { locale: locale, unit: 'minute' }
+      )
+      assert(result === '120 λεπτά')
+    })
+
+    describe('when `addSuffix` option is true', function() {
+      it('adds a future suffix', function() {
+        var result = formatDistanceStrict(
+          new Date(1986, 3, 4, 10, 32, 25),
+          new Date(1986, 3, 4, 10, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === 'σε 25 δευτερόλεπτα')
+      })
+
+      it('adds a past suffix', function() {
+        var result = formatDistanceStrict(
+          new Date(1986, 3, 4, 10, 32, 0),
+          new Date(1986, 3, 4, 11, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === '1 ώρα πριν')
+      })
+    })
+  })
+
+  context('with `formatRelative`', function() {
+    var baseDate = new Date(1986, 3 /* Apr */, 4, 10, 32, 0, 900)
+
+    it('last week', function() {
+      var result = formatRelative(new Date(1986, 3 /* Apr */, 1), baseDate, {
+        locale: locale
+      })
+      assert(result === 'την προηγούμενη Τρίτη στις 12:00 π.μ.')
+    })
+
+    it('yesterday', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 3, 22, 22),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'χθες στις 10:22 μ.μ.')
+    })
+
+    it('today', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 4, 16, 50),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'σήμερα στις 4:50 μ.μ.')
+    })
+
+    it('tomorrow', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 5, 7, 30),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'αύριο στις 7:30 π.μ.')
+    })
+
+    it('next week', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 6, 12, 0),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'Κυριακή στις 12:00 μ.μ.')
+    })
+
+    it('after the next week', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 11, 16, 50),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === '11/4/86')
+    })
+  })
+
+  context('with `parse`', function() {
+    var baseDate = new Date(1986, 3 /* Apr */, 4, 10, 32, 0, 900)
+
+    describe('era', function() {
+      it('abbreviated', function() {
+        var result = parse('10000 π.Χ.', 'yyyyy G', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(-9999, 0 /* Jan */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('2018 μετα Χριστον', 'yyyy GGGG', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(2018, 0 /* Jan */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('44 πΧ', 'y GGGGG', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(-43, 0 /* Jan */, 1))
+      })
+    })
+
+    it('ordinal year', function() {
+      var result = parse('2017', 'yo', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(2017, 0 /* Jan */, 1))
+    })
+
+    describe('quarter', function() {
+      it('ordinal', function() {
+        var result = parse('1ο', 'Qo', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+
+      it('abbreviated', function() {
+        var result = parse('Τ3', 'QQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 6 /* Jul */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('4ο τριμηνο', 'QQQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 9 /* Oct */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('1', 'QQQQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+    })
+
+    describe('month', function() {
+      it('ordinal', function() {
+        var result = parse('6ος', 'Mo', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 5 /* Jun */, 1))
+      })
+
+      it('abbreviated', function() {
+        var result = parse('Νοε', 'MMM', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 10 /* Nov */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('Φεβρουάριος', 'MMMM', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(1986, 1 /* Feb */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('Ι', 'MMMMM', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+    })
+
+    it('ordinal week of year', function() {
+      var result = parse('49η', 'wo', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 10 /* Nov */, 31))
+    })
+
+    it('ordinal day of month', function() {
+      var result = parse('28η', 'do', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 3 /* Apr */, 28))
+    })
+
+    it('ordinal day of year', function() {
+      var result = parse('200η', 'Do', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 6 /* Jul */, 19))
+    })
+
+    describe('day of week', function() {
+      it('abbreviated', function() {
+        var result = parse('Δευ', 'E', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 2 /* Mar */, 31))
+      })
+
+      it('wide', function() {
+        var result = parse('Τρίτη', 'EEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('Δ', 'EEEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 2 /* Apr */, 31))
+      })
+
+      it('short', function() {
+        var result = parse('Πε', 'EEEEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 3))
+      })
+    })
+
+    it('ordinal local day of week', function() {
+      var result = parse(
+        '2η ημέρα της εβδομάδας',
+        "eo 'ημέρα της εβδομάδας'",
+        baseDate,
+        { locale: locale }
+      )
+      assert.deepEqual(result, new Date(1986, 3 /* Mar */, 1))
+    })
+
+    describe('AM, PM', function() {
+      it('abbreviated', function() {
+        var result = parse('5 πμ', 'h a', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 5))
+      })
+
+      it('wide', function() {
+        var result = parse('5 μμ.', 'h aaaa', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 17))
+      })
+
+      it('narrow', function() {
+        var result = parse('11 πμ', 'h aaaaa', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 11))
+      })
+    })
+
+    describe('AM, PM, noon, midnight', function() {
+      it('abbreviated', function() {
+        var result = parse('μεσημερι', 'b', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 12))
+      })
+
+      it('wide', function() {
+        var result = parse('μεσάνυχτα', 'bbbb', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 0))
+      })
+
+      it('narrow', function() {
+        var result = parse('μεσάνυχτα', 'bbbbb', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 0))
+      })
+    })
+
+    describe('flexible day period', function() {
+      it('abbreviated', function() {
+        var result = parse('2 τη νύχτα', 'h B', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 2))
+      })
+
+      it('wide', function() {
+        var result = parse('4 το απόγευμα', 'h BBBB', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 16))
+      })
+
+      it('narrow', function() {
+        var result = parse('5 το βράδυ', 'h BBBBB', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 17))
+      })
+    })
+
+    it('ordinal time', function() {
+      var dateString = '1η ώρα, 2ο λεπτό, 3ο δευτερόλεπτο'
+      var formatString = "ho 'ώρα', mo 'λεπτό', so 'δευτερόλεπτο'"
+      var result = parse(dateString, formatString, baseDate, {
+        locale: locale
+      })
+      assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 1, 2, 3))
+    })
+  })
 })
