1893e0a0232649c85ddd826b2ea12d9be8007ed2
Dhaifallah Alwadani <dalwadani@gmail.com>
2018-10-20 00:57:50 +0300

    add formatRelative with tests

--- src/locale/ar-SA/_lib/formatRelative/index.js
+++ src/locale/ar-SA/_lib/formatRelative/index.js
@@ -1,12 +1,12 @@
 var formatRelativeLocale = {
   lastWeek: "'last' eeee 'at' p",
-  yesterday: "'yesterday at' p",
-  today: "'today at' p",
-  tomorrow: "'tomorrow at' p",
+  yesterday: "'أمس عند' p",
+  today: "'اليوم عند' p",
+  tomorrow: "'غداً عند' p",
   nextWeek: "eeee 'at' p",
   other: 'P'
 }
 
-export default function formatRelative (token, date, baseDate, options) {
+export default function formatRelative(token, date, baseDate, options) {
   return formatRelativeLocale[token]
 }
