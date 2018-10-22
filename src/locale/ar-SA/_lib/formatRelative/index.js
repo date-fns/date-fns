2a89f7f5d00acbf14a25e3bbfcdf32bc9bafb5f5
Dhaifallah Alwadani <dalwadani@gmail.com>
2018-10-22 15:34:28 +0300

    Add Arabic (Saudi Arabia) locale (#943)

---
+++ src/locale/ar-SA/_lib/formatRelative/index.js
@@ -0,0 +1,12 @@
+var formatRelativeLocale = {
+  lastWeek: "'أخر' eeee 'عند' p",
+  yesterday: "'أمس عند' p",
+  today: "'اليوم عند' p",
+  tomorrow: "'غداً عند' p",
+  nextWeek: "eeee 'عند' p",
+  other: 'P'
+}
+
+export default function formatRelative(token, date, baseDate, options) {
+  return formatRelativeLocale[token]
+}
---
+++ src/locale/ar-SA/_lib/localize/index.js
@@ -0,0 +1,143 @@
+import buildLocalizeFn from '../../../_lib/buildLocalizeFn/index.js'
+
+var eraValues = {
+  narrow: ['ق', 'ب'],
+  abbreviated: ['ق.م.', 'ب.م.'],
+  wide: ['قبل الميلاد', 'بعد الميلاد']
+}
+
+var quarterValues = {
+  narrow: ['1', '2', '3', '4'],
+  abbreviated: ['ر1', 'ر2', 'ر3', 'ر4'],
+  wide: ['الربع الأول', 'الربع الثاني', 'الربع الثالث', 'الربع الرابع']
+}
+
+// prettier-ignore
+var monthValues = {
+  narrow: ['ي', 'ف', 'م', 'أ', 'م', 'ي', 'ي', 'أ', 'س', 'أ', 'ن', 'د'],
+  abbreviated:  ['ينا', 'فبر', 'مارس', 'أبريل', 'مايو', 'يونـ', 'يولـ', 'أغسـ', 'سبتـ', 'أكتـ', 'نوفـ', 'ديسـ'],
+  wide:  ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
+}
+
+// prettier-ignore
+var dayValues = {
+  narrow: ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
+  short: ['أحد', 'اثنين', 'ثلاثاء', 'أربعاء', 'خميس', 'جمعة', 'سبت'],
+  abbreviated:  ['أحد', 'اثنـ', 'ثلا', 'أربـ', 'خميـ', 'جمعة', 'سبت'],
+  wide: ['الأحد', 'الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت']
+}
+
+var dayPeriodValues = {
+  narrow: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'ن',
+    noon: 'ظ',
+    morning: 'صباحاً',
+    afternoon: 'بعد الظهر',
+    evening: 'مساءاً',
+    night: 'ليلاً'
+  },
+  abbreviated: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'نصف الليل',
+    noon: 'ظهر',
+    morning: 'صباحاً',
+    afternoon: 'بعد الظهر',
+    evening: 'مساءاً',
+    night: 'ليلاً'
+  },
+  wide: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'نصف الليل',
+    noon: 'ظهر',
+    morning: 'صباحاً',
+    afternoon: 'بعد الظهر',
+    evening: 'مساءاً',
+    night: 'ليلاً'
+  }
+}
+var formattingDayPeriodValues = {
+  narrow: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'ن',
+    noon: 'ظ',
+    morning: 'في الصباح',
+    afternoon: 'بعد الظـهر',
+    evening: 'في المساء',
+    night: 'في الليل'
+  },
+  abbreviated: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'نصف الليل',
+    noon: 'ظهر',
+    morning: 'في الصباح',
+    evening: 'في المساء',
+    night: 'في الليل'
+  },
+  wide: {
+    am: 'ص',
+    pm: 'م',
+    midnight: 'نصف الليل',
+    noon: 'ظهر',
+    afternoon: 'بعد الظـهر',
+    evening: 'في المساء',
+    night: 'في الليل'
+  }
+}
+
+function ordinalNumber(dirtyNumber, dirtyOptions) {
+  var number = Number(dirtyNumber)
+
+  // If ordinal numbers depend on context, for example,
+  // if they are different for different grammatical genders,
+  // use `options.unit`:
+  //
+  //   var options = dirtyOptions || {}
+  //   var unit = String(options.unit)
+  //
+  // where `unit` can be 'year', 'quarter', 'month', 'week', 'date', 'dayOfYear',
+  // 'day', 'hour', 'minute', 'second'
+
+  return String(dirtyNumber)
+}
+
+var localize = {
+  ordinalNumber: ordinalNumber,
+
+  era: buildLocalizeFn({
+    values: eraValues,
+    defaultWidth: 'wide'
+  }),
+
+  quarter: buildLocalizeFn({
+    values: quarterValues,
+    defaultWidth: 'wide',
+    argumentCallback: function(quarter) {
+      return Number(quarter) - 1
+    }
+  }),
+
+  month: buildLocalizeFn({
+    values: monthValues,
+    defaultWidth: 'wide'
+  }),
+
+  day: buildLocalizeFn({
+    values: dayValues,
+    defaultWidth: 'wide'
+  }),
+
+  dayPeriod: buildLocalizeFn({
+    values: dayPeriodValues,
+    defaultWidth: 'wide',
+    formattingValues: formattingDayPeriodValues,
+    defaulFormattingWidth: 'wide'
+  })
+}
+
+export default localize
