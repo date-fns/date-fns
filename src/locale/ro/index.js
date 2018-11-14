3a01e665c2c378c286211a80182f77e5f71afef0
Sasha Koss <kossnocorp@gmail.com>
2018-11-14 13:24:19 +0200

    Update src/locale/ro/index.js
    Co-Authored-By: aocneanu <aocneanu@gmail.com>

--- src/locale/ro/index.js
+++ src/locale/ro/index.js
@@ -12,7 +12,7 @@ import match from './_lib/match/index.js'
  * @iso-639-2 ron
  * @author Sergiu Munteanu [@jsergiu]{@link https://github.com/jsergiu}
  * @author Adrian Ocneanu [@aocneanu]{@link https://github.com/aocneanu}
- * @author Mihai Ocneanu [@aocneanu]{@link https://github.com/gandesc}
+ * @author Mihai Ocneanu [@gandesc]{@link https://github.com/gandesc}
  */
 var locale = {
   formatDistance: formatDistance,
