58357280d441a1c90736b8b31443469810498280
Josh Soref <jsoref@users.noreply.github.com>
2018-11-25 21:03:37 -0500

    spelling: minutes

--- src/subMinutes/index.js
+++ src/subMinutes/index.js
@@ -18,7 +18,7 @@ import addMinutes from '../addMinutes/index.js'
  * @param {Number} amount - the amount of minutes to be subtracted
  * @param {Options} [options] - the object with options. See [Options]{@link https://date-fns.org/docs/Options}
  * @param {0|1|2} [options.additionalDigits=2] - passed to `toDate`. See [toDate]{@link https://date-fns.org/docs/toDate}
- * @returns {Date} the new date with the mintues subtracted
+ * @returns {Date} the new date with the minutes subtracted
  * @throws {TypeError} 2 arguments required
  * @throws {RangeError} `options.additionalDigits` must be 0, 1 or 2
  *
