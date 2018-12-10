5d52fc77ebe674873ef7689d448a7af91f278123
Zoltán Szepesi <2dcube01@gmail.com>
2018-12-10 08:39:48 +0100

    Fix HU locale grammar (#1002) (closes #1001)

--- src/locale/hu/_lib/formatDistance/index.js
+++ src/locale/hu/_lib/formatDistance/index.js
@@ -1,134 +1,73 @@
-var formatDistanceLocale = {
-  lessThanXSeconds: {
-    one: translate,
-    other: translate
-  },
-
-  xSeconds: {
-    one: translate,
-    other: translate
-  },
-
-  halfAMinute: 'fél perce',
-
-  lessThanXMinutes: {
-    one: translate,
-    other: translate
-  },
-
-  xMinutes: {
-    one: translate,
-    other: translate
-  },
-
-  aboutXHours: {
-    one: translate,
-    other: translate
-  },
-
-  xHours: {
-    one: translate,
-    other: translate
-  },
-
-  xDays: {
-    one: translate,
-    other: translate
-  },
-
-  aboutXMonths: {
-    one: translate,
-    other: translate
-  },
-
-  xMonths: {
-    one: translate,
-    other: translate
-  },
-
-  aboutXYears: {
-    one: translate,
-    other: translate
-  },
-
-  xYears: {
-    one: translate,
-    other: translate
-  },
-
-  overXYears: {
-    one: translate,
-    other: translate
-  },
-
-  almostXYears: {
-    one: translate,
-    other: translate
-  }
-}
-
 var translations = {
-  'about': 'körülbelül',
-  'over': 'több mint',
-  'almost': 'majdnem',
-  'lessthan': 'kevesebb, mint'
+  about: 'körülbelül',
+  over: 'több mint',
+  almost: 'majdnem',
+  lessthan: 'kevesebb mint'
 }
 
-function translate (number, addSuffix, key, isFuture) {
+function translate(number, addSuffix, key, comparison) {
   var num = number
   switch (key) {
-    case 'xseconds_one':
-      return (isFuture || !addSuffix) ? 'néhány másodperc' : 'néhány másodperce'
-    case 'xseconds_other':
-      return num + ((isFuture || !addSuffix) ? ' másodperc' : ' másodperce')
-    case 'xminutes_one':
-      return 'egy' + (isFuture || !addSuffix ? ' perc' : ' perce')
-    case 'xminutes_other':
-      return num + ((isFuture || !addSuffix ? ' perc' : ' perce'))
-    case 'xhours_one':
-      return 'egy' + (isFuture || !addSuffix ? ' óra' : ' órája')
-    case 'xhours_other':
-      return num + ((isFuture || !addSuffix ? ' óra' : ' órája'))
-    case 'xdays_one':
-      return 'egy' + (isFuture || !addSuffix ? ' nap' : ' napja')
-    case 'xdays_other':
-      return num + ((isFuture || !addSuffix ? ' nap' : ' napja'))
-    case 'xmonths_one':
-      return 'egy' + (isFuture || !addSuffix ? ' hónap' : ' hónapja')
-    case 'xmonths_other':
-      return num + ((isFuture || !addSuffix ? ' hónap' : ' hónapja'))
-    case 'xyears_one':
-      return 'egy' + (isFuture || !addSuffix ? ' év' : ' éve')
-    case 'xyears_other':
-      return num + ((isFuture || !addSuffix ? ' év' : ' éve'))
+    case 'xseconds':
+      if (comparison === -1 && addSuffix) return num + ' másodperccel ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' másodperce'
+      if (comparison === 1) return num + ' másodperc múlva'
+      return num + ' másodperc'
+
+    case 'halfaminute':
+      if (comparison === -1 && addSuffix) return 'fél perccel ezelőtt'
+      if (comparison === -1 && !addSuffix) return 'fél perce'
+      if (comparison === 1) return 'fél perc múlva'
+      return 'fél perc'
+
+    case 'xminutes':
+      if (comparison === -1 && addSuffix) return num + ' perccel ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' perce'
+      if (comparison === 1) return num + ' perc múlva'
+      return num + ' perc'
+
+    case 'xhours':
+      if (comparison === -1 && addSuffix) return num + ' órával ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' órája'
+      if (comparison === 1) return num + ' óra múlva'
+      return num + ' óra'
+
+    case 'xdays':
+      if (comparison === -1 && addSuffix) return num + ' nappal ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' napja'
+      if (comparison === 1) return num + ' nap múlva'
+      return num + ' nap'
+
+    case 'xmonths':
+      if (comparison === -1 && addSuffix) return num + ' hónappal ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' hónapja'
+      if (comparison === 1) return num + ' hónap múlva'
+      return num + ' hónap'
+
+    case 'xyears':
+      if (comparison === -1 && addSuffix) return num + ' évvel ezelőtt'
+      if (comparison === -1 && !addSuffix) return num + ' éve'
+      if (comparison === 1) return num + ' év múlva'
+      return num + ' év'
   }
   return ''
 }
 
-export default function formatDistance (token, count, options) {
+export default function formatDistance(token, count, options) {
   options = options || {}
   var adverb = token.match(/about|over|almost|lessthan/i)
   var unit = token.replace(adverb, '')
 
   var result
-  if (typeof formatDistanceLocale[token] === 'string') {
-    result = formatDistanceLocale[token]
-  } else if (count === 1) {
-    result = formatDistanceLocale[token].one(count, options.addSuffix, unit.toLowerCase() + '_one', options.comparison > 0)
-  } else {
-    result = formatDistanceLocale[token].other(count, options.addSuffix, unit.toLowerCase() + '_other', options.comparison > 0)
-  }
+  result = translate(
+    count,
+    options.addSuffix,
+    unit.toLowerCase(),
+    options.comparison
+  )
 
   if (adverb) {
     result = translations[adverb[0].toLowerCase()] + ' ' + result
-  }
-
-  if (options.addSuffix) {
-    if (options.comparison > 0) {
-      return result + ' múlva'
-    } else {
-      return result + ' ezelőtt'
-    }
   }
 
   return result
--- src/locale/hu/_lib/formatDistance/test.js
+++ src/locale/hu/_lib/formatDistance/test.js
@@ -4,216 +4,220 @@
 import assert from 'power-assert'
 import formatDistance from '.'
 
-describe('hu locale > formatDistance', function () {
-  describe('lessThanXSeconds', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('lessThanXSeconds', 1) === 'kevesebb, mint néhány másodperc')
+describe('hu locale > formatDistance', function() {
+  describe('lessThanXSeconds', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(
+          formatDistance('lessThanXSeconds', 1) === 'kevesebb mint 1 másodperc'
+        )
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('lessThanXSeconds', 2) === 'kevesebb, mint 2 másodperc')
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
+        assert(
+          formatDistance('lessThanXSeconds', 2) === 'kevesebb mint 2 másodperc'
+        )
       })
     })
   })
 
-  describe('xSeconds', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xSeconds', 1) === 'néhány másodperc')
+  describe('xSeconds', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xSeconds', 1) === '1 másodperc')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xSeconds', 2) === '2 másodperc')
       })
     })
   })
 
-  describe('halfAMinute', function () {
-    it('returns a proper string', function () {
-      assert(formatDistance('halfAMinute') === 'fél perce')
+  describe('halfAMinute', function() {
+    it('returns a proper string', function() {
+      assert(formatDistance('halfAMinute') === 'fél perc')
     })
 
-    it('ignores the second argument', function () {
-      assert(formatDistance('halfAMinute', 123) === 'fél perce')
-    })
-  })
-
-  describe('lessThanXMinutes', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('lessThanXMinutes', 1) === 'kevesebb, mint egy perc')
-      })
-    })
-
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('lessThanXMinutes', 2) === 'kevesebb, mint 2 perc')
-      })
+    it('ignores the second argument', function() {
+      assert(formatDistance('halfAMinute', 123) === 'fél perc')
     })
   })
 
-  describe('xMinutes', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xMinutes', 1) === 'egy perc')
+  describe('lessThanXMinutes', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('lessThanXMinutes', 1) === 'kevesebb mint 1 perc')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('lessThanXMinutes', 2) === 'kevesebb mint 2 perc')
+      })
+    })
+  })
+
+  describe('xMinutes', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xMinutes', 1) === '1 perc')
+      })
+    })
+
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMinutes', 2) === '2 perc')
       })
     })
   })
 
-  describe('aboutXHours', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('aboutXHours', 1) === 'körülbelül egy óra')
+  describe('aboutXHours', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('aboutXHours', 1) === 'körülbelül 1 óra')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXHours', 2) === 'körülbelül 2 óra')
       })
     })
   })
 
-  describe('xHours', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xHours', 1) === 'egy óra')
+  describe('xHours', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xHours', 1) === '1 óra')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xHours', 2) === '2 óra')
       })
     })
   })
 
-  describe('xDays', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xDays', 1) === 'egy nap')
+  describe('xDays', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xDays', 1) === '1 nap')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xDays', 2) === '2 nap')
       })
     })
   })
 
-  describe('aboutXMonths', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('aboutXMonths', 1) === 'körülbelül egy hónap')
+  describe('aboutXMonths', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('aboutXMonths', 1) === 'körülbelül 1 hónap')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXMonths', 2) === 'körülbelül 2 hónap')
       })
     })
   })
 
-  describe('xMonths', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xMonths', 1) === 'egy hónap')
+  describe('xMonths', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xMonths', 1) === '1 hónap')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMonths', 2) === '2 hónap')
       })
     })
   })
 
-  describe('aboutXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('aboutXYears', 1) === 'körülbelül egy év')
+  describe('aboutXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('aboutXYears', 1) === 'körülbelül 1 év')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXYears', 2) === 'körülbelül 2 év')
       })
     })
   })
 
-  describe('xYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('xYears', 1) === 'egy év')
+  describe('xYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('xYears', 1) === '1 év')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xYears', 2) === '2 év')
       })
     })
   })
 
-  describe('overXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('overXYears', 1) === 'több mint egy év')
+  describe('overXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('overXYears', 1) === 'több mint 1 év')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('overXYears', 2) === 'több mint 2 év')
       })
     })
   })
 
-  describe('almostXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
-        assert(formatDistance('almostXYears', 1) === 'majdnem egy év')
+  describe('almostXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
+        assert(formatDistance('almostXYears', 1) === 'majdnem 1 év')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('almostXYears', 2) === 'majdnem 2 év')
       })
     })
   })
 
-  context('with a past suffix', function () {
-    it('adds `ezelőtt` to a string', function () {
+  context('with a past suffix', function() {
+    it('adds `ezelőtt` to a string', function() {
       var result = formatDistance('aboutXYears', 1, {
         addSuffix: true,
         comparison: -1
       })
-      assert(result === 'körülbelül egy éve ezelőtt')
+      assert(result === 'körülbelül 1 évvel ezelőtt')
     })
   })
 
-  context('with a future suffix', function () {
-    it('adds `múlva` to a string', function () {
+  context('with a future suffix', function() {
+    it('adds `múlva` to a string', function() {
       var result = formatDistance('halfAMinute', null, {
         addSuffix: true,
         comparison: 1
       })
-      assert(result === 'fél perce múlva')
+      assert(result === 'fél perc múlva')
     })
   })
 })
