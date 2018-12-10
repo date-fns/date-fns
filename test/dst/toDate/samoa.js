baaa5c0333be83117183ec2f8d2dc90123a0d1ee
Sasha Koss <koss@nocorp.me>
2018-12-10 21:28:45 +1400

    Remove dublicated assertion

--- test/dst/toDate/samoa.js
+++ test/dst/toDate/samoa.js
@@ -10,8 +10,6 @@ if (parseInt(process.version.match(/^v(\d+)\./)[1]) < 10)
   throw new Error('The test must be run on Node.js version >= 10')
 
 assert.equal(toDate('2011-12-30').getDate(), 31)
-assert.equal(toDate('2011-12-30').getDate(), 31)
-
 assert.equal(
   toDate('2011-12-30T03:30').toString(),
   'Sat Dec 31 2011 03:30:00 GMT+1400 (Apia Daylight Time)'
