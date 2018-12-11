98aacdc2d31c1113306286d116656d2c5cdf89f3
Sasha Koss <koss@nocorp.me>
2018-12-11 11:28:08 +0530

    Backport be and sl locales from v1

---
+++ src/locale/be/index.js
@@ -0,0 +1,16 @@
+// var buildDistanceInWordsLocale = require('./build_distance_in_words_locale/index.js')
+// var buildFormatLocale = require('./build_format_locale/index.js')
+
+// /**
+//  * @category Locales
+//  * @summary Belarusian locale.
+//  * @author Martin Wind [@arvigeus]{@link https://github.com/mawi12345}
+//  */
+// module.exports = {
+//   distanceInWords: buildDistanceInWordsLocale(),
+//   format: buildFormatLocale()
+// }
+
+throw new Error(
+  'be locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA'
+)
---
+++ src/locale/sl/index.js
@@ -0,0 +1,16 @@
+// var buildDistanceInWordsLocale = require('./build_distance_in_words_locale/index.js')
+// var buildFormatLocale = require('./build_format_locale/index.js')
+
+// /**
+//  * @category Locales
+//  * @summary Slovenian locale.
+//  * @author Adam Stradovnik [@Neoglyph]{@link https://github.com/Neoglyph}
+//  */
+// module.exports = {
+//   distanceInWords: buildDistanceInWordsLocale(),
+//   format: buildFormatLocale()
+// }
+
+throw new Error(
+  'sl locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA'
+)
