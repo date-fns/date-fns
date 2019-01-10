48f1694c71f34af7516194a9090cdfd458a87d46
Arnór Ýmir Guðjónsson <arnor.y.gudjonsson@landsbankinn.is>
2019-01-10 04:43:33 +0000

    Add Icelandic locale (is) for v2 (#1026)

--- src/locale/is/_lib/formatRelative/index.js
+++ src/locale/is/_lib/formatRelative/index.js
@@ -1,9 +1,9 @@
 var formatRelativeLocale = {
-  lastWeek: '[last] dddd [at] LT',
-  yesterday: '[yesterday at] LT',
-  today: '[today at] LT',
-  tomorrow: '[tomorrow at] LT',
-  nextWeek: 'dddd [at] LT',
+  lastWeek: "'síðasta' dddd 'kl.' p",
+  yesterday: "'í gær kl.' p",
+  today: "'í dag kl.' p",
+  tomorrow: "'á morgun kl.' p",
+  nextWeek: "dddd 'kl.' p",
   other: 'L'
 }
 
--- src/locale/is/_lib/localize/index.js
+++ src/locale/is/_lib/localize/index.js
@@ -1,38 +1,133 @@
 import buildLocalizeFn from '../../../_lib/buildLocalizeFn/index.js'
-import buildLocalizeArrayFn from '../../../_lib/buildLocalizeArrayFn/index.js'
 
-var weekdayValues = {
-  narrow: ['su', 'má', 'þr', 'mi', 'fi', 'fö', 'la'],
-  short: ['sun', 'mán', 'þri', 'mið', 'fim', 'fös', 'lau'],
-  long: ['sunnudaginn', 'mánudaginn', 'þriðjudaginn', 'miðvikudaginn', 'fimmtudaginn', 'föstudaginn', 'laugardaginn']
+var eraValues = {
+  narrow: ['f.Kr.', 'e.Kr.'],
+  abbreviated: ['f.Kr.', 'e.Kr.'],
+  wide: ['fyrir Krist', 'eftir Krist']
+}
+
+var quarterValues = {
+  narrow: ['1', '2', '3', '4'],
+  abbreviated: ['1F', '2F', '3F', '4F'],
+  wide: ['1. fjórðungur', '2. fjórðungur', '3. fjórðungur', '4. fjórðungur']
 }
 
 var monthValues = {
-  short: ['jan', 'feb', 'mar', 'apr', 'maí', 'jún', 'júl', 'ágú', 'sep', 'okt', 'nóv', 'des'],
-  long: ['janúar', 'febrúar', 'mars', 'apríl', 'maí', 'júní', 'júlí', 'ágúst', 'september', 'október', 'nóvember', 'desember']
+  narrow: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'Á', 'S', 'Ó', 'N', 'D'],
+  abbreviated: ['jan.', 'feb.', 'mars', 'apríl', 'maí', 'júní', 'júlí', 'ágúst', 'sept.', 'okt.', 'nóv.', 'des.'],
+  wide: ['janúar', 'febrúar', 'mars', 'apríl', 'maí', 'júní', 'júlí', 'ágúst', 'september', 'október', 'nóvember', 'desember']
 }
 
-var timeOfDayValues = {
-  uppercase: ['AM', 'PM'],
-  lowercase: ['am', 'pm'],
-  long: ['a.m.', 'p.m.']
+var dayValues = {
+  narrow: ['S', 'M', 'Þ', 'M', 'F', 'F', 'L'],
+  short: ['Su', 'Má', 'Þr', 'Mi', 'Fi', 'Fö', 'La'],
+  abbreviated: ['sun.', 'mán.', 'þri.', 'mið.', 'fim.', 'fös.', 'lau'],
+  wide: ['sunnudagur', 'mánudagur', 'þriðjudagur', 'miðvikudagur', 'fimmtudagur', 'föstudagur', 'laugardagur']
 }
 
-function ordinalNumber (dirtyNumber) {
+var dayPeriodValues = {
+  narrow: {
+    am: 'f',
+    pm: 'e',
+    midnight: 'miðnætti',
+    noon: 'hádegi',
+    morning: 'morgunn',
+    afternoon: 'síðdegi',
+    evening: 'kvöld',
+    night: 'nótt'
+  },
+  abbreviated: {
+    am: 'f.h.',
+    pm: 'e.h.',
+    midnight: 'miðnætti',
+    noon: 'hádegi',
+    morning: 'morgunn',
+    afternoon: 'síðdegi',
+    evening: 'kvöld',
+    night: 'nótt'
+  },
+  wide: {
+    am: 'fyrir hádegi',
+    pm: 'eftir hádegi',
+    midnight: 'miðnætti',
+    noon: 'hádegi',
+    morning: 'morgunn',
+    afternoon: 'síðdegi',
+    evening: 'kvöld',
+    night: 'nótt'
+  }
+}
+var formattingDayPeriodValues = {
+  narrow: {
+    am: 'f',
+    pm: 'e',
+    midnight: 'á miðnætti',
+    noon: 'á hádegi',
+    morning: 'að morgni',
+    afternoon: 'síðdegis',
+    evening: 'um kvöld',
+    night: 'um nótt'
+  },
+  abbreviated: {
+    am: 'f.h.',
+    pm: 'e.h.',
+    midnight: 'á miðnætti',
+    noon: 'á hádegi',
+    morning: 'að morgni',
+    afternoon: 'síðdegis',
+    evening: 'um kvöld',
+    night: 'um nótt'
+  },
+  wide: {
+    am: 'fyrir hádegi',
+    pm: 'eftir hádegi',
+    midnight: 'á miðnætti',
+    noon: 'á hádegi',
+    morning: 'að morgni',
+    afternoon: 'síðdegis',
+    evening: 'um kvöld',
+    night: 'um nótt'
+  }
+}
+
+function ordinalNumber(dirtyNumber, dirtyOptions) {
   var number = Number(dirtyNumber)
-  return String(number)
+
+  return number + '.'
 }
 
 var localize = {
   ordinalNumber: ordinalNumber,
-  weekday: buildLocalizeFn(weekdayValues, 'long'),
-  weekdays: buildLocalizeArrayFn(weekdayValues, 'long'),
-  month: buildLocalizeFn(monthValues, 'long'),
-  months: buildLocalizeArrayFn(monthValues, 'long'),
-  timeOfDay: buildLocalizeFn(timeOfDayValues, 'long', function (hours) {
-    return (hours / 12) >= 1 ? 1 : 0
+
+  era: buildLocalizeFn({
+    values: eraValues,
+    defaultWidth: 'wide'
   }),
-  timesOfDay: buildLocalizeArrayFn(timeOfDayValues, 'long')
+
+  quarter: buildLocalizeFn({
+    values: quarterValues,
+    defaultWidth: 'wide',
+    argumentCallback: function (quarter) {
+      return Number(quarter) - 1
+    }
+  }),
+
+  month: buildLocalizeFn({
+    values: monthValues,
+    defaultWidth: 'wide'
+  }),
+
+  day: buildLocalizeFn({
+    values: dayValues,
+    defaultWidth: 'wide'
+  }),
+
+  dayPeriod: buildLocalizeFn({
+    values: dayPeriodValues,
+    defaultWidth: 'wide',
+    formattingValues: formattingDayPeriodValues,
+    defaultFormattingWidth: 'wide'
+  })
 }
 
 export default localize
