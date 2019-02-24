abd9fbd904ef4c1ebe8f431843dc08c5c52f9b4e
Jack Hsu <jackhsu978@gmail.com>
2019-02-23 23:31:58 -0800

    Update zh-TW locale based on zh-CN

--- src/locale/zh-TW/_lib/formatLong/index.js
+++ src/locale/zh-TW/_lib/formatLong/index.js
@@ -1,12 +1,41 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js'
 
-var formatLong = buildFormatLongFn({
-  LT: 'h:mm aa',
-  LTS: 'h:mm:ss aa',
-  L: 'MM/DD/YYYY',
-  LL: 'MMMM D YYYY',
-  LLL: 'MMMM D YYYY h:mm aa',
-  LLLL: 'dddd, MMMM D YYYY h:mm aa'
-})
+var dateFormats = {
+  full: "y'年'M'月'd'日' EEEE",
+  long: "y'年'M'月'd'日'",
+  medium: 'yyyy-MM-dd',
+  short: 'yy-MM-dd'
+}
+
+var timeFormats = {
+  full: 'zzzz a h:mm:ss',
+  long: 'z a h:mm:ss',
+  medium: 'a h:mm:ss',
+  short: 'a h:mm'
+}
+
+var dateTimeFormats = {
+  full: '{{date}} {{time}}',
+  long: '{{date}} {{time}}',
+  medium: '{{date}} {{time}}',
+  short: '{{date}} {{time}}'
+}
+
+var formatLong = {
+  date: buildFormatLongFn({
+    formats: dateFormats,
+    defaultWidth: 'full'
+  }),
+
+  time: buildFormatLongFn({
+    formats: timeFormats,
+    defaultWidth: 'full'
+  }),
+
+  dateTime: buildFormatLongFn({
+    formats: dateTimeFormats,
+    defaultWidth: 'full'
+  })
+}
 
 export default formatLong
