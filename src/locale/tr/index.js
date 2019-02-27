a5c4c26d52089ad6f861f2c838e1c1d670a6bb43
İsmail Demirbilek <ce.demirbilek@gmail.com>
2019-02-27 14:01:50 +0300

    add myself

--- src/locale/tr/index.js
+++ src/locale/tr/index.js
@@ -12,6 +12,7 @@ import match from './_lib/match/index.js'
  * @iso-639-2 tur
  * @author Alpcan Aydın [@alpcanaydin]{@link https://github.com/alpcanaydin}
  * @author Berkay Sargın [@berkaey]{@link https://github.com/berkaey}
+ * @author Ismail Demirbilek [@dbtek]{@link https://github.com/dbtek}
  * @author İsmail Kayar [@ikayar]{@link https://github.com/ikayar}
  *
  *
