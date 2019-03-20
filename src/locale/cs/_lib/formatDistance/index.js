8a9195409dfd1424117fac07d141c7e4c836fea0
Jozef Biros <jozef.biros91@gmail.com>
2019-03-20 13:35:46 +0100

    urobeny formatDistance

--- src/locale/cs/_lib/formatDistance/index.js
+++ src/locale/cs/_lib/formatDistance/index.js
@@ -1,399 +1,278 @@
-// v1
-// function declensionGroup (scheme, count) {
-//   if (count === 1) {
-//     return scheme.one
-//   }
-
-//   if (count >= 2 && count <= 4) {
-//     return scheme.twoFour
-//   }
-
-//   // if count === null || count === 0 || count >= 5
-//   return scheme.other
-// }
-
-// function declension (scheme, count, time) {
-//   var group = declensionGroup(scheme, count)
-//   var finalText = group[time] || group
-//   return finalText.replace('{{count}}', count)
-// }
-
-// function extractPreposition (token) {
-//   var result = ['lessThan', 'about', 'over', 'almost'].filter(function (preposition) {
-//     return !!token.match(new RegExp('^' + preposition))
-//   })
-
-//   return result[0]
-// }
-
-// function prefixPreposition (preposition) {
-//   var translation = ''
-
-//   if (preposition === 'almost') {
-//     translation = 'skoro'
-//   }
-
-//   if (preposition === 'about') {
-//     translation = 'přibližně'
-//   }
-
-//   return translation.length > 0 ? translation + ' ' : ''
-// }
-
-// function suffixPreposition (preposition) {
-//   var translation = ''
-
-//   if (preposition === 'lessThan') {
-//     translation = 'méně než'
-//   }
-
-//   if (preposition === 'over') {
-//     translation = 'více než'
-//   }
-
-//   return translation.length > 0 ? translation + ' ' : ''
-// }
-
-// function lowercaseFirstLetter (string) {
-//   return string.charAt(0).toLowerCase() + string.slice(1)
-// }
-
-// var formatDistanceLocale = {
-//   xSeconds: {
-//     one: {
-//       regular: 'vteřina',
-//       past: 'vteřinou',
-//       future: 'vteřinu'
-//     },
-//     twoFour: {
-//       regular: '{{count}} vteřiny',
-//       past: '{{count}} vteřinami',
-//       future: '{{count}} vteřiny'
-//     },
-//     other: {
-//       regular: '{{count}} vteřin',
-//       past: '{{count}} vteřinami',
-//       future: '{{count}} vteřin'
-//     }
-//   },
-
-//   halfAMinute: {
-//     other: {
-//       regular: 'půl minuty',
-//       past: 'půl minutou',
-//       future: 'půl minuty'
-//     }
-//   },
-
-//   xMinutes: {
-//     one: {
-//       regular: 'minuta',
-//       past: 'minutou',
-//       future: 'minutu'
-//     },
-//     twoFour: {
-//       regular: '{{count}} minuty',
-//       past: '{{count}} minutami',
-//       future: '{{count}} minuty'
-//     },
-//     other: {
-//       regular: '{{count}} minut',
-//       past: '{{count}} minutami',
-//       future: '{{count}} minut'
-//     }
-//   },
-
-//   xHours: {
-//     one: {
-//       regular: 'hodina',
-//       past: 'hodinou',
-//       future: 'hodinu'
-//     },
-//     twoFour: {
-//       regular: '{{count}} hodiny',
-//       past: '{{count}} hodinami',
-//       future: '{{count}} hodiny'
-//     },
-//     other: {
-//       regular: '{{count}} hodin',
-//       past: '{{count}} hodinami',
-//       future: '{{count}} hodin'
-//     }
-//   },
-
-//   xDays: {
-//     one: {
-//       regular: 'den',
-//       past: 'dnem',
-//       future: 'den'
-//     },
-//     twoFour: {
-//       regular: '{{count}} dni',
-//       past: '{{count}} dny',
-//       future: '{{count}} dny'
-//     },
-//     other: {
-//       regular: '{{count}} dní',
-//       past: '{{count}} dny',
-//       future: '{{count}} dní'
-//     }
-//   },
-
-//   xMonths: {
-//     one: {
-//       regular: 'měsíc',
-//       past: 'měsícem',
-//       future: 'měsíc'
-//     },
-//     twoFour: {
-//       regular: '{{count}} měsíce',
-//       past: '{{count}} měsíci',
-//       future: '{{count}} měsíce'
-//     },
-//     other: {
-//       regular: '{{count}} měsíců',
-//       past: '{{count}} měsíci',
-//       future: '{{count}} měsíců'
-//     }
-//   },
-
-//   xYears: {
-//     one: {
-//       regular: 'rok',
-//       past: 'rokem',
-//       future: 'rok'
-//     },
-//     twoFour: {
-//       regular: '{{count}} roky',
-//       past: '{{count}} roky',
-//       future: '{{count}} roky'
-//     },
-//     other: {
-//       regular: '{{count}} roků',
-//       past: '{{count}} roky',
-//       future: '{{count}} roků'
-//     }
-//   }
-// }
-
-// v2
 var formatDistanceLocale = {
   lessThanXSeconds: {
-    one: 'less than a second',
-    other: 'less than {{count}} seconds'
-  },
-
-  // xSeconds: {
-  //   one: '1 second',
-  //   other: '{{count}} seconds'
-  // },
-  xSeconds: {
     one: {
-      regular: 'vteřina',
-      past: 'vteřinou',
-      future: 'vteřinu'
+      regular: 'méně než vteřina',
+      past: 'před méně než vteřinou',
+      future: 'za méně než vteřinu'
     },
-    twoFour: {
-      regular: '{{count}} vteřiny',
-      past: '{{count}} vteřinami',
-      future: '{{count}} vteřiny'
+    few: {
+      regular: 'méně než {{count}} vteřiny',
+      past: 'před méně než {{count}} vteřinami',
+      future: 'za méně než {{count}} vteřiny'
     },
-    other: {
-      regular: '{{count}} vteřin',
-      past: '{{count}} vteřinami',
-      future: '{{count}} vteřin'
+    many: {
+      regular: 'méně než {{count}} vteřin',
+      past: 'před méně než {{count}} vteřinami',
+      future: 'za méně než {{count}} vteřin'
     }
   },
 
-  //halfAMinute: 'half a minute',
+  xSeconds: {
+    one: {
+      regular: 'vteřina',
+      past: 'před vteřinou',
+      future: 'za vteřinu'
+    },
+    few: {
+      regular: '{{count}} vteřiny',
+      past: 'před {{count}} vteřinami',
+      future: 'za {{count}} vteřiny'
+    },
+    many: {
+      regular: '{{count}} vteřin',
+      past: 'před {{count}} vteřinami',
+      future: 'za {{count}} vteřin'
+    }
+  },
+
   halfAMinute: {
     other: {
       regular: 'půl minuty',
-      past: 'půl minutou',
-      future: 'půl minuty'
+      past: 'před půl minutou',
+      future: 'za půl minuty'
     }
   },
 
   lessThanXMinutes: {
-    one: 'less than a minute',
-    other: 'less than {{count}} minutes'
+    one: {
+      regular: 'méně než minuta',
+      past: 'před méně než minutou',
+      future: 'za méně než minutu'
+    },
+    few: {
+      regular: 'méně než {{count}} minuty',
+      past: 'před méně než {{count}} minutami',
+      future: 'za méně než {{count}} minuty'
+    },
+    many: {
+      regular: 'méně než {{count}} minut',
+      past: 'před méně než {{count}} minutami',
+      future: 'za méně než {{count}} minut'
+    }
   },
 
-  // xMinutes: {
-  //   one: '1 minute',
-  //   other: '{{count}} minutes'
-  // },
   xMinutes: {
     one: {
       regular: 'minuta',
-      past: 'minutou',
-      future: 'minutu'
+      past: 'před minutou',
+      future: 'za minutu'
     },
-    twoFour: {
+    few: {
       regular: '{{count}} minuty',
-      past: '{{count}} minutami',
-      future: '{{count}} minuty'
+      past: 'před {{count}} minutami',
+      future: 'za {{count}} minuty'
     },
-    other: {
+    many: {
       regular: '{{count}} minut',
-      past: '{{count}} minutami',
-      future: '{{count}} minut'
+      past: 'před {{count}} minutami',
+      future: 'za {{count}} minut'
     }
   },
 
   aboutXHours: {
-    one: 'about 1 hour',
-    other: 'about {{count}} hours'
+    one: {
+      regular: 'přibližně hodina',
+      past: 'přibližně před hodinou',
+      future: 'přibližně za hodinu'
+    },
+    few: {
+      regular: 'přibližně {{count}} hodiny',
+      past: 'přibližně před {{count}} hodinami',
+      future: 'přibližně za {{count}} hodiny'
+    },
+    many: {
+      regular: 'přibližně {{count}} hodin',
+      past: 'přibližně před {{count}} hodinami',
+      future: 'přibližně za {{count}} hodin'
+    }
   },
 
-  // xHours: {
-  //   one: '1 hour',
-  //   other: '{{count}} hours'
-  // },
   xHours: {
     one: {
       regular: 'hodina',
-      past: 'hodinou',
-      future: 'hodinu'
+      past: 'před hodinou',
+      future: 'za hodinu'
     },
-    twoFour: {
+    few: {
       regular: '{{count}} hodiny',
-      past: '{{count}} hodinami',
-      future: '{{count}} hodiny'
+      past: 'před {{count}} hodinami',
+      future: 'za {{count}} hodiny'
     },
-    other: {
+    many: {
       regular: '{{count}} hodin',
-      past: '{{count}} hodinami',
-      future: '{{count}} hodin'
+      past: 'před {{count}} hodinami',
+      future: 'za {{count}} hodin'
     }
   },
 
-  // xDays: {
-  //   one: '1 day',
-  //   other: '{{count}} days'
-  // },
   xDays: {
     one: {
       regular: 'den',
-      past: 'dnem',
-      future: 'den'
+      past: 'před dnem',
+      future: 'za den'
     },
-    twoFour: {
+    few: {
       regular: '{{count}} dni',
-      past: '{{count}} dny',
-      future: '{{count}} dny'
+      past: 'před {{count}} dny',
+      future: 'za {{count}} dny'
     },
-    other: {
+    many: {
       regular: '{{count}} dní',
-      past: '{{count}} dny',
-      future: '{{count}} dní'
+      past: 'před {{count}} dny',
+      future: 'za {{count}} dní'
     }
   },
 
+  // aboutXMonths: {
+  //   one: 'about 1 month',
+  //   other: 'about {{count}} months'
+  // },
   aboutXMonths: {
-    one: 'about 1 month',
-    other: 'about {{count}} months'
+    one: {
+      regular: 'přibližně měsíc',
+      past: 'přibližně před měsícem',
+      future: 'přibližně za měsíc'
+    },
+    few: {
+      regular: 'přibližně {{count}} měsíce',
+      past: 'přibližně před {{count}} měsíci',
+      future: 'přibližně za {{count}} měsíce'
+    },
+    many: {
+      regular: 'přibližně {{count}} měsíců',
+      past: 'přibližně před {{count}} měsíci',
+      future: 'přibližně za {{count}} měsíců'
+    }
   },
 
-  // xMonths: {
-  //   one: '1 month',
-  //   other: '{{count}} months'
-  // },
   xMonths: {
     one: {
       regular: 'měsíc',
-      past: 'měsícem',
-      future: 'měsíc'
+      past: 'před měsícem',
+      future: 'za měsíc'
     },
-    twoFour: {
+    few: {
       regular: '{{count}} měsíce',
-      past: '{{count}} měsíci',
-      future: '{{count}} měsíce'
+      past: 'před {{count}} měsíci',
+      future: 'za {{count}} měsíce'
     },
-    other: {
+    many: {
       regular: '{{count}} měsíců',
-      past: '{{count}} měsíci',
-      future: '{{count}} měsíců'
+      past: 'před {{count}} měsíci',
+      future: 'za {{count}} měsíců'
     }
   },
 
   aboutXYears: {
-    one: 'about 1 year',
-    other: 'about {{count}} years'
+    one: {
+      regular: 'přibližně rok',
+      past: 'přibližně před rokem',
+      future: 'přibližně za rok'
+    },
+    few: {
+      regular: 'přibližně {{count}} roky',
+      past: 'přibližně před {{count}} roky',
+      future: 'přibližně za {{count}} roky'
+    },
+    many: {
+      regular: 'přibližně {{count}} roků',
+      past: 'přibližně před {{count}} roky',
+      future: 'přibližně za {{count}} roků'
+    }
   },
 
-  // xYears: {
-  //   one: '1 year',
-  //   other: '{{count}} years'
-  // },
   xYears: {
     one: {
       regular: 'rok',
-      past: 'rokem',
-      future: 'rok'
+      past: 'před rokem',
+      future: 'za rok'
     },
-    twoFour: {
+    few: {
       regular: '{{count}} roky',
-      past: '{{count}} roky',
-      future: '{{count}} roky'
+      past: 'před {{count}} roky',
+      future: 'za {{count}} roky'
     },
-    other: {
+    many: {
       regular: '{{count}} roků',
-      past: '{{count}} roky',
-      future: '{{count}} roků'
+      past: 'před {{count}} roky',
+      future: 'za {{count}} roků'
     }
   },
 
   overXYears: {
-    one: 'over 1 year',
-    other: 'over {{count}} years'
+    one: {
+      regular: 'více než rok',
+      past: 'před více než rokem',
+      future: 'za více než rok'
+    },
+    few: {
+      regular: 'více než {{count}} roky',
+      past: 'před více než {{count}} roky',
+      future: 'za více než {{count}} roky'
+    },
+    many: {
+      regular: 'více než {{count}} roků',
+      past: 'před více než {{count}} roky',
+      future: 'za více než {{count}} roků'
+    }
   },
 
   almostXYears: {
-    one: 'almost 1 year',
-    other: 'almost {{count}} years'
+    one: {
+      regular: 'skoro rok',
+      past: 'skoro před rokem',
+      future: 'skoro za rok'
+    },
+    few: {
+      regular: 'skoro {{count}} roky',
+      past: 'skoro před {{count}} roky',
+      future: 'skoro za {{count}} roky'
+    },
+    many: {
+      regular: 'skoro {{count}} roků',
+      past: 'skoro před {{count}} roky',
+      future: 'skoro za {{count}} roků'
+    }
   }
 }
 
-// v1
-// export default function formatDistance (token, count, options) {
-//   options = options || {}
-
-//   var preposition = extractPreposition(token) || ''
-//   var key = lowercaseFirstLetter(token.substring(preposition.length))
-//   var scheme = formatDistanceLocale[key]
-
-//   if (!options.addSuffix) {
-//     return prefixPreposition(preposition) + suffixPreposition(preposition) + declension(scheme, count, 'regular')
-//   }
-
-//   if (options.comparison > 0) {
-//     return prefixPreposition(preposition) + 'za ' + suffixPreposition(preposition) + declension(scheme, count, 'future')
-//   } else {
-//     return prefixPreposition(preposition) + 'před ' + suffixPreposition(preposition) + declension(scheme, count, 'past')
-//   }
-// }
-
-// v2
 export default function formatDistance(token, count, options) {
   options = options || {}
+  var scheme = formatDistanceLocale[token]
 
-  var result
-  if (typeof formatDistanceLocale[token] === 'string') {
-    result = formatDistanceLocale[token]
+  // cs pluralization
+  var pluralToken
+  if (typeof scheme.other === 'object') {
+    pluralToken = 'other'
   } else if (count === 1) {
-    result = formatDistanceLocale[token].one
+    pluralToken = 'one'
+  } else if ((count > 1 && count < 5) || count === 0) {
+    pluralToken = 'few'
   } else {
-    result = formatDistanceLocale[token].other.replace('{{count}}', count)
+    pluralToken = 'many'
   }
 
-  if (options.addSuffix) {
-    if (options.comparison > 0) {
-      return 'za ' + result
-    } else {
-      return result + ' před'
-    }
+  // times
+  var suffixExist = options.addSuffix === true
+  var comparison = options.comparison
+  var timeToken
+  if (suffixExist && comparison === -1) {
+    timeToken = 'past'
+  } else if (suffixExist && comparison === 1) {
+    timeToken = 'future'
+  } else {
+    timeToken = 'regular'
   }
 
-  return result
+  return scheme[pluralToken][timeToken].replace('{{count}}', count)
 }
