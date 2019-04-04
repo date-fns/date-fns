503c844742e10e962cdf0c41ee5b135ad21d446d
Mutisz <mati.tokarski@gmail.com>
2019-04-04 18:50:43 +0200

    Rewrite for formatRelative

--- src/locale/pl/_lib/formatRelative/index.js
+++ src/locale/pl/_lib/formatRelative/index.js
@@ -1,12 +1,69 @@
+import isSameUTCWeek from '../../../../_lib/isSameUTCWeek/index.js'
+
+var adjectivesLastWeek = {
+  masculine: "ostatni",
+  feminine: "ostatnia",
+}
+
+var adjectivesThisWeek = {
+  masculine: "ten",
+  feminine: "ta",
+}
+
+var adjectivesNextWeek = {
+  masculine: "następny",
+  feminine: "następna",
+}
+
+var dayGrammaticalGender = {
+  0: "masculine",
+  1: "masculine",
+  2: "feminine",
+  3: "masculine",
+  4: "masculine",
+  5: "feminine",
+  6: "feminine",
+}
+
+function getAdjectives (token, date, baseDate, options) {
+  if (isSameUTCWeek(date, baseDate, options)) {
+    return adjectivesThisWeek;
+  } else if (token === "lastWeek") {
+    return adjectivesLastWeek;
+  } else if (token === "nextWeek") {
+    return adjectivesNextWeek;
+  } else {
+    throw new Error(`Cannot determine adjectives for token ${token}`)
+  }
+}
+
+function getAdjective (token, date, baseDate, options) {
+  var day = date.getUTCDay();
+  var adjectives = getAdjectives(token, date, baseDate, options);
+  var grammaticalGender = dayGrammaticalGender[day];
+
+  return adjectives[grammaticalGender];
+}
+
+function dayAndTimeWithAdjective (token, date, baseDate, options) {
+  const adjective = getAdjective(token, date, baseDate, options);
+  return `'${adjective}' dddd 'o' p`;
+}
+
 var formatRelativeLocale = {
-  lastWeek: '[last] dddd [at] LT',
-  yesterday: '[yesterday at] LT',
-  today: '[today at] LT',
-  tomorrow: '[tomorrow at] LT',
-  nextWeek: 'dddd [at] LT',
-  other: 'L'
+  lastWeek: dayAndTimeWithAdjective,
+  yesterday: "'wczoraj o' p",
+  today: "'dzisiaj o' p",
+  tomorrow: "'jutro o' p",
+  nextWeek: dayAndTimeWithAdjective,
+  other: 'P'
 }
 
 export default function formatRelative (token, date, baseDate, options) {
-  return formatRelativeLocale[token]
+  var format = formatRelativeLocale[token];
+  if (typeof format === 'function') {
+    return format(token, date, baseDate, options)
+  }
+
+  return format;
 }
