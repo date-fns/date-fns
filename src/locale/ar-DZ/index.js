07d9043dc9da8aa5923a9c3747ec3380bce3ffb1
Ahmed ElShahat <ahmed@elshahat.com>
2019-05-15 11:35:51 +0200

    Adding the ar-DZ for version 2

--- src/locale/ar-DZ/index.js
+++ src/locale/ar-DZ/index.js
@@ -1,8 +1,8 @@
-// import formatDistance from './_lib/formatDistance/index.js'
-// import formatLong from './_lib/formatLong/index.js'
-// import formatRelative from './_lib/formatRelative/index.js'
-// import localize from './_lib/localize/index.js'
-// import match from './_lib/match/index.js'
+import formatDistance from './_lib/formatDistance/index.js'
+import formatLong from './_lib/formatLong/index.js'
+import formatRelative from './_lib/formatRelative/index.js'
+import localize from './_lib/localize/index.js'
+import match from './_lib/match/index.js'
 
 /**
  * @type {Locale}
@@ -10,20 +10,18 @@
  * @summary Arabic locale (Modern Standard Arabic ).
  * @language Modern Standard Arabic (Algeria) [ar-dz]
  * @iso-639-2 ara
- * @author BADREDDINE BOUMAZA [@BADRE429]{@link https://github.com/badre429}
+ * @author Ahmed ElShahat [@elshahat]{@link https://github.com/elshahat}
  */
-// var locale = {
-//   formatDistance: formatDistance,
-//   formatLong: formatLong,
-//   formatRelative: formatRelative,
-//   localize: localize,
-//   match: match,
-//   options: {
-//     weekStartsOn: 6 /* Saturday */,
-//     firstWeekContainsDate: 1
-//   }
-// }
+var locale = {
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 0 /* Sunday */,
+    firstWeekContainsDate: 1
+  }
+}
 
-// export default locale
-
-throw new Error('ar-DZ locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA')
+export default locale
