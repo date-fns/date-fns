9525d4ea2408cb9e4490190ea11ce60e1ea492e5
Jack Hsu <jackhsu978@users.noreply.github.com>
2019-06-03 05:08:20 -0700

    Update zh-TW locale based on zh-CN (#1101)

--- src/locale/zh-TW/_lib/formatDistance/test.js
+++ src/locale/zh-TW/_lib/formatDistance/test.js
@@ -4,201 +4,201 @@
 import assert from 'power-assert'
 import formatDistance from '.'
 
-describe('zh-TW locale > formatDistance', function () {
-  describe('lessThanXSeconds', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+describe('zh-TW locale > formatDistance', function() {
+  describe('lessThanXSeconds', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('lessThanXSeconds', 1) === '少於 1 秒')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('lessThanXSeconds', 2) === '少於 2 秒')
       })
     })
   })
 
-  describe('xSeconds', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xSeconds', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xSeconds', 1) === '1 秒')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xSeconds', 2) === '2 秒')
       })
     })
   })
 
-  describe('halfAMinute', function () {
-    it('returns a proper string', function () {
+  describe('halfAMinute', function() {
+    it('returns a proper string', function() {
       assert(formatDistance('halfAMinute') === '半分鐘')
     })
 
-    it('ignores the second argument', function () {
+    it('ignores the second argument', function() {
       assert(formatDistance('halfAMinute', 123) === '半分鐘')
     })
   })
 
-  describe('lessThanXMinutes', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('lessThanXMinutes', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('lessThanXMinutes', 1) === '少於 1 分鐘')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('lessThanXMinutes', 2) === '少於 2 分鐘')
       })
     })
   })
 
-  describe('xMinutes', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xMinutes', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMinutes', 1) === '1 分鐘')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMinutes', 2) === '2 分鐘')
       })
     })
   })
 
-  describe('aboutXHours', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('aboutXHours', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXHours', 1) === '大約 1 小時')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXHours', 2) === '大約 2 小時')
       })
     })
   })
 
-  describe('xHours', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xHours', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xHours', 1) === '1 小時')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xHours', 2) === '2 小時')
       })
     })
   })
 
-  describe('xDays', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xDays', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xDays', 1) === '1 天')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xDays', 2) === '2 天')
       })
     })
   })
 
-  describe('aboutXMonths', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('aboutXMonths', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXMonths', 1) === '大約 1 個月')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXMonths', 2) === '大約 2 個月')
       })
     })
   })
 
-  describe('xMonths', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xMonths', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMonths', 1) === '1 個月')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xMonths', 2) === '2 個月')
       })
     })
   })
 
-  describe('aboutXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('aboutXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXYears', 1) === '大約 1 年')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('aboutXYears', 2) === '大約 2 年')
       })
     })
   })
 
-  describe('xYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('xYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xYears', 1) === '1 年')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('xYears', 2) === '2 年')
       })
     })
   })
 
-  describe('overXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('overXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('overXYears', 1) === '超過 1 年')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('overXYears', 2) === '超過 2 年')
       })
     })
   })
 
-  describe('almostXYears', function () {
-    context('when the count equals 1', function () {
-      it('returns a proper string', function () {
+  describe('almostXYears', function() {
+    context('when the count equals 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('almostXYears', 1) === '將近 1 年')
       })
     })
 
-    context('when the count is more than 1', function () {
-      it('returns a proper string', function () {
+    context('when the count is more than 1', function() {
+      it('returns a proper string', function() {
         assert(formatDistance('almostXYears', 2) === '將近 2 年')
       })
     })
   })
 
-  context('with a past suffix', function () {
-    it('adds `前` to a string', function () {
+  context('with a past suffix', function() {
+    it('adds `前` to a string', function() {
       var result = formatDistance('aboutXYears', 1, {
         addSuffix: true,
         comparison: -1
@@ -207,8 +207,8 @@ describe('zh-TW locale > formatDistance', function () {
     })
   })
 
-  context('with a future suffix', function () {
-    it('adds `内` to a string', function () {
+  context('with a future suffix', function() {
+    it('adds `內` to a string', function() {
       var result = formatDistance('halfAMinute', null, {
         addSuffix: true,
         comparison: 1
--- src/locale/zh-TW/index.js
+++ src/locale/zh-TW/index.js
@@ -1,8 +1,8 @@
-// import formatDistance from './_lib/formatDistance/index.js'
-// import formatLong from './_lib/formatLong/index.js'
-// import formatRelative from './_lib/formatRelative/index.js'
-// import localize from './_lib/localize/index.js'
-// import match from './_lib/match/index.js'
+import formatDistance from './_lib/formatDistance/index.js'
+import formatLong from './_lib/formatLong/index.js'
+import formatRelative from './_lib/formatRelative/index.js'
+import localize from './_lib/localize/index.js'
+import match from './_lib/match/index.js'
 
 /**
  * @type {Locale}
@@ -11,19 +11,18 @@
  * @language Chinese Traditional
  * @iso-639-2 zho
  * @author tonypai [@tpai]{@link https://github.com/tpai}
+ * @author Jack Hsu [@jackhsu978]{@link https://github.com/jackhsu978}
  */
-// var locale = {
-//   formatDistance: formatDistance,
-//   formatLong: formatLong,
-//   formatRelative: formatRelative,
-//   localize: localize,
-//   match: match,
-//   options: {
-//     weekStartsOn: 0 /* Sunday */,
-//     firstWeekContainsDate: 1
-//   }
-// }
+var locale = {
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 1 /* Monday */,
+    firstWeekContainsDate: 4
+  }
+}
 
-// export default locale
-
-throw new Error('zh-TW locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github: TBA')
+export default locale
