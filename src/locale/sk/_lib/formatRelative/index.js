a7e47791835053041de13331f9817b9f43c360af
Marek Suscak <marek.suscak@gmail.com>
2019-06-04 10:30:26 +0200

    Update Slovak locale for v2 (#1090)

--- src/locale/sk/_lib/formatRelative/index.js
+++ src/locale/sk/_lib/formatRelative/index.js
@@ -1,12 +1,81 @@
-var formatRelativeLocale = {
-  lastWeek: '[last] dddd [at] LT',
-  yesterday: '[yesterday at] LT',
-  today: '[today at] LT',
-  tomorrow: '[tomorrow at] LT',
-  nextWeek: 'dddd [at] LT',
-  other: 'L'
+import isSameUTCWeek from '../../../../_lib/isSameUTCWeek/index.js'
+
+// https://www.unicode.org/cldr/charts/32/summary/sk.html?hide#1308
+var accusativeWeekdays = [
+  'nedeľu',
+  'pondelok',
+  'utorok',
+  'stredu',
+  'štvrtok',
+  'piatok',
+  'sobotu'
+]
+
+function lastWeek(day) {
+  var weekday = accusativeWeekdays[day]
+
+  switch (day) {
+    case 0: /* Sun */
+    case 4: /* Wed */
+    case 6 /* Sat */:
+      return "'minulú " + weekday + " o' p"
+    default:
+      return "'minulý' eeee 'o' p"
+  }
 }
 
-export default function formatRelative (token, date, baseDate, options) {
-  return formatRelativeLocale[token]
+function thisWeek(day) {
+  var weekday = accusativeWeekdays[day]
+
+  if (day === 4 /* Thu */) {
+    return "'vo' eeee 'o' p"
+  } else {
+    return "'v " + weekday + " o' p"
+  }
+}
+
+function nextWeek(day) {
+  var weekday = accusativeWeekdays[day]
+
+  switch (day) {
+    case 0: /* Sun */
+    case 4: /* Wed */
+    case 6 /* Sat */:
+      return "'budúcu' " + weekday + " 'o' p"
+    default:
+      return "'budúci' eeee 'o' p"
+  }
+}
+
+var formatRelativeLocale = {
+  lastWeek: function(date, baseDate, options) {
+    var day = date.getUTCDay()
+    if (isSameUTCWeek(date, baseDate, options)) {
+      return thisWeek(day)
+    } else {
+      return lastWeek(day)
+    }
+  },
+  yesterday: "'včera o' p",
+  today: "'dnes o' p",
+  tomorrow: "'zajtra o' p",
+  nextWeek: function(date, baseDate, options) {
+    var day = date.getUTCDay()
+    if (isSameUTCWeek(date, baseDate, options)) {
+      return thisWeek(day)
+    } else {
+      return nextWeek(day)
+    }
+  },
+  other: 'P'
+}
+
+export default function formatRelative(token, date, baseDate, options) {
+  var format = formatRelativeLocale[token]
+
+  if (typeof format === 'function') {
+    return format(date, baseDate, options)
+  }
+
+  return format
 }
