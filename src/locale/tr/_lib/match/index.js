61921484adf1252f59e94be007a39e6cab1f9ad4
İsmail Demirbilek <ce.demirbilek@gmail.com>
2019-06-05 11:40:46 +0300

    Update Turkish (tr) locale for v2 (#1107)

--- src/locale/tr/_lib/match/index.js
+++ src/locale/tr/_lib/match/index.js
@@ -1,47 +1,153 @@
-import buildMatchFn from '../../../_lib/buildMatchFn/index.js'
-import buildParseFn from '../../../_lib/buildParseFn/index.js'
 import buildMatchPatternFn from '../../../_lib/buildMatchPatternFn/index.js'
-import parseDecimal from '../../../_lib/parseDecimal/index.js'
+import buildMatchFn from '../../../_lib/buildMatchFn/index.js'
 
-var matchOrdinalNumbersPattern = /^(\d+)(th|st|nd|rd)?/i
+var matchOrdinalNumberPattern = /^(\d+)(\.)?/i
+var parseOrdinalNumberPattern = /\d+/i
 
-var matchWeekdaysPatterns = {
-  narrow: /^(su|mo|tu|we|th|fr|sa)/i,
-  short: /^(sun|mon|tue|wed|thu|fri|sat)/i,
-  long: /^(sunday|monday|tuesday|wednesday|thursday|friday|saturday)/i
+var matchEraPatterns = {
+  narrow: /^(mö|ms)/i,
+  abbreviated: /^(mö|ms)/i,
+  wide: /^(milattan önce|milattan sonra)/i
+}
+var parseEraPatterns = {
+  any: [/(^mö|^milattan önce)/i, /(^ms|^milattan sonra)/i]
 }
 
-var parseWeekdayPatterns = {
-  any: [/^su/i, /^m/i, /^tu/i, /^w/i, /^th/i, /^f/i, /^sa/i]
+var matchQuarterPatterns = {
+  narrow: /^[1234]/i,
+  abbreviated: /^[1234]ç/i,
+  wide: /^((i|İ)lk|(i|İ)kinci|üçüncü|son) çeyrek/i
+}
+var parseQuarterPatterns = {
+  any: [/1/i, /2/i, /3/i, /4/i],
+  abbreviated: [/1ç/i, /2ç/i, /3ç/i, /4ç/i],
+  wide: [
+    /^(i|İ)lk çeyrek/i,
+    /(i|İ)kinci çeyrek/i,
+    /üçüncü çeyrek/i,
+    /son çeyrek/i
+  ]
 }
 
-var matchMonthsPatterns = {
-  short: /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/i,
-  long: /^(january|february|march|april|may|june|july|august|september|october|november|december)/i
+var matchMonthPatterns = {
+  narrow: /^[jfmasond]/i,
+  abbreviated: /^(oca|şub|mar|nis|may|haz|tem|ağu|eyl|eki|kas|ara)/i,
+  wide: /^(ocak|şubat|mart|nisan|mayıs|haziran|temmuz|ağustos|eylül|ekim|kasım|aralık)/i
 }
-
 var parseMonthPatterns = {
-  any: [/^ja/i, /^f/i, /^mar/i, /^ap/i, /^may/i, /^jun/i, /^jul/i, /^au/i, /^s/i, /^o/i, /^n/i, /^d/i]
+  narrow: [
+    /^o/i,
+    /^ş/i,
+    /^m/i,
+    /^n/i,
+    /^m/i,
+    /^h/i,
+    /^t/i,
+    /^a/i,
+    /^e/i,
+    /^e/i,
+    /^k/i,
+    /^a/i
+  ],
+  any: [
+    /^o/i,
+    /^ş/i,
+    /^ma/i,
+    /^n/i,
+    /^ma/i,
+    /^h/i,
+    /^t/i,
+    /^a/i,
+    /^ey/i,
+    /^ek/i,
+    /^k/i,
+    /^a/i
+  ]
 }
 
-var matchTimesOfDayPatterns = {
-  short: /^(am|pm)/i,
-  long: /^([ap]\.?\s?m\.?)/i
+var matchDayPatterns = {
+  narrow: /^[psçc]/i,
+  short: /^(pz|pt|sa|ça|pe|cu|ct)/i,
+  abbreviated: /^(paz|pts|sal|çar|per|cum|cts)/i,
+  wide: /^(pazar|pazartesi|salı|çarşamba|perşembe|cuma|cumartesi)/i
+}
+var parseDayPatterns = {
+  narrow: [/^p/i, /^p/i, /^s/i, /^ç/i, /^p/i, /^c/i, /^c/i],
+  any: [/^pz/i, /^pt/i, /^sa/i, /^ça/i, /^pe/i, /^cu/i, /^ct/i],
+  wide: [
+    /^pazar/i,
+    /^pazartesi/i,
+    /^salı/i,
+    /^çarşamba/i,
+    /^perşembe/i,
+    /^cuma/i,
+    /cumartesi/i
+  ]
 }
 
-var parseTimeOfDayPatterns = {
-  any: [/^a/i, /^p/i]
+var matchDayPeriodPatterns = {
+  narrow: /^(öö|ös|gy|ö|sa|ös|ak|ge)/i,
+  any: /^(ö\.?\s?[ös]\.?|öğleden sonra|gece yarısı|öğle|(sabah|öğ|akşam|gece)(leyin))/i
+}
+var parseDayPeriodPatterns = {
+  any: {
+    am: /^ö\.?ö\.?/i,
+    pm: /^ö\.?s\.?/i,
+    midnight: /^(gy|gece yarısı)/i,
+    noon: /^öğ/i,
+    morning: /^sa/i,
+    afternoon: /^öğleden sonra/i,
+    evening: /^ak/i,
+    night: /^ge/i
+  }
 }
 
 var match = {
-  ordinalNumbers: buildMatchPatternFn(matchOrdinalNumbersPattern),
-  ordinalNumber: parseDecimal,
-  weekdays: buildMatchFn(matchWeekdaysPatterns, 'long'),
-  weekday: buildParseFn(parseWeekdayPatterns, 'any'),
-  months: buildMatchFn(matchMonthsPatterns, 'long'),
-  month: buildParseFn(parseMonthPatterns, 'any'),
-  timesOfDay: buildMatchFn(matchTimesOfDayPatterns, 'long'),
-  timeOfDay: buildParseFn(parseTimeOfDayPatterns, 'any')
+  ordinalNumber: buildMatchPatternFn({
+    matchPattern: matchOrdinalNumberPattern,
+    parsePattern: parseOrdinalNumberPattern,
+    valueCallback: function(value) {
+      return parseInt(value, 10)
+    }
+  }),
+
+  era: buildMatchFn({
+    matchPatterns: matchEraPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseEraPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  quarter: buildMatchFn({
+    matchPatterns: matchQuarterPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseQuarterPatterns,
+    defaultParseWidth: 'any',
+    valueCallback: function(index) {
+      return index + 1
+    }
+  }),
+
+  month: buildMatchFn({
+    matchPatterns: matchMonthPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseMonthPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  day: buildMatchFn({
+    matchPatterns: matchDayPatterns,
+    defaultMatchWidth: 'wide',
+    parsePatterns: parseDayPatterns,
+    defaultParseWidth: 'any'
+  }),
+
+  dayPeriod: buildMatchFn({
+    matchPatterns: matchDayPeriodPatterns,
+    defaultMatchWidth: 'any',
+    parsePatterns: parseDayPeriodPatterns,
+    defaultParseWidth: 'any'
+  })
 }
 
 export default match
--- src/locale/tr/test.js
+++ src/locale/tr/test.js
@@ -1,9 +1,491 @@
 // @flow
 /* eslint-env mocha */
 
-// import assert from 'power-assert'
-// import locale from '.'
+import assert from 'power-assert'
+import locale from '.'
 
-describe.skip('tr locale', function () {
+import format from '../../format'
+import formatDistance from '../../formatDistance'
+import formatDistanceStrict from '../../formatDistanceStrict'
+import formatRelative from '../../formatRelative'
+import parse from '../../parse'
 
+describe('tr locale', function() {
+  context('with `format`', function() {
+    var date = new Date(1986, 3 /* Apr */, 5, 10, 32, 0, 900)
+
+    it('era', function() {
+      var result = format(date, 'G, GGGG, GGGGG', { locale: locale })
+      assert(result === 'MS, Milattan Sonra, MS')
+    })
+
+    describe('year', function() {
+      it('ordinal regular year', function() {
+        var result = format(date, "yo 'yıl'", { locale: locale })
+        assert(result === '1986. yıl')
+      })
+
+      it('ordinal local week-numbering year', function() {
+        var result = format(date, "Yo 'hafta numaralandırma yılı'", {
+          locale: locale
+        })
+        assert(result === '1986. hafta numaralandırma yılı')
+      })
+    })
+
+    describe('quarter', function() {
+      it('formatting quarter', function() {
+        var result = format(date, "Qo 'çeyrek', QQQ, QQQQ, QQQQQ", {
+          locale: locale
+        })
+        assert(result === '2. çeyrek, 2Ç, İkinci Çeyrek, 2')
+      })
+
+      it('stand-alone quarter', function() {
+        var result = format(date, "qo 'çeyrek', qqq, qqqq, qqqqq", {
+          locale: locale
+        })
+        assert(result === '2. çeyrek, 2Ç, İkinci Çeyrek, 2')
+      })
+    })
+
+    describe('month', function() {
+      it('formatting month', function() {
+        var result = format(date, 'd MMMM', { locale: locale })
+        assert(result === '5 Nisan')
+      })
+
+      it('stand-alone month', function() {
+        var result = format(date, "Lo 'ay', LLL, LLLL, LLLLL", {
+          locale: locale
+        })
+        assert(result === '4. ay, Nis, Nisan, N')
+      })
+    })
+
+    describe('week', function() {
+      it('ordinal local week of year', function() {
+        var date = new Date(1986, 3 /* Apr */, 6)
+        var result = format(date, "wo 'hafta'", { locale: locale })
+        assert(result === '14. hafta')
+      })
+
+      it('ordinal ISO week of year', function() {
+        var date = new Date(1986, 3 /* Apr */, 6)
+        var result = format(date, "Io 'ISO hafta'", { locale: locale })
+        assert(result === '14. ISO hafta')
+      })
+    })
+
+    describe('day', function() {
+      it('ordinal date', function() {
+        var result = format(date, "'bugün' do 'gün'", { locale: locale })
+        assert(result === 'bugün 5. gün')
+      })
+
+      it('ordinal day of year', function() {
+        var result = format(date, "'yılın' Do 'günü'", { locale: locale })
+        assert(result === 'yılın 95. günü')
+      })
+    })
+
+    describe('week day', function() {
+      it('day of week', function() {
+        var result = format(date, 'E, EEEE, EEEEE, EEEEEE', { locale: locale })
+        assert(result === 'Cts, Cumartesi, C, Ct')
+      })
+
+      it('ordinal day of week', function() {
+        var result = format(date, "'haftanın' eo 'günü'", { locale: locale })
+        assert(result === 'haftanın 6. günü')
+      })
+    })
+
+    describe('day period and hour', function() {
+      it('ordinal hour', function() {
+        var result = format(date, "ho 'saat'", { locale: locale })
+        assert(result === '10. saat')
+      })
+
+      it('AM, PM', function() {
+        var result = format(date, 'h a, h aaaa, haaaaa', { locale: locale })
+        assert(result === '10 ÖÖ, 10 ö.ö., 10öö')
+      })
+
+      it('AM, PM, noon, midnight', function() {
+        var result = format(
+          new Date(1986, 3 /* Apr */, 6, 0),
+          'b, bbbb, bbbbb',
+          { locale: locale }
+        )
+        assert(result === 'gece yarısı, gece yarısı, gy')
+      })
+
+      it('flexible day periods', function() {
+        var result = format(date, 'B h', { locale: locale })
+        assert(result === 'sabahleyin 10')
+      })
+    })
+
+    it('ordinal minute', function() {
+      var result = format(date, "mo 'dakika'", { locale: locale })
+      assert(result === '32. dakika')
+    })
+
+    it('ordinal second', function() {
+      var result = format(date, "so 'saniye'", { locale: locale })
+      assert(result === '0. saniye')
+    })
+
+    describe('long format', function() {
+      it('short date', function() {
+        var result = format(date, 'P', { locale: locale })
+        assert(result === '05.04.1986')
+      })
+
+      it('medium date', function() {
+        var result = format(date, 'PP', { locale: locale })
+        assert(result === '5 Nis 1986')
+      })
+
+      it('long date', function() {
+        var result = format(date, 'PPP', { locale: locale })
+        assert(result === '5 Nisan 1986')
+      })
+
+      it('full date', function() {
+        var result = format(date, 'PPPP', { locale: locale })
+        assert(result === '5 Nisan 1986 Cumartesi')
+      })
+
+      it('short time', function() {
+        var result = format(date, 'p', { locale: locale })
+        assert(result === '10:32')
+      })
+
+      it('medium time', function() {
+        var result = format(date, 'pp', { locale: locale })
+        assert(result === '10:32:00')
+      })
+
+      it('short date + time', function() {
+        var result = format(date, 'Pp', { locale: locale })
+        assert(result === '05.04.1986, 10:32')
+      })
+
+      it('medium date + time', function() {
+        var result = format(date, 'PPpp', { locale: locale })
+        assert(result === '5 Nis 1986, 10:32:00')
+      })
+
+      it('long date + time', function() {
+        var result = format(date, 'PPPp', { locale: locale })
+        assert(result === '5 Nisan 1986 saat 10:32')
+      })
+
+      it('full date + time', function() {
+        var result = format(date, 'PPPPp', { locale: locale })
+        assert(result === '5 Nisan 1986 Cumartesi saat 10:32')
+      })
+    })
+  })
+
+  context('with `formatDistance`', function() {
+    it('works as expected', function() {
+      var result = formatDistance(
+        new Date(1986, 3, 4, 10, 32, 25),
+        new Date(1986, 3, 4, 10, 32, 0),
+        { locale: locale, includeSeconds: true }
+      )
+      assert(result === 'yarım dakika')
+    })
+
+    context('when `addSuffix` option is true', function() {
+      it('adds a future suffix', function() {
+        var result = formatDistance(
+          new Date(1986, 3, 4, 10, 32, 7),
+          new Date(1986, 3, 4, 10, 32, 0),
+          { locale: locale, includeSeconds: true, addSuffix: true }
+        )
+        assert(result === '10 saniyeden az sonra')
+      })
+
+      it('adds a past suffix', function() {
+        var result = formatDistance(
+          new Date(1986, 3, 4, 10, 32, 0),
+          new Date(1986, 3, 4, 11, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === 'yaklaşık 1 saat önce')
+      })
+    })
+  })
+
+  context('with `formatDistanceStrict`', function() {
+    it('works as expected', function() {
+      var result = formatDistanceStrict(
+        new Date(1986, 3, 4, 10, 32, 0),
+        new Date(1986, 3, 4, 12, 32, 0),
+        { locale: locale, unit: 'minute' }
+      )
+      assert(result === '120 dakika')
+    })
+
+    describe('when `addSuffix` option is true', function() {
+      it('adds a future suffix', function() {
+        var result = formatDistanceStrict(
+          new Date(1986, 3, 4, 10, 32, 25),
+          new Date(1986, 3, 4, 10, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === '25 saniye sonra')
+      })
+
+      it('adds a past suffix', function() {
+        var result = formatDistanceStrict(
+          new Date(1986, 3, 4, 10, 32, 0),
+          new Date(1986, 3, 4, 11, 32, 0),
+          { locale: locale, addSuffix: true }
+        )
+        assert(result === '1 saat önce')
+      })
+    })
+  })
+
+  context('with `formatRelative`', function() {
+    var baseDate = new Date(1986, 3 /* Apr */, 4, 10, 32, 0, 900)
+
+    it('last week', function() {
+      var result = formatRelative(new Date(1986, 3 /* Apr */, 1), baseDate, {
+        locale: locale
+      })
+      assert(result === 'geçen hafta Salı saat 00:00')
+    })
+
+    it('yesterday', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 3, 22, 22),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'dün saat 22:22')
+    })
+
+    it('today', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 4, 16, 50),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'bugün saat 16:50')
+    })
+
+    it('tomorrow', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 5, 7, 30),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'yarın saat 07:30')
+    })
+
+    it('next week', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 6, 12, 0),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === 'Pazar saat 12:00')
+    })
+
+    it('after the next week', function() {
+      var result = formatRelative(
+        new Date(1986, 3 /* Apr */, 11, 16, 50),
+        baseDate,
+        { locale: locale }
+      )
+      assert(result === '11.04.1986')
+    })
+  })
+
+  context('with `parse`', function() {
+    var baseDate = new Date(1986, 3 /* Apr */, 4, 10, 32, 0, 900)
+
+    describe('era', function() {
+      it('abbreviated', function() {
+        var result = parse('MÖ 10000', 'G yyyyy', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(-9999, 0 /* Jan */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('Milattan Sonra 2018', 'GGGG yyyy', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(2018, 0 /* Jan */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('MÖ 44', 'GGGGG y', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(-43, 0 /* Jan */, 1))
+      })
+    })
+
+    it('ordinal year', function() {
+      var result = parse('2017.', 'yo', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(2017, 0 /* Jan */, 1))
+    })
+
+    describe('quarter', function() {
+      it('ordinal', function() {
+        var result = parse('1.', 'Qo', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+
+      it('abbreviated', function() {
+        var result = parse('3Ç', 'QQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 6 /* Jul */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('Son çeyrek', 'QQQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 9 /* Oct */, 1))
+      })
+
+      it('wide İ', function() {
+        var result = parse('İlk çeyrek', 'QQQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('1', 'QQQQQ', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+    })
+
+    describe('month', function() {
+      it('ordinal', function() {
+        var result = parse('6.', 'Mo', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 5 /* Jun */, 1))
+      })
+
+      it('abbreviated', function() {
+        var result = parse('Kas', 'MMM', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 10 /* Nov */, 1))
+      })
+
+      it('wide', function() {
+        var result = parse('Şubat', 'MMMM', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 1 /* Feb */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('O', 'MMMMM', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 0 /* Jan */, 1))
+      })
+    })
+
+    it('ordinal week of year', function() {
+      var result = parse('49.', 'wo', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 10 /* Nov */, 31))
+    })
+
+    it('ordinal day of month', function() {
+      var result = parse('28.', 'do', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 3 /* Apr */, 28))
+    })
+
+    it('ordinal day of year', function() {
+      var result = parse('200.', 'Do', baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 6 /* Jul */, 19))
+    })
+
+    describe('day of week', function() {
+      it('abbreviated', function() {
+        var result = parse('Pts', 'E', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 2 /* Mar */, 31))
+      })
+
+      it('wide', function() {
+        var result = parse('Salı', 'EEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 1))
+      })
+
+      it('narrow', function() {
+        var result = parse('Ç', 'EEEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 2))
+      })
+
+      it('short', function() {
+        var result = parse('P', 'EEEEEE', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 6))
+      })
+    })
+
+    it('ordinal local day of week', function() {
+      var result = parse('haftanın 2. günü', "'haftanın' eo 'günü'", baseDate, {
+        locale: locale
+      })
+      assert.deepEqual(result, new Date(1986, 3 /* Apr */, 1))
+    })
+
+    describe('ÖÖ, ÖS', function() {
+      it('abbreviated', function() {
+        var result = parse('5 ÖÖ', 'h a', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 5))
+      })
+
+      it('wide', function() {
+        var result = parse('5 ö.s.', 'h aaaa', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 17))
+      })
+
+      it('narrow', function() {
+        var result = parse('11 öö', 'h aaaaa', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 11))
+      })
+    })
+
+    describe('AM, PM, noon, midnight', function() {
+      it('abbreviated', function() {
+        var result = parse('öğle', 'b', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 12))
+      })
+
+      it('wide', function() {
+        var result = parse('gece yarısı', 'bbbb', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 0))
+      })
+
+      it('narrow', function() {
+        var result = parse('gy', 'bbbbb', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 0))
+      })
+    })
+
+    describe('flexible day period', function() {
+      it('abbreviated', function() {
+        var result = parse('gece yarısı 2', 'B h', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 2))
+      })
+
+      it('wide', function() {
+        var result = parse('öğleden sonra 12', 'BBBB h', baseDate, {
+          locale: locale
+        })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 12))
+      })
+
+      it('narrow', function() {
+        var result = parse('ak 5', 'BBBBB h', baseDate, { locale: locale })
+        assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 17))
+      })
+    })
+
+    it('ordinal time', function() {
+      var dateString = '1. saat, 2. dakika, 3. saniye'
+      var formatString = "ho 'saat', mo 'dakika', so 'saniye'"
+      var result = parse(dateString, formatString, baseDate, { locale: locale })
+      assert.deepEqual(result, new Date(1986, 3 /* Apr */, 4, 1, 2, 3))
+    })
+  })
 })
