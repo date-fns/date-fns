5a36a80b169ef43630f20d2a2845babe1984867a
Sasha Koss <koss@nocorp.me>
2019-06-11 20:40:11 +0400

    Add upgrade guide note to the startOfToday JSDoc

--- src/startOfToday/index.js
+++ src/startOfToday/index.js
@@ -12,6 +12,10 @@ import startOfDay from '../startOfDay/index.js'
  * > ⚠️ Please note that this function is not present in the FP submodule as
  * > it uses `Date.now()` internally hence impure and can't be safely curried.
  *
+ * ### v2.0.0 breaking changes:
+ *
+ * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
+ *
  * @returns {Date} the start of today
  *
  * @example
