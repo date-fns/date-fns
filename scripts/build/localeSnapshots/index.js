eb98a1e7ce8ff3ed739b6db9cadc19638e40b2e7
Sasha Koss <koss@nocorp.me>
2019-06-19 19:18:12 +0400

    Integrate locale snapshots into Travis CI

--- scripts/build/localeSnapshots/index.js
+++ scripts/build/localeSnapshots/index.js
@@ -7,7 +7,7 @@
  * It's a part of the build process.
  */
 
-import { readFileSync, writeFile } from 'mz/fs'
+import { readFile, readFileSync, writeFile } from 'mz/fs'
 import path from 'path'
 import listLocales from '../../_lib/listLocales'
 import prettier from '../_lib/prettier'
@@ -16,6 +16,11 @@ import renderFormatDistanceStrict from './renderFormatDistanceStrict'
 import renderFormatParse from './renderFormatParse'
 import renderFormatRelative from './renderFormatRelative'
 
+const mode = process.argv[2] || 'generate'
+
+if (process.env.TZ.toLowerCase() !== 'utc')
+  throw new Error('The locale snapshots generation must be run with TZ=utc')
+
 const outdatedLocales = JSON.parse(
   readFileSync(path.join(process.cwd(), 'outdatedLocales.json'), 'utf8')
 )
@@ -23,13 +28,14 @@ const locales = listLocales().filter(
   ({ code }) => !outdatedLocales.includes(code)
 )
 
-const snapshots = locales.map(localeObj => {
-  const { code, fullPath } = localeObj
-  const locale = require(`../../../src/locale/${code}`)
-  const source = readFileSync(path.join(process.cwd(), fullPath)).toString()
-  const languageName = source.match(/\* @language (.*)/)[1]
+Promise.all(
+  locales.map(localeObj => {
+    const { code, fullPath } = localeObj
+    const locale = require(`../../../src/locale/${code}`)
+    const source = readFileSync(path.join(process.cwd(), fullPath)).toString()
+    const languageName = source.match(/\* @language (.*)/)[1]
 
-  const snapshot = `# ${languageName} (${code}) locale
+    const snapshot = `# ${languageName} (${code}) locale
 
 ${renderFormatParse(locale)}
 
@@ -40,17 +46,23 @@ ${renderFormatDistanceStrict(locale)}
 ${renderFormatRelative(locale)}
 `
 
-  const snapshotPath = path.join(
-    path.resolve(process.cwd(), path.dirname(fullPath)),
-    'snapshot.md'
-  )
-  return { snapshot, snapshotPath }
-})
+    const snapshotPath = path.join(
+      path.resolve(process.cwd(), path.dirname(fullPath)),
+      'snapshot.md'
+    )
+    const formattedSnapshot = prettier(snapshot, 'markdown')
 
-Promise.all(
-  snapshots.map(({ snapshot, snapshotPath }) =>
-    writeFile(snapshotPath, prettier(snapshot, 'markdown'))
-  )
+    if (mode === 'test') {
+      return readFile(snapshotPath, 'utf8').then(snapshotFileContent => {
+        if (snapshotFileContent !== formattedSnapshot)
+          throw new Error(
+            `The snapshot on the disk doesn't match the generated snapshot: ${snapshotPath}. Please run yarn locale-snapshots and commit the results.`
+          )
+      })
+    } else {
+      return writeFile(snapshotPath, formattedSnapshot)
+    }
+  })
 ).catch(err => {
   console.error(err.stack)
   process.exit(1)
