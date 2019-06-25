5a7b442a08ca0be1ac3c1a2a26c424a7e9c00c17
Nitesh Phadatare <nsp.590@gmail.com>
2019-06-25 09:39:24 +0530

    Added UTC version of getWeekOfMonth and all functions that used in there i.e. getDate, getDay and startOfMonth

---
+++ src/_lib/getUTCDay/index.js
@@ -0,0 +1,15 @@
+import toDate from '../../toDate/index.js'
+
+// This function will be a part of public API when UTC function will be implemented.
+// See issue: https://github.com/date-fns/date-fns/issues/376
+export default function getUTCDay(dirtyDate) {
+  if (arguments.length < 1) {
+    throw new TypeError(
+      '1 argument required, but only ' + arguments.length + ' present'
+    )
+  }
+
+  var date = toDate(dirtyDate)
+  var day = date.getUTCDay()
+  return day
+}
---
+++ src/_lib/startOfUTCMonth/index.js
@@ -0,0 +1,16 @@
+import toDate from '../../toDate/index.js'
+
+// This function will be a part of public API when UTC function will be implemented.
+// See issue: https://github.com/date-fns/date-fns/issues/376
+export default function startOfUTCMonth(dirtyDate) {
+  if (arguments.length < 1) {
+    throw new TypeError(
+      '1 argument required, but only ' + arguments.length + ' present'
+    )
+  }
+
+  var date = toDate(dirtyDate)
+  date.setUTCDate(1)
+  date.setUTCHours(0, 0, 0, 0)
+  return date
+}
