318e35eaaea157d023a9d2ef055543f5e71cba84
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2019-07-29 18:44:03 +0200

    Fixing relative formatting

--- src/locale/hr/_lib/formatRelative/index.js
+++ src/locale/hr/_lib/formatRelative/index.js
@@ -1,12 +1,52 @@
 var formatRelativeLocale = {
-  lastWeek: "'prošli' EEEE 'u' p",
+  lastWeek: function(date) {
+    var day = date.getUTCDay()
+
+    switch (day) {
+      case 0:
+        return "'prošlu nedjelju u' p"
+      case 1:
+      case 2:
+      case 3:
+        return "'prošlu srijedu u' p"
+      case 4:
+      case 5:
+      case 6:
+        return "'prošlu subotu u' p"
+    }
+
+    return "'prošli' EEEE 'u' p"
+  },
   yesterday: "'jučer u' p",
   today: "'danas u' p",
   tomorrow: "'sutra u' p",
-  nextWeek: "'idući' EEEE 'u' p",
+  nextWeek: function(date) {
+    var day = date.getUTCDay()
+
+    switch (day) {
+      case 0:
+        return "'iduću nedjelju u' p"
+      case 1:
+      case 2:
+      case 3:
+        return "'iduću srijedu u' p"
+      case 4:
+      case 5:
+      case 6:
+        return "'iduću subotu u' p"
+    }
+
+    return "'idući' EEEE 'u' p"
+  },
   other: 'P'
 }
 
-export default function formatRelative(token, _date, _baseDate, _options) {
-  return formatRelativeLocale[token]
+export default function formatRelative(token, date, _baseDate, _options) {
+  var format = formatRelativeLocale[token]
+
+  if (typeof format === 'function') {
+    return format(date)
+  }
+
+  return format
 }
