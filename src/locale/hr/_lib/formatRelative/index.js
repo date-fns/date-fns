0c9d238d386708eb8c2f01c2a51661f94408e3bf
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2019-07-29 15:43:35 +0000

    Fixed relative formatting

--- src/locale/hr/_lib/formatRelative/index.js
+++ src/locale/hr/_lib/formatRelative/index.js
@@ -1,12 +1,12 @@
 var formatRelativeLocale = {
-  lastWeek: "'posljednji' dddd 'u' LT",
-  yesterday: "'jučer u' LT",
-  today: "'danas u' LT",
-  tomorrow: "'sutra u' LT",
-  nextWeek: "dddd 'u' LT",
-  other: "L"
+  lastWeek: "'prošli' EEEE 'u' p",
+  yesterday: "'jučer u' p",
+  today: "'danas u' p",
+  tomorrow: "'sutra u' p",
+  nextWeek: "'idući' EEEE 'u' p",
+  other: 'P'
 }
 
-export default function formatRelative (token, date, baseDate, options) {
+export default function formatRelative(token, _date, _baseDate, _options) {
   return formatRelativeLocale[token]
 }
--- src/locale/hr/_lib/localize/index.js
+++ src/locale/hr/_lib/localize/index.js
@@ -12,28 +12,114 @@ var eraValues = {
 }
 
 var monthValues = {
-  narrow: ['1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.', '11.', '12.'],
-  abbreviated: ['sij', 'velj', 'ožu', 'tra', 'svi', 'lip', 'srp', 'kol', 'ruj', 'lis', 'stu', 'pro'],
-  wide: ['siječanj', 'veljača', 'ožujak', 'travanj', 'svibanj', 'lipanj', 'srpanj', 'kolovoz', 'rujan', 'listopad', 'studeni', 'prosinac']
+  narrow: [
+    '1.',
+    '2.',
+    '3.',
+    '4.',
+    '5.',
+    '6.',
+    '7.',
+    '8.',
+    '9.',
+    '10.',
+    '11.',
+    '12.'
+  ],
+  abbreviated: [
+    'sij',
+    'velj',
+    'ožu',
+    'tra',
+    'svi',
+    'lip',
+    'srp',
+    'kol',
+    'ruj',
+    'lis',
+    'stu',
+    'pro'
+  ],
+  wide: [
+    'siječanj',
+    'veljača',
+    'ožujak',
+    'travanj',
+    'svibanj',
+    'lipanj',
+    'srpanj',
+    'kolovoz',
+    'rujan',
+    'listopad',
+    'studeni',
+    'prosinac'
+  ]
 }
 
 var formattingMonthValues = {
-  narrow: ['1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.', '11.', '12.'],
-  abbreviated: ['sij', 'velj', 'ožu', 'tra', 'svi', 'lip', 'srp', 'kol', 'ruj', 'lis', 'stu', 'pro'],
-  wide: ['siječnja', 'veljače', 'ožujka', 'travnja', 'svibnja', 'lipnja', 'srpnja', 'kolovoza', 'rujna', 'listopada', 'studenog', 'prosinca']
+  narrow: [
+    '1.',
+    '2.',
+    '3.',
+    '4.',
+    '5.',
+    '6.',
+    '7.',
+    '8.',
+    '9.',
+    '10.',
+    '11.',
+    '12.'
+  ],
+  abbreviated: [
+    'sij',
+    'velj',
+    'ožu',
+    'tra',
+    'svi',
+    'lip',
+    'srp',
+    'kol',
+    'ruj',
+    'lis',
+    'stu',
+    'pro'
+  ],
+  wide: [
+    'siječnja',
+    'veljače',
+    'ožujka',
+    'travnja',
+    'svibnja',
+    'lipnja',
+    'srpnja',
+    'kolovoza',
+    'rujna',
+    'listopada',
+    'studenog',
+    'prosinca'
+  ]
 }
 
 var quarterValues = {
   narrow: ['1.', '2.', '3.', '4.'],
   abbreviated: ['1. kv.', '2. kv.', '3. kv.', '4. kv.'],
-  wide: ['1. kvartal', '2. kvartal', '3. kvartal', '4. kvartal'],
+  wide: ['1. kvartal', '2. kvartal', '3. kvartal', '4. kvartal']
 }
 
 var dayValues = {
-  narrow: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  narrow: ['N', 'P', 'U', 'S', 'Č', 'P', 'S'],
   short: ['ned', 'pon', 'uto', 'sri', 'čet', 'pet', 'sub'],
   abbreviated: ['ned', 'pon', 'uto', 'sri', 'čet', 'pet', 'sub'],
-  wide: ['nedjelja', 'ponedjeljak', 'utorak', 'srijeda', 'četvrtak', 'petak', 'subota']
+  wide: [
+    'nedjelja',
+    'ponedjeljak',
+    'utorak',
+    'srijeda',
+    'četvrtak',
+    'petak',
+    'subota'
+  ]
 }
 
 var formattingDayPeriodValues = {
@@ -111,7 +197,7 @@ var localize = {
   quarter: buildLocalizeFn({
     values: quarterValues,
     defaultWidth: 'wide',
-    argumentCallback: function (quarter) {
+    argumentCallback: function(quarter) {
       return Number(quarter) - 1
     }
   }),
