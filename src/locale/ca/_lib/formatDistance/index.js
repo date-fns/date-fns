f059bd5f1863ed0accab7df1e4bcd25f0192ea27
Alex Vizcaino <avizcaino@uxland.es>
2019-08-13 16:42:05 +0200

    fix CA spelling errors and update following official gramatical rules

--- src/locale/ca/_lib/formatDistance/index.js
+++ src/locale/ca/_lib/formatDistance/index.js
@@ -1,6 +1,17 @@
+/**
+ * Davant de les xifres que es diuen amb vocal inicial, 1 i 11, s'apostrofen els articles el i la i la preposició de igual que si estiguessin escrits amb lletres.
+ *    l'1 de juliol ('l'u')
+ *    l'11 de novembre ('l'onze')
+ *    l'11a clàusula del contracte ('l'onzena')
+ *    la contractació d'11 jugadors ('d'onze')
+ *    l'aval d'11.000 socis ('d'onze mil')
+ *
+ * Reference: https://aplicacions.llengua.gencat.cat/llc/AppJava/index.html?input_cercar=apostrofaci%25F3+davant+xifres&action=Principal&method=detall_completa&numPagina=1&idHit=11236&database=FITXES_PUB&tipusFont=Fitxes%20de%20l%27Optimot&idFont=11236&titol=apostrofaci%F3%20davant%20de%20xifres%20%2F%20apostrofaci%F3%20davant%20de%201%20i%2011&numeroResultat=1&clickLink=detall&tipusCerca=cerca.normes
+ */
 var formatDistanceLocale = {
   lessThanXSeconds: {
     one: "menys d'un segon",
+    eleven: "menys d'onze segons",
     other: 'menys de {{count}} segons'
   },
 
@@ -13,6 +24,7 @@ var formatDistanceLocale = {
 
   lessThanXMinutes: {
     one: "menys d'un minut",
+    eleven: "menys d'onze minuts",
     other: 'menys de {{count}} minuts'
   },
 
@@ -58,6 +70,7 @@ var formatDistanceLocale = {
 
   overXYears: {
     one: "més d'un any",
+    eleven: "més d'onze anys",
     other: 'més de {{count}} anys'
   },
 
@@ -67,7 +80,7 @@ var formatDistanceLocale = {
   }
 }
 
-export default function formatDistance (token, count, options) {
+export default function formatDistance(token, count, options) {
   options = options || {}
 
   var result
@@ -75,6 +88,8 @@ export default function formatDistance (token, count, options) {
     result = formatDistanceLocale[token]
   } else if (count === 1) {
     result = formatDistanceLocale[token].one
+  } else if (count === 11 && formatDistanceLocale[token].eleven) {
+    result = formatDistanceLocale[token].eleven
   } else {
     result = formatDistanceLocale[token].other.replace('{{count}}', count)
   }
