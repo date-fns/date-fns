c00042ddf46c4c2ef2b24bc28861445961c3be98
Sasha Koss <koss@nocorp.me>
2019-10-16 13:26:04 +0700

    Set 12-hour-clock format in the en-AU locale

--- src/locale/en-AU/_lib/formatLong/index.js
+++ src/locale/en-AU/_lib/formatLong/index.js
@@ -8,10 +8,10 @@ var dateFormats = {
 }
 
 var timeFormats = {
-  full: 'HH:mm:ss zzzz',
-  long: 'HH:mm:ss z',
-  medium: 'HH:mm:ss',
-  short: 'HH:mm'
+  full: 'h:mm:ss a zzzz',
+  long: 'h:mm:ss a z',
+  medium: 'h:mm:ss a',
+  short: 'h:mm a'
 }
 
 var dateTimeFormats = {
