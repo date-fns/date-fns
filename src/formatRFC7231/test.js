cb8fee64065e881f32a6a2664762e1eaab18f092
Try Ajitiono <ballinst@gmail.com>
2019-10-21 11:59:58 +1100

    Attempt to fix test
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/formatRFC7231/test.js
+++ src/formatRFC7231/test.js
@@ -6,13 +6,14 @@ import formatRFC7231 from '.'
 import addMinutes from '../addMinutes/index.js'
 
 describe('formatRFC7231', () => {
-  const dt = new Date()
-  const tzOffset = dt.getTimezoneOffset()
+  let sampleDate1 = new Date(2019, 2, 3, 19, 0, 52)
+  let sampleDate2 = new Date(2019, 9, 4, 12, 30, 13)
+  let sampleDate3 = new Date(2019, 11, 11, 1, 0, 0)
 
   // Force normalize the date so it will work fine in other timezones.
-  const sampleDate1 = addMinutes(new Date(2019, 2, 3, 19, 0, 52), tzOffset)
-  const sampleDate2 = addMinutes(new Date(2019, 9, 4, 12, 30, 13), tzOffset)
-  const sampleDate3 = addMinutes(new Date(2019, 11, 11, 1, 0, 0), tzOffset)
+  sampleDate1 = addMinutes(sampleDate1, sampleDate1.getTimezoneOffset() * -1)
+  sampleDate2 = addMinutes(sampleDate2, sampleDate2.getTimezoneOffset() * -1)
+  sampleDate3 = addMinutes(sampleDate3, sampleDate3.getTimezoneOffset() * -1)
 
   it('should convert to the RFC 7231 format', () => {
     assert.deepEqual(
@@ -23,7 +24,10 @@ describe('formatRFC7231', () => {
       formatRFC7231(sampleDate2),
       'Fri, 04 Oct 2019 12:30:13 GMT'
     )
-    assert.deepEqual(formatRFC7231(sampleDate3), 'Wed, 11 Dec 2019 1:00:00 GMT')
+    assert.deepEqual(
+      formatRFC7231(sampleDate3),
+      'Wed, 11 Dec 2019 01:00:00 GMT'
+    )
   })
 
   it('throws TypeError if no parameters are passed', function() {
