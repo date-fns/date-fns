b613f4457825a8846236696b384d7d16b6dc209b
Zyten <sruban707@hotmail.com>
2019-11-10 01:47:46 +0800

    Update Malay locale for v2

--- src/locale/ms/_lib/localize/index.js
+++ src/locale/ms/_lib/localize/index.js
@@ -1,37 +1,132 @@
 import buildLocalizeFn from '../../../_lib/buildLocalizeFn/index.js'
-import buildLocalizeArrayFn from '../../../_lib/buildLocalizeArrayFn/index.js'
+
+// Most data for localization are taken from this page
+// https://www.unicode.org/cldr/charts/32/summary/ms.html
+var eraValues = {
+  narrow: ['SM', 'M'],
+  abbreviated: ['SM', 'M'],
+  wide: ['Sebelum Masihi', 'Masihi']
+}
+
+var quarterValues = {
+  narrow: ['1', '2', '3', '4'],
+  abbreviated: ['S1', 'S2', 'S3', 'S4'],
+  wide: ['Suku pertama', 'Suku kedua', 'Suku ketiga', 'Suku keempat']
+}
 
 // Note: in Malay, the names of days of the week and months are capitalized.
 // If you are making a new locale based on this one, check if the same is true for the language you're working on.
 // Generally, formatted dates should look like they are in the middle of a sentence,
 // e.g. in Spanish language the weekdays and months should be in the lowercase.
-var weekdayValues = {
-  narrow: ['Ah', 'Is', 'Se', 'Ra', 'Kh', 'Ju', 'Sa'],
-  short: ['Ahd', 'Isn', 'Sel', 'Rab', 'Kha', 'Jum', 'Sab'],
-  long: ['Ahad', 'Isnin', 'Selasa', 'Rabu', 'Khamis', 'Jumaat', 'Sabtu']
-}
-
 var monthValues = {
-  short: ['Jan', 'Feb', 'Mac', 'Apr', 'Mei', 'Jun', 'Jul', 'Ogo', 'Sep', 'Okt', 'Nov', 'Dis'],
-  long: ['Januari', 'Februari', 'Mac', 'April', 'Mei', 'Jun', 'Julai', 'Ogos', 'September', 'Oktober', 'November', 'Disember']
+  narrow: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'O', 'S', 'O', 'N', 'D'],
+  abbreviated: [
+    'Jan',
+    'Feb',
+    'Mac',
+    'Apr',
+    'Mei',
+    'Jun',
+    'Jul',
+    'Ogo',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Dis'
+  ],
+  wide: [
+    'Januari',
+    'Februari',
+    'Mac',
+    'April',
+    'Mei',
+    'Jun',
+    'Julai',
+    'Ogos',
+    'September',
+    'Oktober',
+    'November',
+    'Disember'
+  ]
 }
 
-var timeOfDayValues = {
-  uppercase: ['AM', 'PM'],
-  lowercase: ['am', 'pm'],
-  long: ['a.m.', 'p.m.']
+var dayValues = {
+  narrow: ['A', 'I', 'S', 'R', 'K', 'J', 'S'],
+  short: ['Ahd', 'Isn', 'Sel', 'Rab', 'Kha', 'Jum', 'Sab'],
+  abbreviated: ['Ahd', 'Isn', 'Sel', 'Rab', 'Kha', 'Jum', 'Sab'],
+  wide: ['Ahad', 'Isnin', 'Selasa', 'Rabu', 'Khamis', 'Jumaat', 'Sabtu']
 }
 
-function ordinalNumber (dirtyNumber) {
+var dayPeriodValues = {
+  narrow: {
+    am: 'am',
+    pm: 'pm',
+    midnight: 'tgh malam',
+    noon: 'tgh hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  },
+  abbreviated: {
+    am: 'AM',
+    pm: 'PM',
+    midnight: 'tengah malam',
+    noon: 'tengah hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  },
+  wide: {
+    am: 'a.m.',
+    pm: 'p.m.',
+    midnight: 'tengah malam',
+    noon: 'tengah hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  }
+}
+var formattingDayPeriodValues = {
+  narrow: {
+    am: 'am',
+    pm: 'pm',
+    midnight: 'tengah malam',
+    noon: 'tengah hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  },
+  abbreviated: {
+    am: 'AM',
+    pm: 'PM',
+    midnight: 'tengah malam',
+    noon: 'tengah hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  },
+  wide: {
+    am: 'a.m.',
+    pm: 'p.m.',
+    midnight: 'tengah malam',
+    noon: 'tengah hari',
+    morning: 'pagi',
+    afternoon: 'tengah hari',
+    evening: 'petang',
+    night: 'malam'
+  }
+}
+
+function ordinalNumber(dirtyNumber, _dirtyOptions) {
   var number = Number(dirtyNumber)
 
+  // Can't use "pertama", "kedua" because can't be parsed
   switch (number) {
-    case 1:
-      return 'pertama'
-    case 2:
-      return 'kedua'
-    case 3:
-      return 'ketiga'
     default:
       return 'ke-' + number
   }
@@ -39,14 +134,36 @@ function ordinalNumber (dirtyNumber) {
 
 var localize = {
   ordinalNumber: ordinalNumber,
-  weekday: buildLocalizeFn(weekdayValues, 'long'),
-  weekdays: buildLocalizeArrayFn(weekdayValues, 'long'),
-  month: buildLocalizeFn(monthValues, 'long'),
-  months: buildLocalizeArrayFn(monthValues, 'long'),
-  timeOfDay: buildLocalizeFn(timeOfDayValues, 'long', function (hours) {
-    return (hours / 12) >= 1 ? 1 : 0
+
+  era: buildLocalizeFn({
+    values: eraValues,
+    defaultWidth: 'wide'
   }),
-  timesOfDay: buildLocalizeArrayFn(timeOfDayValues, 'long')
+
+  quarter: buildLocalizeFn({
+    values: quarterValues,
+    defaultWidth: 'wide',
+    argumentCallback: function(quarter) {
+      return Number(quarter) - 1
+    }
+  }),
+
+  month: buildLocalizeFn({
+    values: monthValues,
+    defaultWidth: 'wide'
+  }),
+
+  day: buildLocalizeFn({
+    values: dayValues,
+    defaultWidth: 'wide'
+  }),
+
+  dayPeriod: buildLocalizeFn({
+    values: dayPeriodValues,
+    defaultWidth: 'wide',
+    formattingValues: formattingDayPeriodValues,
+    defaultFormattingWidth: 'wide'
+  })
 }
 
 export default localize
