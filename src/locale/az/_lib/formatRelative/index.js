08babc3ab82fe8145d069967a987fe3c8f4f345c
Emil Baghirov <ebaghirov@kibrit.tech>
2019-11-21 17:35:25 +0400

    Add Azerbaijani language az

---
+++ src/locale/az/_lib/formatRelative/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+exports.default = formatRelative
+var formatRelativeLocale = {
+  lastWeek: "'sonuncu' eeee p -'də'",
+  yesterday: "'dünən' p -'də'",
+  today: "'bugün' p -'də'",
+  tomorrow: "'sabah' p -'də'",
+  nextWeek: "eeee p -'də'",
+  other: 'P'
+}
+
+function formatRelative(token, _date, _baseDate, _options) {
+  return formatRelativeLocale[token]
+}
+
+module.exports = exports.default
