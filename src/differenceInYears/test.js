74deeecaba9da94a4d66346eb8084b1f7b0f236d
Lee Powell <lee@leepowell.co.uk>
2020-03-03 20:14:11 +0000

    differenceInYears - Better test description for past leap days

--- src/differenceInYears/test.js
+++ src/differenceInYears/test.js
@@ -30,7 +30,7 @@ describe('differenceInYears', function() {
   })
 
   describe('leap days', function() {
-    it('supports past dates with right side larger', () => {
+    it('supports past dates with right side after leap day', () => {
       var result = differenceInYears(
         new Date(2004, 1 /* Feb */, 29, 0, 0),
         new Date(2002, 2 /* Mar */, 1, 0, 0)
@@ -38,7 +38,7 @@ describe('differenceInYears', function() {
       assert(result === 1)
     })
 
-    it('supports past dates with right side smaller', () => {
+    it('supports past dates with right side before leap day', () => {
       var result = differenceInYears(
         new Date(2004, 1 /* Feb */, 29, 0, 0),
         new Date(2002, 1 /* Feb */, 28, 0, 0)
