1be045b882b74c2c5953c9f683250df41f4dc7a0
Eugene Platov <eugene.platov@icloud.com>
2020-03-13 23:13:09 +0300

    Fixed `format` to work correctly with GMT-0752/GMT-0456 and similar timezones (#1666)

--- src/_lib/getTimezoneOffsetInMilliseconds/index.js
+++ src/_lib/getTimezoneOffsetInMilliseconds/index.js
@@ -1,5 +1,9 @@
 var MILLISECONDS_IN_MINUTE = 60000
 
+function getDateMillisecondsPart(date) {
+  return date.getTime() % MILLISECONDS_IN_MINUTE
+}
+
 /**
  * Google Chrome as of 67.0.3396.87 introduced timezones with offset that includes seconds.
  * They usually appear for dates that denote time before the timezones were introduced
@@ -15,7 +19,11 @@ export default function getTimezoneOffsetInMilliseconds(dirtyDate) {
   var date = new Date(dirtyDate.getTime())
   var baseTimezoneOffset = Math.ceil(date.getTimezoneOffset())
   date.setSeconds(0, 0)
-  var millisecondsPartOfTimezoneOffset = date.getTime() % MILLISECONDS_IN_MINUTE
+  var hasNegativeUTCOffset = baseTimezoneOffset > 0
+  var millisecondsPartOfTimezoneOffset = hasNegativeUTCOffset
+    ? (MILLISECONDS_IN_MINUTE + getDateMillisecondsPart(date)) %
+      MILLISECONDS_IN_MINUTE
+    : getDateMillisecondsPart(date)
 
   return (
     baseTimezoneOffset * MILLISECONDS_IN_MINUTE +
