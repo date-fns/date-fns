3ef76048014a34d37334594f62b855cd0e6058a8
Rushan Alyautdinov <akgondber@gmail.com>
2020-03-15 20:55:39 +0300

    Fix requiredArgs error message

--- src/_lib/requiredArgs/index.js
+++ src/_lib/requiredArgs/index.js
@@ -1,9 +1,12 @@
 export default function requiredArgs(required, args) {
   if (args.length < required) {
     throw new TypeError(
-      required + ' argument' + required > 1
-        ? 's'
-        : '' + ' required, but only ' + args.length + ' present'
+      required +
+        ' argument' +
+        (required > 1 ? 's' : '') +
+        ' required, but only ' +
+        args.length +
+        ' present'
     )
   }
 }
