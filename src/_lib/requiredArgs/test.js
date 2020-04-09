2663e92f22f176669c40816c0a3bcbc8c45e8536
akgondber <akgondber@gmail.com>
2020-04-09 18:04:44 +0300

    Fix requiredArgs error message (#1674) (closes #1673)

---
+++ src/_lib/requiredArgs/test.js
@@ -0,0 +1,37 @@
+// @flow
+/* eslint-env mocha */
+
+import assert from 'power-assert'
+import requiredArgs from '.'
+
+describe('requiredArgs', function() {
+  function wrapperFn(required) {
+    // $ExpectedMistake
+    return function() {
+      requiredArgs(required, arguments)
+    }
+  }
+  const twoArgsRequired = wrapperFn(2)
+
+  context('with correct number of passed arguments', function() {
+    it('does not throw an error', function() {
+      assert.doesNotThrow(() => twoArgsRequired(1, 2))
+    })
+  })
+
+  context('with wrong number of arguments', function() {
+    it('throws correct error message', function() {
+      assert.throws(
+        function() {
+          twoArgsRequired(1)
+        },
+        function(err) {
+          return (
+            err instanceof TypeError &&
+            err.message === '2 arguments required, but only 1 present'
+          )
+        }
+      )
+    })
+  })
+})
