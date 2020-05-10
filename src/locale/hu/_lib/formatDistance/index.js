897085b4022d1a0a37da0bafd4ed343e10887ea7
Jenei <jmate0321@gmail.com>
2020-05-10 18:40:58 +0200

    Fix hungarian locale in formatDistance

--- src/locale/hu/_lib/formatDistance/index.js
+++ src/locale/hu/_lib/formatDistance/index.js
@@ -11,49 +11,49 @@ function translate(number, addSuffix, key, comparison) {
     case 'xseconds':
       if (comparison === -1 && addSuffix) return num + ' másodperccel ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' másodperce'
-      if (comparison === 1) return num + ' másodperc múlva'
+      if (comparison === 1 && addSuffix) return num + ' másodperc múlva'
       return num + ' másodperc'
 
     case 'halfaminute':
       if (comparison === -1 && addSuffix) return 'fél perccel ezelőtt'
       if (comparison === -1 && !addSuffix) return 'fél perce'
-      if (comparison === 1) return 'fél perc múlva'
+      if (comparison === 1 && addSuffix) return 'fél perc múlva'
       return 'fél perc'
 
     case 'xminutes':
       if (comparison === -1 && addSuffix) return num + ' perccel ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' perce'
-      if (comparison === 1) return num + ' perc múlva'
+      if (comparison === 1 && addSuffix) return num + ' perc múlva'
       return num + ' perc'
 
     case 'xhours':
       if (comparison === -1 && addSuffix) return num + ' órával ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' órája'
-      if (comparison === 1) return num + ' óra múlva'
+      if (comparison === 1 && addSuffix) return num + ' óra múlva'
       return num + ' óra'
 
     case 'xdays':
       if (comparison === -1 && addSuffix) return num + ' nappal ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' napja'
-      if (comparison === 1) return num + ' nap múlva'
+      if (comparison === 1 && addSuffix) return num + ' nap múlva'
       return num + ' nap'
 
     case 'xweeks':
       if (comparison === -1 && addSuffix) return num + ' héttel ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' hete'
-      if (comparison === 1) return num + ' hét múlva'
+      if (comparison === 1 && addSuffix) return num + ' hét múlva'
       return num + ' hét'
 
     case 'xmonths':
       if (comparison === -1 && addSuffix) return num + ' hónappal ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' hónapja'
-      if (comparison === 1) return num + ' hónap múlva'
+      if (comparison === 1 && addSuffix) return num + ' hónap múlva'
       return num + ' hónap'
 
     case 'xyears':
       if (comparison === -1 && addSuffix) return num + ' évvel ezelőtt'
       if (comparison === -1 && !addSuffix) return num + ' éve'
-      if (comparison === 1) return num + ' év múlva'
+      if (comparison === 1 && addSuffix) return num + ' év múlva'
       return num + ' év'
   }
   return ''
