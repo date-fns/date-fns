8b268bf48168cbaef61b47dcc5217e2c4e6d7467
Sasha Koss <koss@nocorp.me>
2020-05-20 14:17:50 +0400

    Migrate parseISO

--- src/parseISO/index.ts
+++ src/parseISO/index.ts
@@ -1,19 +1,19 @@
-import toInteger from '../_lib/toInteger/index.js'
 import requiredArgs from '../_lib/requiredArgs/index.js'
+import { AdditionalDigitsOptions, AdditionaDigits } from 'src/types.js'
 
-var MILLISECONDS_IN_HOUR = 3600000
-var MILLISECONDS_IN_MINUTE = 60000
-var DEFAULT_ADDITIONAL_DIGITS = 2
+const MILLISECONDS_IN_HOUR = 3600000
+const MILLISECONDS_IN_MINUTE = 60000
+const DEFAULT_ADDITIONAL_DIGITS: AdditionaDigits = 2
 
-var patterns = {
+const patterns = {
   dateTimeDelimiter: /[T ]/,
   timeZoneDelimiter: /[Z ]/i,
-  timezone: /([Z+-].*)$/
+  timezone: /([Z+-].*)$/,
 }
 
-var dateRegex = /^-?(?:(\d{3})|(\d{2})(?:-?(\d{2}))?|W(\d{2})(?:-?(\d{1}))?|)$/
-var timeRegex = /^(\d{2}(?:[.,]\d*)?)(?::?(\d{2}(?:[.,]\d*)?))?(?::?(\d{2}(?:[.,]\d*)?))?$/
-var timezoneRegex = /^([+-])(\d{2})(?::?(\d{2}))?$/
+const dateRegex = /^-?(?:(\d{3})|(\d{2})(?:-?(\d{2}))?|W(\d{2})(?:-?(\d{1}))?|)$/
+const timeRegex = /^(\d{2}(?:[.,]\d*)?)(?::?(\d{2}(?:[.,]\d*)?))?(?::?(\d{2}(?:[.,]\d*)?))?$/
+const timezoneRegex = /^([+-])(\d{2})(?::?(\d{2}))?$/
 
 /**
  * @name parseISO
@@ -54,69 +54,49 @@ var timezoneRegex = /^([+-])(\d{2})(?::?(\d{2}))?$/
  * - `parseISO` now doesn't fall back to `new Date` constructor
  *   if it fails to parse a string argument. Instead, it returns `Invalid Date`.
  *
- * @param {String} argument - the value to convert
- * @param {Object} [options] - an object with options.
- * @param {0|1|2} [options.additionalDigits=2] - the additional number of digits in the extended year format
- * @returns {Date} the parsed date in the local time zone
- * @throws {TypeError} 1 argument required
- * @throws {RangeError} `options.additionalDigits` must be 0, 1 or 2
+ * @param argument - The value to convert
+ * @param options - The options object
+ * @returns The parsed date in the local time zone
  *
  * @example
  * // Convert string '2014-02-11T11:30:30' to date:
- * var result = parseISO('2014-02-11T11:30:30')
+ * const result = parseISO('2014-02-11T11:30:30')
  * //=> Tue Feb 11 2014 11:30:30
  *
  * @example
  * // Convert string '+02014101' to date,
  * // if the additional number of digits in the extended year format is 1:
- * var result = parseISO('+02014101', { additionalDigits: 1 })
+ * const result = parseISO('+02014101', { additionalDigits: 1 })
  * //=> Fri Apr 11 2014 00:00:00
  */
-export default function parseISO(argument, dirtyOptions) {
+export default function parseISO(
+  argument: string,
+  options?: AdditionalDigitsOptions
+) {
   requiredArgs(1, arguments)
 
-  var options = dirtyOptions || {}
+  const additionalDigits =
+    options?.additionalDigits ?? DEFAULT_ADDITIONAL_DIGITS
 
-  var additionalDigits =
-    options.additionalDigits == null
-      ? DEFAULT_ADDITIONAL_DIGITS
-      : toInteger(options.additionalDigits)
-  if (
-    additionalDigits !== 2 &&
-    additionalDigits !== 1 &&
-    additionalDigits !== 0
-  ) {
-    throw new RangeError('additionalDigits must be 0, 1 or 2')
-  }
+  const dateStrings = splitDateString(argument)
 
-  if (
-    !(
-      typeof argument === 'string' ||
-      Object.prototype.toString.call(argument) === '[object String]'
-    )
-  ) {
-    return new Date(NaN)
-  }
-
-  var dateStrings = splitDateString(argument)
-
-  var date
+  let date
   if (dateStrings.date) {
-    var parseYearResult = parseYear(dateStrings.date, additionalDigits)
+    const parseYearResult = parseYear(dateStrings.date, additionalDigits)
     date = parseDate(parseYearResult.restDateString, parseYearResult.year)
   }
 
-  if (isNaN(date) || !date) {
+  if (!date || isNaN(date.getTime())) {
     return new Date(NaN)
   }
 
-  var timestamp = date.getTime()
-  var time = 0
-  var offset
+  const timestamp = date.getTime()
+  let time: number | null = 0
+  let offset
 
   if (dateStrings.time) {
     time = parseTime(dateStrings.time)
-    if (isNaN(time) || time === null) {
+    if (time === null || isNaN(time)) {
       return new Date(NaN)
     }
   }
@@ -127,13 +107,13 @@ export default function parseISO(argument, dirtyOptions) {
       return new Date(NaN)
     }
   } else {
-    var dirtyDate = new Date(timestamp + time)
+    const dirtyDate = new Date(timestamp + time)
     // js parsed string assuming it's in UTC timezone
     // but we need it to be parsed in our timezone
     // so we use utc values to build date in our timezone.
     // Year values from 0 to 99 map to the years 1900 to 1999
     // so set year explicitly with setFullYear.
-    var result = new Date(
+    const result = new Date(
       dirtyDate.getUTCFullYear(),
       dirtyDate.getUTCMonth(),
       dirtyDate.getUTCDate(),
@@ -149,10 +129,16 @@ export default function parseISO(argument, dirtyOptions) {
   return new Date(timestamp + time + offset)
 }
 
-function splitDateString(dateString) {
-  var dateStrings = {}
-  var array = dateString.split(patterns.dateTimeDelimiter)
-  var timeString
+type DateStringValues = {
+  date?: null | string
+  time?: string
+  timezone?: string
+}
+
+function splitDateString(dateString: string) {
+  const dateStrings: DateStringValues = {}
+  const array = dateString.split(patterns.dateTimeDelimiter)
+  let timeString
 
   if (/:/.test(array[0])) {
     dateStrings.date = null
@@ -167,7 +153,7 @@ function splitDateString(dateString) {
   }
 
   if (timeString) {
-    var token = patterns.timezone.exec(timeString)
+    const token = patterns.timezone.exec(timeString)
     if (token) {
       dateStrings.time = timeString.replace(token[1], '')
       dateStrings.timezone = token[1]
@@ -179,8 +165,8 @@ function splitDateString(dateString) {
   return dateStrings
 }
 
-function parseYear(dateString, additionalDigits) {
-  var regex = new RegExp(
+function parseYear(dateString: string, additionalDigits: AdditionaDigits) {
+  const regex = new RegExp(
     '^(?:(\\d{4}|[+-]\\d{' +
       (4 + additionalDigits) +
       '})|(\\d{2}|[+-]\\d{' +
@@ -188,33 +174,33 @@ function parseYear(dateString, additionalDigits) {
       '})$)'
   )
 
-  var captures = dateString.match(regex)
+  const captures = dateString.match(regex)
   // Invalid ISO-formatted year
   if (!captures) return { year: null }
 
-  var year = captures[1] && parseInt(captures[1])
-  var century = captures[2] && parseInt(captures[2])
+  const year = captures[1] && parseInt(captures[1])
+  const century = captures[2] && parseInt(captures[2])
 
   return {
-    year: century == null ? year : century * 100,
-    restDateString: dateString.slice((captures[1] || captures[2]).length)
+    year: typeof century == 'number' ? century * 100 : year,
+    restDateString: dateString.slice((captures[1] || captures[2]).length),
   }
 }
 
-function parseDate(dateString, year) {
+function parseDate(dateString: string | undefined, year: number | null | '') {
   // Invalid ISO-formatted year
-  if (year === null) return null
+  if (typeof year !== 'number') return null
 
-  var captures = dateString.match(dateRegex)
+  const captures = dateString?.match(dateRegex)
   // Invalid ISO-formatted string
   if (!captures) return null
 
-  var isWeekDate = !!captures[4]
-  var dayOfYear = parseDateUnit(captures[1])
-  var month = parseDateUnit(captures[2]) - 1
-  var day = parseDateUnit(captures[3])
-  var week = parseDateUnit(captures[4])
-  var dayOfWeek = parseDateUnit(captures[5]) - 1
+  const isWeekDate = !!captures[4]
+  const dayOfYear = parseDateUnit(captures[1])
+  const month = parseDateUnit(captures[2]) - 1
+  const day = parseDateUnit(captures[3])
+  const week = parseDateUnit(captures[4])
+  const dayOfWeek = parseDateUnit(captures[5]) - 1
 
   if (isWeekDate) {
     if (!validateWeekDate(year, week, dayOfWeek)) {
@@ -222,7 +208,7 @@ function parseDate(dateString, year) {
     }
     return dayOfISOWeekYear(year, week, dayOfWeek)
   } else {
-    var date = new Date(0)
+    const date = new Date(0)
     if (
       !validateDate(year, month, day) ||
       !validateDayOfYearDate(year, dayOfYear)
@@ -234,17 +220,17 @@ function parseDate(dateString, year) {
   }
 }
 
-function parseDateUnit(value) {
-  return value ? parseInt(value) : 1
+function parseDateUnit(value: string | '') {
+  return (value && parseInt(value || '1')) || 0
 }
 
-function parseTime(timeString) {
-  var captures = timeString.match(timeRegex)
+function parseTime(timeString: string) {
+  const captures = timeString.match(timeRegex)
   if (!captures) return null // Invalid ISO-formatted time
 
-  var hours = parseTimeUnit(captures[1])
-  var minutes = parseTimeUnit(captures[2])
-  var seconds = parseTimeUnit(captures[3])
+  const hours = parseTimeUnit(captures[1])
+  const minutes = parseTimeUnit(captures[2])
+  const seconds = parseTimeUnit(captures[3])
 
   if (!validateTime(hours, minutes, seconds)) {
     return NaN
@@ -257,19 +243,19 @@ function parseTime(timeString) {
   )
 }
 
-function parseTimeUnit(value) {
+function parseTimeUnit(value: string | '') {
   return (value && parseFloat(value.replace(',', '.'))) || 0
 }
 
-function parseTimezone(timezoneString) {
+function parseTimezone(timezoneString: string) {
   if (timezoneString === 'Z') return 0
 
-  var captures = timezoneString.match(timezoneRegex)
+  const captures = timezoneString.match(timezoneRegex)
   if (!captures) return 0
 
-  var sign = captures[1] === '+' ? -1 : 1
-  var hours = parseInt(captures[2])
-  var minutes = (captures[3] && parseInt(captures[3])) || 0
+  const sign = captures[1] === '+' ? -1 : 1
+  const hours = parseInt(captures[2])
+  const minutes = (captures[3] && parseInt(captures[3])) || 0
 
   if (!validateTimezone(hours, minutes)) {
     return NaN
@@ -280,11 +266,11 @@ function parseTimezone(timezoneString) {
   )
 }
 
-function dayOfISOWeekYear(isoWeekYear, week, day) {
-  var date = new Date(0)
+function dayOfISOWeekYear(isoWeekYear: number, week: number, day: number) {
+  const date = new Date(0)
   date.setUTCFullYear(isoWeekYear, 0, 4)
-  var fourthOfJanuaryDay = date.getUTCDay() || 7
-  var diff = (week - 1) * 7 + day + 1 - fourthOfJanuaryDay
+  const fourthOfJanuaryDay = date.getUTCDay() || 7
+  const diff = (week - 1) * 7 + day + 1 - fourthOfJanuaryDay
   date.setUTCDate(date.getUTCDate() + diff)
   return date
 }
@@ -292,13 +278,13 @@ function dayOfISOWeekYear(isoWeekYear, week, day) {
 // Validation functions
 
 // February is null to handle the leap year (using ||)
-var daysInMonths = [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const daysInMonths = [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-function isLeapYearIndex(year): boolean {
+function isLeapYearIndex(year: number) {
   return year % 400 === 0 || (year % 4 === 0 && year % 100)
 }
 
-function validateDate(year, month, date) {
+function validateDate(year: number, month: number, date: number) {
   return (
     month >= 0 &&
     month <= 11 &&
@@ -307,15 +293,15 @@ function validateDate(year, month, date) {
   )
 }
 
-function validateDayOfYearDate(year, dayOfYear) {
+function validateDayOfYearDate(year: number, dayOfYear: number) {
   return dayOfYear >= 1 && dayOfYear <= (isLeapYearIndex(year) ? 366 : 365)
 }
 
-function validateWeekDate(_year, week, day) {
+function validateWeekDate(_year: number, week: number, day: number) {
   return week >= 1 && week <= 53 && day >= 0 && day <= 6
 }
 
-function validateTime(hours, minutes, seconds) {
+function validateTime(hours: number, minutes: number, seconds: number) {
   if (hours === 24) {
     return minutes === 0 && seconds === 0
   }
@@ -330,6 +316,6 @@ function validateTime(hours, minutes, seconds) {
   )
 }
 
-function validateTimezone(_hours, minutes) {
+function validateTimezone(_hours: number, minutes: number) {
   return minutes >= 0 && minutes <= 59
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,23 @@
+import Locale from './_types/Locale'
+
+export type AdditionaDigits = 0 | 1 | 2
+
+export interface AdditionalDigitsOptions {
+  additionalDigits?: AdditionaDigits
+}
+
+export interface LocaleOptions {
+  locale?: Locale
+}
+
+export type WeekStart = 0 | 1 | 2 | 3 | 4 | 5 | 6
+
+export interface WeekStartOptions {
+  weekStartsOn?: WeekStart
+}
+
+export type WeekFnOptions = LocaleOptions & WeekStartOptions
+
 export interface DateValues {
   year?: number
   month?: number
