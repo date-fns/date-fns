df2fe11974e948dbe3577ce9725a297269b4dd05
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2020-09-18 08:07:59 -0400

    Feature: migrate addQuarters to TS

--- src/addQuarters/index.ts
+++ src/addQuarters/index.ts
@@ -24,10 +24,13 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * var result = addQuarters(new Date(2014, 8, 1), 1)
  * //=> Mon Dec 01 2014 00:00:00
  */
-export default function addQuarters(dirtyDate, dirtyAmount) {
+export default function addQuarters(
+  dirtyDate: Date | number,
+  dirtyAmount: number
+): Date {
   requiredArgs(2, arguments)
 
-  var amount = toInteger(dirtyAmount)
-  var months = amount * 3
+  const amount = toInteger(dirtyAmount)
+  const months = amount * 3
   return addMonths(dirtyDate, months)
 }
