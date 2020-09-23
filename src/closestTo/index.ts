b16d2cd3c30b2e4132d3d91d683f4853094c3a6a
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2020-09-23 17:43:25 -0400

    Chore: migrate closestTo to TS

--- src/closestTo/index.ts
+++ src/closestTo/index.ts
@@ -23,25 +23,26 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @example
  * // Which date is closer to 6 September 2015: 1 January 2000 or 1 January 2030?
- * var dateToCompare = new Date(2015, 8, 6)
- * var result = closestTo(dateToCompare, [
+ * const dateToCompare = new Date(2015, 8, 6)
+ * const result = closestTo(dateToCompare, [
  *   new Date(2000, 0, 1),
  *   new Date(2030, 0, 1)
  * ])
  * //=> Tue Jan 01 2030 00:00:00
  */
-export default function closestTo(dirtyDateToCompare, dirtyDatesArray) {
+export default function closestTo(
+  dirtyDateToCompare: Date | number,
+  dirtyDatesArray: Date[] | number[]
+): Date {
   requiredArgs(2, arguments)
 
-  var dateToCompare = toDate(dirtyDateToCompare)
+  const dateToCompare = toDate(dirtyDateToCompare)
 
-  if (isNaN(dateToCompare)) {
-    return new Date(NaN)
-  }
+  if (isNaN(Number(dateToCompare))) return new Date(NaN)
 
-  var timeToCompare = dateToCompare.getTime()
+  const timeToCompare = dateToCompare.getTime()
 
-  var datesArray
+  let datesArray: unknown[]
   // `dirtyDatesArray` is undefined or null
   if (dirtyDatesArray == null) {
     datesArray = []
@@ -55,23 +56,23 @@ export default function closestTo(dirtyDateToCompare, dirtyDatesArray) {
     datesArray = Array.prototype.slice.call(dirtyDatesArray)
   }
 
-  var result
-  var minDistance
+  let result: unknown
+  let minDistance: unknown
   datesArray.forEach(function(dirtyDate) {
-    var currentDate = toDate(dirtyDate)
+    const currentDate = toDate(dirtyDate as number | Date)
 
-    if (isNaN(currentDate)) {
+    if (isNaN(Number(currentDate))) {
       result = new Date(NaN)
       minDistance = NaN
       return
     }
 
-    var distance = Math.abs(timeToCompare - currentDate.getTime())
-    if (result == null || distance < minDistance) {
+    const distance = Math.abs(timeToCompare - currentDate.getTime())
+    if (result == null || distance < Number(minDistance)) {
       result = currentDate
       minDistance = distance
     }
   })
 
-  return result
+  return result as Date
 }
--- src/closestTo/test.ts
+++ src/closestTo/test.ts
@@ -1,112 +1,121 @@
 // @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import closestTo from '.'
 
 describe('closestTo', function() {
   it('returns the date from the given array closest to the given date', function() {
-    var date = new Date(2014, 6 /* Jul */, 2)
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2)
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31),
       new Date(2012, 6 /* Jul */, 2)
     ])
-    assert.deepEqual(result, new Date(2015, 7 /* Aug */, 31))
+    assert.deepStrictEqual(result, new Date(2015, 7 /* Aug */, 31))
   })
 
   it('works if the closest date from the given array is before the given date', function() {
-    var date = new Date(2014, 6 /* Jul */, 2, 6, 30, 4, 500)
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2, 6, 30, 4, 500)
+    const result = closestTo(date, [
       new Date(2014, 6 /* Jul */, 2, 6, 30, 5, 900),
       new Date(2014, 6 /* Jul */, 2, 6, 30, 3, 900),
       new Date(2014, 6 /* Jul */, 2, 6, 30, 10)
     ])
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 2, 6, 30, 3, 900))
+    assert.deepStrictEqual(
+      result,
+      new Date(2014, 6 /* Jul */, 2, 6, 30, 3, 900)
+    )
   })
 
   it('accepts timestamps', function() {
-    var date = new Date(2014, 6 /* Jul */, 2).getTime()
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2).getTime()
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31).getTime(),
       new Date(2012, 6 /* Jul */, 2).getTime()
     ])
-    assert.deepEqual(result, new Date(2015, 7 /* Aug */, 31))
+    assert.deepStrictEqual(result, new Date(2015, 7 /* Aug */, 31))
   })
 
   it('returns undefined if the given array is empty', function() {
-    var date = new Date(2014, 6 /* Jul */, 2).getTime()
-    var result = closestTo(date, [])
+    const date = new Date(2014, 6 /* Jul */, 2).getTime()
+    const result = closestTo(date, [])
     assert(result == null)
   })
 
   it('returns `Invalid Date` if the given date is `Invalid Date`', function() {
-    var date = new Date(NaN)
-    var result = closestTo(date, [
+    const date = new Date(NaN)
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31),
       new Date(2012, 6 /* Jul */, 2)
     ])
-    assert(result instanceof Date && isNaN(result))
+
+    assert(result instanceof Date && isNaN(Number(result)))
   })
 
   it('returns `Invalid Date` if any date in the given array is `Invalid Date`', function() {
-    var date = new Date(2014, 6 /* Jul */, 2)
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2)
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31),
       new Date(NaN),
       new Date(2012, 6 /* Jul */, 2)
     ])
-    assert(result instanceof Date && isNaN(result))
+
+    assert(result instanceof Date && isNaN(Number(result)))
   })
 
   it('returns `Invalid Date` if any date in the given array is `Invalid Date`', function() {
-    var date = new Date(2014, 6 /* Jul */, 2)
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2)
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31),
       new Date(NaN),
       new Date(2012, 6 /* Jul */, 2)
     ])
-    assert(result instanceof Date && isNaN(result))
+
+    assert(result instanceof Date && isNaN(Number(result)))
   })
 
   it('returns `Invalid Date` if any value in the given array is undefined', function() {
-    var date = new Date(2014, 6 /* Jul */, 2)
-    var result = closestTo(date, [
+    const date = new Date(2014, 6 /* Jul */, 2)
+    const result = closestTo(date, [
       new Date(2015, 7 /* Aug */, 31),
-      // $ExpectedMistake
+      //@ts-expect-error
       undefined,
       new Date(2012, 6 /* Jul */, 2)
     ])
-    assert(result instanceof Date && isNaN(result))
+
+    assert(result instanceof Date && isNaN(Number(result)))
   })
 
   it('converts Array-like objects into Array', function() {
-    var date = new Date(2014, 6 /* Jul */, 2)
-    var object = {
+    const date = new Date(2014, 6 /* Jul */, 2)
+    const object = {
       '0': new Date(2015, 7 /* Aug */, 31),
       '1': new Date(2012, 6 /* Jul */, 2),
       length: 2
     }
-    // $ExpectedMistake
-    var result = closestTo(date, object)
-    assert.deepEqual(result, new Date(2015, 7 /* Aug */, 31))
+    //@ts-ignore
+    const result = closestTo(date, object)
+    assert.deepStrictEqual(result, new Date(2015, 7 /* Aug */, 31))
   })
 
   it('converts undefined into empty array', function() {
-    var date = new Date(2014, 6 /* Jul */, 2).getTime()
-    // $ExpectedMistake
-    var result = closestTo(date, undefined)
+    const date = new Date(2014, 6 /* Jul */, 2).getTime()
+    //@ts-expect-error
+    const result = closestTo(date, undefined)
     assert(result == null)
   })
 
   it('converts null into empty array', function() {
-    var date = new Date(2014, 6 /* Jul */, 2).getTime()
-    // $ExpectedMistake
-    var result = closestTo(date, null)
+    const date = new Date(2014, 6 /* Jul */, 2).getTime()
+    //@ts-expect-error
+    const result = closestTo(date, null)
     assert(result == null)
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
+    //@ts-expect-error
     assert.throws(closestTo.bind(null), TypeError)
+    //@ts-expect-error
     assert.throws(closestTo.bind(null, 1), TypeError)
   })
 })
