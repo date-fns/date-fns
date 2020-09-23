6ca8c56c0d01882c0c6c7482b3c19a29ad3d7976
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2020-09-23 18:27:16 -0400

    Chore: migrate closestTo Index to TS

---
+++ src/closestIndexTo/index.ts
@@ -0,0 +1,80 @@
+import toDate from '../toDate/index'
+import requiredArgs from '../_lib/requiredArgs/index'
+
+/**
+ * @name closestIndexTo
+ * @category Common Helpers
+ * @summary Return an index of the closest date from the array comparing to the given date.
+ *
+ * @description
+ * Return an index of the closest date from the array comparing to the given date.
+ *
+ * ### v2.0.0 breaking changes:
+ *
+ * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
+ *
+ * - Now, `closestIndexTo` doesn't throw an exception
+ *   when the second argument is not an array, and returns Invalid Date instead.
+ *
+ * @param {Date|Number} dateToCompare - the date to compare with
+ * @param {Date[]|Number[]} datesArray - the array to search
+ * @returns {Number} an index of the date closest to the given date
+ * @throws {TypeError} 2 arguments required
+ *
+ * @example
+ * // Which date is closer to 6 September 2015?
+ * const dateToCompare = new Date(2015, 8, 6)
+ * const datesArray = [
+ *   new Date(2015, 0, 1),
+ *   new Date(2016, 0, 1),
+ *   new Date(2017, 0, 1)
+ * ]
+ * const result = closestIndexTo(dateToCompare, datesArray)
+ * //=> 1
+ */
+export default function closestIndexTo(
+  dirtyDateToCompare: Date | number,
+  dirtyDatesArray: Date[] | number[]
+): number {
+  requiredArgs(2, arguments)
+
+  const dateToCompare = toDate(dirtyDateToCompare)
+
+  if (isNaN(Number(dateToCompare))) return NaN
+
+  const timeToCompare = dateToCompare.getTime()
+
+  let datesArray: unknown[]
+  // `dirtyDatesArray` is undefined or null
+  if (dirtyDatesArray == null) {
+    datesArray = []
+
+    // `dirtyDatesArray` is Array, Set or Map, or object with custom `forEach` method
+  } else if (typeof dirtyDatesArray.forEach === 'function') {
+    datesArray = dirtyDatesArray
+
+    // If `dirtyDatesArray` is Array-like Object, convert to Array. Otherwise, make it empty Array
+  } else {
+    datesArray = Array.prototype.slice.call(dirtyDatesArray)
+  }
+
+  let result: unknown
+  let minDistance: unknown
+  datesArray.forEach(function(dirtyDate, index) {
+    const currentDate = toDate(dirtyDate as number | Date)
+
+    if (isNaN(Number(currentDate))) {
+      result = NaN
+      minDistance = NaN
+      return
+    }
+
+    const distance = Math.abs(timeToCompare - currentDate.getTime())
+    if (result == null || distance < Number(minDistance)) {
+      result = index
+      minDistance = distance
+    }
+  })
+
+  return result as number
+}
