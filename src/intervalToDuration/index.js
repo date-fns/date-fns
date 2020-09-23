461d36ee5fe67b160b81b8ad01f64c1af1f89aec
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-09-23 17:19:07 -0300

    Feature: show weeks in intervalToDuration.
    Closes #1962

--- src/intervalToDuration/index.js
+++ src/intervalToDuration/index.js
@@ -1,6 +1,7 @@
 import compareAsc from '../compareAsc/index'
 import differenceInYears from '../differenceInYears/index'
 import differenceInMonths from '../differenceInMonths/index'
+import differenceInWeeks from '../differenceInWeeks/index'
 import differenceInDays from '../differenceInDays/index'
 import differenceInHours from '../differenceInHours/index'
 import differenceInMinutes from '../differenceInMinutes/index'
@@ -9,6 +10,7 @@ import isValid from '../isValid/index'
 import requiredArgs from '../_lib/requiredArgs/index'
 import toDate from '../toDate/index'
 import sub from '../sub/index'
+import { isBoolean } from 'lodash'
 
 /**
  * @name intervalToDuration
@@ -22,6 +24,7 @@ import sub from '../sub/index'
  *
  * @returns {Duration} The duration Object
  * @throws {TypeError} Requires 2 arguments
+ * @throws {TypeError} `showWeeks` must be boolean type
  * @throws {RangeError} `start` must not be Invalid Date
  * @throws {RangeError} `end` must not be Invalid Date
  *
@@ -32,9 +35,17 @@ import sub from '../sub/index'
  *   end: new Date(1968, 3, 4, 19, 5, 0)
  * })
  * // => { years: 39, months: 2, days: 20, hours: 7, minutes: 5, seconds: 0 }
+ *
+ * // Get the duration between January 15, 1929 and April 4, 1968 counting weeks.
+ * intervalToDuration({
+ *   start: new Date(1929, 0, 15, 12, 0, 0),
+ *   end: new Date(1968, 3, 4, 19, 5, 0),
+ *   showWeeks: true,
+ * })
+ * // => { years: 39, months: 2, weeks:2,  days: 6, hours: 7, minutes: 5, seconds: 0 }
  */
 
-export default function intervalToDuration({ start, end }) {
+export default function intervalToDuration({ start, end, showWeeks = false }) {
   requiredArgs(1, arguments)
 
   const dateLeft = toDate(start)
@@ -46,14 +57,30 @@ export default function intervalToDuration({ start, end }) {
   if (!isValid(dateRight)) {
     throw new RangeError('End Date is invalid')
   }
+  if (!isBoolean(showWeeks)) {
+    throw new TypeError('ShowWeeks is invalid')
+  }
 
-  const duration = {
-    years: 0,
-    months: 0,
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0
+  let duration
+  if (showWeeks) {
+    duration = {
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  } else {
+    duration = {
+      years: 0,
+      months: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
   }
 
   const sign = compareAsc(dateLeft, dateRight)
@@ -63,8 +90,19 @@ export default function intervalToDuration({ start, end }) {
   const remainingMonths = sub(dateLeft, { years: sign * duration.years })
   duration.months = Math.abs(differenceInMonths(remainingMonths, dateRight))
 
-  const remainingDays = sub(remainingMonths, { months: sign * duration.months })
-  duration.days = Math.abs(differenceInDays(remainingDays, dateRight))
+  let remainingDays
+
+  if (showWeeks) {
+    const remainingWeeks = sub(remainingMonths, {
+      months: sign * duration.months
+    })
+    duration.weeks = Math.abs(differenceInWeeks(remainingWeeks, dateRight))
+    remainingDays = sub(remainingWeeks, { weeks: sign * duration.weeks })
+    duration.days = Math.abs(differenceInDays(remainingDays, dateRight))
+  } else {
+    remainingDays = sub(remainingMonths, { months: sign * duration.months })
+    duration.days = Math.abs(differenceInDays(remainingDays, dateRight))
+  }
 
   const remainingHours = sub(remainingDays, { days: sign * duration.days })
   duration.hours = Math.abs(differenceInHours(remainingHours, dateRight))
--- src/intervalToDuration/test.js
+++ src/intervalToDuration/test.js
@@ -19,6 +19,22 @@ describe('intervalToDuration', function() {
       seconds: 0
     })
   })
+
+  it('Returns correct duration for arbitrary dates with weeks enabled', function() {
+    const start = new Date(1929, 0, 15, 12, 0, 0)
+    const end = new Date(1968, 3, 4, 19, 5, 0)
+    const result = intervalToDuration({ start, end, showWeeks: true })
+
+    assert.deepEqual(result, {
+      years: 39,
+      months: 2,
+      weeks: 2,
+      days: 6,
+      hours: 7,
+      minutes: 5,
+      seconds: 0
+    })
+  })
   it('Returns correct duration (1 of everything)', function() {
     const start = new Date(2020, 2, 1, 12, 0, 0)
     const end = new Date(2021, 3, 2, 13, 1, 1)
