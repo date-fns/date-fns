c6693c7ba58cb1174161ce2418ffad0333057f5e
Daniil Demidovich <daniil.demidovich@gmail.com>
2020-09-27 21:31:19 +0300

    Fix count reported data type for node 14

--- config/_lib/countReporter.js
+++ config/_lib/countReporter.js
@@ -2,18 +2,21 @@ var fs = require('fs')
 
 var countFilename = './tmp/tests_count.txt'
 
-function countReporter () {
-  this.onRunComplete = function (_, result) {
+function countReporter() {
+  this.onRunComplete = function(_, result) {
     var runCount = result.success
 
-    fs.readFile(countFilename, {encoding: 'utf-8', flag: 'a+'}, function (err, data) {
+    fs.readFile(countFilename, { encoding: 'utf-8', flag: 'a+' }, function(
+      err,
+      data
+    ) {
       if (err) {
         throw err
       }
 
       var totalCount = (parseInt(data, 10) || 0) + runCount
 
-      fs.writeFile(countFilename, totalCount, 'utf-8', function (err) {
+      fs.writeFile(countFilename, String(totalCount), 'utf-8', function(err) {
         if (err) {
           throw err
         }
