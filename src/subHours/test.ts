d18d02722398adb3899341379ca62bc0addbee59
Abner Soares Alves Junior <abner.alves@codeminer42.com>
2020-09-29 01:44:29 -0300

    migrate subHours to TS

--- src/subHours/test.ts
+++ src/subHours/test.ts
@@ -6,39 +6,42 @@ import subHours from '.'
 
 describe('subHours', function() {
   it('subtracts the given numbers of hours', function() {
-    var result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), 2)
+    const result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), 2)
     assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 23, 0))
   })
 
   it('accepts a timestamp', function() {
-    var result = subHours(new Date(2014, 6 /* Jul */, 12, 1, 0).getTime(), 26)
+    const result = subHours(new Date(2014, 6 /* Jul */, 12, 1, 0).getTime(), 26)
     assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 23, 0))
   })
 
   it('converts a fractional number to an integer', function() {
-    var result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), 2.22)
+    const result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), 2.22)
     assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 23, 0))
   })
 
   it('implicitly converts number arguments', function() {
     // $ExpectedMistake
-    var result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), '2')
+    // @ts-expect-error
+    const result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), '2')
     assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 23, 0))
   })
 
   it('does not mutate the original date', function() {
-    var date = new Date(2014, 6 /* Jul */, 10, 23, 0)
+    const date = new Date(2014, 6 /* Jul */, 10, 23, 0)
     subHours(date, 10)
     assert.deepEqual(date, new Date(2014, 6 /* Jul */, 10, 23, 0))
   })
 
   it('returns `Invalid Date` if the given date is invalid', function() {
-    var result = subHours(new Date(NaN), 2)
+    const result = subHours(new Date(NaN), 2)
+    // @ts-expect-error
     assert(result instanceof Date && isNaN(result))
   })
 
   it('returns `Invalid Date` if the given amount is NaN', function() {
-    var result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), NaN)
+    const result = subHours(new Date(2014, 6 /* Jul */, 11, 1, 0), NaN)
+    // @ts-expect-error
     assert(result instanceof Date && isNaN(result))
   })
 
