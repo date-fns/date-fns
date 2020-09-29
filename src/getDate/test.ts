468e78da06a9fad212f309d1e691204e06ef7f25
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-09-29 10:25:39 -0300

    Migration: migrating getDate, getDay, getDayOfYear, getDaysInMonth and getDaysInYear to TS.

--- src/getDate/test.ts
+++ src/getDate/test.ts
@@ -6,17 +6,17 @@ import getDate from '.'
 
 describe('getDate', function() {
   it('returns the day of the month of the given date', function() {
-    var result = getDate(new Date(2012, 1 /* Feb */, 29))
+    const result = getDate(new Date(2012, 1 /* Feb */, 29))
     assert(result === 29)
   })
 
   it('accepts a timestamp', function() {
-    var result = getDate(new Date(2014, 11 /* Dec */, 31).getTime())
+    const result = getDate(new Date(2014, 11 /* Dec */, 31).getTime())
     assert(result === 31)
   })
 
   it('returns NaN if the given date is invalid', function() {
-    var result = getDate(new Date(NaN))
+    const result = getDate(new Date(NaN))
     assert(isNaN(result))
   })
 
--- src/getDay/test.ts
+++ src/getDay/test.ts
@@ -6,17 +6,17 @@ import getDay from '.'
 
 describe('getDay', function() {
   it('returns the day of the week of the given date', function() {
-    var result = getDay(new Date(2012, 1 /* Feb */, 29))
+    const result = getDay(new Date(2012, 1 /* Feb */, 29))
     assert(result === 3)
   })
 
   it('accepts a timestamp', function() {
-    var result = getDay(new Date(2014, 5 /* Jun */, 1).getTime())
+    const result = getDay(new Date(2014, 5 /* Jun */, 1).getTime())
     assert(result === 0)
   })
 
   it('returns NaN if the given date is invalid', function() {
-    var result = getDay(new Date(NaN))
+    const result = getDay(new Date(NaN))
     assert(isNaN(result))
   })
 
