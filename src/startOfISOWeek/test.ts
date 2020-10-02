dab8b6e52efdf8bfd00f6bbbcefc66ac5cd0fe77
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-10-02 14:40:27 -0300

    Migrate to TS: startOf functions

--- src/startOfISOWeek/test.ts
+++ src/startOfISOWeek/test.ts
@@ -6,25 +6,26 @@ import startOfISOWeek from '.'
 
 describe('startOfISOWeek', function() {
   it('returns the date with the time set to 00:00:00 and the date set to the first day of an ISO week', function() {
-    var date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
-    var result = startOfISOWeek(date)
+    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    const result = startOfISOWeek(date)
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1))
   })
 
   it('accepts a timestamp', function() {
-    var date = new Date(2014, 1 /* Feb */, 11, 11, 55, 0).getTime()
-    var result = startOfISOWeek(date)
+    const date = new Date(2014, 1 /* Feb */, 11, 11, 55, 0).getTime()
+    const result = startOfISOWeek(date)
     assert.deepEqual(result, new Date(2014, 1 /* Feb */, 10))
   })
 
   it('does not mutate the original date', function() {
-    var date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
     startOfISOWeek(date)
     assert.deepEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0))
   })
 
   it('returns `Invalid Date` if the given date is invalid', function() {
-    var result = startOfISOWeek(new Date(NaN))
+    const result = startOfISOWeek(new Date(NaN))
+    //@ts-expect-error
     assert(result instanceof Date && isNaN(result))
   })
 
