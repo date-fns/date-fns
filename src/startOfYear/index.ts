55a0f5c8a88628fbd588d3a4d8c39a6fe9e00d75
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-10-16 17:05:03 -0300

    Add suport for start of year offset at startOfYear function

--- src/startOfYear/index.ts
+++ src/startOfYear/index.ts
@@ -1,3 +1,4 @@
+import { YearStart } from '../types'
 import toDate from '../toDate/index'
 import requiredArgs from '../_lib/requiredArgs/index'
 
@@ -15,6 +16,8 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param {Date|Number} date - the original date
+ * @param {Object} [options] - an object with options.
+ * @param {0|1|2|3|4|5|6|7|8|9|10|11} [options.yearStartsOn=0] - the index of the first month of the year (0 - January)
  * @returns {Date} the start of a year
  * @throws {TypeError} 1 argument required
  *
@@ -22,13 +25,19 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * // The start of a year for 2 September 2014 11:55:00:
  * const result = startOfYear(new Date(2014, 8, 2, 11, 55, 00))
  * //=> Wed Jan 01 2014 00:00:00
+ *
+ * // The start of a year for 2 September 2014 11:55:00:
+ * const result = startOfYear(new Date(2014, 8, 2, 11, 55, 00), { yearStartsOn: 3})
+ * //=> Tue Apr 01 2014 00:00:00
  */
-export default function startOfYear(dirtyDate: Date | number): Date {
+export default function startOfYear(dirtyDate: Date | number, options?: YearStart ): Date {
   requiredArgs(1, arguments)
 
   const cleanDate = toDate(dirtyDate)
   const date = new Date(0)
-  date.setFullYear(cleanDate.getFullYear(), 0, 1)
+
+  date.setFullYear(cleanDate.getFullYear(), options?.yearStartsOn ?? 0, 1)
+
   date.setHours(0, 0, 0, 0)
   return date
 }
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,7 @@ export interface Duration {
   minutes?: number
   seconds?: number
 }
+
+export interface YearStart {
+  yearStartsOn: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+}
