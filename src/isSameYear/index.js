97b541d075c28b5ed97d4f804636b1b61c62bede
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-10-27 10:50:59 -0300

    Add suport for start of year offset at isSameYear function

--- src/isSameYear/index.js
+++ src/isSameYear/index.js
@@ -1,5 +1,8 @@
+import { YearStart } from '../types'
 import toDate from '../toDate/index'
 import requiredArgs from '../_lib/requiredArgs/index'
+import startOfYear from '../startOfYear'
+import endOfYear from '../endOfYear'
 
 /**
  * @name isSameYear
@@ -13,20 +16,38 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
- * @param {Date|Number} dateLeft - the first date to check
- * @param {Date|Number} dateRight - the second date to check
+ * @param {Date|Number} date - the date to check
+ * @param {Date|Number} dateToCompare - the date to compare
+ * @param {Object} [options] - an object with options.
+ * @param {0|1|2|3|4|5|6|7|8|9|10|11} [options.yearStartsOn=0] - the index of the first month of the year (0 - January)
  * @returns {Boolean} the dates are in the same year
+ * @throws {Error} month must be after the start of year param
  * @throws {TypeError} 2 arguments required
  *
  * @example
  * // Are 2 September 2014 and 25 September 2014 in the same year?
  * var result = isSameYear(new Date(2014, 8, 2), new Date(2014, 8, 25))
  * //=> true
+ *
+ * // Are 2 September 2014 and 25 September 2014 in the same year?
+ * var result = isSameYear(new Date(2014, 8, 2), new Date(2014, 2, 25), { yearStartsOn: 3 })
+ * //=> false
  */
-export default function isSameYear(dirtyDateLeft, dirtyDateRight) {
+export default function isSameYear(
+  dirtyDate: Date | number,
+  dirtyDateToCompare: Date | number,
+  options?: YearStart
+): boolean {
   requiredArgs(2, arguments)
 
-  var dateLeft = toDate(dirtyDateLeft)
-  var dateRight = toDate(dirtyDateRight)
-  return dateLeft.getFullYear() === dateRight.getFullYear()
+  if (options && options?.yearStartsOn > toDate(dirtyDate).getMonth()) {
+    throw Error('Month must be after the start of year param')
+  }
+
+  const startTime = startOfYear(dirtyDate, options).getTime()
+  const endTime = endOfYear(dirtyDate, options).getTime()
+
+  const time = toDate(dirtyDateToCompare).getTime()
+
+  return time >= startTime && time <= endTime
 }
--- src/isSameYear/test.js
+++ src/isSameYear/test.js
@@ -6,23 +6,41 @@ import isSameYear from '.'
 
 describe('isSameYear', function() {
   it('returns true if the given dates have the same year', function() {
-    var result = isSameYear(
-      new Date(2014, 8 /* Sep */, 2),
-      new Date(2014, 8 /* Sep */, 25)
+    const result = isSameYear(
+      new Date(2014, 8 /* Sep */, 25),
+      new Date(2014, 8 /* Sep */, 2)
+    )
+    assert(result === true)
+  })
+
+  it('returns true if the earlier date is within an interval of 12 months of the later date', function() {
+    const result = isSameYear(
+      new Date(2014, 6 /* Jul */, 2),
+      new Date(2015, 2 /* Mar */, 25),
+      { yearStartsOn: 6 }
     )
     assert(result === true)
   })
 
   it('returns false if the given dates have different years', function() {
-    var result = isSameYear(
+    const result = isSameYear(
       new Date(2014, 8 /* Sep */, 2),
       new Date(2013, 8 /* Sep */, 25)
     )
     assert(result === false)
   })
 
+  it('returns false if the earlier date is not within an interval of 12 months of the later date', function() {
+    const result = isSameYear(
+      new Date(2014, 6 /* Jul */, 2),
+      new Date(2014, 2 /* Mar */, 25),
+      { yearStartsOn: 4 }
+    )
+    assert(result === false)
+  })
+
   it('accepts a timestamp', function() {
-    var result = isSameYear(
+    const result = isSameYear(
       new Date(2014, 8 /* Sep */, 2).getTime(),
       new Date(2014, 8 /* Sep */, 25).getTime()
     )
@@ -30,18 +48,29 @@ describe('isSameYear', function() {
   })
 
   it('returns false if the first date is `Invalid Date`', function() {
-    var result = isSameYear(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
+    const result = isSameYear(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
     assert(result === false)
   })
 
   it('returns false if the second date is `Invalid Date`', function() {
-    var result = isSameYear(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
+    const result = isSameYear(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
     assert(result === false)
   })
 
   it('returns false if the both dates are `Invalid Date`', function() {
-    var result = isSameYear(new Date(NaN), new Date(NaN))
+    const result = isSameYear(new Date(NaN), new Date(NaN))
     assert(result === false)
+  })
+  it('throws Error exception if the month of the first date is before the start of year', function() {
+    assert.throws(
+      () =>
+        isSameYear(
+          new Date(2014, 6 /* Jul */, 2),
+          new Date(2014, 11 /* Dec */, 25),
+          { yearStartsOn: 8 }
+        ),
+      Error
+    )
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
