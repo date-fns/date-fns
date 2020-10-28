7758ecc21ac5d1499b522062cf421c5df58e2535
Filip Š <filip.stamcar@hotmail.com>
2020-10-28 22:29:18 +0100

    Migrate isSameISOWeek to TypeScript

--- src/isSameISOWeek/test.ts
+++ src/isSameISOWeek/test.ts
@@ -6,7 +6,7 @@ import isSameISOWeek from '.'
 
 describe('isSameISOWeek', function() {
   it('returns true if the given dates have the same ISO week', function() {
-    var result = isSameISOWeek(
+    const result = isSameISOWeek(
       new Date(2014, 8 /* Sep */, 1),
       new Date(2014, 8 /* Sep */, 7)
     )
@@ -14,7 +14,7 @@ describe('isSameISOWeek', function() {
   })
 
   it('returns false if the given dates have different ISO weeks', function() {
-    var result = isSameISOWeek(
+    const result = isSameISOWeek(
       new Date(2014, 8 /* Sep */, 1),
       new Date(2014, 8 /* Sep */, 14)
     )
@@ -22,7 +22,7 @@ describe('isSameISOWeek', function() {
   })
 
   it('accepts a timestamp', function() {
-    var result = isSameISOWeek(
+    const result = isSameISOWeek(
       new Date(2014, 5 /* Jun */, 30).getTime(),
       new Date(2014, 6 /* Jul */, 2).getTime()
     )
@@ -30,17 +30,17 @@ describe('isSameISOWeek', function() {
   })
 
   it('returns false if the first date is `Invalid Date`', function() {
-    var result = isSameISOWeek(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
+    const result = isSameISOWeek(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
     assert(result === false)
   })
 
   it('returns false if the second date is `Invalid Date`', function() {
-    var result = isSameISOWeek(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
+    const result = isSameISOWeek(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
     assert(result === false)
   })
 
   it('returns false if the both dates are `Invalid Date`', function() {
-    var result = isSameISOWeek(new Date(NaN), new Date(NaN))
+    const result = isSameISOWeek(new Date(NaN), new Date(NaN))
     assert(result === false)
   })
 
