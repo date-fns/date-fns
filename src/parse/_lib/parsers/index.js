a1899a149a0f39ee10a5b9a567142e0bed49a67f
Vaclav Grohling <grohling.v@clav.cz>
2020-11-22 09:02:40 +0100

    chore: Remove unused numericPatterns

--- src/parse/_lib/parsers/index.js
+++ src/parse/_lib/parsers/index.js
@@ -22,16 +22,7 @@ var numericPatterns = {
   minute: /^[0-5]?\d/, // 0 to 59
   second: /^[0-5]?\d/, // 0 to 59
 
-  singleDigit: /^\d/, // 0 to 9
-  twoDigits: /^\d{1,2}/, // 0 to 99
-  threeDigits: /^\d{1,3}/, // 0 to 999
-  fourDigits: /^\d{1,4}/, // 0 to 9999
-
-  anyDigitsSigned: /^-?\d+/,
-  singleDigitSigned: /^-?\d/, // 0 to 9, -0 to -9
-  twoDigitsSigned: /^-?\d{1,2}/, // 0 to 99, -0 to -99
-  threeDigitsSigned: /^-?\d{1,3}/, // 0 to 999, -0 to -999
-  fourDigitsSigned: /^-?\d{1,4}/ // 0 to 9999, -0 to -9999
+  anyDigitsSigned: /^-?\d+/
 }
 
 var timezonePatterns = {
