7bab4e4045ec6ac62cf4cd36a8306f3e0345be3c
Sasha Koss <koss@nocorp.me>
2021-01-12 12:04:06 +0300

    Adjust build system
    - Fix locale types generation by Ignoring types.ts in the locales
    - Stop adding .js in FP generation (to enable Node.js ESM)

--- scripts/_lib/listLocales.js
+++ scripts/_lib/listLocales.js
@@ -1,18 +1,24 @@
 const path = require('path')
 const fs = require('fs')
 
-const ignoredFiles = ['index.js', 'test.js', 'index.js.flow', 'package.json']
+const ignoredFiles = [
+  'index.js',
+  'test.js',
+  'index.js.flow',
+  'package.json',
+  'types.ts',
+]
 
 module.exports = listLocales
 
 function listLocales() {
   const locales = fs.readdirSync(path.join(process.cwd(), 'src', 'locale'))
   return locales
-    .filter(file => /^[^._]/.test(file) && !ignoredFiles.includes(file))
-    .map(locale => ({
+    .filter((file) => /^[^._]/.test(file) && !ignoredFiles.includes(file))
+    .map((locale) => ({
       name: locale.replace(/-/g, ''),
       code: locale,
       path: `./${locale}`,
-      fullPath: `./src/locale/${locale}/index.js`
+      fullPath: `./src/locale/${locale}/index.js`,
     }))
 }
