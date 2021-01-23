11d2945e888c5483378a7fe742431f28e0c25d71
pop stefan <stefanpop999@gmail.com>
2021-01-23 18:56:50 +0200

    Updated typescript version to 4.1.2 for Intl API type definitions

--- src/intlFormatRelative/index.ts
+++ src/intlFormatRelative/index.ts
@@ -1,40 +1,13 @@
-type Numeric = 'always' | 'auto'
-type Style = 'long' | 'short' | 'narrow'
-type LocaleMatcher = 'lookup' | 'best fit'
-type Unit =
-  | 'year'
-  | 'quarter'
-  | 'month'
-  | 'week'
-  | 'day'
-  | 'hour'
-  | 'minute'
-  | 'second'
-
-declare namespace Intl {
-  class RelativeTimeFormat {
-    constructor(locale: string, listFormatOptions: ListFormatOptions)
-
-    format(value: number, unit: Unit): string
-  }
-}
-
-interface ListFormatOptions {
-  style: Style
-  numeric: Numeric
-  localeMatcher: LocaleMatcher
-}
-
 export default function intlFormatRelative(
   value: number,
-  unit: Unit,
-  locale: string = 'en',
-  rtfOptions: ListFormatOptions = {
+  unit: Intl.RelativeTimeFormatUnit,
+  locale: Intl.BCP47LanguageTag = 'en',
+  rtfOptions: Intl.RelativeTimeFormatOptions = {
     localeMatcher: 'best fit',
     numeric: 'always',
     style: 'long',
   }
-): string {
+) {
   // Create a relative time formatter in your locale
   // with default values explicitly passed in.
   const rtf = new Intl.RelativeTimeFormat(locale, {
