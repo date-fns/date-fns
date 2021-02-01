2760e9efbdcecea0b7b8c815caf7a833de81a2f5
Anastasiya Lopatina <stasialopatina@gmail.com>
2021-02-01 21:11:23 +0200

    fix jsdoc error

--- src/intlFormat/index.ts
+++ src/intlFormat/index.ts
@@ -1,17 +1,36 @@
 import requiredArgs from '../_lib/requiredArgs/index'
 
+type Locale = Intl.ResolvedDateTimeFormatOptions['locale']
+type FormatOptions = Intl.DateTimeFormatOptions
+type LocaleOptions = { locale: Locale | Locale[] }
+
+export default function intlFormat(date: Date): string
+export default function intlFormat(
+  date: Date,
+  localeOptions: LocaleOptions
+): string
+export default function intlFormat(
+  date: Date,
+  formatOptions: FormatOptions
+): string
+export default function intlFormat(
+  date: Date,
+  formatOptions: FormatOptions,
+  localeOptions: LocaleOptions
+): string
+
 /**
  * @name intlFormat
  * @category Common Helpers
  * @summary  Format the date with Intl.DateTimeFormat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat).
  *
  * @description
- * Return the formatted date string in the given format. 
+ * Return the formatted date string in the given format.
  * The method uses [`Intl.DateTimeFormat`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat) inside.
  * formatOptions are the same as [`Intl.DateTimeFormat` options](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#using_options)
- * 
+ *
  * > ⚠️ Please note that before Node version 13.0.0, only the locale data for en-US is available by default.
- * 
+ *
  * @param {Date|Number} argument - the original date.
  * @param {Object} [formatOptions] - an object with options.
  * @param {'lookup'|'best fit'} [formatOptions.localeMatcher='best fit'] - locale selection algorithm.
@@ -37,32 +56,32 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * // Represent 10 October 2019 in German.
  * // Convert the date with format's options and locale's options.
  * const result = intlFormat(new Date(2019, 9, 4, 12, 30, 13, 456), {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      }, {
-        locale: 'de-DE',
-    })
+ *      weekday: 'long',
+ *      year: 'numeric',
+ *      month: 'long',
+ *      day: 'numeric',
+ *    }, {
+ *      locale: 'de-DE',
+ *  })
  * //=> Freitag, 4. Oktober 2019
  *
  * @example
  * // Represent 10 October 2019.
  * // Convert the date with format's options.
  * const result = intlFormat.default(new Date(2019, 9, 4, 12, 30, 13, 456), {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-    })
+ *      year: 'numeric',
+ *      month: 'numeric',
+ *      day: 'numeric',
+ *      hour: 'numeric',
+ *  })
  * //=> 10/4/2019, 12 PM
  *
  * @example
  * // Represent 10 October 2019 in Korean.
  * // Convert the date with locale's options.
  * const result = intlFormat(new Date(2019, 9, 4, 12, 30, 13, 456), {
-        locale: 'ko-KR',
-    })
+ *      locale: 'ko-KR',
+ *  })
  * //=> 2019. 10. 4.
  *
  * @example
@@ -70,25 +89,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = intlFormat(new Date(2019, 9, 4, 12, 30, 13, 456))
  * //=> 10/4/2019
  */
-
-type Locale = Intl.ResolvedDateTimeFormatOptions['locale']
-type FormatOptions = Intl.DateTimeFormatOptions
-type LocaleOptions = { locale: Locale | Locale[] }
-
-export default function intlFormat(date: Date): string
-export default function intlFormat(
-  date: Date,
-  localeOptions: LocaleOptions
-): string
-export default function intlFormat(
-  date: Date,
-  formatOptions: FormatOptions
-): string
-export default function intlFormat(
-  date: Date,
-  formatOptions: FormatOptions,
-  localeOptions: LocaleOptions
-): string
 export default function intlFormat(
   date: Date,
   formatOrLocale?: FormatOptions | LocaleOptions,
