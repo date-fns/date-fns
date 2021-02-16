b8b609244da923bdc436bda74208e58449ea6565
Guy Roberts <guyroberts21@outlook.com>
2021-02-16 20:40:24 +0000

    Migrate to TS: differenceInHours

--- src/differenceInHours/test.ts
+++ src/differenceInHours/test.ts
@@ -1,12 +1,12 @@
 // @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import differenceInHours from '.'
 
 describe('differenceInHours', function() {
   it('returns the number of hours between the given dates', function() {
-    var result = differenceInHours(
+    const result = differenceInHours(
       new Date(2014, 6 /* Jul */, 2, 20, 0),
       new Date(2014, 6 /* Jul */, 2, 6, 0)
     )
@@ -14,7 +14,7 @@ describe('differenceInHours', function() {
   })
 
   it('returns a negative number if the time value of the first date is smaller', function() {
-    var result = differenceInHours(
+    const result = differenceInHours(
       new Date(2014, 6 /* Jul */, 2, 6, 0),
       new Date(2014, 6 /* Jul */, 2, 20, 0)
     )
@@ -22,7 +22,7 @@ describe('differenceInHours', function() {
   })
 
   it('accepts timestamps', function() {
-    var result = differenceInHours(
+    const result = differenceInHours(
       new Date(2014, 8 /* Sep */, 5, 18, 0).getTime(),
       new Date(2014, 8 /* Sep */, 5, 6, 0).getTime()
     )
@@ -31,7 +31,7 @@ describe('differenceInHours', function() {
 
   describe('edge cases', function() {
     it('the difference is less than an hour, but the given dates are in different calendar hours', function() {
-      var result = differenceInHours(
+      const result = differenceInHours(
         new Date(2014, 8 /* Sep */, 5, 12),
         new Date(2014, 8 /* Sep */, 5, 11, 59)
       )
@@ -39,7 +39,7 @@ describe('differenceInHours', function() {
     })
 
     it('the same for the swapped dates', function() {
-      var result = differenceInHours(
+      const result = differenceInHours(
         new Date(2014, 8 /* Sep */, 5, 11, 59),
         new Date(2014, 8 /* Sep */, 5, 12)
       )
@@ -47,7 +47,7 @@ describe('differenceInHours', function() {
     })
 
     it('the difference is an integral number of hours', function() {
-      var result = differenceInHours(
+      const result = differenceInHours(
         new Date(2014, 8 /* Sep */, 5, 13, 0),
         new Date(2014, 8 /* Sep */, 5, 12, 0)
       )
@@ -55,7 +55,7 @@ describe('differenceInHours', function() {
     })
 
     it('the given dates are the same', function() {
-      var result = differenceInHours(
+      const result = differenceInHours(
         new Date(2014, 8 /* Sep */, 5, 0, 0),
         new Date(2014, 8 /* Sep */, 5, 0, 0)
       )
@@ -63,22 +63,22 @@ describe('differenceInHours', function() {
     })
 
     it('does not return -0 when the given dates are the same', () => {
-      function isNegativeZero(x) {
+      function isNegativeZero(x: number): boolean {
         return x === 0 && 1 / x < 0
       }
 
-      var result = differenceInHours(
+      const result = differenceInHours(
         new Date(2014, 8 /* Sep */, 5, 0, 0),
         new Date(2014, 8 /* Sep */, 5, 0, 0)
       )
 
-      var resultIsNegative = isNegativeZero(result)
+      const resultIsNegative = isNegativeZero(result)
       assert(resultIsNegative === false)
     })
   })
 
   it('returns NaN if the first date is `Invalid Date`', function() {
-    var result = differenceInHours(
+    const result = differenceInHours(
       new Date(NaN),
       new Date(2017, 0 /* Jan */, 1)
     )
@@ -86,7 +86,7 @@ describe('differenceInHours', function() {
   })
 
   it('returns NaN if the second date is `Invalid Date`', function() {
-    var result = differenceInHours(
+    const result = differenceInHours(
       new Date(2017, 0 /* Jan */, 1),
       new Date(NaN)
     )
@@ -94,12 +94,14 @@ describe('differenceInHours', function() {
   })
 
   it('returns NaN if the both dates are `Invalid Date`', function() {
-    var result = differenceInHours(new Date(NaN), new Date(NaN))
+    const result = differenceInHours(new Date(NaN), new Date(NaN))
     assert(isNaN(result))
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
+    //@ts-expect-error
     assert.throws(differenceInHours.bind(null), TypeError)
+    //@ts-expect-error
     assert.throws(differenceInHours.bind(null, 1), TypeError)
   })
 })
