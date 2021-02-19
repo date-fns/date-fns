c6c3010229be84834e01f287fe5564ad780c2b59
Tetiana <ttobin@protonmail.ch>
2021-02-19 23:14:35 +0100

    Add nextTuesday()
    - Minor changes to the original PR
    - Test added

--- src/nextTuesday/index.ts
+++ src/nextTuesday/index.ts
@@ -1,5 +1,7 @@
+import requiredArgs from '../_lib/requiredArgs/index'
 import getDay from '../getDay'
 import addDays from '../addDays'
+import toDate from '../toDate/index'
 /**
  * @name nextTuesday
  * @category Weekday Helpers
@@ -12,29 +14,34 @@ import addDays from '../addDays'
  * @param {Date} date - the date to check
  * @returns {Date} the date is Tuesday
  * @throws {TypeError} 1 argument required
+ * @throws {TypeError} the date should be a valid date
  *
  * @example
  * When is the next Tuesday after Mar, 22, 2020?
- * var result = nextTuesday(new Date(2020, 2, 22))
+ * const result = nextTuesday(new Date(2020, 2, 22))
  * => new Date(2020, 2, 24)
  **/
-export default function nextTuesday(date: Date) {
-  switch (getDay(date)) {
+
+export default function nextTuesday(date: Date | number): Date {
+  requiredArgs(1, arguments)
+  const convertedDate = toDate(date)
+
+  switch (getDay(convertedDate)) {
     case 0:
-      return addDays(date, 2)
+      return addDays(convertedDate, 2)
     case 1:
-      return addDays(date, 1)
+      return addDays(convertedDate, 1)
     case 2:
-      return addDays(date, 7)
+      return addDays(convertedDate, 7)
     case 3:
-      return addDays(date, 6)
+      return addDays(convertedDate, 6)
     case 4:
-      return addDays(date, 5)
+      return addDays(convertedDate, 5)
     case 5:
-      return addDays(date, 4)
+      return addDays(convertedDate, 4)
     case 6:
-      return addDays(date, 3)
+      return addDays(convertedDate, 3)
     default:
-      return date
+      return convertedDate
   }
 }
