1aeec88deb288f286225b9900b917d90508d2771
Josh Kelly <josh@codecog.dev>
2021-02-22 21:51:17 +0000

    Relocate prettier and eslint configuration options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
   extends: ['eslint:recommended', 'prettier'],
+
+  ignorePatterns: ['node_modules', '/tmp', '/examples', '/dist'],
+
   parser: 'babel-eslint',
+
   env: {
     browser: true,
     node: true,
-    es6: true
+    es6: true,
   },
+
+  root: true,
+
   rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
-  }
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 }
