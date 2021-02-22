c5f333f63a07e82ac52db8df5990fdee6102932e
Maxim Topciu <maximtop@gmail.com>
2021-02-22 12:17:18 +0200

    move isAfter to ts

--- src/isAfter/index.ts
+++ src/isAfter/index.ts
@@ -13,8 +13,8 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
- * @param {Date|Number} date - the date that should be after the other one to return true
- * @param {Date|Number} dateToCompare - the date to compare with
+ * @param {Date|Number} dirtyDate - the date that should be after the other one to return true
+ * @param {Date|Number} dirtyDateToCompare - the date to compare with
  * @returns {Boolean} the first date is after the second date
  * @throws {TypeError} 2 arguments required
  *
@@ -23,10 +23,10 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * var result = isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))
  * //=> true
  */
-export default function isAfter(dirtyDate, dirtyDateToCompare) {
+export default function isAfter(dirtyDate: Date | number, dirtyDateToCompare: Date | number): boolean {
   requiredArgs(2, arguments)
 
-  var date = toDate(dirtyDate)
-  var dateToCompare = toDate(dirtyDateToCompare)
+  const date = toDate(dirtyDate)
+  const dateToCompare = toDate(dirtyDateToCompare)
   return date.getTime() > dateToCompare.getTime()
 }
