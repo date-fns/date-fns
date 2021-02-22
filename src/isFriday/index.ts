488023a7713157d81f39708d24aedfad5f337666
Maxim Topciu <maximtop@gmail.com>
2021-02-22 15:21:09 +0200

    revert js doc argument name

--- src/isFriday/index.ts
+++ src/isFriday/index.ts
@@ -13,7 +13,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
- * @param {Date|Number} dirtyDate - the date to check
+ * @param {Date|Number} date - the date to check
  * @returns {Boolean} the date is Friday
  * @throws {TypeError} 1 argument required
  *
