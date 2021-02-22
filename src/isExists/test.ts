848af3dbb1774525e7809328faa566889ed80448
Maxim Topciu <maximtop@gmail.com>
2021-02-22 17:29:46 +0300

    Move isExists to TS (#2225)

--- src/isExists/test.ts
+++ src/isExists/test.ts
@@ -6,12 +6,12 @@ import isExists from '.'
 
 describe('isValid', function() {
   it('returns true if the given date is valid', function() {
-    var result = isExists(2018, 0, 31)
+    const result = isExists(2018, 0, 31)
     assert(result === true)
   })
 
   it('returns false if the given date is invalid', function() {
-    var result = isExists(2018, 1 /* Feb */, 31)
+    const result = isExists(2018, 1 /* Feb */, 31)
     assert(result === false)
   })
 
