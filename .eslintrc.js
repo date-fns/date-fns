82de801fed8db223b77749222b4a25bbc9a0ae4e
Josh Kelly <josh@codecog.dev>
2021-02-22 22:34:09 +0000

    Make use of eslintrc `overrides` in place of `ts-eslintrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,49 @@
 module.exports = {
-  extends: ['eslint:recommended', 'prettier'],
-
-  ignorePatterns: ['node_modules', '/tmp', '/examples', '/dist'],
-
-  parser: 'babel-eslint',
-
   env: {
     browser: true,
     node: true,
     es6: true,
   },
 
-  root: true,
+  extends: ['eslint:recommended', 'prettier'],
 
-  rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-  },
+  ignorePatterns: ['.github', 'node_modules', 'tmp/', 'examples/', 'dist/'],
+
+  overrides: [
+    {
+      files: ['*.js'],
+      parser: 'babel-eslint',
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        // @typescript-eslint/eslint-plugin
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/class-name-casing': 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
+            multiline: {
+              delimiter: 'none',
+            },
+            singleline: {
+              delimiter: 'semi',
+            },
+          },
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-params': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/prefer-interface': 'off',
+      },
+    },
+  ],
+
+  root: true,
 }
