006439f5268a7d6e22c8dc0e1fe13a2d335e35df
Guy Roberts <guyroberts21@outlook.com>
2021-02-25 17:13:06 +0000

    Migrate max to TS

--- src/max/test.ts
+++ src/max/test.ts
@@ -5,12 +5,12 @@ import assert from 'power-assert'
 import max from '.'
 
 describe('max', function() {
-  function isInvalidDate(dirtyDate) {
-    return dirtyDate instanceof Date && isNaN(dirtyDate)
+  function isInvalidDate(dirtyDate: Date | number): boolean {
+    return dirtyDate instanceof Date && isNaN(Number(dirtyDate))
   }
 
   it('returns the latest date', function() {
-    var result = max([
+    const result = max([
       new Date(1989, 6 /* Jul */, 10),
       new Date(1987, 1 /* Feb */, 11)
     ])
@@ -18,7 +18,7 @@ describe('max', function() {
   })
 
   it('accepts array with more than 2 entries', function() {
-    var result = max([
+    const result = max([
       new Date(1987, 1 /* Feb */, 11),
       new Date(1989, 6 /* Jul */, 10),
       new Date(1995, 6 /* Jul */, 2),
@@ -28,7 +28,7 @@ describe('max', function() {
   })
 
   it('accepts timestamps', function() {
-    var result = max([
+    const result = max([
       new Date(1989, 6 /* Jul */, 10).getTime(),
       new Date(1987, 1 /* Feb */, 11).getTime()
     ])
@@ -36,7 +36,7 @@ describe('max', function() {
   })
 
   it('returns `Invalid Date` if any given date is invalid', function() {
-    var result = max([
+    const result = max([
       new Date(1989, 6 /* Jul */, 10),
       new Date(NaN),
       new Date(1987, 1 /* Feb */, 11)
@@ -45,9 +45,9 @@ describe('max', function() {
   })
 
   it('returns `Invalid Date` if any given value is undefined', function() {
-    var result = max([
+    const result = max([
       new Date(1989, 6 /* Jul */, 10),
-      // $ExpectedMistake
+      // @ts-expect-error
       undefined,
       new Date(1987, 1 /* Feb */, 11)
     ])
@@ -55,13 +55,13 @@ describe('max', function() {
   })
 
   it('returns `Invalid Date` for empty array', function() {
-    var result = max([])
+    const result = max([])
     assert(isInvalidDate(result))
   })
 
   it('converts Array-like objects into Array', function() {
-    // $ExpectedMistake
-    var result = max({
+    // @ts-expect-error
+    const result = max({
       '0': new Date(1989, 6 /* Jul */, 10),
       '1': new Date(1987, 1 /* Feb */, 11),
       length: 2
@@ -70,8 +70,8 @@ describe('max', function() {
   })
 
   it('converts iterable objects into Array', function() {
-    var result = max(
-      // $ExpectedMistake
+    const result = max(
+      // @ts-expect-error
       new Set([
         new Date(1989, 6 /* Jul */, 10),
         new Date(1987, 1 /* Feb */, 11)
@@ -81,8 +81,8 @@ describe('max', function() {
   })
 
   it('returns `Invalid Date` if given a non-iterable value', function() {
-    // $ExpectedMistake
-    var result = max(undefined)
+    // @ts-expect-error
+    const result = max(undefined)
     assert(isInvalidDate(result))
   })
 
