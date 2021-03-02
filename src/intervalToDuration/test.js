0e3604fcfa1325bbfbaaa661b987bbdb816dcd81
Tan75 <ttobin@protonmail.ch>
2021-03-02 11:38:28 +0000

    Add check for Feb 28 and 29 of the same year (#2256) (closes #2255)
    Fixed a bug in `differenceInMonths` and `intervalToDuration` that occurs when dealing with the 28th of February.

--- src/intervalToDuration/test.js
+++ src/intervalToDuration/test.js
@@ -4,8 +4,8 @@
 import assert from 'power-assert'
 import intervalToDuration from '.'
 
-describe('intervalToDuration', function() {
-  it('Returns correct duration for arbitrary dates', function() {
+describe('intervalToDuration', function () {
+  it('returns correct duration for arbitrary dates', function () {
     const start = new Date(1929, 0, 15, 12, 0, 0)
     const end = new Date(1968, 3, 4, 19, 5, 0)
     const result = intervalToDuration({ start, end })
@@ -16,10 +16,11 @@ describe('intervalToDuration', function() {
       days: 20,
       hours: 7,
       minutes: 5,
-      seconds: 0
+      seconds: 0,
     })
   })
-  it('Returns correct duration (1 of everything)', function() {
+
+  it('returns correct duration (1 of everything)', function () {
     const start = new Date(2020, 2, 1, 12, 0, 0)
     const end = new Date(2021, 3, 2, 13, 1, 1)
     const result = intervalToDuration({ start, end })
@@ -30,10 +31,11 @@ describe('intervalToDuration', function() {
       days: 1,
       hours: 1,
       minutes: 1,
-      seconds: 1
+      seconds: 1,
     })
   })
-  it('Returns duration of 0 when the dates are the same', function() {
+
+  it('returns duration of 0 when the dates are the same', function () {
     const start = new Date(2020, 2, 1, 12, 0, 0)
     const end = new Date(2020, 2, 1, 12, 0, 0)
     const result = intervalToDuration({ start, end })
@@ -44,7 +46,72 @@ describe('intervalToDuration', function() {
       days: 0,
       hours: 0,
       minutes: 0,
-      seconds: 0
+      seconds: 0,
+    })
+  })
+
+  describe('edge cases', function () {
+    it('returns correct duration for dates in the end of Feb - issue 2255', function () {
+      assert.deepEqual(
+        intervalToDuration({
+          start: new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
+          end: new Date(2012, 1 /* Feb */, 29, 10, 0, 0),
+        }),
+        {
+          years: 0,
+          months: 0,
+          days: 1,
+          hours: 1,
+          minutes: 0,
+          seconds: 0,
+        }
+      )
+
+      assert.deepEqual(
+        intervalToDuration({
+          start: new Date(2012, 1 /* Feb */, 29, 9, 0, 0),
+          end: new Date(2012, 1 /* Feb */, 29, 10, 0, 0),
+        }),
+        {
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 1,
+          minutes: 0,
+          seconds: 0,
+        }
+      )
+
+      assert.deepEqual(
+        intervalToDuration({
+          start: new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
+          end: new Date(2012, 1 /* Feb */, 28, 10, 0, 0),
+        }),
+        {
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 1,
+          minutes: 0,
+          seconds: 0,
+        }
+      )
+
+      // Issue 2261
+      assert.deepEqual(
+        intervalToDuration({
+          start: new Date(2021, 1 /* Feb */, 28, 7, 23, 7),
+          end: new Date(2021, 1 /* Feb */, 28, 7, 38, 18),
+        }),
+        {
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 15,
+          seconds: 11,
+        }
+      )
     })
   })
 })
