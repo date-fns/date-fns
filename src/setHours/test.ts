419f060398c4649f87ac1dcac92b35b846f60e96
Maxim Topciu <maximtop@gmail.com>
2021-03-04 22:31:27 +0200

    migrate set hours function

--- src/setHours/test.ts
+++ src/setHours/test.ts
@@ -6,23 +6,23 @@ import setHours from '.'
 
 describe('setHours', function() {
   it('sets the amount of hours', function() {
-    var result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4)
+    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4)
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
   })
 
   it('accepts a timestamp', function() {
-    var result = setHours(new Date(2014, 8 /* Sep */, 1, 11).getTime(), 5)
+    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11).getTime(), 5)
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 5))
   })
 
   it('converts a fractional number to an integer', function() {
-    var result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4.123)
+    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4.123)
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
   })
 
   it('implicitly converts number arguments', function() {
-    // $ExpectedMistake
-    var result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), '4')
+    // @ts-expect-error
+    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), '4')
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
   })
 
@@ -33,13 +33,13 @@ describe('setHours', function() {
   })
 
   it('returns `Invalid Date` if the given date is invalid', function() {
-    var result = setHours(new Date(NaN), 4)
-    assert(result instanceof Date && isNaN(result))
+    const result = setHours(new Date(NaN), 4)
+    assert(result instanceof Date && isNaN(result.getTime()))
   })
 
   it('returns `Invalid Date` if the given amount is NaN', function() {
-    var result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), NaN)
-    assert(result instanceof Date && isNaN(result))
+    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), NaN)
+    assert(result instanceof Date && isNaN(result.getTime()))
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
