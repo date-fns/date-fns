62eddc54bb5d0aa77f4c320d7636bb3d7f561fca
Maxim Topciu <maximtop@gmail.com>
2021-03-04 22:25:56 +0200

    migrate set day function

--- src/setDay/index.ts
+++ src/setDay/index.ts
@@ -2,6 +2,7 @@ import addDays from '../addDays/index'
 import toDate from '../toDate/index'
 import toInteger from '../_lib/toInteger/index'
 import requiredArgs from '../_lib/requiredArgs/index'
+import { LocalOptions, WeekStartOptions } from '../types';
 
 /**
  * @name setDay
@@ -34,36 +35,40 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * var result = setDay(new Date(2014, 8, 1), 0, { weekStartsOn: 1 })
  * //=> Sun Sep 07 2014 00:00:00
  */
-export default function setDay(dirtyDate, dirtyDay, dirtyOptions) {
+export default function setDay(
+    dirtyDate: Date | number,
+    dirtyDay: number,
+    dirtyOptions?: WeekStartOptions & LocalOptions
+): Date {
   requiredArgs(2, arguments)
 
-  var options = dirtyOptions || {}
-  var locale = options.locale
-  var localeWeekStartsOn =
-    locale && locale.options && locale.options.weekStartsOn
-  var defaultWeekStartsOn =
-    localeWeekStartsOn == null ? 0 : toInteger(localeWeekStartsOn)
-  var weekStartsOn =
-    options.weekStartsOn == null
-      ? defaultWeekStartsOn
-      : toInteger(options.weekStartsOn)
+  const options = dirtyOptions || {};
+  const locale = options.locale;
+  const localeWeekStartsOn =
+      locale && locale.options && locale.options.weekStartsOn;
+  const defaultWeekStartsOn =
+      localeWeekStartsOn == null ? 0 : toInteger(localeWeekStartsOn);
+  const weekStartsOn =
+      options.weekStartsOn == null
+          ? defaultWeekStartsOn
+          : toInteger(options.weekStartsOn);
 
   // Test if weekStartsOn is between 0 and 6 _and_ is not NaN
   if (!(weekStartsOn >= 0 && weekStartsOn <= 6)) {
     throw new RangeError('weekStartsOn must be between 0 and 6 inclusively')
   }
 
-  var date = toDate(dirtyDate, options)
-  var day = toInteger(dirtyDay)
-  var currentDay = date.getDay()
+  const date = toDate(dirtyDate);
+  const day = toInteger(dirtyDay);
+  const currentDay = date.getDay();
 
-  var remainder = day % 7
-  var dayIndex = (remainder + 7) % 7
+  const remainder = day % 7;
+  const dayIndex = (remainder + 7) % 7;
 
-  var delta = 7 - weekStartsOn
-  var diff =
-    day < 0 || day > 6
-      ? day - ((currentDay + delta) % 7)
-      : ((dayIndex + delta) % 7) - ((currentDay + delta) % 7)
-  return addDays(date, diff, options)
+  const delta = 7 - weekStartsOn;
+  const diff =
+      day < 0 || day > 6
+          ? day - ((currentDay + delta) % 7)
+          : ((dayIndex + delta) % 7) - ((currentDay + delta) % 7);
+  return addDays(date, diff)
 }
