8e749fbcfaf8a4cd766225562f4bfa067486691b
Maxim Topciu <maximtop@gmail.com>
2021-03-17 08:46:20 +0200

    Migrate setYear function to TS (#2288)

--- src/setYear/test.ts
+++ src/setYear/test.ts
@@ -6,23 +6,23 @@ import setYear from '.'
 
 describe('setYear', function() {
   it('sets the year', function() {
-    var result = setYear(new Date(2014, 8 /* Sep */, 1), 2013)
+    const result = setYear(new Date(2014, 8 /* Sep */, 1), 2013)
     assert.deepEqual(result, new Date(2013, 8 /* Sep */, 1))
   })
 
   it('accepts a timestamp', function() {
-    var result = setYear(new Date(2014, 8 /* Sep */, 1).getTime(), 2016)
+    const result = setYear(new Date(2014, 8 /* Sep */, 1).getTime(), 2016)
     assert.deepEqual(result, new Date(2016, 8 /* Sep */, 1))
   })
 
   it('converts a fractional number to an integer', function() {
-    var result = setYear(new Date(2014, 8 /* Sep */, 1), 2013.987654321)
+    const result = setYear(new Date(2014, 8 /* Sep */, 1), 2013.987654321)
     assert.deepEqual(result, new Date(2013, 8 /* Sep */, 1))
   })
 
   it('implicitly converts number arguments', function() {
-    // $ExpectedMistake
-    var result = setYear(new Date(2014, 8 /* Sep */, 1), '2013')
+    // @ts-expect-error
+    const result = setYear(new Date(2014, 8 /* Sep */, 1), '2013')
     assert.deepEqual(result, new Date(2013, 8 /* Sep */, 1))
   })
 
@@ -33,13 +33,13 @@ describe('setYear', function() {
   })
 
   it('returns `Invalid Date` if the given date is invalid', function() {
-    var result = setYear(new Date(NaN), 2013)
-    assert(result instanceof Date && isNaN(result))
+    const result = setYear(new Date(NaN), 2013)
+    assert(result instanceof Date && isNaN(result.getTime()))
   })
 
   it('returns `Invalid Date` if the given amount is NaN', function() {
-    var result = setYear(new Date(2014, 8 /* Sep */, 1), NaN)
-    assert(result instanceof Date && isNaN(result))
+    const result = setYear(new Date(2014, 8 /* Sep */, 1), NaN)
+    assert(result instanceof Date && isNaN(result.getTime()))
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
