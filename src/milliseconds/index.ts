dda247511d643ecc2707add63779736b239b0cf6
Mateusz Krzak <9784620+mateuszkrzak@users.noreply.github.com>
2021-03-18 03:08:13 +0100

    Fix typo in milliseconds summary (#2326)

--- src/milliseconds/index.ts
+++ src/milliseconds/index.ts
@@ -15,7 +15,7 @@ const yearInDays = 365.2425
  * > Leap year occures every 4 years, except for years that are divisable by 100 and not divisable by 400.
  * > 1 mean year = (365+1/4-1/100+1/400) days = 365.2425 days
  *
- * One month is a year devided by 12.
+ * One month is a year divided by 12.
  *
  * @description
  * Returns the number of milliseconds in the specified, years, months, weeks, days, hours, minutes and seconds.
