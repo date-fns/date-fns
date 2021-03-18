24e6e24586a11c53b7318210906bc3e75d7df79c
Maxim Topciu <maximtop@gmail.com>
2021-03-18 23:51:35 +0200

    Migrate isThursday function

--- src/isThursday/test.ts
+++ src/isThursday/test.ts
@@ -6,22 +6,22 @@ import isThursday from '.'
 
 describe('isThursday', function() {
   it('returns true if the given date is Thursday', function() {
-    var result = isThursday(new Date(2014, 8 /* Sep */, 25))
+    const result = isThursday(new Date(2014, 8 /* Sep */, 25))
     assert(result === true)
   })
 
   it('returns false if the given date is not Thursday', function() {
-    var result = isThursday(new Date(2014, 8 /* Sep */, 24))
+    const result = isThursday(new Date(2014, 8 /* Sep */, 24))
     assert(result === false)
   })
 
   it('accepts a timestamp', function() {
-    var result = isThursday(new Date(2014, 1 /* Feb */, 13).getTime())
+    const result = isThursday(new Date(2014, 1 /* Feb */, 13).getTime())
     assert(result === true)
   })
 
   it('returns false if the given date is `Invalid Date`', function() {
-    var result = isThursday(new Date(NaN))
+    const result = isThursday(new Date(NaN))
     assert(result === false)
   })
 
