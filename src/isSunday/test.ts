bd49a28220c7dea689b7edbed40ecf26f05816b5
Maxim Topciu <maximtop@gmail.com>
2021-03-21 06:20:07 +0200

    Migrate isSunday function

--- src/isSunday/test.ts
+++ src/isSunday/test.ts
@@ -6,22 +6,22 @@ import isSunday from '.'
 
 describe('isSunday', function() {
   it('returns true if the given date is Sunday', function() {
-    var result = isSunday(new Date(2014, 8 /* Sep */, 21))
+    const result = isSunday(new Date(2014, 8 /* Sep */, 21))
     assert(result === true)
   })
 
   it('returns false if the given date is not Sunday', function() {
-    var result = isSunday(new Date(2014, 8 /* Sep */, 25))
+    const result = isSunday(new Date(2014, 8 /* Sep */, 25))
     assert(result === false)
   })
 
   it('accepts a timestamp', function() {
-    var result = isSunday(new Date(2014, 1 /* Feb */, 9).getTime())
+    const result = isSunday(new Date(2014, 1 /* Feb */, 9).getTime())
     assert(result === true)
   })
 
   it('returns false if the given date is `Invalid Date`', function() {
-    var result = isSunday(new Date(NaN))
+    const result = isSunday(new Date(NaN))
     assert(result === false)
   })
 
