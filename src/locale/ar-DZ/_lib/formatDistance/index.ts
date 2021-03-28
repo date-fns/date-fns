d60031d4015402a716f972d1ce946e0b338c532a
Maxim Topciu <maximtop@gmail.com>
2021-03-28 12:15:36 +0300

    Migrate arDZ locale

--- src/locale/ar-DZ/_lib/formatDistance/index.ts
+++ src/locale/ar-DZ/_lib/formatDistance/index.ts
@@ -1,16 +1,18 @@
-var formatDistanceLocale = {
+import type { FormatDistanceFn } from '../../../types'
+
+const formatDistanceLocale = {
   lessThanXSeconds: {
     one: 'أقل من ثانية واحدة',
     two: 'أقل من ثانتين',
     threeToTen: 'أقل من {{count}} ثواني',
-    other: 'أقل من {{count}} ثانية'
+    other: 'أقل من {{count}} ثانية',
   },
 
   xSeconds: {
     one: 'ثانية واحدة',
     two: 'ثانتين',
     threeToTen: '{{count}} ثواني',
-    other: '{{count}} ثانية'
+    other: '{{count}} ثانية',
   },
 
   halfAMinute: 'نصف دقيقة',
@@ -19,112 +21,113 @@ var formatDistanceLocale = {
     one: 'أقل من دقيقة',
     two: 'أقل من دقيقتين',
     threeToTen: 'أقل من {{count}} دقائق',
-    other: 'أقل من {{count}} دقيقة'
+    other: 'أقل من {{count}} دقيقة',
   },
 
   xMinutes: {
     one: 'دقيقة واحدة',
     two: 'دقيقتين',
     threeToTen: '{{count}} دقائق',
-    other: '{{count}} دقيقة'
+    other: '{{count}} دقيقة',
   },
 
   aboutXHours: {
     one: 'ساعة واحدة تقريباً',
     two: 'ساعتين تقريباً',
     threeToTen: '{{count}} ساعات تقريباً',
-    other: '{{count}} ساعة تقريباً'
+    other: '{{count}} ساعة تقريباً',
   },
 
   xHours: {
     one: 'ساعة واحدة',
     two: 'ساعتين',
     threeToTen: '{{count}} ساعات',
-    other: '{{count}} ساعة'
+    other: '{{count}} ساعة',
   },
 
   xDays: {
     one: 'يوم واحد',
     two: 'يومين',
     threeToTen: '{{count}} أيام',
-    other: '{{count}} يوم'
+    other: '{{count}} يوم',
   },
 
   aboutXWeeks: {
     one: 'أسبوع واحد تقريباً',
     two: 'أسبوعين تقريباً',
     threeToTen: '{{count}} أسابيع تقريباً',
-    other: '{{count}} أسبوع تقريباً'
+    other: '{{count}} أسبوع تقريباً',
   },
 
   xWeeks: {
     one: 'أسبوع واحد',
     two: 'أسبوعين',
     threeToTen: '{{count}} أسابيع',
-    other: '{{count}} أسبوع'
+    other: '{{count}} أسبوع',
   },
 
   aboutXMonths: {
     one: 'شهر واحد تقريباً',
     two: 'شهرين تقريباً',
     threeToTen: '{{count}} أشهر تقريباً',
-    other: '{{count}} شهر تقريباً'
+    other: '{{count}} شهر تقريباً',
   },
 
   xMonths: {
     one: 'شهر واحد',
     two: 'شهرين',
     threeToTen: '{{count}} أشهر',
-    other: '{{count}} شهر'
+    other: '{{count}} شهر',
   },
 
   aboutXYears: {
     one: 'عام واحد تقريباً',
     two: 'عامين تقريباً',
     threeToTen: '{{count}} أعوام تقريباً',
-    other: '{{count}} عام تقريباً'
+    other: '{{count}} عام تقريباً',
   },
 
   xYears: {
     one: 'عام واحد',
     two: 'عامين',
     threeToTen: '{{count}} أعوام',
-    other: '{{count}} عام'
+    other: '{{count}} عام',
   },
 
   overXYears: {
     one: 'أكثر من عام',
     two: 'أكثر من عامين',
     threeToTen: 'أكثر من {{count}} أعوام',
-    other: 'أكثر من {{count}} عام'
+    other: 'أكثر من {{count}} عام',
   },
 
   almostXYears: {
     one: 'عام واحد تقريباً',
     two: 'عامين تقريباً',
     threeToTen: '{{count}} أعوام تقريباً',
-    other: '{{count}} عام تقريباً'
-  }
+    other: '{{count}} عام تقريباً',
+  },
 }
 
-export default function formatDistance(token, count, options) {
+const formatDistance: FormatDistanceFn = (token, count, options) => {
   options = options || {}
 
-  var result
-  if (typeof formatDistanceLocale[token] === 'string') {
-    result = formatDistanceLocale[token]
+  const usageGroup = formatDistanceLocale[token]
+  let result
+  if (typeof usageGroup === 'string') {
+    result = usageGroup
   } else if (count === 1) {
-    result = formatDistanceLocale[token].one
+    result = usageGroup.one
   } else if (count === 2) {
-    result = formatDistanceLocale[token].two
+    result = usageGroup.two
   } else if (count <= 10) {
-    result = formatDistanceLocale[token].threeToTen.replace('{{count}}', count)
+    result = usageGroup.threeToTen.replace('{{count}}', String(count))
   } else {
-    result = formatDistanceLocale[token].other.replace('{{count}}', count)
+    result = usageGroup.other.replace('{{count}}', String(count))
   }
 
   if (options.addSuffix) {
-    if (options.comparison > 0) {
+    if (options.comparison && options.comparison > 0) {
       return 'في خلال ' + result
     } else {
       return 'منذ ' + result
@@ -133,3 +136,5 @@ export default function formatDistance(token, count, options) {
 
   return result
 }
+
+export default formatDistance
