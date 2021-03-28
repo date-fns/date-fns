6ef04c46472a96916c4655db6d660d432797bfb1
Maxim Topciu <maximtop@gmail.com>
2021-03-28 12:29:12 +0300

    Migrate arMA locale

--- src/locale/ar-MA/_lib/formatLong/index.ts
+++ src/locale/ar-MA/_lib/formatLong/index.ts
@@ -1,41 +1,41 @@
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index'
 
-var dateFormats = {
+const dateFormats = {
   full: 'EEEE, MMMM do, y',
   long: 'MMMM do, y',
   medium: 'MMM d, y',
-  short: 'MM/dd/yyyy'
+  short: 'MM/dd/yyyy',
 }
 
-var timeFormats = {
+const timeFormats = {
   full: 'h:mm:ss a zzzz',
   long: 'h:mm:ss a z',
   medium: 'h:mm:ss a',
-  short: 'h:mm a'
+  short: 'h:mm a',
 }
 
-var dateTimeFormats = {
+const dateTimeFormats = {
   full: "{{date}} 'عند' {{time}}",
   long: "{{date}} 'عند' {{time}}",
   medium: '{{date}}, {{time}}',
-  short: '{{date}}, {{time}}'
+  short: '{{date}}, {{time}}',
 }
 
-var formatLong = {
+const formatLong = {
   date: buildFormatLongFn({
     formats: dateFormats,
-    defaultWidth: 'full'
+    defaultWidth: 'full',
   }),
 
   time: buildFormatLongFn({
     formats: timeFormats,
-    defaultWidth: 'full'
+    defaultWidth: 'full',
   }),
 
   dateTime: buildFormatLongFn({
     formats: dateTimeFormats,
-    defaultWidth: 'full'
-  })
+    defaultWidth: 'full',
+  }),
 }
 
 export default formatLong
