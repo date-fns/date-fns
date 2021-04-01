5df882d943a084b67ead5b91eef14c2c12f3ce7a
Sasha Koss <koss@nocorp.me>
2021-04-01 18:50:23 +0800

    Update src/isDate/index.ts

--- src/isDate/index.ts
+++ src/isDate/index.ts
@@ -36,7 +36,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = isDate({})
  * //=> false
  */
-export default function isDate(value: any): value is Date {
+export default function isDate(value: unknown): value is Date {
   requiredArgs(1, arguments)
 
   return (
