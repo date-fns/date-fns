65b79987ccb1ff3fb320d0b94bfaa1d915e4a433
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2021-04-10 15:01:56 +0530

    differenceInYears

--- src/differenceInYears/index.ts
+++ src/differenceInYears/index.ts
@@ -25,7 +25,10 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * var result = differenceInYears(new Date(2015, 1, 11), new Date(2013, 11, 31))
  * //=> 1
  */
-export default function differenceInYears(dirtyDateLeft, dirtyDateRight) {
+export default function differenceInYears(
+  dirtyDateLeft: Date | number,
+  dirtyDateRight: Date | number
+) {
   requiredArgs(2, arguments)
 
   var dateLeft = toDate(dirtyDateLeft)
@@ -36,13 +39,13 @@ export default function differenceInYears(dirtyDateLeft, dirtyDateRight) {
 
   // Set both dates to a valid leap year for accurate comparison when dealing
   // with leap days
-  dateLeft.setFullYear('1584')
-  dateRight.setFullYear('1584')
+  dateLeft.setFullYear(1584)
+  dateRight.setFullYear(1584)
 
   // Math.abs(diff in full years - diff in calendar years) === 1 if last calendar year is not full
   // If so, result must be decreased by 1 in absolute value
   var isLastYearNotFull = compareAsc(dateLeft, dateRight) === -sign
-  var result = sign * (difference - isLastYearNotFull)
+  var result = sign * (difference - Number(isLastYearNotFull))
   // Prevent negative zero
   return result === 0 ? 0 : result
 }
--- src/differenceInYears/test.ts
+++ src/differenceInYears/test.ts
@@ -105,7 +105,7 @@ describe('differenceInYears', function() {
     })
 
     it('does not return -0 when the given dates are the same', () => {
-      function isNegativeZero(x) {
+      function isNegativeZero(x: number) {
         return x === 0 && 1 / x < 0
       }
 
