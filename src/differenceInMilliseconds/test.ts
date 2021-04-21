b23f3daefa7bfb1c34182a768f281e178e9e1417
Maxim Topciu <maximtop@gmail.com>
2021-04-21 22:36:51 +0300

    Migrate differenceInMinutes function

--- src/differenceInMilliseconds/test.ts
+++ src/differenceInMilliseconds/test.ts
@@ -1,28 +1,27 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'power-assert'
 import differenceInMilliseconds from '.'
 
-describe('differenceInMilliseconds', function() {
-  it('returns the number of milliseconds between the given dates', function() {
-    var result = differenceInMilliseconds(
+describe('differenceInMilliseconds', function () {
+  it('returns the number of milliseconds between the given dates', function () {
+    const result = differenceInMilliseconds(
       new Date(2014, 6 /* Jul */, 2, 12, 30, 20, 700),
       new Date(2014, 6 /* Jul */, 2, 12, 30, 20, 600)
     )
     assert(result === 100)
   })
 
-  it('returns a negative number if the time value of the first date is smaller', function() {
-    var result = differenceInMilliseconds(
+  it('returns a negative number if the time value of the first date is smaller', function () {
+    const result = differenceInMilliseconds(
       new Date(2014, 6 /* Jul */, 2, 12, 30, 20, 600),
       new Date(2014, 6 /* Jul */, 2, 12, 30, 20, 700)
     )
     assert(result === -100)
   })
 
-  it('accepts timestamps', function() {
-    var result = differenceInMilliseconds(
+  it('accepts timestamps', function () {
+    const result = differenceInMilliseconds(
       new Date(2014, 8 /* Sep */, 5, 18, 30, 45, 500).getTime(),
       new Date(2014, 8 /* Sep */, 5, 18, 30, 45, 500).getTime()
     )
@@ -30,41 +29,41 @@ describe('differenceInMilliseconds', function() {
   })
 
   it('does not return -0 when the given dates are the same', () => {
-    function isNegativeZero(x) {
+    function isNegativeZero(x: number): boolean {
       return x === 0 && 1 / x < 0
     }
 
-    var result = differenceInMilliseconds(
+    const result = differenceInMilliseconds(
       new Date(2014, 8 /* Sep */, 5, 0, 0),
       new Date(2014, 8 /* Sep */, 5, 0, 0)
     )
 
-    var resultIsNegative = isNegativeZero(result)
+    const resultIsNegative = isNegativeZero(result)
     assert(resultIsNegative === false)
   })
 
-  it('returns NaN if the first date is `Invalid Date`', function() {
-    var result = differenceInMilliseconds(
+  it('returns NaN if the first date is `Invalid Date`', function () {
+    const result = differenceInMilliseconds(
       new Date(NaN),
       new Date(2017, 0 /* Jan */, 1)
     )
     assert(isNaN(result))
   })
 
-  it('returns NaN if the second date is `Invalid Date`', function() {
-    var result = differenceInMilliseconds(
+  it('returns NaN if the second date is `Invalid Date`', function () {
+    const result = differenceInMilliseconds(
       new Date(2017, 0 /* Jan */, 1),
       new Date(NaN)
     )
     assert(isNaN(result))
   })
 
-  it('returns NaN if the both dates are `Invalid Date`', function() {
-    var result = differenceInMilliseconds(new Date(NaN), new Date(NaN))
+  it('returns NaN if the both dates are `Invalid Date`', function () {
+    const result = differenceInMilliseconds(new Date(NaN), new Date(NaN))
     assert(isNaN(result))
   })
 
-  it('throws TypeError exception if passed less than 2 arguments', function() {
+  it('throws TypeError exception if passed less than 2 arguments', function () {
     assert.throws(differenceInMilliseconds.bind(null), TypeError)
     assert.throws(differenceInMilliseconds.bind(null, 1), TypeError)
   })
