1b3d5fab150a95d5bd5519c6e6245a0ce95a5277
Tetiana <ttobin@protonmail.ch>
2021-05-02 15:07:15 +0100

    Add minor changes, fix docs
    Review of millisecondsToHours

--- src/millisecondsToHours/index.ts
+++ src/millisecondsToHours/index.ts
@@ -1,11 +1,13 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function millisecondsToHours(milliseconds: number): number
 /**
  * @name millisecondsToHours
- * @category Common Helpers
+ * @category Conversion Helpers
  * @summary Convert milliseconds to hours.
  *
  * @description
- * Convert milliseconds number to hours numbers.
+ * Convert a number of milliseconds to a number of hours.
  *
  * @param { number } milliseconds - number of milliseconds to be converted.
  *
@@ -13,14 +15,13 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 720000 milliseconds to hours
+ * // Convert 720000 milliseconds to hours
  * const result = millisecondsToHours(7200000)
  * //=> 2
  */
-
 export default function millisecondsToHours(milliseconds: number): number {
-  requiredArgs(1, arguments);
+  requiredArgs(1, arguments)
   // milliseconds in 1 hour => 3600000;
-  const hours = milliseconds/3600000;
-  return Math.floor(hours);
+  const hours = milliseconds / 3600000
+  return Math.floor(hours)
 }
--- src/millisecondsToHours/test.ts
+++ src/millisecondsToHours/test.ts
@@ -1,27 +1,26 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'assert'
 import millisecondsToHours from '.'
 
-describe('millisecondsToHours', function() {
-  it('converts 3500000 milliseconds to hours', function() {
-    const result = millisecondsToHours(3500000);
+describe('millisecondsToHours', function () {
+  it('converts 3500000 milliseconds to hours', function () {
+    const result = millisecondsToHours(3500000)
     assert.deepStrictEqual(result, 0)
-  });
+  })
 
-  it('converts 7000000 milliseconds to hours', function() {
-    const result = millisecondsToHours(7000000);
+  it('converts 7000000 milliseconds to hours', function () {
+    const result = millisecondsToHours(7000000)
     assert.deepStrictEqual(result, 1)
-  });
+  })
 
-  it('converts 7600000 milliseconds to hours', function() {
-    const result = millisecondsToHours(7600000);
+  it('converts 7600000 milliseconds to hours', function () {
+    const result = millisecondsToHours(7600000)
     assert.deepStrictEqual(result, 2)
-  });
+  })
 
-  it('converts 18000000 milliseconds to hours', function() {
-    const result = millisecondsToHours(18000000);
+  it('converts 18000000 milliseconds to hours', function () {
+    const result = millisecondsToHours(18000000)
     assert.deepStrictEqual(result, 5)
-  });
-});
+  })
+})
