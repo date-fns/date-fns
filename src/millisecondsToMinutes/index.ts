95515980fe290dec13cf8611cfb9286320c5dfd3
Tetiana <ttobin@protonmail.ch>
2021-05-02 20:23:49 +0100

    Add minor changes, doc fix
    Review millisecondsToMinutes

--- src/millisecondsToMinutes/index.ts
+++ src/millisecondsToMinutes/index.ts
@@ -1,11 +1,14 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function millisecondsToMinutes(milliseconds: number): number
+
 /**
  * @name millisecondsToMinutes
- * @category Common Helpers
+ * @category Conversion Helpers
  * @summary Convert milliseconds to minutes.
  *
  * @description
- * //Converting milliseconds number to minutes numbers.
+ * Convert a number of milliseconds to a number of minutes.
  *
  * @param { number } milliseconds - number of milliseconds to be converted.
  *
@@ -13,14 +16,14 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 60000 milliseconds to minutes
+ * // Convert 60000 milliseconds to minutes
  * const result = millisecondsToMinutes(60000)
  * //=> 1
  */
 
 export default function millisecondsToMinutes(milliseconds: number): number {
-  requiredArgs(1, arguments);
-  // const milliseconds in 1 minute => 60000;
-  const minutes = milliseconds/60000;
-  return Math.floor(minutes);
+  requiredArgs(1, arguments)
+  // 60000 milliseconds in 1 minute
+  const minutes = milliseconds / 60000
+  return Math.floor(minutes)
 }
--- src/millisecondsToMinutes/test.ts
+++ src/millisecondsToMinutes/test.ts
@@ -1,27 +1,26 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'assert'
 import millisecondsToMinutes from '.'
 
-describe('millisecondsToMinutes', function() {
-  it('converts 50000 milliseconds to minutes', function() {
-    const result = millisecondsToMinutes(50000);
+describe('millisecondsToMinutes', function () {
+  it('converts 50000 milliseconds to minutes', function () {
+    const result = millisecondsToMinutes(50000)
     assert.deepStrictEqual(result, 0)
-  });
+  })
 
-  it('converts 65000 milliseconds to minutes', function() {
-    const result = millisecondsToMinutes(65000);
+  it('converts 65000 milliseconds to minutes', function () {
+    const result = millisecondsToMinutes(65000)
     assert.deepStrictEqual(result, 1)
-  });
+  })
 
-  it('converts 190000 milliseconds to minutes', function() {
-    const result = millisecondsToMinutes(190000);
+  it('converts 190000 milliseconds to minutes', function () {
+    const result = millisecondsToMinutes(190000)
     assert.deepStrictEqual(result, 3)
-  });
+  })
 
-  it('converts 310000 milliseconds to minutes', function() {
-    const result = millisecondsToMinutes(310000);
+  it('converts 310000 milliseconds to minutes', function () {
+    const result = millisecondsToMinutes(310000)
     assert.deepStrictEqual(result, 5)
-  });
-});
+  })
+})
