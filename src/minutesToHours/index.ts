98afdc166f018a938c78a3a247036d2db1d2a397
Tetiana <ttobin@protonmail.ch>
2021-05-02 20:33:40 +0100

    Add minor changes, fix docs
    Review of minuteToHours

--- src/minutesToHours/index.ts
+++ src/minutesToHours/index.ts
@@ -1,11 +1,14 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function minutesToHours(minutes: number): number
+
 /**
  * @name minutesToHours
- * @category Common Helpers
+ * @category Conversion Helpers
  * @summary Convert minutes to hours.
  *
  * @description
- * Convert minutes number to hours numbers.
+ * Convert a number of minutes to a number of hours.
  *
  * @param { number } minutes - number of minutes to be converted.
  *
@@ -13,13 +16,12 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 140 minutes to hours
+ * // Convert 140 minutes to hours
  * const result = minutesToHours(140)
  * //=> 2
  */
-
 export default function minutesToHours(minutes: number): number {
-  requiredArgs(1, arguments);
-  const hours = minutes/60;
-  return Math.floor(hours);
+  requiredArgs(1, arguments)
+  const hours = minutes / 60
+  return Math.floor(hours)
 }
--- src/minutesToHours/test.ts
+++ src/minutesToHours/test.ts
@@ -1,24 +1,26 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'assert'
 import minuteToHours from '.'
 
-describe('minuteToHours', function() {
-  it('converts 50 minutes to hours', function() {
-    const result = minuteToHours(50);
+describe('minuteToHours', function () {
+  it('converts 50 minutes to hours', function () {
+    const result = minuteToHours(50)
     assert.deepStrictEqual(result, 0)
-  });
-  it('converts 90 minutes to hours', function() {
-    const result = minuteToHours(90);
+  })
+
+  it('converts 90 minutes to hours', function () {
+    const result = minuteToHours(90)
     assert.deepStrictEqual(result, 1)
-  });
-  it('converts 140 minutes to hours', function() {
-    const result = minuteToHours(140);
+  })
+
+  it('converts 140 minutes to hours', function () {
+    const result = minuteToHours(140)
     assert.deepStrictEqual(result, 2)
-  });
-  it('converts 310 minutes to hours', function() {
-    const result = minuteToHours(310);
+  })
+
+  it('converts 310 minutes to hours', function () {
+    const result = minuteToHours(310)
     assert.deepStrictEqual(result, 5)
-  });
-});
+  })
+})
