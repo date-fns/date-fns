b4305a44509a70253a7652a557f74e25e6155a53
Tetiana <ttobin@protonmail.ch>
2021-05-02 21:53:12 +0100

    Add minor changes, fix docs
    Review yearsToMonths

--- src/yearsToMonths/index.ts
+++ src/yearsToMonths/index.ts
@@ -1,24 +1,26 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function yearsToMonths(years: number): number
+
 /**
  * @name yearsToMonths
- * @category Common Helpers
- * @summary Convert year to months.
+ * @category Conversion Helpers
+ * @summary Convert years to months.
  *
  * @description
- * Convert years number to months numbers.
+ * Convert a number of years to a number of months.
  *
- * @param { number } years - number of months to be converted.
+ * @param { number } years - number of years to be converted.
  *
  * @returns {number} the number of years converted in months
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 2 years into months
+ * // Convert 2 years into months
  * const result = yearsToMonths(2)
  * //=> 24
  */
-
 export default function yearsToMonths(years: number): number {
-  requiredArgs(1, arguments);
-  return years*12;
+  requiredArgs(1, arguments)
+  return years * 12
 }
--- src/yearsToMonths/test.ts
+++ src/yearsToMonths/test.ts
@@ -1,20 +1,21 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'assert'
 import yearsToMonths from '.'
 
-describe('yearsToMonths', function() {
-  it('converts 1 year to months', function() {
-    const result = yearsToMonths(1);
+describe('yearsToMonths', function () {
+  it('converts 1 year to months', function () {
+    const result = yearsToMonths(1)
     assert.deepStrictEqual(result, 12)
-  });
-  it('converts 3 years to months', function() {
-    const result = yearsToMonths(3);
+  })
+
+  it('converts 3 years to months', function () {
+    const result = yearsToMonths(3)
     assert.deepStrictEqual(result, 36)
-  });
-  it('converts 5 years to months', function() {
-    const result = yearsToMonths(5);
+  })
+
+  it('converts 5 years to months', function () {
+    const result = yearsToMonths(5)
     assert.deepStrictEqual(result, 60)
-  });
-});
+  })
+})
