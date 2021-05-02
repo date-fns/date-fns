03cbd906949f1ccff9f8191681869f327cf8673e
Tetiana <ttobin@protonmail.ch>
2021-05-02 21:33:57 +0100

    Add minor changes, fix docs

--- src/weeksToDays/index.ts
+++ src/weeksToDays/index.ts
@@ -1,11 +1,14 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function weeksToDays(weeks: number): number
+
 /**
  * @name weeksToDays
- * @category Common Helpers
+ * @category Conversion Helpers
  * @summary Convert weeks to days.
  *
  * @description
- * Convert weeks number to days numbers.
+ * Convert a number of weeks to a number of days.
  *
  * @param { number } weeks - number of days to be converted.
  *
@@ -13,12 +16,11 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 2 weeks into days
+ * // Convert 2 weeks into days
  * const result = weeksToDays(2)
  * //=> 14
  */
-
 export default function weeksToDays(weeks: number): number {
-  requiredArgs(1, arguments);
-  return weeks*7;
+  requiredArgs(1, arguments)
+  return weeks * 7
 }
--- src/weeksToDays/test.ts
+++ src/weeksToDays/test.ts
@@ -1,20 +1,21 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'assert'
 import weeksToDays from '.'
 
-describe('weeksToDays', function() {
-  it('converts 1 week to days', function() {
-    const result = weeksToDays(1);
+describe('weeksToDays', function () {
+  it('converts 1 week to days', function () {
+    const result = weeksToDays(1)
     assert.deepStrictEqual(result, 7)
-  });
-  it('converts 3 weeks to days', function() {
-    const result = weeksToDays(3);
+  })
+
+  it('converts 3 weeks to days', function () {
+    const result = weeksToDays(3)
     assert.deepStrictEqual(result, 21)
-  });
-  it('converts 5 weeks to days', function() {
-    const result = weeksToDays(5);
+  })
+
+  it('converts 5 weeks to days', function () {
+    const result = weeksToDays(5)
     assert.deepStrictEqual(result, 35)
-  });
-});
+  })
+})
