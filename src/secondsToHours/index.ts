31bd40ed441c12ebbd89f20bb47d70228b4a1002
Tetiana <ttobin@protonmail.ch>
2021-05-02 21:24:34 +0100

    Add minor changes, fix docs
    Review secondsToHours

--- src/secondsToHours/index.ts
+++ src/secondsToHours/index.ts
@@ -1,11 +1,14 @@
 import requiredArgs from '../_lib/requiredArgs/index'
+
+export default function secondsToHours(seconds: number): number
+
 /**
  * @name secondsToHours
- * @category Common Helpers
+ * @category Conversion Helpers
  * @summary Convert seconds to hours.
  *
  * @description
- * Convert seconds number to hours numbers.
+ * Convert a number of seconds to a number of hours.
  *
  * @param { number } seconds - number of seconds to be converted.
  *
@@ -13,13 +16,12 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @throws {TypeError} 1 argument required
  *
  * @example
- * //Convert 7200 seconds into hours
+ * // Convert 7200 seconds into hours
  * const result = secondsToHours(7200)
  * //=> 2
  */
-
 export default function secondsToHours(seconds: number): number {
-  requiredArgs(1, arguments);
-  const hours = seconds/3600;
-  return Math.floor(hours);
+  requiredArgs(1, arguments)
+  const hours = seconds / 3600
+  return Math.floor(hours)
 }
