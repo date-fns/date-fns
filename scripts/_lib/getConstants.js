2f9e2368dafd41da0dccd55402fd9bc4a1565a31
Tetiana <ttobin@protonmail.ch>
2021-05-24 09:05:39 +0100

    Rename index.js to index.ts

--- scripts/_lib/getConstants.js
+++ scripts/_lib/getConstants.js
@@ -6,8 +6,8 @@ module.exports = getConstants
 function getConstants() {
   return jsDocParser
     .getJsdocDataSync({
-      files: path.resolve(process.cwd(), 'src/constants/index.js'),
-      'no-cache': true
+      files: path.resolve(process.cwd(), 'src/constants/index.ts'),
+      'no-cache': true,
     })
-    .filter(c => c.kind === 'constant' && !c.undocumented)
+    .filter((c) => c.kind === 'constant' && !c.undocumented)
 }
