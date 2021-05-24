331b7adc37aac9ea034c9df870650ee64066f039
Francis Turmel <fturmel@gmail.com>
2021-05-24 09:54:12 -0400

    Fix for `typings` build process now that the contants file has been converted to typescript.

--- scripts/_lib/getConstants.js
+++ scripts/_lib/getConstants.js
@@ -8,6 +8,7 @@ function getConstants() {
     .getJsdocDataSync({
       files: path.resolve(process.cwd(), 'src/constants/index.ts'),
       'no-cache': true,
+      configure: path.resolve(process.cwd(), 'jsdoc2md.json'),
     })
     .filter((c) => c.kind === 'constant' && !c.undocumented)
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,15 +1,19 @@
 /**
- *  Maximum allowed time.
- *  @constant
- *  @type {number}
- *  @default
+ * Maximum allowed time.
+ *
+ * @name maxTime
+ * @constant
+ * @type {number}
+ * @default
  */
 export const maxTime = Math.pow(10, 8) * 24 * 60 * 60 * 1000
 
 /**
- *  Minimum allowed time.
- *  @constant
- *  @type {number}
- *  @default
+ * Minimum allowed time.
+ *
+ * @name minTime
+ * @constant
+ * @type {number}
+ * @default
  */
 export const minTime = -maxTime
