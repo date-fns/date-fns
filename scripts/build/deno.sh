b0c6a04a5bc7ac86ae82baff6a61ed0aee0b4aea
Sasha Koss <koss@nocorp.me>
2021-05-28 19:46:25 +0800

    Exclude snapshots from the deno script

--- scripts/build/deno.sh
+++ scripts/build/deno.sh
@@ -2,6 +2,6 @@
 
 # The script builds the Deno package.
 
-rsync --archive --prune-empty-dirs --relative --exclude={'*.flow','benchmark.*','test.*'} src/./ deno
+rsync --archive --prune-empty-dirs --relative --exclude={'*.flow','benchmark.*','test.*','snapshot.md'} src/./ deno
 cp {CHANGELOG.md,LICENSE.md,typings.d.ts} deno
 yarn ts-node scripts/build/_lib/addDenoExtensions.ts
