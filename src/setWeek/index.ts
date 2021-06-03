ea3f05069605388af0bb6a27f8cf4539f3d70db6
Sasha Koss <koss@nocorp.me>
2021-06-03 11:16:40 +0800

    Adjust setWeek to not use default argument value

--- src/setWeek/index.ts
+++ src/setWeek/index.ts
@@ -55,7 +55,7 @@ import {
 export default function setWeek(
   dirtyDate: Date | number,
   dirtyWeek: number,
-  options: LocaleOptions & WeekStartOptions & FirstWeekContainsDateOptions = {}
+  options: LocaleOptions & WeekStartOptions & FirstWeekContainsDateOptions
 ): Date {
   requiredArgs(2, arguments)
 
