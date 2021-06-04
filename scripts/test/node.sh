90923373527b7faf18b1de929363be9b42c317bc
Sasha Koss <koss@nocorp.me>
2021-06-04 12:17:03 +0800

    Drop support for Node.js 8 in tests

--- scripts/test/node.sh
+++ scripts/test/node.sh
@@ -10,7 +10,7 @@ export PATH="$(yarn bin):$PATH"
 curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/nvm.sh
 source ~/.nvm/nvm.sh
 
-for version in 8 9 10 11 12
+for version in 9 10 11 12
 do
   echo "Running tests using Node.js $version"
   nvm install $version
