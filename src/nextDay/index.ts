6fc3536b59b19609bc41f02b695a71d7d8cedbd3
Francis Turmel <fturmel@gmail.com>
2021-06-19 11:21:47 -0400

    Simpler and more performant implementation for nextDay function

--- src/nextDay/index.ts
+++ src/nextDay/index.ts
@@ -1,10 +1,8 @@
-import requiredArgs from '../_lib/requiredArgs/index'
-import getDay from '../getDay'
 import addDays from '../addDays'
+import getDay from '../getDay'
 import toDate from '../toDate'
 import { Day } from '../types'
-
-const baseMap = [7, 6, 5, 4, 3, 2, 1]
+import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name nextDay
@@ -23,24 +21,14 @@ const baseMap = [7, 6, 5, 4, 3, 2, 1]
  * // When is the next Monday after Mar, 20, 2020?
  * const result = nextDay(new Date(2020, 2, 20), 1)
  * //=> Mon Mar 23 2020 00:00:00
- *
- * @example
- * // When is the next Tuesday after Mar, 21, 2020?
- * const result = nextDay(new Date(2020, 2, 21), 2)
- * //=> Tue Mar 24 2020 00:00:00
  */
-export default function nextDay(date: Date | number, day: Day): Date {
+export default function nextDay(dirtyDate: Date | number, day: Day): Date {
   requiredArgs(2, arguments)
-  const map = genMap(day)
-  return addDays(toDate(date), map[getDay(toDate(date))])
-}
 
-function genMap(daysToMove: number): number[] {
-  if (daysToMove === 0) {
-    return baseMap
-  } else {
-    const mapStart = baseMap.slice(-daysToMove)
-    const mapEnd = baseMap.slice(0, baseMap.length - daysToMove)
-    return mapStart.concat(mapEnd)
-  }
+  const date = toDate(dirtyDate)
+
+  let delta = day - getDay(date)
+  if (delta <= 0) delta += 7
+
+  return addDays(date, delta)
 }
