f55b34136b18112eddb6493556c8c98858781df3
Vitor Ferreira <vitorfrs98@gmail.com>
2021-06-28 23:01:56 -0300

    feat: add new function nextBusinessDay

---
+++ src/nextBusinessDay/index.ts
@@ -0,0 +1,58 @@
+import { isWeekend, addDays, toDate, isEqual, startOfDay } from "..";
+
+/**
+ * @name nextBusinessDay
+ * @category Day Helpers
+ * @summary Returns the next business day
+ *
+ * @description Returns the next business day (Mon - Fri)
+ *
+ * @param {Date|Number} date - the initial date
+ * @param {Number} [startFromDay=0] - The day that it starts to count, the
+ * default is zero. So if the date is on a Monday it will return the current
+ * date. To start counting from the next date set this to one.
+ * @param {[Date]} excludeDates - An array of holidays or other dates that
+ * should not be identified as business days
+ *
+ * @returns {Date} the next business day
+ *
+ * @throws {TypeError} startFrom can't be a negative number
+ *
+ *
+ * @example
+ * // returns the next business date from  Sun July 4th, 2021
+ * const date = nextBusinessDay(new Date(2021, 6, 4));
+ * // => Mon Jul 4 2021 00:00:00
+ *
+ * // returns the next business day and skip the holidays
+ * especified on the excludeDates param
+ * const holidays = [new Date(2021, 8, 7)] // September 7h - Brazil Independence Day
+ * const date = nextBusinessDay(new Date(2021, 8, 6), 1, holidays)
+ * // => Wed Sep 8 2021 00:00:00
+ */
+
+export default function nextBusinessDay(
+  dirtyDate: Date,
+  startFromDay = 0,
+  excludeDates: Array<Date> = [],
+): Date {
+  const date = startOfDay(toDate(dirtyDate));
+
+  if (startFromDay < 0) throw new Error("startFrom can't be a negative number");
+
+  const initialDate = startFromDay ? addDays(date, startFromDay) : date;
+
+  const isOnTheExcludeDatesList = () => {
+    if (excludeDates.length === 0) return false;
+
+    const isPresent = excludeDates.reduce((_, current): boolean => {
+      return isEqual(initialDate, startOfDay(current));
+    }, false);
+
+    return isPresent;
+  };
+
+  if (!isWeekend(initialDate) && !isOnTheExcludeDatesList()) return initialDate;
+
+  return nextBusinessDay(addDays(initialDate, 1));
+}
---
+++ src/nextBusinessDay/test.ts
@@ -0,0 +1,50 @@
+/* eslint-env mocha */
+
+import assert from "power-assert";
+import nextBusinessDay from ".";
+
+describe("nextBusinessDay function", () => {
+  describe("when startFrom is zero and the current date is a bussiness day", () => {
+    it("returns the current date", () => {
+      const initial = new Date(2021, 6, 28); // Monday
+
+      assert.deepStrictEqual(nextBusinessDay(initial), initial);
+    });
+  });
+
+  describe("when startFrom is zero but the current date isn't a business day", () => {
+    it("returns the next business day", () => {
+      const initial = new Date(2021, 6, 3); // Saturday
+      const expected = new Date(2021, 6, 5); // Monday
+
+      assert.deepStrictEqual(nextBusinessDay(initial),expected);
+    });
+  });
+
+  describe("when startFrom is greater than zero", () => {
+    it("moves the initial date adding the number of days specified on the startFrom param", () => {
+      const initial = new Date(2021, 6, 2); // Friday
+      const expected = new Date(2021, 6, 5); // Monday
+
+      // Will start counting from June 4th, 2021
+      assert.deepStrictEqual(nextBusinessDay(initial, 2), expected);
+    });
+  });
+
+  it("doesn't accept negative numbers for the startFrom param", () => {
+    const initial = new Date(2021, 6, 2); // Friday
+    const errorMessage = "startFrom can't be a negative number";
+
+    assert.throws(() => nextBusinessDay(initial, -1), errorMessage)
+  });
+
+  it("skips the dates specified on the excludeDates array", () => {
+    const brazilianIndependenceDay = new Date(2021, 8, 7); // Tuesday
+    const holidays = [brazilianIndependenceDay];
+
+    assert.deepStrictEqual(
+      nextBusinessDay(new Date(2021, 8, 6), 1, holidays), 
+      new Date(2021, 8, 8)
+    );
+  });
+});
