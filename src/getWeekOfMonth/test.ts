7c226a6553ebf91ab6c8a24bbe2fc8f51e521713
Francis Turmel <fturmel@gmail.com>
2021-08-17 10:35:08 -0400

    convert getWeekOfMonth to TypeScript

--- src/getWeekOfMonth/test.ts
+++ src/getWeekOfMonth/test.ts
@@ -1,4 +1,3 @@
-// @flow
 /* eslint-env mocha */
 
 import assert from 'power-assert'
@@ -6,43 +5,43 @@ import getWeekOfMonth from '.'
 
 describe('getWeekOfMonth', function () {
   it('returns the week of the month of the given date', function () {
-    var result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 15))
+    const result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 15))
     assert(result === 3)
   })
 
   describe('edge cases', function () {
     describe('when the given day is the first of a month', function () {
       it('returns the week of the month of the given date', function () {
-        var result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 1))
+        const result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 1))
         assert(result === 1)
       })
     })
 
     describe('when the given day is the last of a month #1', function () {
       it('returns the week of the month of the given date', function () {
-        var result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 30))
+        const result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 30))
         assert(result === 5)
       })
     })
 
     describe('when the given day is the last of a month #2', function () {
       it('returns the week of the month of the given date', function () {
-        var result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 31))
+        const result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 31))
         assert(result === 5)
       })
     })
   })
 
   it('allows to specify which day is the first day of the week', function () {
-    var result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 1), {
+    const result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 1), {
       weekStartsOn: 1,
     })
     assert(result === 1)
   })
 
   it('allows to specify which day is the first day of the week in locale', function () {
-    var result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 31), {
-      // $ExpectedMistake
+    const result = getWeekOfMonth(new Date(2017, 9 /* Oct */, 31), {
+      // @ts-expect-error
       locale: {
         options: { weekStartsOn: 1 },
       },
@@ -51,9 +50,9 @@ describe('getWeekOfMonth', function () {
   })
 
   it('`options.weekStartsOn` overwrites the first day of the week specified in locale', function () {
-    var result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 13), {
+    const result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 13), {
       weekStartsOn: 1,
-      // $ExpectedMistake
+      // @ts-expect-error
       locale: {
         options: { weekStartsOn: 0 },
       },
@@ -62,12 +61,22 @@ describe('getWeekOfMonth', function () {
   })
 
   it('accepts a timestamp', function () {
-    var result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 1).getTime())
+    const result = getWeekOfMonth(new Date(2017, 10 /* Nov */, 1).getTime())
     assert(result === 1)
   })
 
+  it('throws RangeError exception if `weekStartsOn` is out of bound', function () {
+    assert.throws(
+      // @ts-expect-error
+      getWeekOfMonth.bind(null, new Date(2019, 4 /* May */, 5), {
+        weekStartsOn: 7,
+      }),
+      RangeError
+    )
+  })
+
   it('returns NaN if the given date is invalid', function () {
-    var result = getWeekOfMonth(new Date(NaN))
+    const result = getWeekOfMonth(new Date(NaN))
     assert(isNaN(result))
   })
 
@@ -84,7 +93,7 @@ describe('getWeekOfMonth', function () {
   })
 
   it('returns the week of the month of the given date, when the given date is sunday', function () {
-    var result = getWeekOfMonth(new Date(2019, 4 /* May */, 5), {
+    const result = getWeekOfMonth(new Date(2019, 4 /* May */, 5), {
       weekStartsOn: 1,
     })
     assert(result === 1)
