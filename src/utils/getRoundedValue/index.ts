60c4bde7814d3992870c3280b04d675afa518a6d
Tetiana <ttobin@protonmail.ch>
2021-08-20 15:18:55 +0100

    Delete getRoundedValue

--- src/utils/getRoundedValue/index.ts
+++
@@ -1,8 +0,0 @@
-export default function getRoundedValue(value: number): number {
-  if (value > 0) {
-    return Math.floor(value)
-  } else {
-    const result = Math.ceil(value)
-    return Object.is(result, -0) ? 0 : result
-  }
-}
