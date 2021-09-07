5e6bf09313188c93ef86563744483c59e7482eaa
Jonas L <jooola@users.noreply.github.com>
2021-09-07 07:46:57 +0200

    Migrate fromUnixTime function to TS (#2317)
    Co-authored-by: Tan75 <ttobin@protonmail.ch>

--- src/fromUnixTime/index.ts
+++ src/fromUnixTime/index.ts
@@ -8,25 +8,25 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @summary Create a date from a Unix timestamp.
  *
  * @description
- * Create a date from a Unix timestamp.
+ * Create a date from a Unix timestamp (in seconds). Decimal values will be discarded.
  *
  * ### v2.0.0 breaking changes:
  *
  * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
- * @param {Number} unixTime - the given Unix timestamp
+ * @param {Number} unixTime - the given Unix timestamp (in seconds)
  * @returns {Date} the date
  * @throws {TypeError} 1 argument required
  *
  * @example
  * // Create the date 29 February 2012 11:45:05:
- * var result = fromUnixTime(1330515905)
+ * const result = fromUnixTime(1330515905)
  * //=> Wed Feb 29 2012 11:45:05
  */
-export default function fromUnixTime(dirtyUnixTime) {
+export default function fromUnixTime(dirtyUnixTime: number): Date {
   requiredArgs(1, arguments)
 
-  var unixTime = toInteger(dirtyUnixTime)
+  const unixTime = toInteger(dirtyUnixTime)
 
   return toDate(unixTime * 1000)
 }
