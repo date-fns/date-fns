5cffe310080b7fc7954fe7f4584e016737ca39e1
Jonas L <jooola@users.noreply.github.com>
2021-09-07 07:50:11 +0200

    Migrate formatISODuration function to TS (#2312)
    Co-authored-by: Tan75 <ttobin@protonmail.ch>

--- src/formatISODuration/index.ts
+++ src/formatISODuration/index.ts
@@ -16,7 +16,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @example
  * // Format the given duration as ISO 8601 string
- * formatISODuration({
+ * const result = formatISODuration({
  *   years: 39,
  *   months: 2,
  *   days: 20,
@@ -26,8 +26,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * })
  * //=> 'P39Y2M20DT0H0M0S'
  */
-
-export default function formatISODuration(duration) {
+export default function formatISODuration(duration: Duration): string {
   requiredArgs(1, arguments)
 
   if (typeof duration !== 'object')
@@ -39,7 +38,7 @@ export default function formatISODuration(duration) {
     days = 0,
     hours = 0,
     minutes = 0,
-    seconds = 0
+    seconds = 0,
   } = duration
 
   return `P${years}Y${months}M${days}DT${hours}H${minutes}M${seconds}S`
