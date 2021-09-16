85e8754226d9c550a77409ced1faf5cc8c70c1b9
Sasha Koss <koss@nocorp.me>
2021-09-16 13:11:15 +0800

    Update src/differenceInMinutes/index.ts

--- src/differenceInMinutes/index.ts
+++ src/differenceInMinutes/index.ts
@@ -21,7 +21,7 @@ import {
  *
  * @param {Date|Number} dateLeft - the later date
  * @param {Date|Number} dateRight - the earlier date
- * @param {RoundingMethod} roundingMethod - the rounding method (`ceil`, `floor`, `round` or `trunc`) with `trunc` by default.
+ * @param {String} roundingMethod - the rounding method (`ceil`, `floor`, `round` or `trunc`) with `trunc` by default.
  * @returns {Number} the number of minutes
  * @throws {TypeError} 2 arguments required
  *
