43183234b244e3672155ddfda6e11608c1374987
Eri Hiramatsu <eri@tutteo.com>
2021-09-28 16:49:29 +0200

    feat: add locale ja-Hira
    Implements date-fns/date-fns#2625

---
+++ src/locale/ja-Hira/_lib/formatRelative/index.js
@@ -0,0 +1,12 @@
+var formatRelativeLocale = {
+  lastWeek: 'せんしゅうのeeeeのp',
+  yesterday: 'きのうのp',
+  today: 'きょうのp',
+  tomorrow: 'あしたのp',
+  nextWeek: 'よくしゅうのeeeeのp',
+  other: 'P'
+}
+
+export default function formatRelative(token, _date, _baseDate, _options) {
+  return formatRelativeLocale[token]
+}
