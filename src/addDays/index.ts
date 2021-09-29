fedfd6fb20a2a814e63d84d25e35ae7d14d8c6a0
Sasha Koss <koss@nocorp.me>
2021-09-29 16:45:18 +0800

    Prepare addDays for v3, remove redundant tests

--- src/addDays/index.ts
+++ src/addDays/index.ts
@@ -1,7 +1,3 @@
-import toInteger from '../_lib/toInteger/index'
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name addDays
  * @category Day Helpers
@@ -10,35 +6,21 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Add the specified number of days to the given date.
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the date to be changed
  * @param amount - the amount of days to be added. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
- * @returns - the new date with the days added
- * @throws {TypeError} - 2 arguments required
+ * @returns the new date with the days added
  *
  * @example
  * // Add 10 days to 1 September 2014:
  * const result = addDays(new Date(2014, 8, 1), 10)
  * //=> Thu Sep 11 2014 00:00:00
  */
-export default function addDays(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-): Date {
-  requiredArgs(2, arguments)
+export default function addDays(date: Date | number, amount: number): Date {
+  const result = new Date(date)
 
-  const date = toDate(dirtyDate)
-  const amount = toInteger(dirtyAmount)
-  if (isNaN(amount)) {
-    return new Date(NaN)
-  }
-  if (!amount) {
-    // If 0 days, no-op to avoid changing times in the hour before end of DST
-    return date
-  }
-  date.setDate(date.getDate() + amount)
-  return date
+  // If 0 days, no-op to avoid changing times in the hour before end of DST
+  if (!amount) return result
+
+  result.setDate(result.getDate() + amount)
+  return result
 }
