4a94edbcf88fbe9c72d994b67032db42c45b887c
Sasha Koss <koss@nocorp.me>
2021-09-29 16:33:41 +0800

    Fix FP tests

--- src/fp/formatDistanceStrictWithOptions/index.js
+++
@@ -1,8 +0,0 @@
-// This file is generated automatically by `scripts/build/fp.js`. Please, don't change it.
-
-import fn from '../../formatDistanceStrict/index'
-import convertToFP from '../_lib/convertToFP/index'
-
-var formatDistanceStrictWithOptions = convertToFP(fn, 3)
-
-export default formatDistanceStrictWithOptions
