584205fe46b91c2aa951ce2e8d95ba9c066eb3b5
Sasha Koss <koss@nocorp.me>
2021-09-29 15:23:37 +0800

    Remove benchmark scripts

--- src/endOfYesterday/benchmark.js
+++
@@ -1,9 +0,0 @@
-// @flow
-/* eslint-env mocha */
-/* global suite, benchmark */
-
-import endOfYesterday from '.'
-
-suite('endOfYesterday', () => {
-  benchmark('date-fns', () => endOfYesterday())
-})
