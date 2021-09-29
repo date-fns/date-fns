6a562b53f9162afbc7f4bad26482c10e61e9b379
Sasha Koss <koss@nocorp.me>
2021-09-29 16:33:41 +0800

    Fix FP tests

--- src/fp/format/index.js
+++
@@ -1,8 +0,0 @@
-// This file is generated automatically by `scripts/build/fp.js`. Please, don't change it.
-
-import fn from '../../format/index'
-import convertToFP from '../_lib/convertToFP/index'
-
-var format = convertToFP(fn, 2)
-
-export default format
--- src/fp/formatDuration/index.js
+++
@@ -1,8 +0,0 @@
-// This file is generated automatically by `scripts/build/fp.js`. Please, don't change it.
-
-import fn from '../../formatDuration/index'
-import convertToFP from '../_lib/convertToFP/index'
-
-var formatDuration = convertToFP(fn, 1)
-
-export default formatDuration
