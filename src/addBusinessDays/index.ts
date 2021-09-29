00a429e9b21e33bfaf4572b321d25f8adac5484d
Sasha Koss <koss@nocorp.me>
2021-09-29 16:38:11 +0800

    Prepare addBusinessDays for v3

--- src/addBusinessDays/index.ts
+++ src/addBusinessDays/index.ts
@@ -1,7 +1,4 @@
 import isWeekend from '../isWeekend/index'
-import toDate from '../toDate/index'
-import toInteger from '../_lib/toInteger/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 import isSunday from '../isSunday/index'
 import isSaturday from '../isSaturday/index'
 
@@ -23,44 +20,39 @@ import isSaturday from '../isSaturday/index'
  * //=> Mon Sep 15 2014 00:00:00 (skipped weekend days)
  */
 export default function addBusinessDays(
-  dirtyDate: Date | number,
-  dirtyAmount: number
+  date: Date | number,
+  amount: number
 ): Date {
-  requiredArgs(2, arguments)
-
-  const date = toDate(dirtyDate)
-  const startedOnWeekend = isWeekend(date)
-  const amount = toInteger(dirtyAmount)
-
-  if (isNaN(amount)) return new Date(NaN)
-
-  const hours = date.getHours()
+  const result = new Date(date)
+  const startedOnWeekend = isWeekend(result)
+  const hours = result.getHours()
   const sign = amount < 0 ? -1 : 1
-  const fullWeeks = toInteger(amount / 5)
+  const fullWeeks = Math.trunc(amount / 5)
 
-  date.setDate(date.getDate() + fullWeeks * 7)
+  result.setDate(result.getDate() + fullWeeks * 7)
 
   // Get remaining days not part of a full week
   let restDays = Math.abs(amount % 5)
 
   // Loops over remaining days
   while (restDays > 0) {
-    date.setDate(date.getDate() + sign)
-    if (!isWeekend(date)) restDays -= 1
+    result.setDate(result.getDate() + sign)
+    if (!isWeekend(result)) restDays -= 1
   }
 
   // If the date is a weekend day and we reduce a dividable of
   // 5 from it, we land on a weekend date.
   // To counter this, we add days accordingly to land on the next business day
-  if (startedOnWeekend && isWeekend(date) && amount !== 0) {
+  if (startedOnWeekend && isWeekend(result) && amount !== 0) {
     // If we're reducing days, we want to add days until we land on a weekday
     // If we're adding days we want to reduce days until we land on a weekday
-    if (isSaturday(date)) date.setDate(date.getDate() + (sign < 0 ? 2 : -1))
-    if (isSunday(date)) date.setDate(date.getDate() + (sign < 0 ? 1 : -2))
+    if (isSaturday(result))
+      result.setDate(result.getDate() + (sign < 0 ? 2 : -1))
+    if (isSunday(result)) result.setDate(result.getDate() + (sign < 0 ? 1 : -2))
   }
 
   // Restore hours to avoid DST lag
-  date.setHours(hours)
+  result.setHours(hours)
 
-  return date
+  return result
 }
