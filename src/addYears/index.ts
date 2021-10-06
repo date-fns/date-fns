0bd5eb155970105d7fca61fbd58c9107ee6a6222
Sasha Koss <koss@nocorp.me>
2021-10-06 16:38:24 +0800

    Prepare addYears for v3

--- src/addYears/index.ts
+++ src/addYears/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addMonths from '../addMonths/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name addYears
@@ -9,10 +7,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Add the specified number of years to the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param amount - the amount of years to be added. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
@@ -23,12 +17,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = addYears(new Date(2014, 8, 1), 5)
  * //=> Sun Sep 01 2019 00:00:00
  */
-export default function addYears(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-): Date {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  return addMonths(dirtyDate, amount * 12)
+export default function addYears(date: Date | number, amount: number): Date {
+  return addMonths(date, amount * 12)
 }
