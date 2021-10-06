f7e44fc4551a4e3885f171ac5ca46ef1f555d24c
Sasha Koss <koss@nocorp.me>
2021-10-06 16:37:12 +0800

    Prepare addWeeks for v3

--- src/addWeeks/index.ts
+++ src/addWeeks/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addDays from '../addDays/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name addWeeks
@@ -9,10 +7,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Add the specified number of week to the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param amount - the amount of weeks to be added. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
@@ -23,13 +17,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = addWeeks(new Date(2014, 8, 1), 4)
  * //=> Mon Sep 29 2014 00:00:00
  */
-export default function addWeeks(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-): Date {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  const days = amount * 7
-  return addDays(dirtyDate, days)
+export default function addWeeks(date: Date | number, amount: number): Date {
+  return addDays(date, amount * 7)
 }
