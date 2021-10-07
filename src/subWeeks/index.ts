297b3e3e33090bd41438b43461f7709f234bf111
Tetiana <ttobin@protonmail.ch>
2021-10-07 21:39:06 +0100

    Prepare subWeeks for v3

--- src/subWeeks/index.ts
+++ src/subWeeks/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addWeeks from '../addWeeks/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name subWeeks
@@ -9,10 +7,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Subtract the specified number of weeks from the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param amount - the amount of weeks to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
@@ -23,12 +17,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = subWeeks(new Date(2014, 8, 1), 4)
  * //=> Mon Aug 04 2014 00:00:00
  */
-export default function subWeeks(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-): Date {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  return addWeeks(dirtyDate, -amount)
+export default function subWeeks(date: Date | number, amount: number): Date {
+  return addWeeks(date, -amount)
 }
