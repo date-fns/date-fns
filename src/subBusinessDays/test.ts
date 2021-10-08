6bc778fb1122d906eef89e1486ca906c5f84a3ad
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2021-10-08 18:30:22 +0530

    convert subBusinessDays to TS

--- src/subBusinessDays/test.ts
+++ src/subBusinessDays/test.ts
@@ -4,18 +4,18 @@
 import assert from 'power-assert'
 import subBusinessDays from '.'
 
-describe('subBusinessDays', function() {
-  it('substract the given number of business days', function() {
+describe('subBusinessDays', function () {
+  it('substract the given number of business days', function () {
     const result = subBusinessDays(new Date(2014, 8 /* Sep */, 1), 10)
     assert.deepEqual(result, new Date(2014, 7 /* Aug */, 18))
   })
 
-  it('handles negative amount', function() {
+  it('handles negative amount', function () {
     var result = subBusinessDays(new Date(2014, 7 /* Sep */, 18), -10)
     assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1))
   })
 
-  it('can handle a large number of business days', function() {
+  it('can handle a large number of business days', function () {
     if (typeof this.timeout === 'function') {
       this.timeout(500 /* 500 ms test timeout */)
     }
@@ -24,39 +24,39 @@ describe('subBusinessDays', function() {
     assert.deepEqual(result, new Date(2014, 0 /* Jan */, 1))
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', function () {
     var result = subBusinessDays(new Date(2014, 8 /* Sep */, 1).getTime(), 10)
     assert.deepEqual(result, new Date(2014, 7 /* Aug */, 18))
   })
 
-  it('converts a fractional number to an integer', function() {
+  it('converts a fractional number to an integer', function () {
     var result = subBusinessDays(new Date(2014, 8 /* Sep */, 1), 10.5)
     assert.deepEqual(result, new Date(2014, 7 /* Aug */, 18))
   })
 
-  it('implicitly converts number arguments', function() {
+  it('implicitly converts number arguments', function () {
     // $ExpectedMistake
     var result = subBusinessDays(new Date(2014, 8 /* Sep */, 1), '10')
     assert.deepEqual(result, new Date(2014, 7 /* Aug */, 18))
   })
 
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', function () {
     var date = new Date(2014, 8 /* Sep */, 1)
     subBusinessDays(date, 11)
     assert.deepEqual(date, new Date(2014, 8 /* Sep */, 1))
   })
 
-  it('returns `Invalid Date` if the given date is invalid', function() {
+  it('returns `Invalid Date` if the given date is invalid', function () {
     var result = subBusinessDays(new Date(NaN), 10)
     assert(result instanceof Date && isNaN(result))
   })
 
-  it('returns `Invalid Date` if the given amount is NaN', function() {
+  it('returns `Invalid Date` if the given amount is NaN', function () {
     var result = subBusinessDays(new Date(2014, 8 /* Sep */, 1), NaN)
     assert(result instanceof Date && isNaN(result))
   })
 
-  it('throws TypeError exception if passed less than 2 arguments', function() {
+  it('throws TypeError exception if passed less than 2 arguments', function () {
     assert.throws(subBusinessDays.bind(null), TypeError)
     assert.throws(subBusinessDays.bind(null, 1), TypeError)
   })
