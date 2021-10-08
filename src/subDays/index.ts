4b743a98b2040b4997e230234c28412ed71568fc
Tetiana <ttobin@protonmail.ch>
2021-10-08 22:08:37 +0100

    Prepare subDays for v3

--- src/subDays/index.ts
+++ src/subDays/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addDays from '../addDays/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name subDays
@@ -9,10 +7,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Subtract the specified number of days from the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param amount - the amount of days to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
@@ -23,9 +17,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = subDays(new Date(2014, 8, 1), 10)
  * //=> Fri Aug 22 2014 00:00:00
  */
-export default function subDays(dirtyDate: Date | number, dirtyAmount: number) {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  return addDays(dirtyDate, -amount)
+export default function subDays(date: Date | number, amount: number) {
+  return addDays(date, -amount)
 }
