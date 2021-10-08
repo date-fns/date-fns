ba250b69c53ecf79f5ad96e55fa9bdb723cbe6e0
Tetiana <ttobin@protonmail.ch>
2021-10-08 16:43:43 +0100

    Prepare subMinutes for v3

--- src/subMinutes/index.ts
+++ src/subMinutes/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addMinutes from '../addMinutes/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name subMinutes
@@ -9,10 +7,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Subtract the specified number of minutes from the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param amount - the amount of minutes to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
@@ -23,12 +17,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = subMinutes(new Date(2014, 6, 10, 12, 0), 30)
  * //=> Thu Jul 10 2014 11:30:00
  */
-export default function subMinutes(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-) {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  return addMinutes(dirtyDate, -amount)
+export default function subMinutes(date: Date | number, amount: number) {
+  return addMinutes(date, -amount)
 }
--- src/subMinutes/test.ts
+++ src/subMinutes/test.ts
@@ -1,54 +1,25 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import subMinutes from '.'
 
-describe('subMinutes', function() {
-  it('subtracts the given number of minutes', function() {
+describe('subMinutes', () => {
+  it('subtracts the given number of minutes', () => {
     const result = subMinutes(new Date(2014, 6 /* Jul */, 10, 12, 0), 30)
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 30))
+    assert.deepStrictEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 30))
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const result = subMinutes(
       new Date(2014, 6 /* Jul */, 10, 12, 0).getTime(),
       20
     )
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 40))
+    assert.deepStrictEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 40))
   })
 
-  it('converts a fractional number to an integer', function() {
-    const result = subMinutes(new Date(2014, 6 /* Jul */, 10, 12, 0), 30.4)
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 30))
-  })
-
-  it('implicitly converts number arguments', function() {
-    // $ExpectedMistake
-    const result = subMinutes(new Date(2014, 6 /* Jul */, 10, 12, 0), '30')
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 11, 30))
-  })
-
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 6 /* Jul */, 10, 12, 0)
     subMinutes(date, 25)
-    assert.deepEqual(date, new Date(2014, 6 /* Jul */, 10, 12, 0))
-  })
-
-  it('returns `Invalid Date` if the given date is invalid', function() {
-    const result = subMinutes(new Date(NaN), 30)
-    // @ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('returns `Invalid Date` if the given amount is NaN', function() {
-    const result = subMinutes(new Date(2014, 6 /* Jul */, 10, 12, 0), NaN)
-    // @ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('throws TypeError exception if passed less than 2 arguments', function() {
-    assert.throws(subMinutes.bind(null), TypeError)
-    assert.throws(subMinutes.bind(null, 1), TypeError)
+    assert.deepStrictEqual(date, new Date(2014, 6 /* Jul */, 10, 12, 0))
   })
 })
