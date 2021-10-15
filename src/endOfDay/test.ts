728f774618705e13c10a245443abef1ad856d0f0
Sasha Koss <koss@nocorp.me>
2021-10-15 16:14:45 +0800

    Some more fns

--- src/endOfDay/test.ts
+++ src/endOfDay/test.ts
@@ -1,35 +1,35 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import endOfDay from '.'
 
-describe('endOfDay', function() {
-  it('returns the date with the time set to 23:59:59.999', function() {
+describe('endOfDay', () => {
+  it('returns the date with the time set to 23:59:59.999', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
     const result = endOfDay(date)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 2, 23, 59, 59, 999))
+    assert.deepStrictEqual(
+      result,
+      new Date(2014, 8 /* Sep */, 2, 23, 59, 59, 999)
+    )
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0).getTime()
     const result = endOfDay(date)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 2, 23, 59, 59, 999))
+    assert.deepStrictEqual(
+      result,
+      new Date(2014, 8 /* Sep */, 2, 23, 59, 59, 999)
+    )
   })
 
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
     endOfDay(date)
-    assert.deepEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0))
+    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0))
   })
 
-  it('returns `Invalid Date` if the given date is invalid', function() {
+  it('returns `Invalid Date` if the given date is invalid', () => {
     const result = endOfDay(new Date(NaN))
-    //@ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('throws TypeError exception if passed less than 1 argument', function() {
-    assert.throws(endOfDay.bind(null), TypeError)
+    assert(result instanceof Date && isNaN(+result))
   })
 })
