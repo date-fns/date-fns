1c60e1224573e8559092dc928a9ae70570ab34f8
Sasha Koss <koss@nocorp.me>
2021-10-15 15:46:15 +0800

    Prepare isAfter & isBefore for v3

--- src/isBefore/index.ts
+++ src/isBefore/index.ts
@@ -1,6 +1,3 @@
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name isBefore
  * @category Common Helpers
@@ -9,26 +6,18 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Is the first date before the second one?
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the date that should be before the other one to return true
  * @param dateToCompare - the date to compare with
  * @returns the first date is before the second date
  *
  * @example
  * // Is 10 July 1989 before 11 February 1987?
- * var result = isBefore(new Date(1989, 6, 10), new Date(1987, 1, 11))
+ * isBefore(new Date(1989, 6, 10), new Date(1987, 1, 11))
  * //=> false
  */
 export default function isBefore(
-  dirtyDate: Date | number,
-  dirtyDateToCompare: Date | number
+  date: Date | number,
+  dateToCompare: Date | number
 ): boolean {
-  requiredArgs(2, arguments)
-
-  const date = toDate(dirtyDate)
-  const dateToCompare = toDate(dirtyDateToCompare)
-  return date.getTime() < dateToCompare.getTime()
+  return new Date(date).getTime() < new Date(dateToCompare).getTime()
 }
--- src/isBefore/test.ts
+++ src/isBefore/test.ts
@@ -1,59 +1,53 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import isBefore from '.'
 
-describe('isBefore', function() {
-  it('returns true if the first date is before the second one', function() {
+describe('isBefore', () => {
+  it('returns true if the first date is before the second one', () => {
     const result = isBefore(
       new Date(1987, 1 /* Feb */, 11),
       new Date(1989, 6 /* Jul */, 10)
     )
-    assert(result === true)
+    assert(result)
   })
 
-  it('returns false if the first date is after the second one', function() {
+  it('returns false if the first date is after the second one', () => {
     const result = isBefore(
       new Date(1989, 6 /* Jul */, 10),
       new Date(1987, 1 /* Feb */, 11)
     )
-    assert(result === false)
+    assert(!result)
   })
 
-  it('returns false if the first date is equal to the second one', function() {
+  it('returns false if the first date is equal to the second one', () => {
     const result = isBefore(
       new Date(1989, 6 /* Jul */, 10),
       new Date(1989, 6 /* Jul */, 10)
     )
-    assert(result === false)
+    assert(!result)
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const result = isBefore(
       new Date(1987, 1 /* Feb */, 11).getTime(),
       new Date(1989, 6 /* Jul */, 10).getTime()
     )
-    assert(result === true)
+    assert(result)
   })
 
-  it('returns false if the first date is `Invalid Date`', function() {
+  it('returns false if the first date is `Invalid Date`', () => {
     const result = isBefore(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
-    assert(result === false)
+    assert(!result)
   })
 
-  it('returns false if the second date is `Invalid Date`', function() {
+  it('returns false if the second date is `Invalid Date`', () => {
     const result = isBefore(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
-    assert(result === false)
+    assert(!result)
   })
 
-  it('returns false if the both dates are `Invalid Date`', function() {
+  it('returns false if the both dates are `Invalid Date`', () => {
     const result = isBefore(new Date(NaN), new Date(NaN))
-    assert(result === false)
-  })
-
-  it('throws TypeError exception if passed less than 2 arguments', function() {
-    assert.throws(isBefore.bind(null), TypeError)
-    assert.throws(isBefore.bind(null, 1), TypeError)
+    assert(!result)
   })
 })
