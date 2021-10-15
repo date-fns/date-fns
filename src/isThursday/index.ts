263c4ec60fc3b5d115f8f3b8d8c9add069c9ebec
Sasha Koss <koss@nocorp.me>
2021-10-15 15:41:20 +0800

    Prepare isThursday for v3

--- src/isThursday/index.ts
+++ src/isThursday/index.ts
@@ -1,6 +1,3 @@
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name isThursday
  * @category Weekday Helpers
@@ -9,20 +6,14 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Is the given date Thursday?
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the date to check
  * @returns the date is Thursday
  *
  * @example
  * // Is 25 September 2014 Thursday?
- * var result = isThursday(new Date(2014, 8, 25))
+ * isThursday(new Date(2014, 8, 25))
  * //=> true
  */
 export default function isThursday(dirtyDate: Date | number): boolean {
-  requiredArgs(1, arguments)
-
-  return toDate(dirtyDate).getDay() === 4
+  return new Date(dirtyDate).getDay() === 4
 }
