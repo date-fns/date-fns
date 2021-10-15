f4ee8bd73787c5cadb6148bf0a099c5e741139f1
Sasha Koss <koss@nocorp.me>
2021-10-15 15:41:20 +0800

    Prepare isThursday for v3

--- src/isThursday/test.ts
+++ src/isThursday/test.ts
@@ -6,26 +6,21 @@ import isThursday from '.'
 describe('isThursday', () => {
   it('returns true if the given date is Thursday', () => {
     const result = isThursday(new Date(2014, 8 /* Sep */, 25))
-    assert(result === true)
+    assert(result)
   })
 
   it('returns false if the given date is not Thursday', () => {
     const result = isThursday(new Date(2014, 8 /* Sep */, 24))
-    assert(result === false)
+    assert(!result)
   })
 
   it('accepts a timestamp', () => {
     const result = isThursday(new Date(2014, 1 /* Feb */, 13).getTime())
-    assert(result === true)
+    assert(result)
   })
 
   it('returns false if the given date is `Invalid Date`', () => {
     const result = isThursday(new Date(NaN))
-    assert(result === false)
-  })
-
-  it('throws TypeError exception if passed less than 1 argument', () => {
-    // @ts-expect-error
-    assert.throws(isThursday.bind(null), TypeError)
+    assert(!result)
   })
 })
