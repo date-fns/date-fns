c9c5c76e5823aee3af67003442fba6d985062c7d
Sasha Koss <koss@nocorp.me>
2021-10-15 15:48:43 +0800

    Prepare getYear for v3

--- src/getYear/index.ts
+++ src/getYear/index.ts
@@ -1,6 +1,3 @@
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name getYear
  * @category Year Helpers
@@ -9,22 +6,14 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Get the year of the given date.
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the given date
  * @returns the year
  *
  * @example
  * // Which year is 2 July 2014?
- * const result = getYear(new Date(2014, 6, 2))
+ * getYear(new Date(2014, 6, 2))
  * //=> 2014
  */
-export default function getYear(dirtyDate: Date | number): number {
-  requiredArgs(1, arguments)
-
-  const date = toDate(dirtyDate)
-  const year = date.getFullYear()
-  return year
+export default function getYear(date: Date | number): number {
+  return new Date(date).getFullYear()
 }
