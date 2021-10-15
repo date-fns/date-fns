6ac4c66610b567bb65fc708916197555c3ab281b
Sasha Koss <koss@nocorp.me>
2021-10-15 15:40:12 +0800

    Prepare isToday for v3

--- src/isTomorrow/index.ts
+++ src/isTomorrow/index.ts
@@ -18,7 +18,7 @@ import isSameDay from '../isSameDay/index'
  *
  * @example
  * // If today is 6 October 2014, is 7 October 14:00:00 tomorrow?
- * var result = isTomorrow(new Date(2014, 9, 7, 14, 0))
+ * isTomorrow(new Date(2014, 9, 7, 14, 0))
  * //=> true
  */
 export default function isTomorrow(date: Date | number): boolean {
