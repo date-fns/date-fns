fb97c20e0a68af88929bfe70002104fbfb5d5ba8
Sasha Koss <koss@nocorp.me>
2021-10-15 15:46:15 +0800

    Prepare isAfter & isBefore for v3

--- src/isAfter/index.ts
+++ src/isAfter/index.ts
@@ -1,6 +1,3 @@
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name isAfter
  * @category Common Helpers
@@ -9,26 +6,18 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Is the first date after the second one?
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the date that should be after the other one to return true
  * @param dateToCompare - the date to compare with
  * @returns the first date is after the second date
  *
  * @example
  * // Is 10 July 1989 after 11 February 1987?
- * var result = isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))
+ * isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))
  * //=> true
  */
 export default function isAfter(
-  dirtyDate: Date | number,
-  dirtyDateToCompare: Date | number
+  date: Date | number,
+  dateToCompare: Date | number
 ): boolean {
-  requiredArgs(2, arguments)
-
-  const date = toDate(dirtyDate)
-  const dateToCompare = toDate(dirtyDateToCompare)
-  return date.getTime() > dateToCompare.getTime()
+  return new Date(date).getTime() > new Date(dateToCompare).getTime()
 }
