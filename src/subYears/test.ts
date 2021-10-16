57ef5ebe44d4e6b25e40bc1b5e6b9d34d45d019a
Tetiana <ttobin@protonmail.ch>
2021-10-16 21:06:51 +0100

    Prepare subYears for v3
    Prepare subYears for v3

--- src/subYears/test.ts
+++ src/subYears/test.ts
@@ -1,44 +1,36 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import subYears from '.'
 
-describe.skip('subYears', function () {
-  it('subtracts the given number of years', function () {
+describe.skip('subYears', () => {
+  it('subtracts the given number of years', () => {
     const result = subYears(new Date(2014, 8 /* Sep */, 1), 5)
-    assert.deepEqual(result, new Date(2009, 8 /* Sep */, 1))
+    assert.deepStrictEqual(result, new Date(2009, 8 /* Sep */, 1))
   })
 
-  it('accepts a timestamp', function () {
+  it('accepts a timestamp', () => {
     const result = subYears(new Date(2014, 8 /* Sep */, 1).getTime(), 12)
-    assert.deepEqual(result, new Date(2002, 8 /* Sep */, 1))
+    assert.deepStrictEqual(result, new Date(2002, 8 /* Sep */, 1))
   })
 
-  it('converts a fractional number to an integer', function () {
+  it('converts a fractional number to an integer', () => {
     const result = subYears(new Date(2014, 8 /* Sep */, 1), 5.1)
-    assert.deepEqual(result, new Date(2009, 8 /* Sep */, 1))
+    assert.deepStrictEqual(result, new Date(2009, 8 /* Sep */, 1))
   })
 
-  it('implicitly converts number arguments', function () {
-    // $ExpectedMistake
-    // @ts-expect-error
-    const result = subYears(new Date(2014, 8 /* Sep */, 1), '5')
-    assert.deepEqual(result, new Date(2009, 8 /* Sep */, 1))
-  })
-
-  it('does not mutate the original date', function () {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 8 /* Sep */, 1)
     subYears(date, 12)
-    assert.deepEqual(date, new Date(2014, 8 /* Sep */, 1))
+    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 1))
   })
 
-  it('handles the leap years properly', function () {
+  it('handles the leap years properly', () => {
     const result = subYears(new Date(2016, 1 /* Feb */, 29), 1)
-    assert.deepEqual(result, new Date(2015, 1 /* Feb */, 28))
+    assert.deepStrictEqual(result, new Date(2015, 1 /* Feb */, 28))
   })
 
-  it('handles dates before 100 AD', function () {
+  it('handles dates before 100 AD', () => {
     const initialDate = new Date(0)
     initialDate.setFullYear(0, 1 /* Feb */, 29)
     initialDate.setHours(0, 0, 0, 0)
@@ -46,23 +38,6 @@ describe.skip('subYears', function () {
     expectedResult.setFullYear(-1, 1 /* Feb */, 28)
     expectedResult.setHours(0, 0, 0, 0)
     const result = subYears(initialDate, 1)
-    assert.deepEqual(result, expectedResult)
-  })
-
-  it('returns `Invalid Date` if the given date is invalid', function () {
-    const result = subYears(new Date(NaN), 5)
-    // @ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('returns `Invalid Date` if the given amount is NaN', function () {
-    const result = subYears(new Date(2014, 8 /* Sep */, 1), NaN)
-    // @ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('throws TypeError exception if passed less than 2 arguments', function () {
-    assert.throws(subYears.bind(null), TypeError)
-    assert.throws(subYears.bind(null, 1), TypeError)
+    assert.deepStrictEqual(result, expectedResult)
   })
 })
