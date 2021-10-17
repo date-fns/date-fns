01895e318e39db3a0f866f9c3b4c93d1b714000b
Tan75 <ttobin@protonmail.ch>
2021-10-17 21:29:25 +0100

    Add change to import type

--- src/_lib/isSameUTCWeek/index.ts
+++ src/_lib/isSameUTCWeek/index.ts
@@ -1,4 +1,4 @@
-import { LocaleOptions, WeekStartOptions } from 'src/types'
+import type { LocaleOptions, WeekStartOptions } from 'src/types'
 import requiredArgs from '../requiredArgs/index'
 import startOfUTCWeek from '../startOfUTCWeek/index'
 
