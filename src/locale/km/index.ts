1c0403c3eafafc2788250bf189cc6f01b909056a
Yath Seanghay <seanghay.dev@gmail.com>
2021-10-21 17:06:14 +0700

    Add translation to formatDistance

---
+++ src/locale/km/index.ts
@@ -0,0 +1,29 @@
+import formatDistance from './_lib/formatDistance/index'
+import formatLong from './_lib/formatLong/index'
+import formatRelative from './_lib/formatRelative/index'
+import localize from './_lib/localize/index'
+import match from './_lib/match/index'
+import type { Locale } from '../types'
+
+/**
+ * @type {Locale}
+ * @category Locales
+ * @summary Khmer locale (Cambodian).
+ * @language Khmer
+ * @iso-639-1 km
+ * @author Seanghay Yath [@seanghay]{@link https://github.com/seanghay}
+ */
+const locale: Locale = {
+  code: 'km',
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 0 /* Sunday */,
+    firstWeekContainsDate: 1,
+  },
+}
+
+export default locale
