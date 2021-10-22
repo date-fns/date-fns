bdb0f80b3ef31bb1aeba6d58364532898d72392b
Tetiana <ttobin@protonmail.ch>
2021-10-22 21:21:15 +0100

    Prepare subSeconds for v3

--- src/subSeconds/index.ts
+++ src/subSeconds/index.ts
@@ -1,6 +1,4 @@
-import toInteger from '../_lib/toInteger/index'
 import addSeconds from '../addSeconds/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name subSeconds
@@ -10,12 +8,8 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * @description
  * Subtract the specified number of seconds from the given date.
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @param date - the date to be changed
- * @param amount - the amount of seconds to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
+ * @param amount - the amount of seconds to be subtracted. Decimals will be rounded using `Math.trunc`.
  * @returns the new date with the seconds subtracted
  *
  * @example
@@ -23,12 +17,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = subSeconds(new Date(2014, 6, 10, 12, 45, 0), 30)
  * //=> Thu Jul 10 2014 12:44:30
  */
-export default function subSeconds(
-  dirtyDate: Date | number,
-  dirtyAmount: number
-): Date {
-  requiredArgs(2, arguments)
-
-  const amount = toInteger(dirtyAmount)
-  return addSeconds(dirtyDate, -amount)
+export default function subSeconds(date: Date | number, amount: number): Date {
+  const result = Math.trunc(amount)
+  return addSeconds(date, -result)
 }
--- src/subSeconds/test.ts
+++ src/subSeconds/test.ts
@@ -22,35 +22,9 @@ describe('subSeconds', () => {
     assert.deepStrictEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 44, 30))
   })
 
-  it('implicitly converts number arguments', () => {
-    const result = subSeconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 0),
-      // @ts-expect-error
-      '30'
-    )
-    assert.deepStrictEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 44, 30))
-  })
-
   it('does not mutate the original date', () => {
     const date = new Date(2014, 6 /* Jul */, 10, 12, 45, 0)
     subSeconds(date, 15)
     assert.deepStrictEqual(date, new Date(2014, 6 /* Jul */, 10, 12, 45, 0))
-  })
-
-  it('returns `Invalid Date` if the given date is invalid', () => {
-    const result = subSeconds(new Date(NaN), 30)
-    assert(result instanceof Date && isNaN(result.getTime()))
-  })
-
-  it('returns `Invalid Date` if the given amount is NaN', () => {
-    const result = subSeconds(new Date(2014, 6 /* Jul */, 10, 12, 45, 0), NaN)
-    assert(result instanceof Date && isNaN(result.getTime()))
-  })
-
-  it('throws TypeError exception if passed less than 2 arguments', () => {
-    // @ts-expect-error
-    assert.throws(subSeconds.bind(null), TypeError)
-    // @ts-expect-error
-    assert.throws(subSeconds.bind(null, 1), TypeError)
   })
 })
