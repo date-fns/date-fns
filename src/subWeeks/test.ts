9043988e8696c8ac38a2c94598e2791d3eb7ca84
Tetiana <ttobin@protonmail.ch>
2021-11-13 23:13:21 +0000

    Change functions to arrow functions

--- src/subWeeks/test.ts
+++ src/subWeeks/test.ts
@@ -3,35 +3,35 @@
 import assert from 'assert'
 import subWeeks from '.'
 
-describe('subWeeks', function () {
-  it('subtracts the given number of weeks', function () {
+describe('subWeeks', () => {
+  it('subtracts the given number of weeks', () => {
     const result = subWeeks(new Date(2014, 8 /* Sep */, 1), 4)
     assert.deepStrictEqual(result, new Date(2014, 7 /* Aug */, 4))
   })
 
-  it('accepts a timestamp', function () {
+  it('accepts a timestamp', () => {
     const result = subWeeks(new Date(2014, 8 /* Sep */, 1).getTime(), 1)
     assert.deepStrictEqual(result, new Date(2014, 7 /* Aug */, 25))
   })
 
-  it('converts a fractional number to an integer', function () {
+  it('converts a fractional number to an integer', () => {
     const result = subWeeks(new Date(2014, 8 /* Sep */, 1), 4.2)
     assert.deepStrictEqual(result, new Date(2014, 7 /* Aug */, 4))
   })
 
-  it('does not mutate the original date', function () {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 8 /* Sep */, 1)
     subWeeks(date, 2)
     assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 1))
   })
 
-  it('returns `Invalid Date` if the given date is invalid', function () {
+  it('returns `Invalid Date` if the given date is invalid', () => {
     const result = subWeeks(new Date(NaN), 4)
     // @ts-expect-error
     assert(result instanceof Date && isNaN(result))
   })
 
-  it('returns `Invalid Date` if the given amount is NaN', function () {
+  it('returns `Invalid Date` if the given amount is NaN', () => {
     const result = subWeeks(new Date(2014, 8 /* Sep */, 1), NaN)
     // @ts-expect-error
     assert(result instanceof Date && isNaN(result))
