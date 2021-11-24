9eb39e84769996f9aff7be0c2f27070469a3f071
Tetiana <ttobin@protonmail.ch>
2021-11-24 22:05:34 +0000

    Add minor change

--- src/locale/km/index.ts
+++ src/locale/km/index.ts
@@ -1,9 +1,9 @@
+import type { Locale } from '../types'
 import formatDistance from './_lib/formatDistance/index'
 import formatLong from './_lib/formatLong/index'
 import formatRelative from './_lib/formatRelative/index'
 import localize from './_lib/localize/index'
 import match from './_lib/match/index'
-import type { Locale } from '../types'
 
 /**
  * @type {Locale}
