7add27a68fabe6443c05e6aea5947974543d4952
Aleksei Korzun <akorzun@outlook.com>
2021-11-24 13:04:17 +0300

    migrate formatDuration to TS (#2095)

--- src/formatDuration/index.ts
+++ src/formatDuration/index.ts
@@ -1,6 +1,7 @@
 import defaultLocale from '../locale/en-US/index'
+import type { Duration } from '../types'
 
-const defaultFormat = [
+const defaultFormat: (keyof Duration)[] = [
   'years',
   'months',
   'weeks',
@@ -9,6 +10,13 @@ const defaultFormat = [
   'minutes',
   'seconds',
 ]
+
+interface Options {
+  format?: (keyof Duration)[]
+  zero?: boolean
+  delimiter?: string
+  locale?: Locale
+}
 
 /**
  * @name formatDuration
@@ -20,9 +28,8 @@ const defaultFormat = [
  *
  * @param {Duration} duration - the duration to format
  * @param {Object} [options] - an object with options.
-
  * @param {string[]} [options.format=['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds']] - the array of units to format
- * @param {boolean} [options.zero=false] - should be zeros be included in the output?
+ * @param {boolean} [options.zero=false] - should zeros be included in the output?
  * @param {string} [options.delimiter=' '] - delimiter string
  * @param {Locale} [options.locale=defaultLocale] - the locale object. See [Locale]{@link https://date-fns.org/docs/Locale}
  * @returns {string} the formatted date string
@@ -39,7 +46,7 @@ const defaultFormat = [
  *   minutes: 9,
  *   seconds: 30
  * })
- * //=> '2 years 9 months 1 week 7 days 5 hours 9 minutes 30 seconds
+ * //=> '2 years 9 months 1 week 7 days 5 hours 9 minutes 30 seconds'
  *
  * @example
  * // Format partial duration
@@ -73,7 +80,10 @@ const defaultFormat = [
  * formatDuration({ years: 2, months: 9, weeks: 3 }, { delimiter: ', ' })
  * //=> '2 years, 9 months, 3 weeks'
  */
-export default function formatDuration(duration, options) {
+export default function formatDuration(
+  duration: Duration,
+  options: Options = {}
+): string {
   if (arguments.length < 1) {
     throw new TypeError(
       `1 argument required, but only ${arguments.length} present`
@@ -90,7 +100,7 @@ export default function formatDuration(duration, options) {
       const token = `x${unit.replace(/(^.)/, (m) => m.toUpperCase())}`
       const addChunk =
         typeof duration[unit] === 'number' && (zero || duration[unit])
-      return addChunk
+      return addChunk && locale.formatDistance
         ? acc.concat(locale.formatDistance(token, duration[unit]))
         : acc
     }, [])
--- src/formatDuration/test.ts
+++ src/formatDuration/test.ts
@@ -1,7 +1,6 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import formatDuration from '.'
 
 describe('formatDuration', () => {
@@ -14,7 +13,7 @@ describe('formatDuration', () => {
         days: 7,
         hours: 5,
         minutes: 9,
-        seconds: 30
+        seconds: 30,
       }) === '2 years 9 months 1 week 7 days 5 hours 9 minutes 30 seconds'
     )
   })
@@ -33,7 +32,7 @@ describe('formatDuration', () => {
           days: 7,
           hours: 5,
           minutes: 9,
-          seconds: 30
+          seconds: 30,
         },
         { format: ['months', 'weeks'] }
       ) === '9 months 1 week'
@@ -49,7 +48,7 @@ describe('formatDuration', () => {
         days: 0,
         hours: 0,
         minutes: 0,
-        seconds: 0
+        seconds: 0,
       }) === '1 week'
     )
   })
@@ -64,7 +63,7 @@ describe('formatDuration', () => {
           days: 0,
           hours: 0,
           minutes: 0,
-          seconds: 0
+          seconds: 0,
         },
         { zero: true }
       ) === '0 years 0 months 1 week 0 days 0 hours 0 minutes 0 seconds'
