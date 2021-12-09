ac35dd6c69e94bbec5d20110839520ed671b9ec9
Luca Ban <mesqueeb@users.noreply.github.com>
2021-12-09 11:16:27 +0900

    feat(docs): update isSameDay examples to be more clear

--- src/isSameDay/index.ts
+++ src/isSameDay/index.ts
@@ -4,7 +4,7 @@ import requiredArgs from '../_lib/requiredArgs/index'
 /**
  * @name isSameDay
  * @category Day Helpers
- * @summary Are the given dates in the same day?
+ * @summary Are the given dates in the same day (and year and month)?
  *
  * @description
  * Are the given dates in the same day?
@@ -22,6 +22,16 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * // Are 4 September 06:00:00 and 4 September 18:00:00 in the same day?
  * var result = isSameDay(new Date(2014, 8, 4, 6, 0), new Date(2014, 8, 4, 18, 0))
  * //=> true
+ * 
+ * @example
+ * // Are 4 September and 4 October in the same day?
+ * var result = isSameDay(new Date(2014, 8, 4), new Date(2014, 9, 4))
+ * //=> false
+ * 
+ * @example
+ * // Are 4 September, 2014 and 4 September, 2015 in the same day?
+ * var result = isSameDay(new Date(2014, 8, 4), new Date(2015, 8, 4))
+ * //=> false
  */
 export default function isSameDay(dirtyDateLeft: Date | number, dirtyDateRight: Date | number): boolean {
   requiredArgs(2, arguments)
