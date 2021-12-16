e1117fb8b1cd9e109df23888241521036500c4c9
Francis Turmel <fturmel@gmail.com>
2021-12-16 08:37:36 -0500

    migrate `is` to TypeScript

--- src/locale/is/_lib/formatLong/index.ts
+++ src/locale/is/_lib/formatLong/index.ts
@@ -1,41 +1,42 @@
+import type { FormatLong } from '../../../types'
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index'
 
-var dateFormats = {
+const dateFormats = {
   full: 'EEEE, do MMMM y',
   long: 'do MMMM y',
   medium: 'do MMM y',
-  short: 'd.MM.y'
+  short: 'd.MM.y',
 }
 
-var timeFormats = {
+const timeFormats = {
   full: "'kl'. HH:mm:ss zzzz",
   long: 'HH:mm:ss z',
   medium: 'HH:mm:ss',
-  short: 'HH:mm'
+  short: 'HH:mm',
 }
 
-var dateTimeFormats = {
+const dateTimeFormats = {
   full: "{{date}} 'kl.' {{time}}",
   long: "{{date}} 'kl.' {{time}}",
   medium: '{{date}} {{time}}',
-  short: '{{date}} {{time}}'
+  short: '{{date}} {{time}}',
 }
 
-var formatLong = {
+const formatLong: FormatLong = {
   date: buildFormatLongFn({
     formats: dateFormats,
-    defaultWidth: 'full'
+    defaultWidth: 'full',
   }),
 
   time: buildFormatLongFn({
     formats: timeFormats,
-    defaultWidth: 'full'
+    defaultWidth: 'full',
   }),
 
   dateTime: buildFormatLongFn({
     formats: dateTimeFormats,
-    defaultWidth: 'full'
-  })
+    defaultWidth: 'full',
+  }),
 }
 
 export default formatLong
