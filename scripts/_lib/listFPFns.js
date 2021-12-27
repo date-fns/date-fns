98beeeadaba5c499837265208c5ce5dfc8e52a6f
Francis Turmel <fturmel@gmail.com>
2021-12-27 11:39:38 -0500

    simplify internal list* functions in scripts

--- scripts/_lib/listFPFns.js
+++ scripts/_lib/listFPFns.js
@@ -3,19 +3,13 @@ const fs = require('fs')
 
 module.exports = listFPFns
 
-const ignoredFiles = [
-  'index.js',
-  'index.ts',
-  'test.js',
-  'test.ts',
-  'index.js.flow',
-  'package.json',
-]
+const ignorePattern = /^_|\./ // can't start with `_` or have a `.` in it
 
 function listFPFns() {
   const files = fs.readdirSync(path.join(process.cwd(), 'src', 'fp'))
+
   return files
-    .filter((file) => /^[^._]/.test(file) && !ignoredFiles.includes(file))
+    .filter((file) => !ignorePattern.test(file))
     .map((file) => ({
       name: file,
       path: `./${file}`,
