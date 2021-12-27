be1efa307ef996cd337f124cae090e4302bfacec
Francis Turmel <fturmel@gmail.com>
2021-12-27 14:06:15 -0500

    bs locale typescript migration

--- src/locale/bs/_lib/formatLong/index.ts
+++ src/locale/bs/_lib/formatLong/index.ts
@@ -1,35 +1,38 @@
+import type { FormatLong } from '../../../types'
 import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index'
 
-var dateFormats = {
+const dateFormats = {
   full: 'EEEE, d. MMMM yyyy.',
   long: 'd. MMMM yyyy.',
   medium: 'd. MMM yy.',
   short: 'dd. MM. yy.',
 }
 
-var timeFormats = {
+const timeFormats = {
   full: 'HH:mm:ss (zzzz)',
   long: 'HH:mm:ss z',
   medium: 'HH:mm:ss',
   short: 'HH:mm',
 }
 
-var dateTimeFormats = {
+const dateTimeFormats = {
   full: "{{date}} 'u' {{time}}",
   long: "{{date}} 'u' {{time}}",
   medium: '{{date}} {{time}}',
   short: '{{date}} {{time}}',
 }
 
-var formatLong = {
+const formatLong: FormatLong = {
   date: buildFormatLongFn({
     formats: dateFormats,
     defaultWidth: 'full',
   }),
+
   time: buildFormatLongFn({
     formats: timeFormats,
     defaultWidth: 'full',
   }),
+
   dateTime: buildFormatLongFn({
     formats: dateTimeFormats,
     defaultWidth: 'full',
