2d4231d3ff218ef65e575d3a97783d4d17b23165
fturmel <4009209+fturmel@users.noreply.github.com>
2021-12-28 09:31:18 -0500

    convert `bn` locale to TypeScript (#2881)

--- src/locale/bn/index.ts
+++ src/locale/bn/index.ts
@@ -1,3 +1,4 @@
+import type { Locale } from '../types'
 import formatDistance from './_lib/formatDistance/index'
 import formatLong from './_lib/formatLong/index'
 import formatRelative from './_lib/formatRelative/index'
@@ -13,7 +14,7 @@ import match from './_lib/match/index'
  * @author Touhidur Rahman [@touhidrahman]{@link https://github.com/touhidrahman}
  * @author Farhad Yasir [@nutboltu]{@link https://github.com/nutboltu}
  */
-var locale = {
+const locale: Locale = {
   code: 'bn',
   formatDistance: formatDistance,
   formatLong: formatLong,
@@ -22,8 +23,8 @@ var locale = {
   match: match,
   options: {
     weekStartsOn: 0 /* Sunday */,
-    firstWeekContainsDate: 1
-  }
+    firstWeekContainsDate: 1,
+  },
 }
 
 export default locale
