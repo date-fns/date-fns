d8c5a1b7ad109e9e2d0345bf4a7404b0076873c1
Mike <mike@labcc.ch>
2021-12-28 16:30:28 +0100

    added it-CH locale

---
+++ src/locale/it-CH/index.ts
@@ -0,0 +1,28 @@
+import type { Locale } from '../types'
+import formatDistance from '../it/_lib/formatDistance/index'
+import formatRelative from '../it/_lib/formatRelative/index'
+import localize from '../it/_lib/localize/index'
+import match from '../it/_lib/match/index'
+import formatLong from './_lib/formatLong/index'
+
+/**
+ * @type {Locale}
+ * @category Locales
+ * @summary Italian locale (Switzerland).
+ * @language Italian
+ * @author Mike Peyer [@maic66]{@link https://github.com/maic66}
+ */
+const locale: Locale = {
+  code: 'it-CH',
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 1 /* Monday */,
+    firstWeekContainsDate: 4,
+  },
+}
+
+export default locale
