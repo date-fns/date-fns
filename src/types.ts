ba5021f233e966702d07c47e3ce9985d5638def5
Jonas L <jooola@users.noreply.github.com>
2022-01-05 12:17:52 +0100

    Add comment on FirstWeekContainsDate type (#2427)

--- src/types.ts
+++ src/types.ts
@@ -51,6 +51,13 @@ export type Day = 0 | 1 | 2 | 3 | 4 | 5 | 6
 
 export type Month = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 
+/**
+ * FirstWeekContainsDate is used to determine which week is the first week of the year, based on what day the January, 1 is in that week.
+ *
+ * The day in that week can only be 1 (Monday) or 4 (Thursday).
+ *
+ * Please see https://en.wikipedia.org/wiki/Week#Week_numbering for more information.
+ */
 export type FirstWeekContainsDate = 1 | 4
 
 export interface DateValues {
