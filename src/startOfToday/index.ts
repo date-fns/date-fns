d576fd373ac93ca62559c31589996e3a98c9acf9
Tetiana <ttobin@protonmail.ch>
2022-01-06 21:32:50 +0000

    Convert startOfToday to v3

--- src/startOfToday/index.ts
+++ src/startOfToday/index.ts
@@ -12,15 +12,11 @@ import startOfDay from '../startOfDay/index'
  * > ⚠️ Please note that this function is not present in the FP submodule as
  * > it uses `Date.now()` internally hence impure and can't be safely curried.
  *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
- *
  * @returns the start of today
  *
  * @example
  * // If today is 6 October 2014:
- * var result = startOfToday()
+ * const result = startOfToday()
  * //=> Mon Oct 6 2014 00:00:00
  */
 export default function startOfToday(): Date {
