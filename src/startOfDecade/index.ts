6027cfe6c7587c450dc8352b95b7315551ee883b
Tetiana <ttobin@protonmail.ch>
2022-01-06 22:27:50 +0000

    Convert startOfDecade to v3

--- src/startOfDecade/index.ts
+++ src/startOfDecade/index.ts
@@ -1,6 +1,3 @@
-import toDate from '../toDate/index'
-import requiredArgs from '../_lib/requiredArgs/index'
-
 /**
  * @name startOfDecade
  * @category Decade Helpers
@@ -8,10 +5,6 @@ import requiredArgs from '../_lib/requiredArgs/index'
  *
  * @description
  * Return the start of a decade for the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the original date
  * @returns the start of a decade
@@ -21,13 +14,11 @@ import requiredArgs from '../_lib/requiredArgs/index'
  * const result = startOfDecade(new Date(2015, 9, 21, 00, 00, 00))
  * //=> Jan 01 2010 00:00:00
  */
-export default function startOfDecade(dirtyDate: Date | number): Date {
-  requiredArgs(1, arguments)
-
-  const date = toDate(dirtyDate)
-  const year = date.getFullYear()
+export default function startOfDecade(date: Date | number): Date {
+  const result = new Date(date)
+  const year = result.getFullYear()
   const decade = Math.floor(year / 10) * 10
-  date.setFullYear(decade, 0, 1)
-  date.setHours(0, 0, 0, 0)
-  return date
+  result.setFullYear(decade, 0, 1)
+  result.setHours(0, 0, 0, 0)
+  return result
 }
