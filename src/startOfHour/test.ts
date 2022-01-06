1b1cd80750d47efbca3831899329cdde7bf975f3
Tetiana <ttobin@protonmail.ch>
2022-01-06 22:21:46 +0000

    Convert startOfHour to v3

--- src/startOfHour/test.ts
+++ src/startOfHour/test.ts
@@ -1,35 +1,24 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import startOfHour from '.'
 
-describe('startOfHour', function() {
-  it('returns the date with the time set to the first millisecond of an hour', function() {
+describe('startOfHour', () => {
+  it('returns the date with the time set to the first millisecond of an hour', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55)
     const result = startOfHour(date)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 2, 11))
+    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 2, 11))
   })
 
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55)
     startOfHour(date)
-    assert.deepEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55))
+    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55))
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const date = new Date(2014, 8 /* Sep */, 2, 11, 55).getTime()
     const result = startOfHour(date)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 2, 11))
-  })
-
-  it('returns `Invalid Date` if the given date is invalid', function() {
-    const result = startOfHour(new Date(NaN))
-    //@ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('throws TypeError exception if passed less than 1 argument', function() {
-    assert.throws(startOfHour.bind(null), TypeError)
+    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 2, 11))
   })
 })
