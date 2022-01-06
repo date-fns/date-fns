5edcf69ec3c5215be0b1e0e2d4ae06b59e1b5304
Tetiana <ttobin@protonmail.ch>
2022-01-06 21:39:45 +0000

    Convert startOfSecond to v3

--- src/startOfSecond/test.ts
+++ src/startOfSecond/test.ts
@@ -1,35 +1,27 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import startOfSecond from '.'
 
-describe('startOfSecond', function() {
-  it('returns the date with the time set to the first millisecond of a second', function() {
+describe('startOfSecond', () => {
+  it('returns the date with the time set to the first millisecond of a second', () => {
     const date = new Date(2014, 11 /* Dec */, 1, 22, 15, 45, 400)
     const result = startOfSecond(date)
-    assert.deepEqual(result, new Date(2014, 11 /* Dec */, 1, 22, 15, 45))
+    assert.deepStrictEqual(result, new Date(2014, 11 /* Dec */, 1, 22, 15, 45))
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const date = new Date(2014, 11 /* Dec */, 1, 22, 15, 45, 400).getTime()
     const result = startOfSecond(date)
-    assert.deepEqual(result, new Date(2014, 11 /* Dec */, 1, 22, 15, 45))
+    assert.deepStrictEqual(result, new Date(2014, 11 /* Dec */, 1, 22, 15, 45))
   })
 
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', () => {
     const date = new Date(2014, 11 /* Dec */, 1, 22, 15, 45, 400)
     startOfSecond(date)
-    assert.deepEqual(date, new Date(2014, 11 /* Dec */, 1, 22, 15, 45, 400))
-  })
-
-  it('returns `Invalid Date` if the given date is invalid', function() {
-    const result = startOfSecond(new Date(NaN))
-    //@ts-expect-error
-    assert(result instanceof Date && isNaN(result))
-  })
-
-  it('throws TypeError exception if passed less than 1 argument', function() {
-    assert.throws(startOfSecond.bind(null), TypeError)
+    assert.deepStrictEqual(
+      date,
+      new Date(2014, 11 /* Dec */, 1, 22, 15, 45, 400)
+    )
   })
 })
