efd5a5ee3e8c8ce8bd4448e4b22684cccc7acd64
Tetiana <ttobin@protonmail.ch>
2022-01-07 21:28:56 +0000

    Convert setHours to v3

--- src/setHours/test.ts
+++ src/setHours/test.ts
@@ -1,49 +1,37 @@
-// @flow
 /* eslint-env mocha */
 
-import assert from 'power-assert'
+import assert from 'assert'
 import setHours from '.'
 
-describe('setHours', function() {
-  it('sets the amount of hours', function() {
+describe('setHours', () => {
+  it('sets the amount of hours', () => {
     const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
+    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
   })
 
-  it('accepts a timestamp', function() {
+  it('accepts a timestamp', () => {
     const result = setHours(new Date(2014, 8 /* Sep */, 1, 11).getTime(), 5)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 5))
+    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1, 5))
   })
 
-  it('converts a fractional number to an integer', function() {
+  it('converts a fractional number to an integer', () => {
     const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), 4.123)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
+    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
   })
 
-  it('implicitly converts number arguments', function() {
-    // @ts-expect-error
-    const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), '4')
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 4, 30))
-  })
-
-  it('does not mutate the original date', function() {
+  it('does not mutate the original date', () => {
     var date = new Date(2014, 8 /* Sep */, 1, 11)
     setHours(date, 12)
-    assert.deepEqual(date, new Date(2014, 8 /* Sep */, 1, 11))
+    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 1, 11))
   })
 
-  it('returns `Invalid Date` if the given date is invalid', function() {
+  it('returns `Invalid Date` if the given date is invalid', () => {
     const result = setHours(new Date(NaN), 4)
     assert(result instanceof Date && isNaN(result.getTime()))
   })
 
-  it('returns `Invalid Date` if the given amount is NaN', function() {
+  it('returns `Invalid Date` if the given amount is NaN', () => {
     const result = setHours(new Date(2014, 8 /* Sep */, 1, 11, 30), NaN)
     assert(result instanceof Date && isNaN(result.getTime()))
-  })
-
-  it('throws TypeError exception if passed less than 2 arguments', function() {
-    assert.throws(setHours.bind(null), TypeError)
-    assert.throws(setHours.bind(null, 1), TypeError)
   })
 })
