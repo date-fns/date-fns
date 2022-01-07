faac81fa4886f39839fcd7e3f0d2dd3b4a1a38f1
fturmel <4009209+fturmel@users.noreply.github.com>
2022-01-07 07:08:43 -0500

    build script: FP and Indices conversion to TypeScript (#2880)

--- scripts/_lib/listLocales.js
+++ scripts/_lib/listLocales.js
@@ -5,17 +5,9 @@ const { promisify } = require('util')
 const exists = promisify(fs.exists)
 const readDir = promisify(fs.readdir)
 
-const ignoredFiles = [
-  'index.js',
-  'index.ts',
-  'test.js',
-  'test.ts',
-  'index.js.flow',
-  'package.json',
-  'types.ts',
-]
-
 module.exports = listLocales
+
+const ignorePattern = /^_|\./ // can't start with `_` or have a `.` in it
 
 async function listLocales() {
   const localesPath = path.resolve(process.cwd(), 'src/locale')
@@ -23,9 +15,10 @@ async function listLocales() {
 
   return Promise.all(
     locales
-      .filter((file) => /^[^._]/.test(file) && !ignoredFiles.includes(file))
+      .filter((file) => !ignorePattern.test(file))
       .map(async (locale) => {
         const isTs = await exists(path.join(localesPath, locale, 'index.ts'))
+
         return {
           name: locale.replace(/-/g, ''),
           code: locale,
