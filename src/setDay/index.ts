3407199f7fcb1e14dbe5050035682a7a2e56a291
Tetiana <ttobin@protonmail.ch>
2022-01-08 17:52:22 +0000

    Convert setDay to v3

--- src/setDay/index.ts
+++ src/setDay/index.ts
@@ -1,8 +1,6 @@
 import addDays from '../addDays/index'
-import toDate from '../toDate/index'
-import toInteger from '../_lib/toInteger/index'
-import requiredArgs from '../_lib/requiredArgs/index'
 import type { LocaleOptions, WeekStartOptions } from '../types'
+import toInteger from '../_lib/toInteger/index'
 
 /**
  * @name setDay
@@ -11,10 +9,6 @@ import type { LocaleOptions, WeekStartOptions } from '../types'
  *
  * @description
  * Set the day of the week to the given date.
- *
- * ### v2.0.0 breaking changes:
- *
- * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).
  *
  * @param date - the date to be changed
  * @param day - the day of the week of the new date
@@ -33,39 +27,37 @@ import type { LocaleOptions, WeekStartOptions } from '../types'
  * //=> Sun Sep 07 2014 00:00:00
  */
 export default function setDay(
-  dirtyDate: Date | number,
-  dirtyDay: number,
-  dirtyOptions?: WeekStartOptions & LocaleOptions
+  date: Date | number,
+  day: number,
+  options?: WeekStartOptions & LocaleOptions
 ): Date {
-  requiredArgs(2, arguments)
-
-  const options = dirtyOptions || {}
-  const locale = options.locale
+  const opts = options || {}
+  const locale = opts.locale
   const localeWeekStartsOn =
     locale && locale.options && locale.options.weekStartsOn
   const defaultWeekStartsOn =
     localeWeekStartsOn == null ? 0 : toInteger(localeWeekStartsOn)
   const weekStartsOn =
-    options.weekStartsOn == null
+    opts.weekStartsOn == null
       ? defaultWeekStartsOn
-      : toInteger(options.weekStartsOn)
+      : toInteger(opts.weekStartsOn)
 
   // Test if weekStartsOn is between 0 and 6 _and_ is not NaN
   if (!(weekStartsOn >= 0 && weekStartsOn <= 6)) {
     throw new RangeError('weekStartsOn must be between 0 and 6 inclusively')
   }
 
-  const date = toDate(dirtyDate)
-  const day = toInteger(dirtyDay)
-  const currentDay = date.getDay()
+  const dateTransformed = new Date(date)
+  const dayTransformed = Math.trunc(day)
+  const currentDay = dateTransformed.getDay()
 
-  const remainder = day % 7
+  const remainder = dayTransformed % 7
   const dayIndex = (remainder + 7) % 7
 
   const delta = 7 - weekStartsOn
   const diff =
-    day < 0 || day > 6
-      ? day - ((currentDay + delta) % 7)
+    dayTransformed < 0 || dayTransformed > 6
+      ? dayTransformed - ((currentDay + delta) % 7)
       : ((dayIndex + delta) % 7) - ((currentDay + delta) % 7)
-  return addDays(date, diff)
+  return addDays(dateTransformed, diff)
 }
