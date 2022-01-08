c7b46eae64ab1a131b5c42c047f4207c54f352ff
Tetiana <ttobin@protonmail.ch>
2022-01-08 19:13:50 +0000

    Convert quartersToYears to v3

--- src/quartersToYears/index.ts
+++ src/quartersToYears/index.ts
@@ -1,4 +1,3 @@
-import requiredArgs from '../_lib/requiredArgs/index'
 import { quartersInYear } from '../constants/index'
 
 /**
@@ -24,7 +23,6 @@ import { quartersInYear } from '../constants/index'
  * //=> 2
  */
 export default function quartersToYears(quarters: number): number {
-  requiredArgs(1, arguments)
   const years = quarters / quartersInYear
   return Math.floor(years)
 }
