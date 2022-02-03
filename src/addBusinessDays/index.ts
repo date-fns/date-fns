fa04513773264e025a82c3053d7ad2a06136e557
KSK <kskano@gmail.com>
2022-02-03 12:04:48 -0700

    Add updates from custom business days PR

--- src/addBusinessDays/index.ts
+++ src/addBusinessDays/index.ts
@@ -2,7 +2,9 @@ import constructFrom from '../constructFrom/index'
 import isSaturday from '../isSaturday/index'
 import isSunday from '../isSunday/index'
 import isWeekend from '../isWeekend/index'
-import toDate from '../toDate/index'
+import toInteger from '../_lib/toInteger/index'
+import requiredArgs from '../_lib/requiredArgs/index'
+import toDate from '../toDate'
 
 /**
  * @name addBusinessDays
@@ -12,48 +14,70 @@ import toDate from '../toDate/index'
  * @description
  * Add the specified number of business days (mon - fri) to the given date, ignoring weekends.
  *
- * @param date - the date to be changed
- * @param amount - the amount of business days to be added. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
- * @returns the new date with the business days added
+ * @param {Date|Number} date - the date to be changed
+ * @param {Number} amount - the amount of business days to be added. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
+ * @param {Object} [options] - an object with options.
+ * @param {Number[]} [options.businessDays=[1, 2, 3, 4, 5]] - the business days. default is Monday to Friday.
+ * @returns {Date} the new date with the business days added
+ * @throws {TypeError} 2 arguments required
  *
  * @example
  * // Add 10 business days to 1 September 2014:
  * const result = addBusinessDays(new Date(2014, 8, 1), 10)
  * //=> Mon Sep 15 2014 00:00:00 (skipped weekend days)
  */
-export default function addBusinessDays<DateType extends Date>(
-  dirtyDate: DateType | number,
-  amount: number
-): DateType {
+
+export default function addBusinessDays(
+  dirtyDate: Date | number,
+  dirtyAmount: number,
+  dirtyOptions?: {
+    businessDays?: number[]
+  }
+): Date {
+  requiredArgs(2, arguments)
+  const options = dirtyOptions || {}
+  const businessDays =
+    options.businessDays == null
+      ? [1, 2, 3, 4, 5]
+      : options.businessDays.map(toInteger)
+
   const date = toDate(dirtyDate)
-  const startedOnWeekend = isWeekend(date)
+  const amount = toInteger(dirtyAmount)
+  const isHoliday = (date: Date) => !businessDays.includes(date.getDay())
+  const startedOnHoliday = isHoliday(date)
 
   if (isNaN(amount)) return constructFrom(dirtyDate, NaN)
 
+  if (date.toString() === 'Invalid Date') {
+    return date
+  }
+
   const hours = date.getHours()
   const sign = amount < 0 ? -1 : 1
-  const fullWeeks = Math.trunc(amount / 5)
-
+  const fullWeeks = toInteger(amount / businessDays.length)
   date.setDate(date.getDate() + fullWeeks * 7)
 
   // Get remaining days not part of a full week
-  let restDays = Math.abs(amount % 5)
+  let restDays = Math.abs(amount % businessDays.length)
 
   // Loops over remaining days
   while (restDays > 0) {
     date.setDate(date.getDate() + sign)
-    if (!isWeekend(date)) restDays -= 1
+    if (!isHoliday(date)) restDays -= 1
   }
 
-  // If the date is a weekend day and we reduce a dividable of
-  // 5 from it, we land on a weekend date.
+  // If the date is a holiday and we reduce a dividable of
+  // certain days (say 5 when holidays are Saturday and Sunday) from it, we land on a holiday date.
   // To counter this, we add days accordingly to land on the next business day
-  if (startedOnWeekend && isWeekend(date) && amount !== 0) {
-    // If we're reducing days, we want to add days until we land on a weekday
-    // If we're adding days we want to reduce days until we land on a weekday
-    if (isSaturday(date)) date.setDate(date.getDate() + (sign < 0 ? 2 : -1))
-    if (isSunday(date)) date.setDate(date.getDate() + (sign < 0 ? 1 : -2))
+  const reduceIfHoliday = (date: Date) => {
+    if (startedOnHoliday && isHoliday(date) && amount !== 0) {
+      // If we're reducing days, we want to add days until we land on a business day
+      // If we're adding days we want to reduce days until we land on a business day
+      date.setDate(date.getDate() + (sign < 0 ? 1 : -1))
+      reduceIfHoliday(date)
+    }
   }
+  reduceIfHoliday(date)
 
   // Restore hours to avoid DST lag
   date.setHours(hours)
--- src/subBusinessDays/index.ts
+++ src/subBusinessDays/index.ts
@@ -1,4 +1,6 @@
+import toInteger from '../_lib/toInteger/index'
 import addBusinessDays from '../addBusinessDays/index'
+import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name subBusinessDays
@@ -8,18 +10,32 @@ import addBusinessDays from '../addBusinessDays/index'
  * @description
  * Substract the specified number of business days (mon - fri) to the given date, ignoring weekends.
  *
- * @param date - the date to be changed
- * @param amount - the amount of business days to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
- * @returns the new date with the business days subtracted
+ * @param {Date|Number} date - the date to be changed
+ * @param {Number} amount - the amount of business days to be subtracted. Positive decimals will be rounded using `Math.floor`, decimals less than zero will be rounded using `Math.ceil`.
+ * @param {Object} [options] - an object with options.
+ * @param {Number[]} [options.businessDays=[1, 2, 3, 4, 5]] - the business days. default is Monday to Friday.
+ * @returns {Date} the new date with the business days subtracted
+ * @throws {TypeError} 2 arguments required
  *
  * @example
  * // Substract 10 business days from 1 September 2014:
- * const result = subBusinessDays(new Date(2014, 8, 1), 10)
+ * var result = subBusinessDays(new Date(2014, 8, 1), 10)
  * //=> Mon Aug 18 2014 00:00:00 (skipped weekend days)
  */
-export default function subBusinessDays<DateType extends Date>(
-  dirtyDate: DateType | number,
-  amount: number
-): DateType {
-  return addBusinessDays(dirtyDate, -amount)
+export default function subBusinessDays(
+  dirtyDate: Date | number,
+  dirtyAmount: number,
+  dirtyOptions?: {
+    businessDays?: number[]
+  }
+) {
+  requiredArgs(2, arguments)
+
+  const amount = toInteger(dirtyAmount)
+  const options = dirtyOptions || {}
+  const businessDays =
+    options.businessDays == null
+      ? [1, 2, 3, 4, 5]
+      : options.businessDays.map(toInteger)
+  return addBusinessDays(dirtyDate, -amount, { businessDays })
 }
