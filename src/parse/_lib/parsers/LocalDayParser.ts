cc5b55f021308d692a8fde60e4d17f2eefc30ea7
Tan75 <ttobin@protonmail.ch>
2022-05-26 16:50:55 +0100

    Fix parser

--- src/parse/_lib/parsers/LocalDayParser.ts
+++ src/parse/_lib/parsers/LocalDayParser.ts
@@ -15,7 +15,7 @@ export class LocalDayParser extends Parser<number> {
   ): ParseResult<number> {
     const valueCallback = (value: number) => {
       const wholeWeekDays = Math.floor((value - 1) / 7) * 7
-      return ((value + options.weekStartsOn + 6) % 7) + wholeWeekDays
+      return ((value + (options.weekStartsOn || 0) + 6) % 7) + wholeWeekDays
     }
 
     switch (token) {
