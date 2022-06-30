179841ebc885c37281f2458ef2c209e5ddc8e43a
Tan75 <ttobin@protonmail.ch>
2022-06-30 20:32:28 +0100

    Add more docs

--- src/minutesToMilliseconds/index.ts
+++ src/minutesToMilliseconds/index.ts
@@ -1,5 +1,5 @@
-import requiredArgs from '../_lib/requiredArgs/index'
 import { millisecondsInMinute } from '../constants/index'
+import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name minutesToMilliseconds
--- src/monthsToYears/index.ts
+++ src/monthsToYears/index.ts
@@ -1,5 +1,5 @@
-import requiredArgs from '../_lib/requiredArgs/index'
 import { monthsInYear } from '../constants/index'
+import requiredArgs from '../_lib/requiredArgs/index'
 
 /**
  * @name monthsToYears
