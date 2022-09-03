15bcdb4e8673c7fe0b584c17b9c84da74454c221
Lesha Kostiuchenko <lesha@hey.com>
2022-09-03 11:57:00 +0200

    Improve IE11 compatibility by transforming the thing with preset-env

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,5 @@
-const presets = ['@babel/preset-typescript']
-const plugins = [
-  '@babel/plugin-transform-block-scoping',
-  '@babel/plugin-transform-template-literals',
-  '@babel/plugin-transform-arrow-functions',
-  '@babel/plugin-transform-parameters',
-  '@babel/plugin-transform-destructuring',
-  '@babel/plugin-proposal-optional-chaining',
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-transform-shorthand-properties',
-  '@babel/plugin-proposal-nullish-coalescing-operator',
-]
+const presets = ['@babel/preset-env', '@babel/preset-typescript']
+const plugins = ['@babel/plugin-proposal-class-properties']
 
 if (process.env.BABEL_ENV !== 'esm') {
   plugins.push('@babel/plugin-transform-modules-commonjs')
