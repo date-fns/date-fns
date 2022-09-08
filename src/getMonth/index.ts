7a49fb79f2497a9614c4a9f21beb0f27c0148903
Richard Smith <richardmcsmith@gmail.com>
2022-09-08 18:24:48 +0900

    Improve getMonth docs
    Make it clearer that date-fns follows Javascript January = 0 months.

--- src/getMonth/index.ts
+++ src/getMonth/index.ts
@@ -6,14 +6,15 @@ import toDate from '../toDate/index'
  * @summary Get the month of the given date.
  *
  * @description
- * Get the month of the given date.
+ * Get the month of the given date. Months are indexed from 0, like Javascript dates,
+ * so January = 0, February = 1 etc.
  *
  * @param date - the given date
- * @returns the month
+ * @returns the month number, where January = 0 as with standard Javascript Dates
  *
  * @example
  * // Which month is 29 February 2012?
- * const result = getMonth(new Date(2012, 1, 29))
+ * const result = getMonth(new Date("2012-02-29"))
  * //=> 1
  */
 export default function getMonth<DateType extends Date>(
