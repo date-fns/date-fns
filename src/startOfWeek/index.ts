1aa6dc60448a2bf85cb8356f5645a3de58030177
James R T <jamestiotio@gmail.com>
2022-10-27 20:58:18 +0800

    Fix startOfWeek and formatISODuration docs
    Signed-off-by: James R T <jamestiotio@gmail.com>

--- src/startOfWeek/index.ts
+++ src/startOfWeek/index.ts
@@ -22,12 +22,12 @@ export interface StartOfWeekOptions extends LocaleOptions, WeekStartOptions {}
  *
  * @example
  * // The start of a week for 2 September 2014 11:55:00:
- * const result = startOfWeek(new Date(2014, 8, 2, 11, 55, 0))
+ * const result = startOfWeek(new Date(2014, 9, 2, 11, 55, 0))
  * //=> Sun Aug 31 2014 00:00:00
  *
  * @example
  * // If the week starts on Monday, the start of the week for 2 September 2014 11:55:00:
- * const result = startOfWeek(new Date(2014, 8, 2, 11, 55, 0), { weekStartsOn: 1 })
+ * const result = startOfWeek(new Date(2014, 9, 2, 11, 55, 0), { weekStartsOn: 1 })
  * //=> Mon Sep 01 2014 00:00:00
  */
 export default function startOfWeek<DateType extends Date>(
