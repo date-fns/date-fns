f4d8be36e8874095e8f836afd6f4031bd79bcfcc
Sasha Koss <koss@nocorp.me>
2023-04-30 09:08:00 +0800

    Fix Rollup example by upgrade it

---
+++ examples/rollup/rollup.config.fp.mjs
@@ -0,0 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve' // use the new @rollup/plugin-node-resolve package
+import commonjs from '@rollup/plugin-commonjs' // use the new @rollup/plugin-commonjs package
+
+export default {
+  input: 'fp.js',
+  output: {
+    file: 'dist/fp.js',
+    format: 'cjs',
+  },
+  plugins: [nodeResolve({ preferBuiltins: true }), commonjs()],
+}
