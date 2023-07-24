9b2c336be9456c09943926b26ee540c995da81b5
Sasha Koss <koss@nocorp.me>
2023-07-24 14:29:31 +0800

    Finalize documenting types

--- src/locale/_lib/buildLocalizeFn/index.ts
+++ src/locale/_lib/buildLocalizeFn/index.ts
@@ -1,13 +1,11 @@
 /* eslint-disable no-unused-vars */
 
+import { Day, Era, Month, Quarter } from '../../../types'
 import type {
+  LocaleDayPeriod,
   LocaleUnitValue,
   LocaleWidth,
   LocalizeFn,
-  LocalizeFnArgCallback,
-  LocalizePeriodValuesMap,
-  LocalizeUnitIndex,
-  LocalizeValues,
 } from '../../types'
 
 export type BuildLocalizeFnArgs<
@@ -21,6 +19,92 @@ export type BuildLocalizeFnArgs<
 } & (ArgCallback extends undefined
   ? { argumentCallback?: undefined }
   : { argumentCallback: LocalizeFnArgCallback<Value> })
+
+/**
+ * The localize function argument callback which allows to convert raw value to
+ * the actual type.
+ *
+ * @param value - The value to convert
+ *
+ * @returns The converted value
+ */
+export type LocalizeFnArgCallback<Value extends LocaleUnitValue | number> = (
+  value: Value
+) => LocalizeUnitIndex<Value>
+
+/**
+ * The map of localized values for each width.
+ */
+export type LocalizePeriodValuesMap<Value extends LocaleUnitValue> = {
+  [Pattern in LocaleWidth]?: LocalizeValues<Value>
+}
+
+/**
+ * The index type of the locale unit value. It types conversion of units of
+ * values that don't start at 0 (i.e. quarters).
+ */
+export type LocalizeUnitIndex<
+  Value extends LocaleUnitValue | number
+> = Value extends LocaleUnitValue ? keyof LocalizeValues<Value> : number
+
+/**
+ * Converts the unit value to the tuple of values.
+ */
+export type LocalizeValues<
+  Value extends LocaleUnitValue
+> = Value extends LocaleDayPeriod
+  ? Record<LocaleDayPeriod, string>
+  : Value extends Era
+  ? LocalizeEraValues
+  : Value extends Quarter
+  ? LocalizeQuarterValues
+  : Value extends Day
+  ? LocalizeDayValues
+  : Value extends Month
+  ? LocalizeMonthValues
+  : never
+
+/**
+ * The tuple of localized era values. The first element represents BC,
+ * the second element represents AD.
+ */
+export type LocalizeEraValues = readonly [string, string]
+
+/**
+ * The tuple of localized quarter values. The first element represents Q1.
+ */
+export type LocalizeQuarterValues = readonly [string, string, string, string]
+
+/**
+ * The tuple of localized day values. The first element represents Sunday.
+ */
+export type LocalizeDayValues = readonly [
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string
+]
+
+/**
+ * The tuple of localized month values. The first element represents January.
+ */
+export type LocalizeMonthValues = readonly [
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string
+]
 
 export default function buildLocalizeFn<
   Value extends LocaleUnitValue,
--- src/locale/_lib/buildMatchFn/index.ts
+++ src/locale/_lib/buildMatchFn/index.ts
@@ -1,10 +1,70 @@
-import type {
-  BuildMatchFnArgs,
-  LocaleDayPeriod,
+import { Quarter, Era, Day, Month } from '../../../types'
+import {
   LocaleUnitValue,
   LocaleWidth,
+  LocaleDayPeriod,
   MatchFn,
+  MatchValueCallback,
 } from '../../types'
+
+export interface BuildMatchFnArgs<
+  Result extends LocaleUnitValue,
+  DefaultMatchWidth extends LocaleWidth,
+  DefaultParseWidth extends LocaleWidth
+> {
+  matchPatterns: BuildMatchFnMatchPatterns<DefaultMatchWidth>
+  defaultMatchWidth: DefaultMatchWidth
+  parsePatterns: BuildMatchFnParsePatterns<Result, DefaultParseWidth>
+  defaultParseWidth: DefaultParseWidth
+  valueCallback?: MatchValueCallback<
+    Result extends LocaleDayPeriod ? string : number,
+    Result
+  >
+}
+
+export type BuildMatchFnMatchPatterns<DefaultWidth extends LocaleWidth> = {
+  [Width in LocaleWidth]?: RegExp
+} &
+  {
+    [Width in DefaultWidth]: RegExp
+  }
+
+export type BuildMatchFnParsePatterns<
+  Value extends LocaleUnitValue,
+  DefaultWidth extends LocaleWidth
+> = {
+  [Width in LocaleWidth]?: ParsePattern<Value>
+} &
+  {
+    [Width in DefaultWidth]: ParsePattern<Value>
+  }
+
+export type ParsePattern<
+  Value extends LocaleUnitValue
+> = Value extends LocaleDayPeriod
+  ? Record<LocaleDayPeriod, RegExp>
+  : Value extends Quarter
+  ? readonly [RegExp, RegExp, RegExp, RegExp]
+  : Value extends Era
+  ? readonly [RegExp, RegExp]
+  : Value extends Day
+  ? readonly [RegExp, RegExp, RegExp, RegExp, RegExp, RegExp, RegExp]
+  : Value extends Month
+  ? readonly [
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp,
+      RegExp
+    ]
+  : never
 
 export default function buildMatchFn<
   Value extends LocaleUnitValue,
