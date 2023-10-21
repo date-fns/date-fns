6645925176c85a018a899de29ad2ccc8a796bf66
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-10-21 15:21:28 -0700

    refactor: move playwright install to post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,6 +9,7 @@ if [ -n "${REMOTE_CONTAINERS}" ]; then
 
 	# perform additional one-time setup just after
 	# the devcontainer is created
-	yarn install --cwd "${workspace_root}"
+	yarn install --cwd "${workspace_root}" # install node dependencies
+	npx playwright install                 # install playwright browsers
 
 fi
