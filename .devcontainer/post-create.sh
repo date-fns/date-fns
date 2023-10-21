2d07cb3ed1db1249f2b006b6909a677c80a115ec
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-10-21 14:06:19 -0700

    feat: add devcontainer configuration

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# when in a VS Code or GitHub Codespaces devcontainer
+if [ -n "${REMOTE_CONTAINERS}" ]; then
+	this_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)
+	workspace_root=$(realpath ${this_dir}/..)
+
+	# perform additional one-time setup just after
+	# the devcontainer is created
+	yarn install --cwd "${workspace_root}"
+
+fi
