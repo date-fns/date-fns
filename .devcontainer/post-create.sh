c3949cf506a7706627e18ac9cd49abd0e124819f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-10-22 09:41:05 -0700

    fix: ensure post-create runs in github codespaces

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 # when in a VS Code or GitHub Codespaces devcontainer
-if [ -n "${REMOTE_CONTAINERS}" ]; then
+if [ -n "${REMOTE_CONTAINERS}" ] || [ -n "${CODESPACES}" ]; then
 	this_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)
 	workspace_root=$(realpath ${this_dir}/..)
 
