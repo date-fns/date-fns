fc1c1a5228dd73acd7a2fe3131ebdba74103fb43
Sasha Koss <koss@nocorp.me>
2023-12-03 12:51:18 +0800

    Prepare v3.0.0-beta.1

--- scripts/build/_lib/prettier.ts
+++ scripts/build/_lib/prettier.ts
@@ -1,9 +1,8 @@
 import prettier from "prettier";
-import config from "../../../.prettierrc";
 
 export function formatCode(
   code: string,
   parser: string = "babel",
 ): Promise<string> {
-  return prettier.format(code, { ...config, parser });
+  return prettier.format(code, { parser });
 }
