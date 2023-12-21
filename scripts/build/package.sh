138e31895204f98e25ea5b66723f25870be2a715
Sasha Koss <koss@nocorp.me>
2023-12-21 08:34:03 +0800

    Revert "Fix ESM types problem by removing d.mts"
    This reverts commit fe8868eded5022fd50f57d8fb572431d87089db7.

--- scripts/build/package.sh
+++ scripts/build/package.sh
@@ -52,6 +52,9 @@ fi
 # Flatten the structure
 npx tsx scripts/build/flatten.ts
 
+# Generate .d.mts files
+npx tsx scripts/build/mts.ts
+
 # Copy basic files
 for pattern in CHANGELOG.md \
   package.json \
