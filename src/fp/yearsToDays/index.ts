fadd58b132bff04a0de6597a1b30dfe2aab6cb8f
Sasha Koss <koss@nocorp.me>
2024-01-20 13:53:02 +0800

    Add missing `yearsToDays` to FP
    Added missing `yearsToDays` to the FP submodule.

---
+++ src/fp/yearsToDays/index.ts
@@ -0,0 +1,6 @@
+// This file is generated automatically by `scripts/build/fp.ts`. Please, don't change it.
+
+import { yearsToDays as fn } from "../../yearsToDays/index.js";
+import { convertToFP } from "../_lib/convertToFP/index.js";
+
+export const yearsToDays = convertToFP(fn, 1);
