87016c151fae076a50fa95ba79555290e6e6c646
Audun Rundberg <audun@rundberg.no>
2024-03-04 12:38:47 +0100

    chore: copy nb locale

---
+++ src/locale/se/_lib/formatRelative/index.ts
@@ -0,0 +1,17 @@
+import type { FormatRelativeFn } from "../../../types.js";
+
+const formatRelativeLocale = {
+  lastWeek: "'forrige' eeee 'kl.' p",
+  yesterday: "'i går kl.' p",
+  today: "'i dag kl.' p",
+  tomorrow: "'i morgen kl.' p",
+  nextWeek: "EEEE 'kl.' p",
+  other: "P",
+};
+
+export const formatRelative: FormatRelativeFn = (
+  token,
+  _date,
+  _baseDate,
+  _options,
+) => formatRelativeLocale[token];
