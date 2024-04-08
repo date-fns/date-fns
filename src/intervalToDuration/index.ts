35fe290c16cf9e72b71c5c6db068290309783495
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2024-04-08 17:34:55 +0900

    feature: add `includeZeroValues` option to intervalToDuration
    resolve #3658

--- src/intervalToDuration/index.ts
+++ src/intervalToDuration/index.ts
@@ -8,6 +8,16 @@ import { differenceInYears } from "../differenceInYears/index.js";
 import { toDate } from "../toDate/index.js";
 import type { Duration, Interval } from "../types.js";
 
+type Options = { includeZeroValues?: boolean };
+type RequiredDuration = Pick<
+  Required<Duration>,
+  "years" | "months" | "days" | "hours" | "minutes" | "seconds"
+>;
+
+type NormalizedDuration<O> = O extends { includeZeroValues: true }
+  ? RequiredDuration
+  : Duration;
+
 /**
  * @name intervalToDuration
  * @category Common Helpers
@@ -19,6 +29,7 @@ import type { Duration, Interval } from "../types.js";
  * @typeParam DateType - The `Date` type, the function operates on. Gets inferred from passed arguments. Allows to use extensions like [`UTCDate`](https://github.com/date-fns/utc).
  *
  * @param interval - The interval to convert to duration
+ * @param options - The object with options
  *
  * @returns The duration object
  *
@@ -28,15 +39,35 @@ import type { Duration, Interval } from "../types.js";
  *   start: new Date(1929, 0, 15, 12, 0, 0),
  *   end: new Date(1968, 3, 4, 19, 5, 0)
  * })
+ * // => { years: 39, months: 2, days: 20, hours: 7, minutes: 5 }
+ *
+ * @example
+ * // Get the duration with 0 values.
+ * intervalToDuration({
+ *   start: new Date(1929, 0, 15, 12, 0, 0),
+ *   end: new Date(1968, 3, 4, 19, 5, 0)
+ * }, { includeZeroValues: true })
  * // => { years: 39, months: 2, days: 20, hours: 7, minutes: 5, seconds: 0 }
  */
-export function intervalToDuration<DateType extends Date>(
+export function intervalToDuration<DateType extends Date, O extends Options>(
   interval: Interval<DateType>,
-): Duration {
+  options?: O,
+): NormalizedDuration<O> {
   const start = toDate(interval.start);
   const end = toDate(interval.end);
 
-  const duration: Duration = {};
+  const duration = (
+    options?.includeZeroValues
+      ? ({
+          years: 0,
+          months: 0,
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        } satisfies RequiredDuration)
+      : {}
+  ) as NormalizedDuration<O>;
 
   const years = differenceInYears(end, start);
   if (years) duration.years = years;
