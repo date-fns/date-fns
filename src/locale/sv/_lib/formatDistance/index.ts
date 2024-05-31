a9641cfb71170844ca9b94c425b8b3f94665720b
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2024-05-31 15:01:38 +0200

    Remove non-standard wordMapping in nn and sv locales

--- src/locale/sv/_lib/formatDistance/index.ts
+++ src/locale/sv/_lib/formatDistance/index.ts
@@ -86,22 +86,6 @@ const formatDistanceLocale: FormatDistanceLocale<FormatDistanceTokenValue> = {
   },
 };
 
-const wordMapping = [
-  "noll",
-  "en",
-  "två",
-  "tre",
-  "fyra",
-  "fem",
-  "sex",
-  "sju",
-  "åtta",
-  "nio",
-  "tio",
-  "elva",
-  "tolv",
-];
-
 export const formatDistance: FormatDistanceFn = (token, count, options) => {
   let result;
 
@@ -111,10 +95,7 @@ export const formatDistance: FormatDistanceFn = (token, count, options) => {
   } else if (count === 1) {
     result = tokenValue.one;
   } else {
-    result = tokenValue.other.replace(
-      "{{count}}",
-      count < 13 ? wordMapping[count] : String(count),
-    );
+    result = tokenValue.other.replace("{{count}}", String(count));
   }
 
   if (options?.addSuffix) {
