5f2e5bee72c1683d783471af62efd757c502ac24
berdi00 <berdinazarov@gmail.com>
2024-07-13 21:12:58 +0500

    added: formatDistance

--- src/locale/tk-TM/_lib/formatDistance/index.ts
+++ src/locale/tk-TM/_lib/formatDistance/index.ts
@@ -9,80 +9,80 @@ type FormatDistanceTokenValue =
 
 const formatDistanceLocale: FormatDistanceLocale<FormatDistanceTokenValue> = {
   lessThanXSeconds: {
-    one: "less than a second",
-    other: "less than {{count}} seconds",
+    one: "bir sekuntdan az",
+    other: "{{count}} sekuntdan az",
   },
 
   xSeconds: {
-    one: "1 second",
-    other: "{{count}} seconds",
+    one: "1 sekunt",
+    other: "{{count}} sekunt",
   },
 
-  halfAMinute: "half a minute",
+  halfAMinute: "ýarym minut",
 
   lessThanXMinutes: {
-    one: "less than a minute",
-    other: "less than {{count}} minutes",
+    one: "bir minutdan az",
+    other: "{{count}} minutdan az",
   },
 
   xMinutes: {
-    one: "1 minute",
-    other: "{{count}} minutes",
+    one: "1 minut",
+    other: "{{count}} minut",
   },
 
   aboutXHours: {
-    one: "about 1 hour",
-    other: "about {{count}} hours",
+    one: "takmynan 1 sagat",
+    other: "takmynan {{count}} sagat",
   },
 
   xHours: {
-    one: "1 hour",
-    other: "{{count}} hours",
+    one: "1 sagat",
+    other: "{{count}} sagat",
   },
 
   xDays: {
-    one: "1 day",
-    other: "{{count}} days",
+    one: "1 gün",
+    other: "{{count}} gün",
   },
 
   aboutXWeeks: {
-    one: "about 1 week",
-    other: "about {{count}} weeks",
+    one: "takmynan 1 hepde",
+    other: "takmynan {{count}} hepde",
   },
 
   xWeeks: {
-    one: "1 week",
-    other: "{{count}} weeks",
+    one: "1 hepde",
+    other: "{{count}} hepde",
   },
 
   aboutXMonths: {
-    one: "about 1 month",
-    other: "about {{count}} months",
+    one: "takmynan 1 aý",
+    other: "takmynan {{count}} aý",
   },
 
   xMonths: {
-    one: "1 month",
-    other: "{{count}} months",
+    one: "1 aý",
+    other: "{{count}} aý",
   },
 
   aboutXYears: {
-    one: "about 1 year",
-    other: "about {{count}} years",
+    one: "takmynan 1 ýyl",
+    other: "takmynan {{count}} ýyl",
   },
 
   xYears: {
-    one: "1 year",
-    other: "{{count}} years",
+    one: "1 ýyl",
+    other: "{{count}} ýyl",
   },
 
   overXYears: {
-    one: "over 1 year",
-    other: "over {{count}} years",
+    one: "1 ýyldan gowrak",
+    other: "{{count}} ýyldan gowrak",
   },
 
   almostXYears: {
-    one: "almost 1 year",
-    other: "almost {{count}} years",
+    one: "1 ýyl töweregi",
+    other: "{count}} ýyl töweregi",
   },
 };
 
@@ -100,9 +100,9 @@ export const formatDistance: FormatDistanceFn = (token, count, options) => {
 
   if (options?.addSuffix) {
     if (options.comparison && options.comparison > 0) {
-      return "in " + result;
+      return result + " soň";
     } else {
-      return result + " ago";
+      return result + " öň";
     }
   }
 
