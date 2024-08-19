b4d40474b9afc913e8d83f9182fa5a0b423d8987
Sasha Koss <koss@nocorp.me>
2024-08-19 11:30:49 +0800

    Add tz support to startOfDecade, startOfHour and startOfWeek

--- src/startOfDecade/index.ts
+++ src/startOfDecade/index.ts
@@ -1,4 +1,11 @@
 import { toDate } from "../toDate/index.js";
+import { type DateFns } from "../types.js";
+
+/**
+ * The {@link startOfDecade} options.
+ */
+export interface StartOfDecadeOptions<DateType extends Date>
+  extends DateFns.ContextOptions<DateType> {}
 
 /**
  * @name startOfDecade
@@ -9,8 +16,10 @@ import { toDate } from "../toDate/index.js";
  * Return the start of a decade for the given date.
  *
  * @typeParam DateType - The `Date` type, the function operates on. Gets inferred from passed arguments. Allows to use extensions like [`UTCDate`](https://github.com/date-fns/utc).
+ * @typeParam ResultDate - The result `Date` type, it is the type returned from the context function if it is passed, or inferred from the arguments.
  *
  * @param date - The original date
+ * @param options - An object with options
  *
  * @returns The start of a decade
  *
@@ -19,13 +28,17 @@ import { toDate } from "../toDate/index.js";
  * const result = startOfDecade(new Date(2015, 9, 21, 00, 00, 00))
  * //=> Jan 01 2010 00:00:00
  */
-export function startOfDecade<DateType extends Date>(
+export function startOfDecade<
+  DateType extends Date,
+  ResultDate extends Date = DateType,
+>(
   date: DateType | number | string,
-): DateType {
+  options?: StartOfDecadeOptions<ResultDate> | undefined,
+): ResultDate {
   // TODO: Switch to more technical definition in of decades that start with 1
   // end with 0. I.e. 2001-2010 instead of current 2000-2009. It's a breaking
   // change, so it can only be done in 4.0.
-  const _date = toDate(date);
+  const _date = toDate(date, options?.in);
   const year = _date.getFullYear();
   const decade = Math.floor(year / 10) * 10;
   _date.setFullYear(decade, 0, 1);
--- src/startOfWeek/test.ts
+++ src/startOfWeek/test.ts
@@ -1,4 +1,7 @@
+import { TZDate, tz } from "@date-fns/tz";
+import { UTCDate } from "@date-fns/utc";
 import { describe, expect, it } from "vitest";
+import { assertType } from "../_lib/test/index.js";
 import { startOfWeek } from "./index.js";
 
 describe("startOfWeek", () => {
@@ -82,5 +85,51 @@ describe("startOfWeek", () => {
   it("returns `Invalid Date` if the given date is invalid", () => {
     const result = startOfWeek(new Date(NaN));
     expect(result instanceof Date && isNaN(result.getTime())).toBe(true);
+  });
+
+  it("resolves the date type by default", () => {
+    const result = startOfWeek(Date.now());
+    expect(result).toBeInstanceOf(Date);
+    assertType<assertType.Equal<Date, typeof result>>(true);
+  });
+
+  it("resolves the argument type if a date extension is passed", () => {
+    const result = startOfWeek(new UTCDate());
+    expect(result).toBeInstanceOf(UTCDate);
+    assertType<assertType.Equal<UTCDate, typeof result>>(true);
+  });
+
+  describe("context", () => {
+    it("allows to specify the context", () => {
+      expect(
+        startOfWeek("2024-08-17T15:00:00Z", {
+          in: tz("Asia/Singapore"),
+        }).toISOString(),
+      ).toBe("2024-08-11T00:00:00.000+08:00");
+      expect(
+        startOfWeek("2024-08-17T16:00:00Z", {
+          in: tz("Asia/Singapore"),
+        }).toISOString(),
+      ).toBe("2024-08-18T00:00:00.000+08:00");
+      expect(
+        startOfWeek("2024-08-18T03:00:00Z", {
+          in: tz("America/New_York"),
+        }).toISOString(),
+      ).toBe("2024-08-11T00:00:00.000-04:00");
+      expect(
+        startOfWeek("2024-08-18T04:00:00Z", {
+          in: tz("America/New_York"),
+        }).toISOString(),
+      ).toBe("2024-08-18T00:00:00.000-04:00");
+    });
+
+    it("resolves the context date type", () => {
+      const date = new Date("2014-09-01T00:00:00Z");
+      const result = startOfWeek(date, {
+        in: tz("Asia/Tokyo"),
+      });
+      expect(result).toBeInstanceOf(TZDate);
+      assertType<assertType.Equal<TZDate, typeof result>>(true);
+    });
   });
 });
