0e2c3ae715602a25e2e54a2745922c4fc9a677b5
Sasha Koss <koss@nocorp.me>
2024-08-19 11:30:49 +0800

    Add tz support to startOfDecade, startOfHour and startOfWeek

--- src/startOfDecade/test.ts
+++ src/startOfDecade/test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { startOfDecade } from "./index.js";
+import { TZDate, tz } from "@date-fns/tz";
+import { assertType } from "../_lib/test/index.js";
+import { UTCDate } from "@date-fns/utc";
 
 describe("startOfDecade", () => {
   it("returns the date with the time set to 00:00:00 and the date set to the first day of a year", () => {
@@ -28,5 +31,41 @@ describe("startOfDecade", () => {
   it("properly works with negative numbers", () => {
     expect(startOfDecade(new Date(2009, 0, 1))).toEqual(new Date(2000, 0, 1));
     expect(startOfDecade(new Date(-2001, 0, 1))).toEqual(new Date(-2010, 0, 1));
+  });
+
+  it("resolves the date type by default", () => {
+    const result = startOfDecade(Date.now());
+    expect(result).toBeInstanceOf(Date);
+    assertType<assertType.Equal<Date, typeof result>>(true);
+  });
+
+  it("resolves the argument type if a date extension is passed", () => {
+    const result = startOfDecade(new UTCDate());
+    expect(result).toBeInstanceOf(UTCDate);
+    assertType<assertType.Equal<UTCDate, typeof result>>(true);
+  });
+
+  describe("context", () => {
+    it("allows to specify the context", () => {
+      expect(
+        startOfDecade("2024-08-18T15:00:00Z", {
+          in: tz("Asia/Singapore"),
+        }).toISOString(),
+      ).toBe("2020-01-01T00:00:00.000+08:00");
+      expect(
+        startOfDecade("2024-08-18T17:00:00Z", {
+          in: tz("America/New_York"),
+        }).toISOString(),
+      ).toBe("2020-01-01T00:00:00.000-05:00");
+    });
+
+    it("resolves the context date type", () => {
+      const date = new Date("2014-09-01T00:00:00Z");
+      const result = startOfDecade(date, {
+        in: tz("Asia/Tokyo"),
+      });
+      expect(result).toBeInstanceOf(TZDate);
+      assertType<assertType.Equal<TZDate, typeof result>>(true);
+    });
   });
 });
