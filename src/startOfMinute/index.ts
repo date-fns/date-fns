db4804f94731abd42228f0a1a25e97d013147216
Sasha Koss <koss@nocorp.me>
2024-08-19 12:24:44 +0800

    Add tz support to startOfISOWeekYear, startOfMinute, startOfMonth and startOfQuarter

--- src/startOfMinute/index.ts
+++ src/startOfMinute/index.ts
@@ -1,4 +1,11 @@
 import { toDate } from "../toDate/index.js";
+import { type DateFns } from "../types.js";
+
+/**
+ * The {@link startOfMinute} function options.
+ */
+export interface StartOfMinuteOptions<DateType extends Date>
+  extends DateFns.ContextOptions<DateType> {}
 
 /**
  * @name startOfMinute
@@ -10,8 +17,10 @@ import { toDate } from "../toDate/index.js";
  * The result will be in the local timezone.
  *
  * @typeParam DateType - The `Date` type, the function operates on. Gets inferred from passed arguments. Allows to use extensions like [`UTCDate`](https://github.com/date-fns/utc).
+ * @typeParam ResultDate - The result `Date` type, it is the type returned from the context function if it is passed, or inferred from the arguments.
  *
  * @param date - The original date
+ * @param options - An object with options
  *
  * @returns The start of a minute
  *
@@ -20,10 +29,14 @@ import { toDate } from "../toDate/index.js";
  * const result = startOfMinute(new Date(2014, 11, 1, 22, 15, 45, 400))
  * //=> Mon Dec 01 2014 22:15:00
  */
-export function startOfMinute<DateType extends Date>(
+export function startOfMinute<
+  DateType extends Date,
+  ResultDate extends Date = DateType,
+>(
   date: DateType | number | string,
-): DateType {
-  const _date = toDate(date);
+  options?: StartOfMinuteOptions<ResultDate> | undefined,
+): ResultDate {
+  const _date = toDate(date, options?.in);
   _date.setSeconds(0, 0);
   return _date;
 }
