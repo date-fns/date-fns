4fd26ceafc1911bf025355cf6076ac53aec65594
Sasha Koss <koss@nocorp.me>
2024-08-23 10:13:01 +0800

    Add tz support to endOfSecond

--- src/endOfSecond/test.ts
+++ src/endOfSecond/test.ts
@@ -1,4 +1,7 @@
+import { TZDate, tz } from "@date-fns/tz";
+import { UTCDate } from "@date-fns/utc";
 import { describe, expect, it } from "vitest";
+import { assertType } from "../_lib/test/index.js";
 import { endOfSecond } from "./index.js";
 
 describe("endOfSecond", () => {
@@ -22,5 +25,41 @@ describe("endOfSecond", () => {
   it("returns `Invalid Date` if the given date is invalid", () => {
     const result = endOfSecond(new Date(NaN));
     expect(result instanceof Date && isNaN(result.getTime())).toBe(true);
+  });
+
+  it("resolves the date type by default", () => {
+    const result = endOfSecond(Date.now());
+    expect(result).toBeInstanceOf(Date);
+    assertType<assertType.Equal<Date, typeof result>>(true);
+  });
+
+  it("resolves the argument type if a date extension is passed", () => {
+    const result = endOfSecond(new UTCDate());
+    expect(result).toBeInstanceOf(UTCDate);
+    assertType<assertType.Equal<UTCDate, typeof result>>(true);
+  });
+
+  describe("context", () => {
+    it("allows to specify the context", () => {
+      expect(
+        endOfSecond("2024-04-10T07:00:00Z", {
+          in: tz("Asia/Singapore"),
+        }).toISOString(),
+      ).toBe("2024-04-10T15:00:00.999+08:00");
+      expect(
+        endOfSecond("2024-04-10T07:00:00Z", {
+          in: tz("America/Los_Angeles"),
+        }).toISOString(),
+      ).toBe("2024-04-10T00:00:00.999-07:00");
+    });
+
+    it("resolves the context date type", () => {
+      const date = new Date("2014-09-01T00:00:00Z");
+      const result = endOfSecond(date, {
+        in: tz("Asia/Tokyo"),
+      });
+      expect(result).toBeInstanceOf(TZDate);
+      assertType<assertType.Equal<TZDate, typeof result>>(true);
+    });
   });
 });
