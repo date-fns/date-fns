7914c16d42e9fd186228a572137e623a01d167b4
Sasha Koss <koss@nocorp.me>
2024-08-23 14:04:16 +0800

    Add tz support to endOfYesterday

--- src/endOfYesterday/test.ts
+++ src/endOfYesterday/test.ts
@@ -1,31 +1,69 @@
-import { describe, expect, it } from "vitest";
+import { TZDate, tz } from "@date-fns/tz";
 import sinon from "sinon";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { assertType } from "../_lib/test/index.js";
 import { endOfYesterday } from "./index.js";
 
 describe("endOfYesterday", () => {
-  it("returns yesterday with the time settled to 23:59:59.999", () => {
-    const clock = sinon.useFakeTimers(
+  let clock: sinon.SinonFakeTimers;
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(
       new Date(2014, 8 /* Sep */, 25, 14, 30, 45, 500).getTime(),
     );
+  });
 
+  afterEach(() => {
+    clock.restore();
+  });
+
+  it("returns yesterday with the time settled to 23:59:59.999", () => {
     const result = endOfYesterday();
     expect(result).toEqual(new Date(2014, 8 /* Sep */, 24, 23, 59, 59, 999));
-
-    clock.restore();
   });
 
   it("handles dates before 100 AD", () => {
     const now = new Date(0);
     now.setFullYear(14, 8 /* Sep */, 25);
     now.setHours(14, 30, 45, 500);
-    const clock = sinon.useFakeTimers(now.getTime());
+    sinon.useFakeTimers(+now);
 
     const expectedResult = new Date(0);
     expectedResult.setFullYear(14, 8 /* Sep */, 24);
     expectedResult.setHours(23, 59, 59, 999);
     const result = endOfYesterday();
     expect(result).toEqual(expectedResult);
+  });
 
-    clock.restore();
+  it("resolves the date type by default", () => {
+    const result = endOfYesterday();
+    expect(result).toBeInstanceOf(Date);
+    assertType<assertType.Equal<Date, typeof result>>(true);
+  });
+
+  describe("context", () => {
+    it("allows to specify the context", () => {
+      clock = sinon.useFakeTimers(new Date("2024-08-18T15:00:00Z").getTime());
+      expect(endOfYesterday({ in: tz("Asia/Singapore") }).toISOString()).toBe(
+        "2024-08-17T23:59:59.999+08:00",
+      );
+      clock = sinon.useFakeTimers(new Date("2024-08-18T16:00:00Z").getTime());
+      expect(endOfYesterday({ in: tz("Asia/Singapore") }).toISOString()).toBe(
+        "2024-08-18T23:59:59.999+08:00",
+      );
+      clock = sinon.useFakeTimers(new Date("2024-08-18T03:00:00Z").getTime());
+      expect(endOfYesterday({ in: tz("America/New_York") }).toISOString()).toBe(
+        "2024-08-16T23:59:59.999-04:00",
+      );
+      clock = sinon.useFakeTimers(new Date("2024-08-18T04:00:00Z").getTime());
+      expect(endOfYesterday({ in: tz("America/New_York") }).toISOString()).toBe(
+        "2024-08-17T23:59:59.999-04:00",
+      );
+    });
+
+    it("resolves the context date type", () => {
+      const result = endOfYesterday({ in: tz("Asia/Tokyo") });
+      expect(result).toBeInstanceOf(TZDate);
+      assertType<assertType.Equal<TZDate, typeof result>>(true);
+    });
   });
 });
