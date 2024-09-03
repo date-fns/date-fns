9720d662208fe8d4894133708aee175ed13b656f
Sasha Koss <koss@nocorp.me>
2024-09-03 13:26:07 +0800

    Add tz support to setISODay, setHours, setDayOfYear, setDay, roundToNearestMinutes, roundToNearestHours, previousDay, previousWednesday, previousTuesday, previousThursday, previousSunday, previousSaturday, previousMonday, nextDay and nextFriday

--- src/previousSaturday/index.ts
+++ src/previousSaturday/index.ts
@@ -1,4 +1,11 @@
 import { previousDay } from "../previousDay/index.js";
+import { type DateFns } from "../types.js";
+
+/**
+ * The {@link previousSaturday} function options.
+ */
+export interface PreviousSaturdayOptions<DateType extends Date>
+  extends DateFns.ContextOptions<DateType> {}
 
 /**
  * @name previousSaturday
@@ -9,8 +16,10 @@ import { previousDay } from "../previousDay/index.js";
  * When is the previous Saturday?
  *
  * @typeParam DateType - The `Date` type, the function operates on. Gets inferred from passed arguments. Allows to use extensions like [`UTCDate`](https://github.com/date-fns/utc).
+ * @typeParam ResultDate - The result `Date` type, it is the type returned from the context function if it is passed, or inferred from the arguments.
  *
  * @param date - The date to start counting from
+ * @param options - The options
  *
  * @returns The previous Saturday
  *
@@ -19,8 +28,12 @@ import { previousDay } from "../previousDay/index.js";
  * const result = previousSaturday(new Date(2021, 5, 20))
  * //=> Sat June 19 2021 00:00:00
  */
-export function previousSaturday<DateType extends Date>(
+export function previousSaturday<
+  DateType extends Date,
+  ResultDate extends Date = DateType,
+>(
   date: DateType | number | string,
-): DateType {
-  return previousDay(date, 6);
+  options?: PreviousSaturdayOptions<ResultDate> | undefined,
+): ResultDate {
+  return previousDay(date, 6, options);
 }
