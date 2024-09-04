db3d57cf7a9dcc8f14f35deefdedf12a04edddd3
Sasha Koss <koss@nocorp.me>
2024-09-04 09:01:18 +0800

    Add tz support to lastDayOfYear, lastDayOfQuarter, lastDayOfISOWeekYear and intlFormatDistance

--- src/lastDayOfYear/test.ts
+++ src/lastDayOfYear/test.ts
@@ -1,4 +1,7 @@
+import { TZDate, tz } from "@date-fns/tz";
+import { UTCDate } from "@date-fns/utc";
 import { describe, expect, it } from "vitest";
+import { assertType } from "../_lib/test/index.js";
 import { lastDayOfYear } from "./index.js";
 
 describe("lastDayOfYear", () => {
@@ -23,5 +26,40 @@ describe("lastDayOfYear", () => {
   it("returns `Invalid Date` if the given date is invalid", () => {
     const result = lastDayOfYear(new Date(NaN));
     expect(result instanceof Date && isNaN(result.getTime())).toBe(true);
+  });
+
+  it("resolves the date type by default", () => {
+    const result = lastDayOfYear(Date.now());
+    expect(result).toBeInstanceOf(Date);
+    assertType<assertType.Equal<Date, typeof result>>(true);
+  });
+
+  it("resolves the argument type if a date extension is passed", () => {
+    const result = lastDayOfYear(new UTCDate());
+    expect(result).toBeInstanceOf(UTCDate);
+    assertType<assertType.Equal<UTCDate, typeof result>>(true);
+  });
+
+  describe("context", () => {
+    it("allows to specify the context", () => {
+      expect(
+        lastDayOfYear("2024-04-10T07:00:00Z", {
+          in: tz("Asia/Singapore"),
+        }).toISOString(),
+      ).toBe("2024-12-31T00:00:00.000+08:00");
+      expect(
+        lastDayOfYear("2024-04-10T07:00:00Z", {
+          in: tz("America/Los_Angeles"),
+        }).toISOString(),
+      ).toBe("2024-12-31T00:00:00.000-08:00");
+    });
+
+    it("resolves the context date type", () => {
+      const result = lastDayOfYear("2014-09-01T00:00:00Z", {
+        in: tz("Asia/Tokyo"),
+      });
+      expect(result).toBeInstanceOf(TZDate);
+      assertType<assertType.Equal<TZDate, typeof result>>(true);
+    });
   });
 });
