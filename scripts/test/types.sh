8523656e296102a125d2ff36d4afcf9f6044d855
Sasha Koss <koss@nocorp.me>
2024-09-13 16:06:35 +0800

    Fix scripts/test/types.sh

--- scripts/test/types.sh
+++ scripts/test/types.sh
@@ -8,12 +8,12 @@ set -e
 
 # cd to the root dir
 root="$(pwd)/$(dirname "$0")/../.."
-export PACKAGE_OUTPUT_PATH="$root/tmp/types"
+export PACKAGE_OUTPUT_PATH=$(realpath "$root/tmp/types")
 
 export PACKAGE_SKIP_BEAUTIFY=true
 export PACKAGE_SKIP_CDN=true
 
-# ./scripts/build/package.sh
+./scripts/build/package.sh
 
 echo "$PACKAGE_OUTPUT_PATH"
 npx attw --pack "$PACKAGE_OUTPUT_PATH"
