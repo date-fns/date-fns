a84dea4d7a8f3366519beccdff21e36311876e65
Angelinalim911 <61859285+Angelinalim911@users.noreply.github.com>
2025-04-09 14:56:41 +1000

    Update index.ts

--- src/isMatch/index.ts
+++ src/isMatch/index.ts
@@ -278,7 +278,7 @@ export interface IsMatchOptions
  *   see: https://github.com/date-fns/date-fns/blob/master/docs/unicodeTokens.md
  *   see: https://github.com/date-fns/date-fns/blob/master/docs/unicodeTokens.md
  *
- * @returns Is format a string match for date string?
+ * @returns Is format string a match for date string?
  *
  * @throws `options.locale` must contain `match` property
  * @throws use `yyyy` instead of `YYYY` for formatting years; see: https://github.com/date-fns/date-fns/blob/master/docs/unicodeTokens.md
