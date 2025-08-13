e618c6174e8755140e1bd3a89bb627c7c8df0a37
Sasha Koss <koss@nocorp.me>
2025-08-13 07:30:58 +0000

    Run prettier on misc files

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
