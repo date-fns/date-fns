63ec06ff20113f07835a63b5573e844ca7a279a9
Sasha Koss <koss@nocorp.me>
2026-05-30 06:09:53 +0800

    Add positional argument to `test/smoke.sh` script

--- pkgs/core/scripts/test/smoke.sh
+++ pkgs/core/scripts/test/smoke.sh
@@ -14,11 +14,23 @@ scripts/build/package.sh
 cd "$dir" || exit 1
 pnpm install
 
-for example in `ls`
+if [ -n "$1" ]; then
+  examples="$1"
+else
+  examples="$(ls)"
+fi
+
+for example in $examples
 do
   if [ ! -f "$example/package.json" ]; then
+    if [ -n "$1" ]; then
+      printf "$error_message"
+      printf "Example '$example' does not exist or has no package.json\n"
+      exit 1
+    fi
     continue
   fi
+
   printf "\n\033[0;32mTesting $example...\033[0m\n\n"
   cd "$example" || exit 1
   pnpm run build
