766dea98b704f119e221b8365909b44301500ae4
Sasha Koss <koss@nocorp.me>
2026-09-22 13:30:10 +0800

    Fix tpStartOfISOWeekYear to return `Temporal.ZonedDateTime` when possible

--- pkgs/core/src/tp/startOfISOWeekYear/index.ts
+++ pkgs/core/src/tp/startOfISOWeekYear/index.ts
@@ -1,6 +1,16 @@
 export function tpStartOfISOWeekYear(
+  date: Temporal.ZonedDateTime,
+): Temporal.ZonedDateTime;
+
+export function tpStartOfISOWeekYear(date: number): Temporal.PlainDate;
+
+export function tpStartOfISOWeekYear(
   date: Temporal.ZonedDateTime | number,
-): Temporal.ZonedDateTime {
+): Temporal.ZonedDateTime | Temporal.PlainDate;
+
+export function tpStartOfISOWeekYear(
+  date: Temporal.ZonedDateTime | number,
+): Temporal.ZonedDateTime | Temporal.PlainDate {
   const weekYear = typeof date === "number" ? date : date.yearOfWeek;
   if (weekYear === undefined)
     throw new Error("The date calendar does't have a well-defined week system");
